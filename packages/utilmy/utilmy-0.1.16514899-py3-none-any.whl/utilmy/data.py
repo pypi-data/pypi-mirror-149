710961b7508aeb530faa18da269f5a673a03c223
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:17 +0000

    Add package utilmy/0.1.16514899/utilmy-0.1.16514899-py3-none-any.whl

---
+++ packages/utilmy/utilmy-0.1.16514899-py3-none-any.whl/utilmy/data.py
@@ -0,0 +1,75 @@
+# coding=utf-8
+HELP = """
+DataPrep — the most comprehensive auto EDA [GitHub, Documentation]
+AutoViz — the fastest auto EDA [GitHub]
+PandasProfiling — the earliest and one of the best auto EDA tools [GitHub, Documentation]
+Lux — the most user-friendly and luxurious EDA [GitHub, Documentation]
+
+
+
+### Data performance
+https://github.com/evidentlyai/evidently
+
+
+
+"""
+import os,sys,  pandas as pd, numpy as np
+
+
+
+#################################################################################################
+def log(*s):
+    """function log
+    Args:
+        *s:   
+    Returns:
+        
+    """
+    print(*s, flush=True)
+
+
+def help():
+    """function help
+    Args:
+    Returns:
+        
+    """
+    ss  = ""
+    ss += HELP
+    print(ss)
+
+
+
+
+
+
+#################################################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#################################################################################################
+if __name__ == '__main__':
+    import fire; fire.Fire()
+
+
+
+
+
+
+
+
+
---
+++ packages/utilmy/utilmy-0.1.16514899-py3-none-any.whl/utilmy/docs/test_script/test_script_no_logger.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+MNAME = "utilmy."
+HELP = """ utils for
+
+
+
+
+"""
+import os, sys, glob, time,gc, datetime, numpy as np, pandas as pd
+from typing import List, Optional, Tuple, Union
+from numpy import ndarray
+from box import Box
+
+
+
+
+
+
+#############################################################################################
+
+
+#############################################################################################
+def test_all() -> None:
+    """function test_all
+
+    """
+    log(MNAME)
+    test1()
+    test2()
+
+
+def test1() -> None:
+    """function test1
+    Args:
+    Returns:
+
+    """
+    pass
+
+
+
+
+def test2() -> None:
+    """function test2
+    Args:
+    Returns:
+
+    """
+    pass
+
+#############################################################################################
+
+
+def core1(sasas):
+    pass
+
+
+def core2(sasas):
+    pass
+
+def core3(sasas):
+    pass
+
+def core4(sasas):
+    pass
+
+
+###################################################################################################
+if __name__ == "__main__":
+    import fire
+    fire.Fire()
+
+
