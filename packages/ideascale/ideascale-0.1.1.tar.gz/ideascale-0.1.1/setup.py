20a81b462818ea515891aff4678bcfbd72a6fbac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:15 +0000

    Add package ideascale/0.1.1/ideascale-0.1.1.tar.gz

---
+++ packages/ideascale/ideascale-0.1.1.tar.gz/ideascale-0.1.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+
+
+VERSION = '0.1.1'
+DESCRIPTION = 'IdeaScale Package'
+
+# Setting up
+setup(
+    name="ideascale",
+    version=VERSION,
+    author="Tien Nguyen",
+    author_email="tiennguyenhotel97@gmail.com",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['pandas','openpyxl','xlsxwriter','jinja2'],
+    keywords=['python', 'xlsxformatting', 'Mardown'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
