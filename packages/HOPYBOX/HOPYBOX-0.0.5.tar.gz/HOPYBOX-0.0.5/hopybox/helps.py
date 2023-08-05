83c8c64fe67b53d850df504c6a29a37fd0cf80ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:17 +0000

    Add package HOPYBOX/0.0.5/HOPYBOX-0.0.5.tar.gz

---
+++ packages/HOPYBOX/HOPYBOX-0.0.5.tar.gz/HOPYBOX-0.0.5/hopybox/helps.py
@@ -0,0 +1,24 @@
+def all_help():
+  print('''\033[32mCommand (take command) 
+1.Help
+├ help 
+└ help <module> 
+2.Id
+└ id <text> 
+3.Runpy
+└ run <file> 
+4.Hoget
+└ hoget <url> 
+5.Browse
+└ bowget <url> 
+6.QQ
+├ qqname <qqnumber> 
+└ qqhead <qqnumber> 
+System (take system) 
+Python (take python) 
+
+Last
+└ If you have any questions, please contact the developer as HOStudio.hopybox@foxmail.com ''')
+def module_help(module):
+  print('\033[32m',end='\r')
+  print(help(module))
