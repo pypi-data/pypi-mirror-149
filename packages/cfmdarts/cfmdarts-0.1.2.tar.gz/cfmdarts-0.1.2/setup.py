8a3d3fcdc5edc2a8a6afc821d8b2b162198cb565
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:54 +0000

    Add package cfmdarts/0.1.2/cfmdarts-0.1.2.tar.gz

---
+++ packages/cfmdarts/cfmdarts-0.1.2.tar.gz/cfmdarts-0.1.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="cfmdarts",
+    version="0.1.2",
+    author="Matthew Squire, Luke Sargent, Cameron Trew, Aron Russell",
+    description="cfmdarts is a Python library that simulates the projectile motion of darts.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Araphor0/cfmdarts",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,    # include everything in source control
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
