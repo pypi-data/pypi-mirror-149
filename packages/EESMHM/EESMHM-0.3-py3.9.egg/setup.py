0a1ae2a517a0d450712add097aa67c73334ef4e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:38 +0000

    Add package EESMHM/0.3/EESMHM-0.3-py3.9.egg

---
+++ packages/EESMHM/EESMHM-0.3-py3.9.egg/setup.py
@@ -0,0 +1,46 @@
+from distutils.command.sdist import sdist as sdist_orig
+from distutils.errors import DistutilsExecError
+
+from setuptools import setup
+
+class sdist(sdist_orig):
+
+    def run(self):
+        try:
+            self.spawn(['./evoef_dowload.sh'])
+        except DistutilsExecError:
+            self.warn('listing directory failed')
+        super().run()
+
+
+if __name__ == '__main__':
+    with open("ReadME_pypi.md", "r", encoding="utf-8") as fh:
+        long_description = fh.read()
+
+    setup(name='EESMHM',
+        version="0.3",
+        cmdclass={
+        'sdist': sdist
+        },
+        description="Energy Evaluation of Single Mutant Homology Models",
+        url='https://github.com/eved1018/EESMHM',
+        author='Evan Edelstein',
+        author_email='edelsteinevan@gmail.com',
+        license='MIT',
+        packages=['EESMHM'],
+        data_files = [('', ['setup.py',"ReadME_pypi.md"])],
+        install_requires=[
+            'scipy',
+            'numpy',
+            "halo",
+            "pandas",
+            'intercaat',
+            'pyhull'],
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        entry_points = {
+            'console_scripts': ['EESMHM=EESMHM.main:EESMHM'],
+        })
+
+#python setup.py sdist
+#python3 -m twine upload dist/*
