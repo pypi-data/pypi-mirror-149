36f044776b2edd776b8ef16a41044eb63db59046
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:19 +0000

    Add package parish/0.0.1.dev1/parish-0.0.1.dev1-py3-none-any.whl

---
+++ packages/parish/parish-0.0.1.dev1-py3-none-any.whl/parish/__init__.py
@@ -0,0 +1,40 @@
+
+import warnings
+
+warnings.warn('This parish version is a placeholder, an initial release is coming soon!')
+
+# Considered Synonyms To Name This Library!
+
+# 'environment',
+# 'province',
+# 'realm',
+# 'world',
+# 'precinct', # ??
+# 'empire',
+# 'empires',
+# 'domain',
+# 'district', # ??
+# 'sector',
+# 'ruling',
+# 'ruler',  'matriarch',  # promising?
+# 'kingdom',
+# 'reign',
+# 'afterworld',
+# 'terran',
+# 'ecology',
+# 'earther',
+# 'earthman',
+# 'earthforce',
+# 'lifeforce',
+# 'tellurian',
+# 'mortal',
+# 'alive',
+# 'frankenstein',
+# 'parish',  # *NB*
+# 'pedantry',
+# 'pedantic',
+# 'learner',
+# 'student',
+# 'dominion',
+# 'freshman',  # *NB*
+# 'recruit',
