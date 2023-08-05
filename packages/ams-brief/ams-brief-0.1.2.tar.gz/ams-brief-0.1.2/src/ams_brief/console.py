865c6056a045d8aa893bf2f3380662dbdd776a00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:00 +0000

    Add package ams-brief/0.1.2/ams-brief-0.1.2.tar.gz

---
+++ packages/ams-brief/ams-brief-0.1.2.tar.gz/ams-brief-0.1.2/src/ams_brief/console.py
@@ -0,0 +1,17 @@
+import os
+
+import click
+
+def main():
+  cwd = os.getcwd()
+  if cwd[0] != 'X':
+    click.secho("Please run the briefing tool in the X:/ drive")
+    return
+  print(len(cwd)) 
+  proj_number = cwd[3:6] 
+  
+  print(proj_number)
+  
+  
+
+  print('hey')
