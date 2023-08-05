7b17e37420aa7b2e68d521d6bdf835e9cc77feb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:57 +0000

    Add package liyichenglib666/1.0.1/liyichenglib666-1.0.1.tar.gz

---
+++ packages/liyichenglib666/liyichenglib666-1.0.1.tar.gz/liyichenglib666-1.0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+def readme_file():
+    with open("README.rst", encoding="utf-8") as rf:
+        return rf.read()
+
+setup(
+    name="liyichenglib666",
+    version="1.0.1",
+    description="this is a test lib for study",
+    packages=["liyichenglib"],
+    py_modules=["tools"],
+    author="liyicheng",
+    author_email="leeyc7@foxmail.com",
+    long_description=readme_file(),
+    url="https://www.baidu.com",
+    license="MIT"
+)
