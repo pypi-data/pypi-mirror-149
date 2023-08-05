d86ae6e3800b8fb9cae250a19661efc2d50282cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:57 +0000

    Add package stromer-api/0.2/stromer_api-0.2.tar.gz

---
+++ packages/stromer-api/stromer_api-0.2.tar.gz/stromer_api-0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='stromer_api',
+    version='0.2',
+    packages=[''],
+    url='https://github.com/elnkosc/stromer_api',
+    license='GPL',
+    author='Koen',
+    author_email='koen@schilders.org',
+    description='Stomer API for accessing data from your Stromer Speed Bike.'
+)
