d162bccbf2f789849c92f5dbd6d7d0130e53187b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:53 +0000

    Add package arora/0.0.4/Arora-0.0.4.tar.gz

---
+++ packages/arora/Arora-0.0.4.tar.gz/Arora-0.0.4/Arora/filter.py
@@ -0,0 +1,52 @@
+import pandas as pd
+from scipy import signal
+import numpy as np
+from typing import List
+
+"""
+TODO: Make the code more readable and document/comment it
+"""
+
+
+def low_pass_filter(data: List[int or float], cutoff: int or float,
+					frequency: int or float, order=5) -> np.ndarray:
+	"""
+	:param data: Array of integers or float
+	:param cutoff: The frequency that is to be filtered through --> integer or float
+	:param frequency: The frequency of the data --> integer or float
+	:param order: The order of the filter --> integer
+	:return: An array of filtered data using the Buttersworth method --> Numpy ndarray
+	"""
+	b, a = butter_pass(cutoff, frequency, btype='lowpass', order=order)
+	y = signal.lfilter(b, a, data)
+	return y
+
+
+def high_pass_filter(data: List[int or float] or np.ndarray, cutoff: int or float,
+					 frequency: int or float, order=5) -> np.ndarray:
+	"""
+	:param data: Array of integers or float
+	:param cutoff: The frequency that is to be filtered through --> integer or float
+	:param frequency: The frequency of the data --> integer or float
+	:param order: The order of the filter --> integer
+	:return: An array of filtered data using the Buttersworth method --> Numpy ndarray
+	"""
+	b, a = butter_pass(cutoff, frequency, btype='highpass', order=order)
+	y = signal.filtfilt(b, a, data)
+	return y
+
+
+def butter_pass(cutoff: int or float, frequency: int or float,
+				btype: str, order: int = 5) -> (np.ndarray, np.ndarray):
+	"""
+
+	:param cutoff: The frequency that is to be filtered through --> integer or float
+	:param frequency: The frequency of the data --> integer or float
+	:param order: The order of the filter --> integer
+	:param btype: The type of filtering --> string
+	:return: The denominator and the numerator polynomials of the IIR filter --> Numpy ndarray, numpy ndarray
+	"""
+	nyq = 0.5 * frequency
+	normal_cutoff = cutoff / nyq
+	b, a = signal.butter(order, normal_cutoff, btype=btype, analog=False)
+	return b, a
---
+++ packages/arora/Arora-0.0.4.tar.gz/Arora-0.0.4/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+import os
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Arora",
+    version=os.getenv(key="PACKAGE_VERSION", default="0.0.4"),
+    long_description=long_description,
+    long_description_content="text/markdown",
+    author="Sleep Revolution",
+    author_email="sleeprevolution@ru.is",
+    description="A tool for sleep researcher to preprocess, work, analyze and visualize data",
+    packages=setuptools.find_packages(include=['Arora', 'Arora.*']),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.8"
+        ],
+    install_requires=["numpy", "pandas", "pyEDFlib", "scipy"],
+    python_requires=">=3.8"
+)
