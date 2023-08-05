6779857ded03372f38584c0c7305a08835c4b27f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:16:33 +0000

    Add package xclingo/2.0b12/xclingo-2.0b12-py3-none-any.whl

---
+++ packages/xclingo/xclingo-2.0b12-py3-none-any.whl/xclingo/__main__.py
@@ -0,0 +1,95 @@
+from xclingo import Explainer as Explainer
+from xclingo import __version__ as xclingo_version
+from clingo import Control
+from argparse import ArgumentParser, FileType
+import sys
+
+def check_options():
+    # Handles arguments of xclingo
+    parser = ArgumentParser(description='Tool for explaining (and debugging) ASP programs', prog='xclingo')
+    parser.add_argument('--version', action='version',
+                        version='xclingo {version}'.format(version=xclingo_version),
+                        help='Prints the version and exists.')
+    optional_group = parser.add_mutually_exclusive_group()
+    optional_group.add_argument('--only-translate', action='store_true',
+                        help="Prints the internal translation and exits.")
+    optional_group.add_argument('--only-translate-annotations', action='store_true',
+                        help="Prints the internal translation and exits.")
+    optional_group.add_argument('--only-explanation-atoms', action='store_true',
+                        help="Prints the atoms used by the explainer to build the explanations.")
+    parser.add_argument('--auto-tracing', type=str, choices=["none", "facts", "all"], default="none",
+                        help="Automatically creates traces for the rules of the program. Default: none.")
+    parser.add_argument('-n', nargs=2, default=(1,1), type=int, help="Number of answer sets and number of desired explanations.")
+    parser.add_argument('infiles', nargs='+', type=FileType('r'), default=sys.stdin, help="ASP program")
+    return parser.parse_args()
+
+def read_files(files):
+    return "\n".join([file.read() for file in files])
+
+def translate(program, auto_trace):
+    explainer = Explainer(auto_trace=auto_trace)
+    explainer.add('base', [], program)
+    explainer._translate_program()
+    translation =  explainer._preprocessor.get_translation()
+    translation += explainer._getExplainerLP(auto_trace=auto_trace)
+    return translation   
+
+def print_explanation_atoms(control, explainer: Explainer):
+    with control.solve(yield_=True) as it:
+        nanswer=1
+        for m in it:
+            print(f'Answer {nanswer}')
+            print(' '.join([str(sym) for sym in m.symbols(shown=True)]))
+            nexpl=1
+            for xclingo_m in explainer.get_xclingo_models(m):
+                print(f'Explanation: {nexpl}')
+                print("\n".join([str(sym) for sym in xclingo_m.symbols(shown=True)]))
+                print()
+                nexpl+=1
+            nanswer+=1
+
+def print_text_explanations(control, explainer:Explainer):
+    with control.solve(yield_=True) as it:
+        nanswer=1
+        for m in it:
+            print(f'Answer {nanswer}')
+            for e in explainer.explain(m):
+                print(e.ascii_tree())
+            nanswer+=1
+
+def main():
+    args = check_options()
+
+    if args.only_translate_annotations:
+        program = read_files(args.infiles)
+        from xclingo.preprocessor import Preprocessor
+        print(Preprocessor.translate_annotations(program))
+        return 0
+
+    if args.only_translate:
+        program = read_files(args.infiles)
+        print(translate(program, args.auto_tracing))
+        return 0
+
+    control = Control([str(args.n[0])])
+    explainer = Explainer(
+        [
+            str(args.n[1]), 
+        ], 
+        auto_trace=args.auto_tracing
+    )
+
+    for file in args.infiles:
+        prog = file.read().replace('#show', '%#show')
+        explainer.add(file.name, [], prog)
+        control.add("base", [], prog)
+
+    control.ground([("base", [])])
+
+    if args.only_explanation_atoms:
+        print_explanation_atoms(control, explainer)
+    else:
+        print_text_explanations(control, explainer)    
+
+if __name__ == '__main__':
+    main()
---
+++ packages/xclingo/xclingo-2.0b12-py3-none-any.whl/xclingo/_main.py
@@ -0,0 +1,198 @@
+from typing import Iterable, Sequence
+from clingo import Model, Function, String
+from clingo.ast import ProgramBuilder, parse_string
+from clingo.control import Control
+from clingo.symbol import SymbolType
+from xclingo.explanation import Explanation
+from xclingo.preprocessor import Preprocessor
+
+from clingo.core import MessageCode
+
+class Context:
+    def label(self, text, tup):
+        if text.type == SymbolType.String:
+            text = text.string
+        else:
+            text = str(text).strip('"')
+        for val in tup.arguments:
+            text = text.replace("%", val.string if val.type==SymbolType.String else str(val), 1)
+        return [String(text)]
+
+    def inbody(self, body):
+        if len(body.arguments)>0:
+            return [Function(
+                '',
+                [a, body],
+                True,
+            )
+            for a in body.arguments]
+        else:
+            return Function('empty', [], True)
+
+class Explainer():
+    def __init__(self, internal_control_arguments=['1'], auto_trace="none"):
+        self._preprocessor = Preprocessor()
+        self._memory = []
+        
+        self._internal_control_arguments = internal_control_arguments 
+        self._auto_trace = auto_trace
+        self._translated = False
+        self._current_model = []
+
+        self._no_labels = False
+        self._no_show_trace = False
+
+    def logger(self, _code, msg):
+        if _code == MessageCode.AtomUndefined:
+            if 'xclingo_muted(Cause)' in msg:
+                return
+            if '_xclingo_label_tree/3' in msg:
+                return
+            if '_xclingo_label' in msg:
+                self._no_labels = True
+                return
+            if '_xclingo_show_trace' in msg:
+                self._no_show_trace = True
+        print(msg)
+
+    def print_messages(self):
+        if self._no_labels:
+            print('xclingo info: any atom or rule has been labelled.')
+        if self._no_show_trace:
+            print('xclingo info: any atom has been affected by a %!show_trace annotation.')
+    
+    def clean_log(self):
+        self._no_labels = False
+        self._no_show_trace = False
+
+    def _getExplainerLP(self, auto_trace="none"):
+        if hasattr(self, '_explainerLP') == False:
+            setattr(self, '_explainerLP', self._loadExplainerLP(auto_trace))
+        return self._explainerLP
+
+    def _loadExplainerLP(self, auto_trace="none"):
+        try:
+            import importlib.resources as pkg_resources
+        except ImportError:
+            # Try backported to PY<37 `importlib_resources`.
+            import importlib_resources as pkg_resources
+
+        from . import xclingo_lp  # relative-import the *package* containing the templates
+        program = pkg_resources.read_text(xclingo_lp, 'xclingo.lp')
+        if auto_trace == "all":
+            program += pkg_resources.read_text(xclingo_lp, 'autotrace_all.lp')
+        elif auto_trace == "facts":
+            program += pkg_resources.read_text(xclingo_lp, 'autotrace_facts.lp')
+        return program
+
+    def add(self, program_name:str, parameters: Iterable[str], program:str):
+        self._memory.append((program_name, program))
+
+    def _initialize_control(self):
+        return Control(
+            self._internal_control_arguments + \
+                [
+                    '--project=project'
+                ], 
+            logger=self.logger)
+
+    def _translate_program(self):
+        self._preprocessor._rule_count = 1
+        for name, program in self._memory:
+            self._preprocessor.translate_program(program, name=name)
+
+    def _ground(self, control, model, context=None):
+        if not self._translated:
+            self._translate_program()
+            self._translated
+            
+        with ProgramBuilder(control) as builder:
+            parse_string(
+                self._getExplainerLP(auto_trace=self._auto_trace)+self._preprocessor.get_translation(),
+                lambda ast: builder.add(ast),
+            )
+        
+        with control.backend() as backend:
+            for sym in model.symbols(shown=True):
+                atm_id = backend.add_atom(Function('_xclingo_model', [sym], True))
+                backend.add_rule([atm_id], [], False)
+            
+        control.ground([('base', [])], context=context if context is not None else Context())
+
+
+    def _get_explanations(self, control):
+        with control.solve(yield_=True) as it:
+            for expl_model in it:
+                syms = expl_model.symbols(shown=True)
+                if len(syms)>0:
+                    yield Explanation.from_model(syms)
+    
+    def _get_models(self, control):
+        with control.solve(yield_=True) as it:
+            for expl_model in it:
+                yield expl_model
+
+    def get_xclingo_models(self, model:Model) -> Iterable[Explanation]:
+        control = self._initialize_control()
+        self.clean_log()
+        self._ground(control, model)
+        self.print_messages()
+        return self._get_models(control)
+
+    def explain(self, model:Model, context=None) -> Iterable[Explanation]:
+        control = self._initialize_control()    
+        self.clean_log()
+        self._ground(control, model, context)
+        self.print_messages()
+        return self._get_explanations(control)
+
+
+class XclingoControl:
+    def __init__(self, n_solutions='1', n_explanations='1', auto_trace='none'):
+        self.n_solutions = n_solutions
+        self.n_explanations = n_explanations
+
+        self.control = Control([n_solutions if type(n_solutions)==str else str(n_solutions)])
+        self.explainer = Explainer(
+            [
+                n_explanations if type(n_explanations)==str else str(n_explanations), 
+            ], 
+            auto_trace=auto_trace
+        )
+
+        self._explainer_context = None
+
+    def add(self, name, parameters, program):
+        self.explainer.add(name, [], program)
+        self.control.add("base", parameters, program.replace('#show', '%#show'))
+
+    def ground(self, context=None, explainer_context=None):
+        self.control.ground([("base", [])], context)
+
+        self._explainer_context = explainer_context
+
+    def explain(self, on_explanation=None):
+        with self.control.solve(yield_=True) as it:
+            for m in it:
+                if on_explanation is None:
+                    yield self.explainer.explain(m, context=self._explainer_context)
+                else:
+                    on_explanation(self.explainer.explain(m, context=self._explainer_context))
+
+    def explain_and_print(self):
+        n = 0
+        for answer in self.explain():
+            n += 1
+            print(f'Answer {1}')
+            for expl in answer:
+                print(expl.ascii_tree())
+
+    def _default_output(self):
+        output = ''
+        n = 0
+        for answer in self.explain():
+            n = 1
+            output += f'Answer {n}\n'
+            output += '\n'.join([expl.ascii_tree() for expl in answer])
+            output += '\n'
+        return output
