30f2ac37f5141309f8d0eca889185e169b0277cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:33 +0000

    Add package hklabk2/0.0.12/hklabk2-0.0.12.tar.gz

---
+++ packages/hklabk2/hklabk2-0.0.12.tar.gz/hklabk2-0.0.12/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup 
+
+setup(
+      name="hklabk2", 
+      version="0.0.12", 
+      url="",
+      license="MIT",
+      author="kkm",
+      author_email="kimkyongmin2@daum.net" ,
+      keywords = ["calendar" , "isoweek","listsum"] ,
+      description = "isoweek calculation and so on" ,
+      packages=["hklabk2"],
+      install_requires=["isoweek"]
+      
+     )
