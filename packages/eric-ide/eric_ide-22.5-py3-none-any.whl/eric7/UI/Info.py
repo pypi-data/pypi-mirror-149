de4f9ec9ba2b93089f861aab8752630d93662854
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:04 +0000

    Add package eric-ide/22.5/eric_ide-22.5-py3-none-any.whl

---
+++ packages/eric-ide/eric_ide-22.5-py3-none-any.whl/eric7/UI/Info.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2002 - 2022 Detlev Offenbach <detlev@die-offenbachs.de>
+#
+
+"""
+Module defining some informational strings.
+"""
+
+
+Program = 'eric7'
+Version = '22.5 (rev. 2b9bd8f97576)'
+VersionOnly = '22.5'
+Copyright = (
+    'Copyright (c) 2002 - 2022 Detlev Offenbach'
+    ' <detlev@die-offenbachs.de>'
+)
+CopyrightShort = '(c) 2002 - 2022 Detlev Offenbach'
+BugAddress = 'eric-bugs@eric-ide.python-projects.org'
+FeatureAddress = 'eric-featurerequest@eric-ide.python-projects.org'
+Homepage = "https://eric-ide.python-projects.org/index.html"
