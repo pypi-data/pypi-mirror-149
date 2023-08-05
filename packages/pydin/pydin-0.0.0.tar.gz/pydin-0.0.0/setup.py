346d8bae24421f21860a3f9a138a68e50cdb546d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:54 +0000

    Add package pydin/0.0.0/pydin-0.0.0.tar.gz

---
+++ packages/pydin/pydin-0.0.0.tar.gz/pydin-0.0.0/setup.py
@@ -0,0 +1,34 @@
+"""Setup."""
+
+import pydin
+import setuptools
+
+
+name = pydin.__name__
+version = pydin.__version__
+author = pydin.__author__
+author_email = pydin.__email__
+description = pydin.__doc__.splitlines()[0]
+long_description = open('README.md', 'r').read()
+long_description_content_type = 'text/markdown'
+license = pydin.__license__
+url = 'https://github.com/t3eHawk/pydin'
+install_requires = []
+packages = setuptools.find_packages()
+classifiers = ['Programming Language :: Python :: 3',
+               'License :: OSI Approved :: MIT License',
+               'Operating System :: OS Independent']
+
+
+setuptools.setup(name=name,
+                 version=version,
+                 author=author,
+                 author_email=author_email,
+                 description=description,
+                 long_description=long_description,
+                 long_description_content_type=long_description_content_type,
+                 license=license,
+                 url=url,
+                 install_requires=install_requires,
+                 packages=packages,
+                 classifiers=classifiers)
