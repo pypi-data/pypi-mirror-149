c99d24f0402418d2ef03da449d2067c7a0baac2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:24:00 +0000

    Add package greetmsg/0.0.1/greetmsg-0.0.1-py3-none-any.whl

---
+++ packages/greetmsg/greetmsg-0.0.1-py3-none-any.whl/greetmsg.py
@@ -0,0 +1,6 @@
+def greet(name=None):
+    if name is None:
+        print("Hello There,\nThanks for installing our python package\nEnjoy Coding with us")
+    else:
+        print(f"Hello {name},\nThanks for installing our python package\nEnjoy Coding with us")
+        
