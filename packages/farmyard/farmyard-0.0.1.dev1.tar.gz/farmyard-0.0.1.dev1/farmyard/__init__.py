bf17c44eb23bad9555a032d21a9f57f5b4a44503
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:13 +0000

    Add package farmyard/0.0.1.dev1/farmyard-0.0.1.dev1.tar.gz

---
+++ packages/farmyard/farmyard-0.0.1.dev1.tar.gz/farmyard-0.0.1.dev1/farmyard/__init__.py
@@ -0,0 +1,67 @@
+
+import warnings
+
+warnings.warn('This version of farmyard is a placeholder, an initial release is coming soon!')
+
+# Considered Synonyms To Name This Library!
+
+# 'environment',
+# 'province',
+# 'realm',
+# 'world',
+# 'precinct', # ??
+# 'empire',
+# 'empires',
+# 'domain',
+# 'district', # ??
+# 'sector',
+# 'ruling',
+# 'ruler',  'matriarch',  # promising?
+# 'kingdom',
+# 'reign',
+# 'afterworld',
+# 'terran',
+# 'ecology',
+# 'earther',
+# 'earthman',
+# 'earthforce',
+# 'lifeforce',
+# 'tellurian',
+# 'mortal',
+# 'alive',
+# 'frankenstein',
+# 'parish',  # *NB*
+# 'pedantry',
+# 'pedantic',
+# 'learner',
+# 'student',
+# 'dominion',
+# 'freshman',  # *NB*
+# 'recruit',
+
+# ALTERNATIVE SYNONYMS:
+
+# - 'gym',
+# - 'arena',  # playhouse, rink
+# - 'coliseum',  # court
+# - 'zoo',  # madhouse, game-reserve, city-farm
+# - 'farm',  # breed, farmhouse, farmstead
+# ? 'farmhouse',  # ranch, estate, villa, barnyard, cowshed, farmstead, farmyard
+# ? 'barnyard',  # -- the area of open ground around a barn; a farmyard.
+# ? 'cowshed',  # -- a farm building in which cattle are kept when not at pasture, or in which they are milked.
+# ? 'farmyard',  # *NB* -- a yard surrounded by farm buildings. | denoting coarse speech or behaviour.
+# ? 'farmstead',  # -- a farm and its buildings.
+# - 'farmstead',  # rancho, grassland, farmland
+# - 'playhouse',  # theater, odeum
+# - 'madhouse',  # nuthouse, loony-bin, sanitarium, zoos
+# - 'rink',
+# - 'grassland',  # meadow, prairie, lawn, veldt
+# - 'farmland',
+# ? 'meadow',  # everglade # -- a piece of grassland, especially one used for hay.
+
+
+# DECISION:
+
+# - meadow
+# - farmyard  -- close to zoo!
+# - parish -- nope, maybe for something else?
