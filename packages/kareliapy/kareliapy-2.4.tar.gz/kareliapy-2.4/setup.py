693994ae8eaceb583827c8ae771df15981091578
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:25 +0000

    Add package kareliapy/2.4/kareliapy-2.4.tar.gz

---
+++ packages/kareliapy/kareliapy-2.4.tar.gz/kareliapy-2.4/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setup(  name='kareliapy',
+        version='2.4',
+        description='Python library for euphoria.io',
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        author='StruanDW',
+        author_email='struan@duncan-wilson.co.uk',
+        url='https://github.com/struandw/karelia',
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",],
+        install_requires=[
+            'websocket-client',
+        ],
+        py_modules=['karelia'],)
