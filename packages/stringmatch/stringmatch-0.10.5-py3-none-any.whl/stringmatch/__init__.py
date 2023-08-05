cdb46a0963a17f000d90f4b0a4b8ddff77651b1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:26 +0000

    Add package stringmatch/0.10.5/stringmatch-0.10.5-py3-none-any.whl

---
+++ packages/stringmatch/stringmatch-0.10.5-py3-none-any.whl/stringmatch/__init__.py
@@ -0,0 +1,9 @@
+# flake8: noqa
+from .distance import *
+from .match import *
+from .ratio import *
+from .scorer import *
+from .strings import *
+
+__title__ = "stringmatch"
+__version__ = "0.10.5"
