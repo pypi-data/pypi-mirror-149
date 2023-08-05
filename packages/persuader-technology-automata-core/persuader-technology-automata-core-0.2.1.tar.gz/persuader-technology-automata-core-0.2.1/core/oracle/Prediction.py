89ec5c6bac591df53d398756f1b279d0257ff62c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:53 +0000

    Add package persuader-technology-automata-core/0.2.1/persuader-technology-automata-core-0.2.1.tar.gz

---
+++ packages/persuader-technology-automata-core/persuader-technology-automata-core-0.2.1.tar.gz/persuader-technology-automata-core-0.2.1/core/oracle/Prediction.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+from typing import List
+
+from core.number.BigFloat import BigFloat
+
+
+@dataclass
+class Prediction:
+    outcome: List[str]
+    profit: BigFloat
