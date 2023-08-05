6e012cb2255a3a255c3346b35a8129b9a153b0b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:30 +0000

    Add package web3-constant/0.0.5/web3_constant-0.0.5-py3-none-any.whl

---
+++ packages/web3-constant/web3_constant-0.0.5-py3-none-any.whl/web3constant/Cronos/Url.py
@@ -0,0 +1,3 @@
+# mainnet
+CRYPTO_COM_RPC = "https://evm-cronos.crypto.org/"
+ARTEMIS_ONE_RPC = "https://rpc.artemisone.org/cronos/"
---
+++ packages/web3-constant/web3_constant-0.0.5-py3-none-any.whl/web3constant/__init__.py
@@ -0,0 +1,6 @@
+from . import topics
+from . import abi
+
+__all__ = ["Cronos", "Fantom", "topics", "utility"]
+
+ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
