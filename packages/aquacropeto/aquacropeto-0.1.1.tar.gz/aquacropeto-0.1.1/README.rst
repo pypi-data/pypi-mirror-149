ea61753afafb755e72b503e28ef62ae34f925ffd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:38 +0000

    Add package aquacropeto/0.1.1/aquacropeto-0.1.1.tar.gz

---
+++ packages/aquacropeto/aquacropeto-0.1.1.tar.gz/aquacropeto-0.1.1/README.rst
@@ -0,0 +1,16 @@
+*aquacrop-eto* is an actively developed fork of PyETo (https://github.com/woodcrafty/PyEToPython) for calculating reference crop evapotranspiration
+(ET\ :sub:`o`\ ).
+The library provides numerous functions for estimating missing meteorological
+data.
+
+Three methods for estimating ET\ :sub:`o`\ /PET are implemented:
+
+* FAO-56 Penman-Monteith (Allen et al, 1998)
+* Hargreaves (Hargreaves and Samani, 1982; 1985)
+* Thornthwaite (Thornthwaite, 1948)
+
+```
+pip install aquacrop-eto
+```
+
+
---
+++ packages/aquacropeto/aquacropeto-0.1.1.tar.gz/aquacropeto-0.1.1/setup.py
@@ -0,0 +1,38 @@
+
+from setuptools import setup
+
+requirements = [
+    "numpy",
+    "pandas",
+]
+
+
+setup(
+    name='aquacropeto',
+    version='0.1.1',
+    description=(
+        'Library for estimating reference and potential evapotranspiration.'
+    ),
+    long_description=open('README.rst').read(),
+    author='Tom Kelly',
+    author_email='REMOVETHIS@gmail.com',
+    license='BSD 3-Clause',
+    url='https://github.com/aquacropos/aquacrop-eto',
+    packages=['aquacropeto'],
+    install_requires=requirements,
+    package_data={'': ['*.rst', '*.txt']},
+    classifiers=[
+        'Programming Language :: Python',
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Science/Research',
+        'Development Status :: 3 - Alpha',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Atmospheric Science',
+    ],
+)
