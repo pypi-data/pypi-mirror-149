3bcd4e0f66e847d2b1f68a83a9813527d8539e5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:33 +0000

    Add package dbt-materialize/1.1.0/dbt-materialize-1.1.0.tar.gz

---
+++ packages/dbt-materialize/dbt-materialize-1.1.0.tar.gz/dbt-materialize-1.1.0/dbt/include/materialize/macros/adapters.sql
@@ -0,0 +1,157 @@
+-- Copyright 2020 Josh Wills. All rights reserved.
+-- Copyright Materialize, Inc. and contributors. All rights reserved.
+--
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License in the LICENSE file at the
+-- root of this repository, or online at
+--
+--     http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+-- Most of these macros are direct copies of their PostgreSQL counterparts.
+-- See: https://github.com/dbt-labs/dbt-core/blob/13b18654f/plugins/postgres/dbt/include/postgres/macros/adapters.sql
+
+{% macro materialize__create_view_as(relation, sql) -%}
+  create view {{ relation }}
+  as (
+    {{ sql }}
+  );
+{%- endmacro %}
+
+{% macro materialize__create_materialized_view_as(relation, sql) -%}
+  create materialized view {{ relation }}
+  as (
+    {{ sql }}
+  );
+{%- endmacro %}
+
+{% macro materialize__create_arbitrary_object(sql) -%}
+    {{ sql }}
+{%- endmacro %}
+
+{% macro materialize__create_schema(relation) -%}
+  {% if relation.database -%}
+    {{ adapter.verify_database(relation.database) }}
+  {%- endif -%}
+  {%- call statement('create_schema') -%}
+    create schema if not exists {{ relation.without_identifier().include(database=False) }}
+  {%- endcall -%}
+{% endmacro %}
+
+{% macro materialize__drop_schema(relation) -%}
+  {% if relation.database -%}
+    {{ adapter.verify_database(relation.database) }}
+  {%- endif -%}
+  {%- call statement('drop_schema') -%}
+    drop schema if exists {{ relation.without_identifier().include(database=False) }} cascade
+  {%- endcall -%}
+{% endmacro %}
+
+{% macro materialize__rename_relation(from_relation, to_relation) -%}
+  {% set target_name = adapter.quote_as_configured(to_relation.identifier, 'identifier') %}
+  {% call statement('rename_relation') -%}
+    alter view {{ from_relation }} rename to {{ target_name }}
+  {%- endcall %}
+{% endmacro %}
+
+{% macro materialize__drop_source(source_name) -%}
+  {% call statement('drop_source') -%}
+    drop source if exists {{ source_name }} cascade
+  {%- endcall %}
+{% endmacro %}
+
+{% macro materialize__drop_index(index_name) -%}
+  {% call statement('drop_index') -%}
+    drop index if exists {{ index_name }}
+  {%- endcall %}
+{% endmacro %}
+
+{% macro materialize__drop_sink(sink_name) -%}
+  {% call statement('drop_sink') -%}
+    drop sink if exists {{ sink_name }}
+  {%- endcall %}
+{% endmacro %}
+
+{% macro materialize__drop_relation(relation) -%}
+  {% call statement('drop_relation') -%}
+    drop view if exists {{ relation }} cascade
+  {%- endcall %}
+{% endmacro %}
+
+{% macro materialize__get_columns_in_relation(relation) -%}
+  {% call statement('get_columns_in_relation', fetch_result=True) %}
+      select
+          column_name,
+          data_type,
+          character_maximum_length,
+          numeric_precision,
+          numeric_scale
+
+      from {{ relation.information_schema('columns') }}
+      where table_name = '{{ relation.identifier }}'
+        {% if relation.schema %}
+        and table_schema = '{{ relation.schema }}'
+        {% endif %}
+      order by ordinal_position
+
+  {% endcall %}
+  {% set table = load_result('get_columns_in_relation').table %}
+  {{ return(sql_convert_columns_in_relation(table)) }}
+{% endmacro %}
+
+{% macro materialize__get_create_index_sql(relation, index_dict) -%}
+  {%- set index_config = adapter.parse_index(index_dict) -%}
+  {%- set comma_separated_columns = ", ".join(index_config.columns) -%}
+  {%- set index_name = index_config.render(relation) -%}
+    create index if not exists
+      "{{ index_name }}"
+      on {{ relation }} {% if index_config.type -%}
+        using {{ index_config.type }}
+  {%- endif %}
+  ({{ comma_separated_columns }});
+{%- endmacro %}
+
+{% macro materialize__list_relations_without_caching(schema_relation) %}
+  {% call statement('list_relations_without_caching', fetch_result=True) -%}
+    select
+      '{{ schema_relation.database }}' as database,
+      tablename as name,
+      schemaname as schema,
+      'table' as type
+    from pg_tables
+    where schemaname ilike '{{ schema_relation.schema }}'
+    union all
+    select
+      '{{ schema_relation.database }}' as database,
+      viewname as name,
+      schemaname as schema,
+      'view' as type
+    from pg_views
+    where schemaname ilike '{{ schema_relation.schema }}'
+  {% endcall %}
+  {{ return(load_result('list_relations_without_caching').table) }}
+{% endmacro %}
+
+{% macro materialize__list_schemas(database) %}
+  {% if database -%}
+    {{ adapter.verify_database(database) }}
+  {%- endif -%}
+  {% call statement('list_schemas', fetch_result=True) %}
+    show schemas from {{ database }}
+  {% endcall %}
+  {{ return(load_result('list_schemas').table) }}
+{% endmacro %}
+
+{% macro materialize__current_timestamp() -%}
+  now()
+{%- endmacro %}
+
+{% macro materialize__snapshot_get_time() -%}
+  {{ current_timestamp() }}::timestamp without time zone
+{%- endmacro %}
