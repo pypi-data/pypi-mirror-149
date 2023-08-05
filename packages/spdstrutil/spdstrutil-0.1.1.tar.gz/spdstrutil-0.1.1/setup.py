de9b0020a643035c00addd16855fb4ff90bae03a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:16:31 +0000

    Add package spdstrutil/0.1.1/spdstrutil-0.1.1.tar.gz

---
+++ packages/spdstrutil/spdstrutil-0.1.1.tar.gz/spdstrutil-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['spdstrutil']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['PyYAML>=6.0,<7.0', 'loguru>=0.6.0,<0.7.0']
+
+setup_kwargs = {
+    'name': 'spdstrutil',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'dasdias',
+    'author_email': 'das.dias6@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
