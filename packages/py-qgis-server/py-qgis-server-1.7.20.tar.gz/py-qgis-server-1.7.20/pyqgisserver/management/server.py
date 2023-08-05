061191debe6a02c8fe9352d8c3aa079ff83d9bb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:34 +0000

    Add package py-qgis-server/1.7.20/py-qgis-server-1.7.20.tar.gz

---
+++ packages/py-qgis-server/py-qgis-server-1.7.20.tar.gz/py-qgis-server-1.7.20/pyqgisserver/management/server.py
@@ -0,0 +1,172 @@
+#
+# Copyright 2021 3liz
+# Author David Marteau
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+"""
+    Server management
+"""
+import os
+import tornado.web
+import logging
+
+from urllib.parse import quote_plus
+
+from ..logger import log_request
+from ..config import confservice
+from ..handlers import (BaseHandler, StatusHandler, OwsApiHandler as QgisHandler, NotFoundHandler)
+from ..zeromq import client
+
+from typing import Awaitable
+
+
+LOGGER=logging.getLogger('SRVLOG')
+
+
+class _PoolHandler(BaseHandler):
+
+    def initialize(self, poolserver ) -> None:
+        super().initialize()
+        self._poolserver = poolserver
+
+    def options(self) -> None:
+        """ Implement OPTION for validating CORS
+        """
+        self.set_option_headers()
+
+
+class _RestartHandler(_PoolHandler):
+
+    def post(self, action) -> None:
+        """ Handle POST method
+        """
+        # Broadcast 'restart' to workers
+        if action == 'restart':
+            # Restart workers
+            self._poolserver.restart()
+
+        self.write_json({ 'status': 'ok' }) 
+
+
+class _ReportHandler(_PoolHandler):
+
+    async def get(self) -> Awaitable[None]:
+        """ Return worker reports
+        """
+        req = self.request
+        reports = await self._poolserver.get_reports()
+        for w in reports:
+            for entry in w['cache']:
+                key = entry['key']
+                # Take care of absolute path
+                # see https://github.com/3liz/py-qgis-server/issues/40
+                if key.startswith('/'):
+                    # Use MAP parameter
+                    entry.update(link=f"{req.protocol}://{req.host}/cache/content/?MAP={quote_plus(key)}")
+                else:
+                    entry.update(link=f"{req.protocol}://{req.host}/cache/content/{quote_plus(key)}")
+        self.write_json({'workers': reports, 'num_workers': self._poolserver.num_workers }) 
+
+
+class _RootHandler(BaseHandler):
+
+    def get(self) -> None:
+        """ Return links to default api entries
+        """
+        req = self.request
+        def _link( path: str, title: str ):
+            return {
+                'href' : f"{req.protocol}://{req.host}{path}",
+                'title': title,
+                'type' : "application/json",
+            }
+
+        data = dict(
+            links=[
+                _link("/status" , "Server status and configuration"),
+                _link("/plugins", "Plugins managment"),
+                _link("/cache"  , "Projects cache managment"),
+                _link("/pool"   , "Workers pool status"),
+            ]
+        )
+        self.write(data) 
+
+
+    def options(self) -> None:
+        """ Implement OPTION for validating CORS
+        """
+        self.set_option_headers()
+
+
+
+def configure_handlers( poolserver, client: client.AsyncClient ) -> [tornado.web.RequestHandler]:
+    """
+    """
+    kwargs =  {
+        'client': client, 
+        'timeout': confservice['server'].getint('timeout'),
+        'service': 'Managment',
+    }
+
+    handlers = [
+        (r"/"      , _RootHandler),
+        (r"/status", StatusHandler),
+        (r"/pool/(restart)", _RestartHandler, {'poolserver': poolserver}),
+        (r"/pool/?"         ,_ReportHandler,  {'poolserver': poolserver}),
+        # Forward to Qgis api handlers
+        (r"/.+"             , QgisHandler, kwargs),
+    ]
+    return handlers
+
+
+class _Management(tornado.web.Application):
+
+
+    def __init__(self, poolserver, router: str) -> None:
+        """
+        """
+        identity = bytes(f"MANAGEMENT-{os.getpid()}".encode('ascii'))
+        self._broker_client = client.AsyncClient(router, identity)
+        super().__init__(configure_handlers(poolserver, self._broker_client),
+                         default_handler_class=NotFoundHandler)
+
+
+    def log_request(self, handler: tornado.web.RequestHandler ) -> None:
+        """ Write HTTP requet to the logs
+        """
+        log_request(handler)
+
+    def terminate(self) -> None:
+        self._broker_client.terminate()
+
+
+def create_ssl_context( conf ):
+    import ssl
+    ssl_ctx = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+    ssl_ctx.load_cert_chain(conf['ssl_cert'],conf['ssl_key'])
+    return ssl_ctx
+
+
+def start_management_server( poolserver, router: str ) -> _Management:
+    """ Start management server,
+    """
+    conf = confservice['management']
+
+    port    = conf.getint('port')
+    address = conf['interfaces']
+
+    LOGGER.info("MANAGEMENT: running server on %s:%s",address,port)
+
+    kwargs = {}
+    if conf.getboolean('ssl'):
+        LOGGER.info("MANAGEMENT: SSL enabled")
+        kwargs['ssl_options'] = create_ssl_context(conf)
+
+    app = _Management(poolserver, router)
+    app.listen(port, address=address, xheaders=True, **kwargs)
+
+    return app
+
---
+++ packages/py-qgis-server/py-qgis-server-1.7.20.tar.gz/py-qgis-server-1.7.20/pyqgisservercontrib/core/filters.py
@@ -0,0 +1,80 @@
+#
+# Copyright 2019 3liz
+# Author: David Marteau
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+
+""" Define middleware filters
+
+    Declare filters as
+
+    @blockingfilter(pri=100, uri="/ows/")
+    def myfilter1( handller: tornado.web.RequestHandler, *args ) -> None:
+        ...
+
+    @asyncfilter(pri=100, uri="/ows/")
+    async def myfilter2( handller: tornado.web.RequestHandler, *args) -> None:
+        ...
+
+
+    Filters are applied before sending the request, a filter can return response or 
+    raise exception based on authorization rules.
+
+    Registering filters:
+
+    Modules implementing filters must register their filters using setup.py entry_points with the 
+    key 'pyqgisserver_filters'
+
+    def register_filters():
+        # Do initialization stuff
+        return [ myfilter1, myfilter2 ]
+"""
+
+import logging
+import tornado.web
+
+from typing import Coroutine, Callable, Awaitable, Optional
+
+LOGGER = logging.getLogger('SRVLOG')
+
+
+class ServerFilter:
+    pass
+
+
+class asyncfilter(ServerFilter):
+    """ Decorator for asynchronous request filter
+
+        Assume that the function return a  coroutine
+    """
+
+    def __init__(self, pri=0, uri=""):
+        self.pri = pri
+        self.uri = uri
+
+    def __call__(self, fn: Callable[[tornado.web.RequestHandler], Awaitable[None]]) -> 'asyncfilter':
+        self.fn = fn
+        return self
+
+    def apply( self, handler: tornado.web.RequestHandler ) -> Coroutine:
+        return self.fn(handler)
+
+
+class blockingfilter(ServerFilter):
+    """ Decorator for synchronous request filter
+    """
+
+    def __init__(self, pri=0, uri=""):
+        self.pri = pri
+        self.uri = uri
+
+    def __call__(self, fn):
+        self.fn = fn
+        return self
+
+    async def apply( self, handler: tornado.web.RequestHandler ) -> Optional[str]:
+        return self.fn(handler)
+
