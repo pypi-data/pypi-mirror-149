45e428ef0ee2378115573003dbdc8bd6c86b1cbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:16:45 +0000

    Add package cion/0.0.2/cion-0.0.2-py3-none-any.whl

---
+++ packages/cion/cion-0.0.2-py3-none-any.whl/cion/exceptions.py
@@ -0,0 +1,33 @@
+from typing import Any, Optional
+
+
+class CionException(Exception):
+    """Base class for all exceptions raised by Cion"""
+
+
+class ValidatorError(CionException):
+    """Exception raised by a validator"""
+
+    message: str  #: The message that the validator raised
+
+    def __init__(self, message: str) -> None:
+        self.message = message
+
+
+Errors = dict[str, list[str]]
+ValidData = dict[str, Any]
+
+
+class ValidationError(CionException):
+    """Error raised when validating data"""
+
+    errors: Errors  #: A dictionary containing all of the errors
+    data: Optional[ValidData] = None  #: Contains all of the validated data
+
+    def __init__(self, errors: Errors, valid_data: ValidData) -> None:
+        self.errors = errors
+        self.data = valid_data
+        super().__init__("Errors were raised: " + str(dict(self.errors)))
+
+    def __repr__(self) -> str:
+        return f"<{self.__class__.__name__} errors={self.errors} data={self.data}>"
