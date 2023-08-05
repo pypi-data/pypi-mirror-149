bca3fc30541cf80f906f232a99e33d735be933af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:20:02 +0000

    Add package cinemasci/1.7.2/cinemasci-1.7.2.tar.gz

---
+++ packages/cinemasci/cinemasci-1.7.2.tar.gz/cinemasci-1.7.2/cinemasci/server/__main__.py
@@ -0,0 +1,27 @@
+import http.server
+import socketserver
+from urllib.parse import urlparse
+from urllib.parse import parse_qs
+# from os import path
+# from os.path import relpath
+# from os import getcwd
+# from os import access
+# from os import R_OK
+
+from . import run_cinema_server
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description="run a Cinema Viewer")
+    parser.add_argument("--databases", required=True, nargs="+", default=None, help="database to view (required)") 
+    parser.add_argument("--viewer", required=True, default='explorer', help="viewer type to use. One of [explorer, view] (required)") 
+    parser.add_argument("--assetname", default=None, help="asset name to use (optional)") 
+    parser.add_argument("--port", type=int, default=8000, help="port to use (optional)") 
+    parser.add_argument("--rundir", required=True, help="directory in which to run the server") 
+    args = parser.parse_args()
+
+    if (args.databases is None):
+        print("Error")
+    else:
+        run_cinema_server(args.viewer, args.rundir, args.databases, args.port, args.assetname)
