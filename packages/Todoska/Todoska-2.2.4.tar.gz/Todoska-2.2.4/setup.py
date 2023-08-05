4d806934c0ceaccf88dc9bb87d158a2f64ecabb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:21 +0000

    Add package Todoska/2.2.4/Todoska-2.2.4.tar.gz

---
+++ packages/Todoska/Todoska-2.2.4.tar.gz/Todoska-2.2.4/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import setuptools
+
+
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='Todoska',
+  version='2.2.4',
+  description='A Basic Todo Tracker in Cli',
+  long_description_content_type="text/markdown",
+  long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='https://github.com/Abbhiishek/Todoska',  
+  author='Abhishek Kushwaha',
+  author_email='abhishekkushwaha1479@gmail.com',
+  license='MIT', 
+  entry_points={
+    'console_scripts': [
+      'Todoska = Todoska.todocli:app',
+
+    ]
+  },
+  classifiers=classifiers,
+  keywords=['Python' , 'cli' , 'Todo' ,'Todotracker' , 'todo-tracker' , 'todo-tracker-cli', 'Todoska'], 
+  packages=find_packages(),
+  python_requires='>=3.6',
+  install_requires=['typer' , 'rich' ] 
+)
