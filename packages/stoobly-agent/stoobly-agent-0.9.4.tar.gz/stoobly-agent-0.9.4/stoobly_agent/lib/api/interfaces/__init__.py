ede46e6f32cdd6be5c4fbf346e49f706fb056d15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:24:13 +0000

    Add package stoobly-agent/0.9.4/stoobly-agent-0.9.4.tar.gz

---
+++ packages/stoobly-agent/stoobly-agent-0.9.4.tar.gz/stoobly-agent-0.9.4/stoobly_agent/lib/api/interfaces/__init__.py
@@ -0,0 +1,11 @@
+from .pagination_query_params import PaginationQueryParams
+from .projects import ProjectCreateParams, ProjectShowResponse, ProjectsIndexQueryParams, ProjectsIndexResponse 
+from .report_show_response import ReportShowResponse
+from .request_create_params import RequestCreateParams
+from .request_show_query_params import RequestShowQueryParams
+from .request_show_response import QueryParam, RequestShowResponse
+from .requests_index_query_params import RequestsIndexQueryParams
+from .requests_index_response import RequestsIndexResponse
+from .response_show_response import ResponseShowResponse
+from .scenarios import ScenarioShowResponse, ScenariosIndexResponse
+from .users import UserProfileResponse
---
+++ packages/stoobly-agent/stoobly-agent-0.9.4.tar.gz/stoobly-agent-0.9.4/stoobly_agent/lib/logger.py
@@ -0,0 +1,29 @@
+import logging
+import os
+
+from stoobly_agent.config.constants.env_vars import LOG_LEVEL
+
+class Logger:
+    _instance = None
+
+    def __init__(self):
+        raise RuntimeError('Call instance() instead')
+
+    @classmethod
+    def instance(cls):
+        if cls._instance is None:
+            cls._instance = cls.__new__(cls)
+
+            log_level = os.getenv(LOG_LEVEL) or ''
+
+            if log_level.lower() == 'debug':
+                logging.basicConfig(level=logging.DEBUG)
+            elif log_level.lower() == 'warning':
+                logging.basicConfig(level=logging.WARNING)
+            elif log_level.lower() == 'error':
+                logging.basicConfig(level=logging.ERROR)
+            else:
+                logging.basicConfig(level=logging.INFO)
+
+        return logging
+
