bd09c9c9a63ea077b2ddf5925cb8c99c15d5b3fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:17:50 +0000

    Add package sw-aqueduct/0.2.1/sw_aqueduct-0.2.1-py3-none-any.whl

---
+++ packages/sw-aqueduct/sw_aqueduct-0.2.1-py3-none-any.whl/aqueduct/models/__init__.py
@@ -0,0 +1,24 @@
+from .application import Application
+from .asset import Asset
+from .group import Group
+from .package import Package
+from .report import Report
+from .role import Role
+from .workflow import Workflow
+from .workspace import Workspace
+from .dashboard import (
+    Dashboard,
+    ReportItems
+)
+from .plugin import (
+    Plugin,
+    PluginLight
+)
+from .task import (
+    Task,
+    TaskLight
+)
+from .user import (
+    User,
+    UserLight
+)
