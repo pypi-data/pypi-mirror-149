2291edb0a13e538478b015899a8a4b928f5dd000
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:15 +0000

    Add package popug-sdk/0.0.2/popug_sdk-0.0.2.tar.gz

---
+++ packages/popug-sdk/popug_sdk-0.0.2.tar.gz/popug_sdk-0.0.2/popug_sdk/db/__init__.py
@@ -0,0 +1,58 @@
+from contextlib import contextmanager
+from typing import Any
+
+from sqlalchemy import create_engine as _engine_create
+from sqlalchemy.engine import Engine
+from sqlalchemy.orm import (
+    Session,
+    sessionmaker,
+)
+
+from popug_sdk.conf import settings
+from popug_sdk.db.base_class import BaseModel
+
+__all__ = (
+    "create_session",
+    "BaseModel",
+    "init_db",
+    "create_engine",
+)
+
+
+_engine: Engine | None = None
+_Session: sessionmaker | None = None
+
+
+def create_engine() -> Engine:
+    global _engine
+
+    if not _engine:
+        _engine = _engine_create(settings.database_dsn, pool_pre_ping=True)
+
+    return _engine
+
+
+def init_db() -> sessionmaker:
+    global _Session
+    create_engine()
+
+    if not _Session:
+        _Session = sessionmaker(
+            autocommit=False,
+            autoflush=False,
+            bind=_engine,
+            expire_on_commit=False,
+        )
+
+        return _Session
+
+
+@contextmanager
+def create_session(session: Session | None = None, **kwargs: Any) -> Session:
+    if not session:
+        _session = init_db()
+
+        with _session(**kwargs) as session:
+            yield session
+    else:
+        yield session
