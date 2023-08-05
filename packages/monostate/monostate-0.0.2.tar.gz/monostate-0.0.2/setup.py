35286848d70bd9353fea5fb9caacf5ac19b080e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:34:00 +0000

    Add package monostate/0.0.2/monostate-0.0.2.tar.gz

---
+++ packages/monostate/monostate-0.0.2.tar.gz/monostate-0.0.2/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+from monostate import __version__
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name='monostate',
+    package_dir={str(): 'monostate'},
+    packages=find_packages(where='monostate'),
+    version=__version__,
+    license='MIT',
+    description='Dependency-free monostate owner base class',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    keywords=['singleton', 'borg'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    url='https://github.com/w2sv/monostate',
+    python_requires='>=3.6',
+    author='w2sv',
+    author_email='zangenbergjanek@googlemail.com',
+    platforms=['Linux', 'Windows', 'MacOS']
+)
