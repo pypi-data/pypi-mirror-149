2b8df45015b501d91f52b526e21d3e5f49757019
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:34 +0000

    Add package slack-blocks-wrapper/0.1.2/slack_blocks_wrapper-0.1.2-py3-none-any.whl

---
+++ packages/slack-blocks-wrapper/slack_blocks_wrapper-0.1.2-py3-none-any.whl/slack_blocks_wrapper/elements/text.py
@@ -0,0 +1,42 @@
+from enum import Enum
+
+
+class TextType(Enum):
+    PLAIN_TEXT = "plain_text"
+    MARKDOWN_TEXT = "mrkdwn"
+
+
+def text_element(
+        text: str,
+        text_type: TextType,
+        emoji: bool = True,
+        verbatim: bool = False,
+        value: str = None
+):
+    """
+    Summary: A slack block text node \n
+    Args:
+        text (str): The text to display
+        text_type (TextType): The text type whether plain text or markdown
+        emoji (bool): Whether to display emojis or not
+        verbatim (bool): Whether to display verbatim or not
+        value (str): The value to be passed down the app
+    """
+    if (
+            text_type.value != TextType.PLAIN_TEXT
+            and text_type.value != TextType.MARKDOWN_TEXT
+    ):
+        raise ValueError("text_type must be `plain_text` or `mrkdwn`")
+    node = {
+        "text": {
+            "type": text_type,
+            "text": text,
+            "emoji": emoji,
+            "verbatim": verbatim
+        }
+    }
+    if value is not None:
+        node["value"] = value
+    if text_type == TextType.MARKDOWN_TEXT:
+        node.pop("emoji")
+    return node
