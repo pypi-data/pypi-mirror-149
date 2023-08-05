b3b9c6e39959eff97e47ed46aaeae1f6396d42d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:47 +0000

    Add package distributions-for-dsnd/0.3/distributions_for_dsnd-0.3.tar.gz

---
+++ packages/distributions-for-dsnd/distributions_for_dsnd-0.3.tar.gz/distributions_for_dsnd-0.3/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions_for_dsnd',
+      version='0.3',
+      description='Gaussian distributions',
+      packages=['distributions_for_dsnd'],
+      author = 'Mona Anderssen',
+      zip_safe=False)
