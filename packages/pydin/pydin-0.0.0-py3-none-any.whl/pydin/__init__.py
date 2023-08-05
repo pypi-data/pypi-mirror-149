9867c2eb6c14df101d9fcd64fabc77a41638c927
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:54 +0000

    Add package pydin/0.0.0/pydin-0.0.0-py3-none-any.whl

---
+++ packages/pydin/pydin-0.0.0-py3-none-any.whl/pydin/__init__.py
@@ -0,0 +1,11 @@
+"""Python Data Integrations."""
+
+
+__version__ = '0.0.0'
+__status__ = 'Development'
+
+__author__ = 'Timur Faradzhov'
+__email__ = 'timurfaradzhov@gmail.com'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2022, The PYDIN Project'
+__maintainer__ = __author__
