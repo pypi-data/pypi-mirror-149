16bc08226614b83837286c32058e3084f8c47940
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:22:00 +0000

    Add package nanobot-plugin-splatoon2tools/1.3.1/nanobot_plugin_splatoon2tools-1.3.1-py3-none-any.whl

---
+++ packages/nanobot-plugin-splatoon2tools/nanobot_plugin_splatoon2tools-1.3.1-py3-none-any.whl/splatoon2_tools/__init__.py
@@ -0,0 +1,183 @@
+from lib2to3.pgen2 import driver
+from nonebot.adapters import Event
+from nonebot import get_driver, on_command
+import nonebot
+from nonebot.adapters.onebot.v11 import MessageSegment, Message
+from nonebot.internal.matcher import Matcher
+from .core import SplatoonInfo
+from .config import Config
+from nonebot import require
+from nonebot import get_bots
+from nonebot.permission import SUPERUSER
+from .utils import SplatoonUtiles
+import threading
+
+# 初始化
+global_config = get_driver().config
+config = Config.parse_obj(global_config)
+driver = get_driver()
+
+__splatoon2_tools_version__ = "v1.3.0"
+
+splatoon2 = SplatoonInfo()
+splatoon2_utiles = SplatoonUtiles()
+
+@driver.on_startup
+async def start():
+    get_cookies()
+    scheduler.add_job(cron_push, "cron", hour="*/2", minute = 5, id="cron_push")
+    scheduler.add_job(cron_get_cookies, "cron", hour="*/2", minute = 3, id="cron_get_cookies")
+
+
+if global_config.group_id_list is not None:
+    group_id_list=global_config.group_id_list
+else: group_id_list=[]
+
+if global_config.bot_qq is not None:
+    bot_qq=str(global_config.bot_qq)
+else: bot_qq = ""
+
+salmon_run = on_command('打工', aliases={'工'})
+regular_battle = on_command('涂地', aliases={'涂地'})
+ranked_battle = on_command('单排', aliases={'单排'})
+league_battle = on_command('组排', aliases={'组排'})
+scheduler = require("nonebot_plugin_apscheduler").scheduler
+admin_open= on_command('开启自动推送', permission=SUPERUSER)
+admin_close= on_command('关闭自动推送', permission=SUPERUSER)
+update = on_command('更新', permission=SUPERUSER)
+push = on_command('推送')
+
+# 响应回复板块头
+@regular_battle.handle()
+async def _( matcher: Matcher):
+    if not splatoon2.cookies_exists(): 
+        splatoon2.get_image_cookies()    
+    nonebot.logger.info("推送涂地资讯")
+    await regular_battle.finish(MessageSegment.image(splatoon2.push_regular_battle()))
+
+@ranked_battle.handle()
+async def _(matcher: Matcher):
+    if not splatoon2.cookies_exists(): 
+        splatoon2.get_image_cookies()    
+    nonebot.logger.info("推送单排资讯")
+    await ranked_battle.finish(MessageSegment.image(splatoon2.push_ranked_battle()))
+
+@league_battle.handle()
+async def _(matcher: Matcher):
+    if not splatoon2.cookies_exists(): 
+        splatoon2.get_image_cookies()    
+    nonebot.logger.info("推送组排资讯")
+    await league_battle.finish(MessageSegment.image(splatoon2.push_league_battle()))
+
+@salmon_run.handle()
+async def _(matcher: Matcher):
+    if not splatoon2.cookies_exists(): 
+        splatoon2.get_image_cookies()    
+    nonebot.logger.info("推送打工资讯")
+    await salmon_run.finish(MessageSegment.image(splatoon2.push_salmon_run()))
+
+
+@push.handle()
+async def _(matcher: Matcher, event: Event):
+    if not splatoon2.cookies_exists(): 
+        splatoon2.get_image_cookies()
+    forward_msg=news_list()
+    bot=get_bots()[bot_qq]
+    await bot.send_group_forward_msg(group_id=event.get_session_id().split('_')[1],messages=forward_msg)
+    
+@update.handle()
+async def _(matcher: Matcher):
+    splatoon2.clear_cookies()
+    get_cookies()
+    await update.finish("更新成功")
+
+# 响应回复板块尾
+
+# 定时任务头
+
+def cron_get_cookies():
+    splatoon2.clear_cookies()
+    get_cookies()
+
+
+async def cron_push():
+    if not splatoon2.cookies_exists():
+        splatoon2.get_image_cookies()
+    forward_msg=news_list()
+    bot=get_bots()[bot_qq]
+    if group_id_list is not None:
+        for group_id in group_id_list:
+            try:
+                nonebot.logger.info(group_id)
+                await bot.send_group_forward_msg(group_id=group_id,messages=forward_msg)
+            except:
+                nonebot.logger.error("推送群消息出现错误")
+
+@admin_open.handle()
+async def _(matcher: Matcher):
+    if scheduler.get_job("cron_push") is not None:
+        await admin_close.finish(MessageSegment.text("已经开启自动推送了哦"))
+    else:
+        scheduler.add_job(cron_push, "cron", hour="*/2", minute = 5, id="cron_push")
+        await admin_open.finish(MessageSegment.text("已打开自动推送"))
+
+
+@admin_close.handle()
+async def _(matcher: Matcher):
+    if scheduler.get_job("cron_push") is None:
+        await admin_close.finish(MessageSegment.text("没有开启自动推送哦"))
+    else: 
+        scheduler.remove_job("cron_push")
+        await admin_close.finish(MessageSegment.text("已关闭自动推送"))
+
+# 定时任务尾
+
+# 自定义方法头
+
+def news_list():
+    """
+    @name：news_list
+    @author： Sichongzou
+    @remark： 组装群发消息
+    @param： 
+    @return： 组装好的群发消息
+    """
+    forward_msg=[]
+    #forward_msg.append(to_json("这里是Splatoon2定时推送姬,以下是未来时间段的地图信息！"))
+    forward_msg.append(to_json(Message(MessageSegment.image(splatoon2.push_league_battle()))))
+    forward_msg.append(to_json(Message(MessageSegment.image(splatoon2.push_ranked_battle()))))
+    forward_msg.append(to_json(Message(MessageSegment.image(splatoon2.push_regular_battle()))))
+    forward_msg.append(to_json(Message(MessageSegment.image(splatoon2.push_salmon_run()))))
+
+    return forward_msg
+
+
+def to_json(msg: Message):
+    """
+    @name：cron_push
+    @author： Sichongzou
+    @remark： 转群转发
+    @param： 消息列
+    @return： none
+    """
+    if global_config.bot_qq_name is not None:
+        name = global_config.bot_qq_name
+    else:
+        name = "splatoon2推送机器人"
+    return {"type": "node", "data": {"name": name, "uin": bot_qq, "content": msg}}
+
+
+def get_cookies():
+    """
+    @name：Sichongzou
+    @author： DrinkOolongTea
+    @remark： 并发获取缓存
+    @param： 
+    @return：
+    """
+    nonebot.logger.info("获取splatoon2缓存")
+    t = threading.Thread(target=splatoon2.get_image_cookies)
+    t.start()
+    nonebot.logger.info("获取splatoon2缓存结束")
+
+# 自定义方法尾
---
+++ packages/nanobot-plugin-splatoon2tools/nanobot_plugin_splatoon2tools-1.3.1-py3-none-any.whl/splatoon2_tools/utils.py
@@ -0,0 +1,174 @@
+import base64
+import nonebot
+import datetime
+from io import BytesIO
+from pathlib import Path
+from lxml import etree
+from PIL import Image, ImageDraw, ImageFont
+from typing import List
+import configparser
+import telnetlib
+import httpx
+
+
+class SplatoonUtiles:
+
+    @staticmethod
+    def telnet(ip: str, port: str):
+        """
+        @name：telnet
+        @author： DrinkOolongTea
+        @remark： 判断是否超时
+        @return： boolean
+        """
+        nonebot.logger.info("代理连接测试")
+        try:
+            telnetlib.Telnet(ip, port, timeout=5)
+            nonebot.logger.success("代理连接成功")
+            return True
+        except:
+            nonebot.logger.error("代理连接超时" + ip + ":" + str(port))
+            return False
+    
+    @staticmethod
+    def request_get(url: str, proxy: dict = None):
+        """
+        @name：request_get
+        @author： DrinkOolongTea
+        @remark： 获取数据
+        @return： 获取到的数据
+        """
+        
+        if proxy is not None:
+            nonebot.logger.info("使用代理连接")
+            nonebot.logger.info(proxy)
+            req = httpx.get(url,timeout=(5,30))
+        else:
+            nonebot.logger.info("不使用代理连接")
+            req = httpx.get(url,timeout=(10,60))
+        html = req.text
+        selector = etree.HTML(html)
+        return selector
+    
+    @staticmethod
+    def get_config(path: Path, section: str, option: str) -> str:
+        """
+        @name：get_config
+        @author： DrinkOolongTea
+        @remark： 获取配置文件
+        @param： path:配置文件路径 section:配置项名称 option:配置项key
+        @return： 值
+        """
+        config = configparser.ConfigParser()
+        config.read(path, encoding="utf-8")
+        return config.get(section, option)
+
+    @staticmethod
+    def change_time_zone(time_str: str) -> str:
+        """
+        @name：change_time_zone
+        @author： DrinkOolongTea
+        @remark： 更改时区为UTC+8时区
+        @param： time:当前时间
+        @return： 更改时区后时间
+        """
+        time_str: str = datetime.datetime.strptime(time_str + str(datetime.datetime.today().year), "%b %d %H:%M %Y")
+        time_str: str = (time_str + datetime.timedelta(hours=8)).strftime('%m-%d %H:%M')
+        return time_str
+
+    @staticmethod
+    def battle_time() -> int:
+        """
+        @name：battle_time
+        @author： DrinkOolongTea
+        @remark： 截取以2为倍数的整数时间
+        @return： 返回时间
+        """
+        curr_time = datetime.datetime.now()
+        time_str = curr_time.strftime("%H")
+        if int(time_str) % 2 > 0:
+            return int(time_str) - 1
+        else:
+            return int(time_str)
+
+    @staticmethod
+    def draw_text(info: List, box: List, background_img: Image, color: str, font: ImageFont):
+        """
+        @name：draw_text
+        @author： DrinkOolongTea
+        @remark： 拼接文字图片
+        @param： info: 文字list background_img:图片 color:颜色 font:字体路径
+        @return： 拼接后图片
+        """
+        draw = ImageDraw.Draw(background_img)
+        for i in range(len(info)):
+            draw.text(box[i], str(info[i]), fill=color, font=font)
+
+    @staticmethod
+    def img_base64_str(image: Image, img_format: str) -> str:
+        """
+        @name：img_base64_str
+        @author： DrinkOolongTea
+        @remark： 将图片转为base64存储
+        @param： image: 图片对象 img_format:图片格式
+        @return： base64str
+        """
+        buf = BytesIO()
+        image.save(buf, img_format)
+        base64_str: str = "base64://" + base64.b64encode(buf.getbuffer()).decode()
+        return buf
+
+    @staticmethod
+    def mode_dict(context: str) -> str:
+        """
+        @name：mode_dict
+        @author： DrinkOolongTea
+        @remark： splatoon2模式字典
+        @param： 英文
+        @return： 中文
+        """
+        d = {"Rainmaker": "魚", "Splat Zones": "區域", "Clam Blitz": "蛤蜊", "Tower Control": "塔"}
+        return d[context]
+
+    @staticmethod
+    def save_bytes_file(filename: Path, b: bytes):
+        """
+        @name：push_league_battle
+        @author： DrinkOolongTea
+        @remark： 保存文件
+        @param： filename: 文件路径, s:储存内容
+        @return： 
+        """
+        f = open(filename, 'wb')
+        f.write(b.getvalue())
+        f.close()
+
+    @staticmethod
+    def paste_img(path: Path, info: List, box: List, background_img: Image):
+        """
+        @name：paste_img
+        @author： DrinkOolongTea
+        @remark： 拼接图片
+        @param： path:图片源路径 info: 图片list  box: 文字位置, background_img:背景图片
+        @return： 拼接后结果
+        """
+        for i in range(len(info)):
+            img = Image.open(Path(path / Path((info[i]).replace(" ", "_") + ".png"))).convert(
+                    'RGBA')
+            r, g, b, a = img.split()
+            background_img.paste(img, box[i], mask=a)
+
+    @staticmethod
+    def read_bytes_file(filename: Path) -> bytes:
+        """
+        @name：push_league_battle
+        @author： DrinkOolongTea
+        @remark： 读取文件内容
+        @param： filename: path 文件路径
+        @return： 读取内容
+        """
+        f = open(filename, 'rb')
+        b_io = BytesIO(f.read())
+        f.close()
+        base64_str: str = "base64://" + base64.b64encode(b_io.getbuffer()).decode()
+        return base64_str
