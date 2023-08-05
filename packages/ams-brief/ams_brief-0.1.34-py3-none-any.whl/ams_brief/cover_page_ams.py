b1849831da954760f7cb6ba04f2fae03c20ca1fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:13:35 +0000

    Add package ams-brief/0.1.34/ams_brief-0.1.34-py3-none-any.whl

---
+++ packages/ams-brief/ams_brief-0.1.34-py3-none-any.whl/ams_brief/cover_page_ams.py
@@ -0,0 +1,18 @@
+cover_logo = "X:/9999_BID_AMS/AMS_Logo's/RGB/Animotions_Logo_02_Neg_RGB.png"
+
+
+def create_cover_page(brief):
+    brief.add_page()
+    brief.set_fill_color(224, 157, 157)
+    brief.set_text_color(60, 60, 60)
+    brief.set_font('montserrat', size=48)
+    brief.rect(x=10, y=10, w=275, h=190, style='F')
+    #brief.set_xy(20, 100)
+    brief.image(cover_logo, x=20, y=100, w=100)
+    #brief.cell(w=0, txt='ANIMOTIONS', ln=2)
+    brief.set_xy(20, 110)
+    brief.set_font('nexa', size=16)
+    brief.cell(w=0, txt='beyond the visual')
+    brief.set_xy(20, 185)
+    brief.set_font('nexa', size=14)
+    brief.cell(0, txt='www.animotions.be')
