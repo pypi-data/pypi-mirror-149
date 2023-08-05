14cc059184a155c72568105fb5c87ff7de9b3f2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:04 +0000

    Add package conductor-python/1.0.27/conductor-python-1.0.27.tar.gz

---
+++ packages/conductor-python/conductor-python-1.0.27.tar.gz/conductor-python-1.0.27/src/conductor/client/configuration/settings/metrics_settings.py
@@ -0,0 +1,33 @@
+from conductor.client.configuration.configuration import Configuration
+from pathlib import Path
+import logging
+import os
+
+logger = logging.getLogger(
+    Configuration.get_logging_formatted_name(
+        __name__
+    )
+)
+
+
+def get_default_temporary_folder() -> str:
+    try:
+        metrics_dir = str(Path.home()) + '/tmp/'
+        if not os.path.isdir(metrics_dir):
+            os.mkdir(metrics_dir)
+        return metrics_dir
+    except Exception as e:
+        logger.warning('failed to create metrics temporary folder')
+
+
+class MetricsSettings:
+    def __init__(
+            self,
+            directory: str = None,
+            file_name: str = 'metrics.log',
+            update_interval: float = 0.1):
+        if directory == None:
+            directory = get_default_temporary_folder()
+        self.directory = directory
+        self.file_name = file_name
+        self.update_interval = update_interval
