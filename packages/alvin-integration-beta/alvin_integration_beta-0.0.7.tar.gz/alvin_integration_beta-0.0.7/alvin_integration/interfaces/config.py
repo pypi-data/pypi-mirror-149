b07199ebae5f80fdfeb2acd90fc497bf4ff8aa94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:57 +0000

    Add package alvin-integration-beta/0.0.7/alvin_integration_beta-0.0.7.tar.gz

---
+++ packages/alvin-integration-beta/alvin_integration_beta-0.0.7.tar.gz/alvin_integration_beta-0.0.7/alvin_integration/interfaces/config.py
@@ -0,0 +1,25 @@
+from abc import abstractmethod, ABC
+
+
+class AbstractProducerConfig(ABC):
+
+    @property
+    @abstractmethod
+    def producer_name(self):
+        pass
+
+    @abstractmethod
+    def get_patching_list(self):
+        pass
+
+    @abstractmethod
+    def get_target_packages(self):
+        pass
+
+    @abstractmethod
+    def get_target_pipelines(self):
+        pass
+
+    @abstractmethod
+    def get_lineage_config(self):
+        pass
