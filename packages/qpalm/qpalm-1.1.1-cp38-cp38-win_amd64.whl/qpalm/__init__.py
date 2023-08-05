275c5398775e8b729cc6e4eb26f152c240483b3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:16:23 +0000

    Add package qpalm/1.1.1/qpalm-1.1.1-cp38-cp38-win_amd64.whl

---
+++ packages/qpalm/qpalm-1.1.1-cp38-cp38-win_amd64.whl/qpalm/__init__.py
@@ -0,0 +1,14 @@
+"""Proximal Augmented Lagrangian method for Quadratic Programs"""
+
+__version__ = '1.1.1'
+
+import os
+import typing
+
+if not typing.TYPE_CHECKING and os.getenv('QPALM_PYTHON_DEBUG'):
+    from qpalm._qpalmd import *
+    from qpalm._qpalmd import __version__ as c_version
+else:
+    from qpalm._qpalm import *
+    from qpalm._qpalm import __version__ as c_version
+assert __version__ == c_version
