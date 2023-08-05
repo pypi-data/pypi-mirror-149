aacd5d7964599bc68cb403d5165baa0c4b1ea271
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:26:31 +0000

    Add package trove-classifiers-cli/0.1.2/trove-classifiers-cli-0.1.2.tar.gz

---
+++ packages/trove-classifiers-cli/trove-classifiers-cli-0.1.2.tar.gz/trove-classifiers-cli-0.1.2/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['trove_classifiers_cli']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['trove-classifiers>=2022.4.30,<2023.0.0']
+
+entry_points = \
+{'console_scripts': ['trove-classifers = trove_classifiers_cli.cli:main']}
+
+setup_kwargs = {
+    'name': 'trove-classifiers-cli',
+    'version': '0.1.2',
+    'description': '',
+    'long_description': '',
+    'author': 'Xinyuan Chen',
+    'author_email': '45612704+tddschn@users.noreply.github.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/tddschn/trove-classifers-cli',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
