b5a510454fe73cc5a2cf9e43035e64f2d1ffbad0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:11:59 +0000

    Add package FieldEmission/0.0.2/FieldEmission-0.0.2.tar.gz

---
+++ packages/FieldEmission/FieldEmission-0.0.2.tar.gz/FieldEmission-0.0.2/FieldEmission/feFormulas.py
@@ -0,0 +1,16 @@
+from logging import exception
+from FieldEmission.dataManager import DataProvider as dPrvdr
+import numpy as np
+import math
+
+
+def FowlerNordheim(DatMgr: dPrvdr, ColumnU, ColumnI, distance_m):
+    # Create copys of U and I (also for predefining correct array-size)
+    fnX = abs(DatMgr.GetColumn(ColumnU))  # Here still U!
+    fnY = abs(DatMgr.GetColumn(ColumnI))  # Here still I!
+
+    for iRow in range(len(fnX)):
+        fnX[iRow] = distance_m / fnX[iRow]  # Convert d/U = 1/E
+        fnY[iRow] = math.log(fnY[iRow] * math.pow(fnX[iRow], 2))  # Convert I -> fnY
+
+    return np.transpose([fnX, fnY])
