9db2aa58f97d5bac848ae2c7f8fb0dadbfedc7e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:14:51 +0000

    Add package theory-game/1.6/theory_game-1.6.tar.gz

---
+++ packages/theory-game/theory_game-1.6.tar.gz/theory_game-1.6/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='theory_game',
+    version='1.6',
+    packages=find_packages(),
+    url = 'https://pypi.org/',
+    author = 'Denis Perkov',
+    author_email = 'denisperkov0@gmail.com'
+)
