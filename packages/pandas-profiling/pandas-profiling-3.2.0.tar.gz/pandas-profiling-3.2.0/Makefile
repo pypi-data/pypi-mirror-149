78bcc997b6dc9f0798fb406dc6e26c1b6b8c83db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:03 +0000

    Add package pandas-profiling/3.2.0/pandas-profiling-3.2.0.tar.gz

---
+++ packages/pandas-profiling/pandas-profiling-3.2.0.tar.gz/pandas-profiling-3.2.0/Makefile
@@ -0,0 +1,43 @@
+.PHONY: docs examples
+
+docs:
+	rm -rf docs/
+	mkdir docs/
+	# pdoc3
+	cp -a ./docsrc/assets/ ./docs/assets/
+	pdoc3 --html --force --output-dir docs pandas_profiling
+	mv docs/pandas_profiling/* docs
+	rmdir docs/pandas_profiling
+	# sphinx
+	cd docsrc/ && make github
+
+test:
+	pytest tests/unit/
+	pytest tests/issues/
+	pytest --nbval tests/notebooks/
+	pandas_profiling -h
+
+test_cov:
+	pytest --cov=. tests/unit/
+	pytest --cov=. --cov-append tests/issues/
+	pytest --cov=. --cov-append --nbval tests/notebooks/
+	pandas_profiling -h
+
+examples:
+	find ./examples -maxdepth 2 -type f -name "*.py" -execdir python {} \;
+
+install:
+	pip install -e .[notebook]
+
+lint:
+	pre-commit run --all-files
+
+clean:
+	git rm --cached `git ls-files -i --exclude-from=.gitignore`
+
+all:
+	make lint
+	make install
+	make examples
+	make docs
+	make test
---
+++ packages/pandas-profiling/pandas-profiling-3.2.0.tar.gz/pandas-profiling-3.2.0/make.bat
@@ -0,0 +1,64 @@
+@echo off
+setlocal enabledelayedexpansion
+
+IF "%1%" == "docs" (
+    mkdir docs/
+    :: pdoc3
+    robocopy .\docsrc\assets\ .\docs\assets\
+    pdoc3 --html  --force --output-dir docs pandas_profiling
+    robocopy .\docs\pandas_profiling .\docs /E /MOVE
+    :: sphinx
+    cd docsrc/ && make github
+    ECHO "Docs updated!"
+    GOTO end
+)
+
+IF "%1" == "test" (
+    pytest tests/unit/
+    pytest tests/issues/
+    pytest --nbval tests/notebooks/
+    ECHO "Tests completed!"
+    GOTO end
+)
+
+IF "%1" == "examples" (
+    FOR /R /D %%d in ("examples\*") do (
+        SET B=%%d
+        FOR /R %%f in ("!B:%CD%\=!\*.py") DO (
+            SET C=%%f
+            ECHO "Running !C:%%d\=! (in %%d)"
+            CD %%d && python "!C:%%d\=!"
+            CD %CD%
+        )
+    )
+
+    ECHO "Example runs completed!"
+    GOTO end
+)
+
+IF "%1" == "lint" (
+    pre-commit run --all-files
+    GOTO end
+)
+
+IF "%1" == "install" (
+	pip install -e .[notebook]
+	GOTO end
+)
+
+IF "%1%" == "clean" (
+	ECHO "Not implemented yet"
+	GOTO end
+)
+
+IF "%1%" == "all" (
+    make lint
+    make install
+    make examples
+    make docs
+    make test
+    GOTO end
+)
+
+ECHO "No command matched"
+:end
