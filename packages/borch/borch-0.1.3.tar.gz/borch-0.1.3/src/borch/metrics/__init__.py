5462bb69bfcf3ac37a12739cc69eb859d6a22175
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:32 +0000

    Add package borch/0.1.3/borch-0.1.3.tar.gz

---
+++ packages/borch/borch-0.1.3.tar.gz/borch-0.1.3/src/borch/metrics/__init__.py
@@ -0,0 +1,9 @@
+"""
+Metrics
+=======
+
+Module implementing metrics for torch tensors
+
+"""
+from borch.metrics.rv_metrics import all_metrics, mean_squared_error, accuracy
+from borch.metrics import metrics
