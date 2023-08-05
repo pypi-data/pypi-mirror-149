1596200b1596a5951e2a80512da9918207cd1b03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:38 +0000

    Add package videoall/0.0.6/videoall-0.0.6-py3-none-any.whl

---
+++ packages/videoall/videoall-0.0.6-py3-none-any.whl/videoall/__init__.py
@@ -0,0 +1,9 @@
+from .utils import (
+    touchdir, seconds2hms, loadConfig, filterBadCharacter, Logger, printTable, Downloader
+)
+from .sources import (
+    Ted, CNTV, MGTV, Migu, Pipix, AcFun, Zhihu, Xigua, Iqiyi, Douyin, Haokan, Bilibili, Pipigaoxiao,
+    Yinyuetai, Weibo, BaiduTieba, Ku6, Kuaishou, Sohu
+)
+
+__version__ = '0.0.6'
