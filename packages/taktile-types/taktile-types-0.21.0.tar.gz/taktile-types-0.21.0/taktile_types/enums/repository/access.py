d2236cf0db762bbcd2dd5ba7470d5582601cf995
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:24:41 +0000

    Add package taktile-types/0.21.0/taktile-types-0.21.0.tar.gz

---
+++ packages/taktile-types/taktile-types-0.21.0.tar.gz/taktile-types-0.21.0/taktile_types/enums/repository/access.py
@@ -0,0 +1,9 @@
+"""Access enums for repositories"""
+from taktile_types.enums.common import ExtendedEnum
+
+
+class AccessKind(ExtendedEnum):
+    """Kinds of access to a repository"""
+
+    OWNER = "owner"
+    VIEWER = "viewer"
