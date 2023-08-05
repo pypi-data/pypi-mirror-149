e5af9a07037407908f4b3307f1a107d6a55c8cea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:15:46 +0000

    Add package histomics-stream/2.2.4/histomics_stream-2.2.4-py3-none-any.whl

---
+++ packages/histomics-stream/histomics_stream-2.2.4-py3-none-any.whl/histomics_stream/__init__.py
@@ -0,0 +1,10 @@
+"""Whole-slide image streamer for TensorFlow."""
+
+__version__ = "2.2.4"
+
+"""
+
+This module supports efficient whole-slide reading and processing in a tensorflow graph.
+
+"""
+from . import configure, tensorflow, codecs
