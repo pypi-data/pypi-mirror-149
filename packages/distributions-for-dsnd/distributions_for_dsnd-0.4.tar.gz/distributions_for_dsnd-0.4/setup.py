c0aa2bdd4ac6e90bc434e41ff837d3a265c3e7ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:11:56 +0000

    Add package distributions-for-dsnd/0.4/distributions_for_dsnd-0.4.tar.gz

---
+++ packages/distributions-for-dsnd/distributions_for_dsnd-0.4.tar.gz/distributions_for_dsnd-0.4/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions_for_dsnd',
+      version='0.4',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_for_dsnd'],
+      author = 'Mona Anderssen',
+      zip_safe=False)
