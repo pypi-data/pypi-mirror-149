047cfb09247038448d9cff01b49712b6d67576f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:15 +0000

    Add package musicir/0.0.2/musicir-0.0.2-py3-none-any.whl

---
+++ packages/musicir/musicir-0.0.2-py3-none-any.whl/musicir/__main__.py
@@ -0,0 +1,24 @@
+"""Command-line interface."""
+from pprint import pprint
+
+import click
+
+from musicir.leadsheets.musicxml import HarmonyParser
+
+
+@click.command()
+@click.version_option()
+@click.argument("filename", type=click.File("r"))
+def main(filename: str) -> None:
+    """Music Information Retrieval.
+    Extract the harmony from leadsheets, in musicxml format.
+    returning it as a JSON array.
+
+    FILENAME is the leadsheet from which the harmony is to be extracted.
+    """
+    H = HarmonyParser(filename)
+    click.echo(H.as_json())
+
+
+if __name__ == "__main__":
+    main(prog_name="musicir")  # pragma: no cover
