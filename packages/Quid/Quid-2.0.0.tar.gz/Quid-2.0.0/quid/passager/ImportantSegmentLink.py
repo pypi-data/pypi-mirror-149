fc2103e7667c7f3283363d506be0a7be10c6f9eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:25 +0000

    Add package Quid/2.0.0/Quid-2.0.0.tar.gz

---
+++ packages/Quid/Quid-2.0.0.tar.gz/Quid-2.0.0/quid/passager/ImportantSegmentLink.py
@@ -0,0 +1,4 @@
+class ImportantSegmentLink:
+    def __init__(self, important_segment_id, target_location_selections):
+        self.important_segment_id = important_segment_id
+        self.target_location_selections = target_location_selections
---
+++ packages/Quid/Quid-2.0.0.tar.gz/Quid-2.0.0/quid/passager/TargetTextLocationLink.py
@@ -0,0 +1,6 @@
+class TargetTextLocationLink:
+    def __init__(self, target_text_id, location_id, source_segment_start_id, source_segment_end_id):
+        self.target_text_id = target_text_id
+        self.location_id = location_id
+        self.source_segment_start_id = source_segment_start_id
+        self.source_segment_end_id = source_segment_end_id
