86d9d0e4ecb8b52bfe4b4657d2fe3f85dac4619c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:46 +0000

    Add package weconnect/0.40.0.dev1/weconnect-0.40.0.dev1.tar.gz

---
+++ packages/weconnect/weconnect-0.40.0.dev1.tar.gz/weconnect-0.40.0.dev1/weconnect/elements/climatization_status.py
@@ -0,0 +1,62 @@
+from enum import Enum
+import logging
+
+from weconnect.addressable import AddressableAttribute
+from weconnect.elements.generic_status import GenericStatus
+
+LOG = logging.getLogger("weconnect")
+
+
+class ClimatizationStatus(GenericStatus):
+    def __init__(
+        self,
+        vehicle,
+        parent,
+        statusId,
+        fromDict=None,
+        fixAPI=True,
+    ):
+        self.remainingClimatisationTime_min = AddressableAttribute(
+            localAddress='remainingClimatisationTime_min', parent=self, value=None, valueType=int)
+        self.climatisationState = AddressableAttribute(localAddress='climatisationState', value=None, parent=self,
+                                                       valueType=ClimatizationStatus.ClimatizationState)
+        super().__init__(vehicle=vehicle, parent=parent, statusId=statusId, fromDict=fromDict, fixAPI=fixAPI)
+
+    def update(self, fromDict, ignoreAttributes=None):
+        ignoreAttributes = ignoreAttributes or []
+        LOG.debug('Update Climatization status from dict')
+
+        if 'value' in fromDict:
+            self.remainingClimatisationTime_min.fromDict(fromDict['value'], 'remainingClimatisationTime_min')
+            self.climatisationState.fromDict(fromDict['value'], 'climatisationState')
+            if 'remainingClimatisationTime_min' in fromDict['value']:
+                remainingClimatisationTime_min = int(fromDict['value']['remainingClimatisationTime_min'])
+                if self.fixAPI and remainingClimatisationTime_min != 0 and self.climatisationState.value == ClimatizationStatus.ClimatizationState.OFF:
+                    remainingClimatisationTime_min = 0
+                    LOG.debug('%s: Attribute remainingClimatisationTime_min is %s while climatisationState is %s. Setting 0 instead',
+                              self.getGlobalAddress(), fromDict['value']['remainingClimatisationTime_min'], self.climatisationState.value)
+                self.remainingClimatisationTime_min.setValueWithCarTime(remainingClimatisationTime_min, lastUpdateFromCar=None, fromServer=True)
+            else:
+                self.remainingClimatisationTime_min.enabled = False
+        else:
+            self.remainingClimatisationTime_min.enabled = False
+            self.climatisationState.enabled = False
+
+        super().update(fromDict=fromDict, ignoreAttributes=(
+            ignoreAttributes + ['remainingClimatisationTime_min', 'climatisationState']))
+
+    def __str__(self):
+        string = super().__str__()
+        if self.climatisationState.enabled:
+            string += f'\n\tState: {self.climatisationState.value.value}'  # pylint: disable=no-member
+        if self.remainingClimatisationTime_min.enabled:
+            string += f'\n\tRemaining Climatization Time: {self.remainingClimatisationTime_min.value} min'
+        return string
+
+    class ClimatizationState(Enum,):
+        OFF = 'off'
+        HEATING = 'heating'
+        COOLING = 'cooling'
+        VENTILATION = 'ventilation'
+        INVALID = 'invalid'
+        UNKNOWN = 'unknown climatization state'
