4dfd839dd43bc444e1d7fa6ca996eaafad63bced
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:33:51 +0000

    Add package piccolo-theme/0.10.0/piccolo_theme-0.10.0.tar.gz

---
+++ packages/piccolo-theme/piccolo_theme-0.10.0.tar.gz/piccolo_theme-0.10.0/piccolo_theme/__init__.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+import os
+import typing as t
+
+if t.TYPE_CHECKING:
+    from sphinx.application import Sphinx
+
+
+__VERSION__ = "0.10.0"
+
+
+def setup(app: Sphinx):
+    """
+    :param app:
+        Passed by Sphinx.
+
+    """
+    app.add_html_theme(
+        "piccolo_theme", os.path.abspath(os.path.dirname(__file__))
+    )
