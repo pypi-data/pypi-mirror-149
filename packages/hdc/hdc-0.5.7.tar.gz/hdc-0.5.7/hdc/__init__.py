8a30fe577faa1b16e2b0e02610dd3f81d2359823
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:45 +0000

    Add package hdc/0.5.7/hdc-0.5.7.tar.gz

---
+++ packages/hdc/hdc-0.5.7.tar.gz/hdc-0.5.7/hdc/__init__.py
@@ -0,0 +1,9 @@
+import hdc.functional as functional
+import hdc.embeddings as embeddings
+import hdc.datasets as datasets
+
+__all__ = [
+    "functional",
+    "embeddings",
+    "datasets",
+]
---
+++ packages/hdc/hdc-0.5.7.tar.gz/hdc-0.5.7/hdc/datasets/__init__.py
@@ -0,0 +1,17 @@
+from hdc.datasets.beijing_air_quality import BeijingAirQuality
+from hdc.datasets.isolet import ISOLET
+from hdc.datasets.european_languages import EuropeanLanguages
+from hdc.datasets.ucihar import UCIHAR
+from hdc.datasets.airfoil_self_noise import AirfoilSelfNoise
+from hdc.datasets.emg_hand_gestures import EMGHandGestures
+from hdc.datasets.pamap import PAMAP
+
+__all__ = [
+    "BeijingAirQuality",
+    "ISOLET",
+    "EuropeanLanguages",
+    "UCIHAR",
+    "AirfoilSelfNoise",
+    "EMGHandGestures",
+    "PAMAP",
+]
