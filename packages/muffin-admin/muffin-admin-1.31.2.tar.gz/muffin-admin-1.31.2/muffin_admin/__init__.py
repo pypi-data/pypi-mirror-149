e3b4be5e7e419bf38cebd326319b4e2f2c5971a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:55 +0000

    Add package muffin-admin/1.31.2/muffin-admin-1.31.2.tar.gz

---
+++ packages/muffin-admin/muffin-admin-1.31.2.tar.gz/muffin-admin-1.31.2/muffin_admin/__init__.py
@@ -0,0 +1,27 @@
+"""Plugin meta information."""
+
+from .plugin import Plugin # noqa
+
+# Package information
+# ===================
+
+__version__ = "1.31.2"
+__project__ = "muffin-admin"
+__author__ = "Kirill Klenov <horneds@gmail.com>"
+__license__ = "MIT"
+
+
+from .handler import AdminHandler
+
+try:
+    from .peewee import PWAdminHandler, PWFilter
+except ImportError:
+    pass
+
+try:
+    from .sqlalchemy import SAAdminHandler, SAFilter
+except ImportError:
+    pass
+
+
+__all__ = 'AdminHandler', 'PWAdminHandler', 'PWFilter', 'SAAdminHandler', 'SAFilter', 'Plugin'
