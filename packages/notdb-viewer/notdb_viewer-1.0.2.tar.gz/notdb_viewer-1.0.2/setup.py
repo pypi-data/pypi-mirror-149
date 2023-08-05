3954f4b9a0b5e70be6a2029e7d4a982523533969
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:55 +0000

    Add package notdb-viewer/1.0.2/notdb_viewer-1.0.2.tar.gz

---
+++ packages/notdb-viewer/notdb_viewer-1.0.2.tar.gz/notdb_viewer-1.0.2/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup, find_packages
+from notdb_viewer import __version__ as v
+
+# README.md
+with open('README.md', 'r', encoding='utf-8') as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name='notdb_viewer',
+    packages=find_packages(),
+    version=v,
+    description='Viewer for NotDB Databases',
+    package_dir={
+        'app': 'notdb_viewer/app'
+    },
+    author='Nawaf Alqari',
+    author_email='nawafalqari13@gmail.com',
+    keywords=['db'],
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    entry_points={
+    'console_scripts': [ 'notdb_viewer=notdb_viewer.__main__:main']
+    },
+    project_urls={
+        'Documentation': 'https://github.com/nawafalqari/NotDB#readme',
+        'Bug Tracker': 'https://github.com/nawafalqari/NotDB/issues',
+        'Source Code': 'https://github.com/nawafalqari/NotDB/',
+        'Discord': 'https://discord.gg/cpvynqk4XT'
+    },
+    license='MIT',
+    url='https://github.com/nawafalqari/notdb/',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
