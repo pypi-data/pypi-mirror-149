edf744f2cbd2f8fb71af828cbc55461de6d076a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:24:35 +0000

    Add package oscope-scpi/0.1.13/oscope_scpi-0.1.13-py3-none-any.whl

---
+++ packages/oscope-scpi/oscope_scpi-0.1.13-py3-none-any.whl/oscope_scpi/__init__.py
@@ -0,0 +1,48 @@
+
+# Standard SCPI commands
+from .scpi import SCPI
+
+# Common Oscillocope SCPI commands
+from .oscilloscope import Oscilloscope
+
+# Common Keysight Oscilloscope SCPI commands
+from .keysight import Keysight
+
+# Support of Keysight MXR-series oscilloscopes
+from .mxr import MXR
+
+# Support of Keysight MXRxx8A oscilloscope
+from .mxr import MXRxx8A
+
+# Support of Keysight MXRxx4A oscilloscope
+from .mxr import MXRxx4A
+
+# Support of Keysight UXR-series oscilloscopes
+from .uxr import UXR
+
+# Support of Keysight UXRxxx4A oscilloscope
+from .uxr import UXRxxx4A
+
+# Support of Keysight UXRxxx2A oscilloscope
+from .uxr import UXRxxx2A
+
+# Support of HP/Agilent/Keysight DSO-X oscilloscopes
+from .dso import DSOX
+
+# Support of HP/Agilent/Keysight DSO-X 3000A 2-channel oscilloscope
+from .dso import DSOX3xx2A
+
+# Support of HP/Agilent/Keysight DSO-X 3000A 4-channel oscilloscope
+from .dso import DSOX3xx4A
+
+# Support of HP/Agilent/Keysight MSO-X oscilloscopes
+from .dso import MSOX
+
+# Support of HP/Agilent/Keysight MSO-X 3000A 2-channel oscilloscope
+from .dso import MSOX3xx2A
+
+# Support of HP/Agilent/Keysight MSO-X 3000A 4-channel oscilloscope
+from .dso import MSOX3xx4A
+
+
+
