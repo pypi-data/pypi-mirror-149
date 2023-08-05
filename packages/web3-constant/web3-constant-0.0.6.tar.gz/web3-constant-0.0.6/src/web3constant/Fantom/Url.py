0c310e883b1369e8db150dc7667a4ceea4dcebd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:38 +0000

    Add package web3-constant/0.0.6/web3-constant-0.0.6.tar.gz

---
+++ packages/web3-constant/web3-constant-0.0.6.tar.gz/web3-constant-0.0.6/src/web3constant/Fantom/Url.py
@@ -0,0 +1,8 @@
+# FTM mainnet
+FTM_RPC = "https://rpcapi.fantom.network/"
+FTMSCAN_TOKEN = "https://ftmscan.com/token/"
+FTM_WSS = "wss://wsapi.fantom.network/"
+
+# FTM testnet
+FTM_TESTNET_RPC = "https://rpc.testnet.fantom.network"
+FTM_TESTNET_TOKEN = "https://testnet.ftmscan.com/token/"
