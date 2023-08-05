cc01d7739fde6c6cd7f91de20c585da55e91cbc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:12 +0000

    Add package pkg-calibration/0.0.4/pkg_calibration-0.0.4.tar.gz

---
+++ packages/pkg-calibration/pkg_calibration-0.0.4.tar.gz/pkg_calibration-0.0.4/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+setuptools.setup(
+    name="pkg_calibration",
+    version="0.0.4",
+    author="Dieg Oatlib",
+    description="Package To Calibrate Volatility Parameters",
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+    py_modules=["volatility_flat_price",
+                "volatility_spread",
+                "volatility_time_spread",
+                "volatility_underlying",
+                "time_series"],
+    package_dir={'': 'src'},
+    install_requires=["numpy", "pandas", "numbers_parser"]
+)
