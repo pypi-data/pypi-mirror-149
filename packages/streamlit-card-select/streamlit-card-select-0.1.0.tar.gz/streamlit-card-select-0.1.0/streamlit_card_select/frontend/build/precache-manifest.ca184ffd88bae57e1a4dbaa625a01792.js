39ab232e6866516d83b1973822286ca2e91a229c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:45 +0000

    Add package streamlit-card-select/0.1.0/streamlit-card-select-0.1.0.tar.gz

---
+++ packages/streamlit-card-select/streamlit-card-select-0.1.0.tar.gz/streamlit-card-select-0.1.0/streamlit_card_select/frontend/build/precache-manifest.ca184ffd88bae57e1a4dbaa625a01792.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b57835a0c40f494b707a6f2c07633a7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0769fac20b08d0684c8d",
+    "url": "./static/js/2.c5dfe4cf.chunk.js"
+  },
+  {
+    "revision": "6276ebf994b8a19d14e38897a4c487da",
+    "url": "./static/js/2.c5dfe4cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "400911f0d27a34b8bc27",
+    "url": "./static/js/main.ffe65e8d.chunk.js"
+  },
+  {
+    "revision": "fdfe98b2bd55e8333be3",
+    "url": "./static/js/runtime-main.8d8cf6da.js"
+  }
+]);
