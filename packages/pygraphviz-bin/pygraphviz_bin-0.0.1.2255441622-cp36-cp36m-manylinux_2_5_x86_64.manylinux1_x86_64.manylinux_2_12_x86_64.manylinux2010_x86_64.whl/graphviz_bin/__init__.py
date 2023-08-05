e8fa45bbbf1347285fb629003df064d9128a96d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:27:58 +0000

    Add package pygraphviz-bin/0.0.1.2255441622/pygraphviz_bin-0.0.1.2255441622-cp36-cp36m-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_12_x86_64.manylinux2010_x86_64.whl

---
+++ packages/pygraphviz-bin/pygraphviz_bin-0.0.1.2255441622-cp36-cp36m-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_12_x86_64.manylinux2010_x86_64.whl/graphviz_bin/__init__.py
@@ -0,0 +1,41 @@
+#****************************************************************************
+#*
+#****************************************************************************
+
+import subprocess
+import os
+
+def dot(args, in_s=None, out_s=None):
+    pkg_dir=os.path.dirname(os.path.abspath(__file__))
+    
+    dot_bin=os.path.join(pkg_dir, "bin", "dot")
+    
+    if not os.path.isfile(dot_bin):
+        raise Exception("Failed to find dot")
+    
+    cmdline = [dot_bin]
+    cmdline.extend(args)
+
+    if in_s is not None:
+        with open("tmp.in", "w") as fp:
+            fp.write(in_s.read())
+        cmdline.append("tmp.in")
+        
+    if out_s is not None:
+        cmdline.extend(['-o', "tmp.out"])            
+
+    print("cmdline: %s" % str(cmdline))    
+    p = subprocess.run(cmdline)
+    
+    if p.returncode != 0:
+        raise Exception("Command run failed")
+    
+    if out_s is not None:
+        with open("tmp.out", "r") as fp:
+            out_s.write(fp.read())
+    
+    print("Dot")
+    
+    
+    
+    
