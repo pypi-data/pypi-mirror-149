7e13ade28d20fc9f7462c04208b32bb076931ce8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:14:15 +0000

    Add package minidumpwriter/0.9766/minidumpwriter-0.9766.tar.gz

---
+++ packages/minidumpwriter/minidumpwriter-0.9766.tar.gz/minidumpwriter-0.9766/setup.py
@@ -0,0 +1,14 @@
+try:
+    from setuptools import setup, Command
+except ImportError as excp:
+    from distutils.core import setup, Command
+
+
+setup(name='minidumpwriter',
+      version='0.9766',
+      description='Minidump writer',
+      author='Paul Kermann',
+      author_email='paulkermann@tutanota.com',
+      url='https://github.com/paulkermann/MinidumpWriter',
+      py_modules=['minidump_writer', 'minidump_structs', 'minidump_enums'],
+     )
