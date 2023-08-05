8f1789ebd236d9ad3926c56984b509a53b5ca99e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:16:51 +0000

    Add package pyobs-gui/0.18.3/pyobs_gui-0.18.3-py3-none-any.whl

---
+++ packages/pyobs-gui/pyobs_gui-0.18.3-py3-none-any.whl/pyobs_gui/__init__.py
@@ -0,0 +1,14 @@
+"""
+TODO: write doc
+"""
+__title__ = "GUI"
+
+from .gui import GUI
+from .widgetcamera import WidgetCamera
+from .widgetdatadisplay import WidgetDataDisplay
+from .widgetcooling import WidgetCooling
+from .widgetshell import WidgetShell
+from .widgetvideo import WidgetVideo
+from .widgetfocus import WidgetFocus
+from .widgettelescope import WidgetTelescope
+from .widgetweather import WidgetWeather
