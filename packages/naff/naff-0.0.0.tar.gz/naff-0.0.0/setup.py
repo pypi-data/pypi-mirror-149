dbb7d54f98b59c9ed4c28749bcea35583a7883ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:24:37 +0000

    Add package naff/0.0.0/naff-0.0.0.tar.gz

---
+++ packages/naff/naff-0.0.0.tar.gz/naff-0.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='naff',
+    version='0.0.0',
+    description='',
+    long_description='',
+    url='https://github.com/Discord-Snake-Pit/Dis-Snek',
+    author='LordOfPolls',
+    author_email='ddavidallen13@gmail.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
