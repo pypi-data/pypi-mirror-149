e834c81aaca033422aec95bec4779e7295aa1a98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:18:14 +0000

    Add package projectmanagement/0.0.1/projectmanagement-0.0.1.tar.gz

---
+++ packages/projectmanagement/projectmanagement-0.0.1.tar.gz/projectmanagement-0.0.1/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name='projectmanagement',
+    version='0.0.1',
+    author='Filippo Maria Ottaviani',
+    author_email='filippomariaottaviani@gmail.com',
+    description='Project Management',
+    packages=['projectmanagement']
+)
