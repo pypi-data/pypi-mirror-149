629c82a12cf5865771fa7178c6fb34350870b939
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:14:45 +0000

    Add package localstack-ext/0.14.2.16/localstack-ext-0.14.2.16.tar.gz

---
+++ packages/localstack-ext/localstack-ext-0.14.2.16.tar.gz/localstack-ext-0.14.2.16/localstack_ext/services/cloudformation/models/ecr.py
@@ -0,0 +1,31 @@
+from localstack.services.cloudformation.service_models import GenericBaseModel
+from localstack.utils.aws import aws_stack
+from localstack_ext.utils.aws import aws_utils
+class ECRRepository(GenericBaseModel):
+ @staticmethod
+ def cloudformation_type():
+  return "AWS::ECR::Repository"
+ def get_physical_resource_id(self,attribute,**kwargs):
+  repo_name=self.props.get("RepositoryName")
+  if attribute=="Arn":
+   return aws_utils.get_ecr_repository_arn(repo_name)
+  return repo_name
+ def fetch_state(self,stack_name,resources):
+  client=aws_stack.connect_to_service("ecr")
+  props=self.props
+  repo_name=props.get("RepositoryName")
+  registry_id=props.get("LifecyclePolicy",{}).get("RegistryId")
+  kwargs={"registryId":registry_id}if registry_id else{}
+  result=client.describe_repositories(repositoryNames=[repo_name],**kwargs).get("repositories",[])
+  return(result or[None])[0]
+ @staticmethod
+ def get_deploy_templates():
+  def _create_params(params,**kwargs):
+   encryption_type=params.get("EncryptionConfiguration",{}).get("EncryptionType","AES-256")
+   response={"repositoryName":params["RepositoryName"],"tags":params.get("Tags",[]),"imageTagMutability":params.get("ImageTagMutability","MUTABLE"),"imageScanningConfiguration":{"scanOnPush":params.get("ImageScanningConfiguration",{}).get("ScanOnPush",False)},"encryptionConfiguration":{"encryptionType":encryption_type}}
+   if(encryption_type=="KMS" and params.get("EncryptionConfiguration",{}).get("KmsKey")is not None):
+    response["encryptionConfiguration"]["kmsKey"]=params["EncryptionConfiguration"]["KmsKey"]
+   return response
+  result={"create":{"function":"create_repository","parameters":_create_params},"delete":{"function":"delete_repository","parameters":{"repositoryName":"RepositoryName"}}}
+  return result
+# Created by pyminifier (https://github.com/liftoff/pyminifier)
