b97f42716703d88ae204143098551d177e1279cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:03:28 +0000

    Add package esm-tools/6.6.2/esm_tools-6.6.2-py2.py3-none-any.whl

---
+++ packages/esm-tools/esm_tools-6.6.2-py2.py3-none-any.whl/esm_tests/__init__.py
@@ -0,0 +1,10 @@
+"""Top-level package for ESM Tests."""
+
+__author__ = """Miguel Andres-Martinez"""
+__email__ = "miguel.andres-martinez@awi.de"
+__version__ = "6.6.2"
+
+from .initialization import *
+from .tests import *
+from .test_utilities import *
+from .read_shipped_data import *
