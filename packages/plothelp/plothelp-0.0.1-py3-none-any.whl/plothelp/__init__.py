9eed8b453ead4596b58902226475ef05a42579d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:06 +0000

    Add package plothelp/0.0.1/plothelp-0.0.1-py3-none-any.whl

---
+++ packages/plothelp/plothelp-0.0.1-py3-none-any.whl/plothelp/__init__.py
@@ -0,0 +1,14 @@
+"""
+Copyright (c) 2022 Nathan . All rights reserved.
+
+plothelp: Simple auto-grid pyplot util.
+"""
+
+
+from __future__ import annotations
+
+from plothelp.plot import autoplot
+
+from ._version import version as __version__
+
+__all__ = ("__version__", "autoplot")
