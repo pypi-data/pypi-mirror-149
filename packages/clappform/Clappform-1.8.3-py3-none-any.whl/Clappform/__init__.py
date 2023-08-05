045dd6c823bb142274802569a054c8d464e2a355
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:13:33 +0000

    Add package clappform/1.8.3/Clappform-1.8.3-py3-none-any.whl

---
+++ packages/clappform/Clappform-1.8.3-py3-none-any.whl/Clappform/__init__.py
@@ -0,0 +1,11 @@
+from .settings import settings
+from .auth import Auth
+from .app import App
+from .notification import Notification
+from .whatsapp import Whatsapp
+from .sms import SMS
+from .email import Email
+from .user import User
+from .actionflow import Actionflow
+
+settings
