811b9f190ad98ab668161eff4867722862ded27c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:37 +0000

    Add package udemy-gaussian-binomial/0.1/udemy_gaussian_binomial-0.1.tar.gz

---
+++ packages/udemy-gaussian-binomial/udemy_gaussian_binomial-0.1.tar.gz/udemy_gaussian_binomial-0.1/README.md
@@ -0,0 +1,4 @@
+This package is a execise of Udemy course, and calculate Gaussian and Binomial features like:
+- mean
+- std
+- pdf
---
+++ packages/udemy-gaussian-binomial/udemy_gaussian_binomial-0.1.tar.gz/udemy_gaussian_binomial-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='udemy_gaussian_binomial',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['udemy_gaussian_binomial'],
+      zip_safe=False)
