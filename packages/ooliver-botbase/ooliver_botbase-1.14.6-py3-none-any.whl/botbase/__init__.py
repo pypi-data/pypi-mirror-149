2e5624c6e8f30c83c13a1c02622de91abadd7798
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:14 +0000

    Add package ooliver-botbase/1.14.6/ooliver_botbase-1.14.6-py3-none-any.whl

---
+++ packages/ooliver-botbase/ooliver_botbase-1.14.6-py3-none-any.whl/botbase/__init__.py
@@ -0,0 +1,23 @@
+from logging import NullHandler, getLogger
+from typing import NamedTuple, Literal
+
+
+from .botbase import BotBase
+from .emojis import Emojis
+from .exceptions import Blacklisted
+from .wraps import (
+    MyContext,
+    MyInter,
+    WrappedChannel,
+    WrappedMember,
+    WrappedUser,
+    WrappedThread,
+    Wrap,
+)
+from .checks import *
+
+
+__version__ = "1.14.6"
+
+
+getLogger(__name__).addHandler(NullHandler())
