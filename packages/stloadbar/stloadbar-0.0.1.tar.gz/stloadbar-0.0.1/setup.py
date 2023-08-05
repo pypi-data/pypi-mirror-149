622495bc40109c066d690a3e561c455ef3350753
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:42 +0000

    Add package stloadbar/0.0.1/stloadbar-0.0.1.tar.gz

---
+++ packages/stloadbar/stloadbar-0.0.1.tar.gz/stloadbar-0.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+ 
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='stloadbar',
+  version='0.0.1',
+  description='A Basic Loading Progress Bar',
+  long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+  url='',  
+  author='ST',
+  author_email='amazingfalcon1@gmail.com',
+  license='MIT', 
+  classifiers=classifiers,
+  keywords='loadbar',
+  packages=find_packages(),
+  install_requires=['colorama'] 
+)
