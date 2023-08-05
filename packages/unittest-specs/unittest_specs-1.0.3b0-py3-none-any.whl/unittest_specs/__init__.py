c631ee864ba2ffed02ce0f360d78900d888a4952
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:15:53 +0000

    Add package unittest-specs/1.0.3b0/unittest_specs-1.0.3b0-py3-none-any.whl

---
+++ packages/unittest-specs/unittest_specs-1.0.3b0-py3-none-any.whl/unittest_specs/__init__.py
@@ -0,0 +1,11 @@
+"""
+unittest-specs
+
+A simple wrapper around the unittest package with different styles for specs
+"""
+
+__version__ = "1.0.3b"
+__author__ = "Phillip Goellner"
+
+
+from unittest_specs.simple_test_spec import SimpleFlatSpec
