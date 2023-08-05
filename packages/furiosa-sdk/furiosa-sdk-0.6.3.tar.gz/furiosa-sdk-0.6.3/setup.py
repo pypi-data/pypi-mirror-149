c22d74594d4b74ecb3e83c54116287a3d3690bb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:26:32 +0000

    Add package furiosa-sdk/0.6.3/furiosa-sdk-0.6.3.tar.gz

---
+++ packages/furiosa-sdk/furiosa-sdk-0.6.3.tar.gz/furiosa-sdk-0.6.3/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from typing import Dict, List
+
+from setuptools import setup
+
+version = "0.6.3"
+
+EXTRAS_REQUIREMENTS: Dict[str, List[str]] = {
+    "server": ["furiosa-server~=" + version],
+    "quantizer": ["furiosa-quantizer~=" + version],
+    "litmus": ["furiosa-litmus~=" + version],
+    "models": ["furiosa-models~=" + version],
+    "serving": ["furiosa-serving~=" + version],
+}
+
+EXTRAS_REQUIREMENTS["full"] = [
+    req for extras_reqs in EXTRAS_REQUIREMENTS.values() for req in extras_reqs
+]
+
+if __name__ == "__main__":
+    setup(
+        version=version,
+        extras_require=EXTRAS_REQUIREMENTS,
+    )
