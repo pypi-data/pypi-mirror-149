644baf7af56010aecaa22c16b29cf832d87aa950
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:18:10 +0000

    Add package simple-socket-chat-client/0.1.0/simple_socket_chat_client-0.1.0-py3-none-any.whl

---
+++ packages/simple-socket-chat-client/simple_socket_chat_client-0.1.0-py3-none-any.whl/client/add_contact.py
@@ -0,0 +1,76 @@
+import os
+import sys
+
+from PyQt5.QtWidgets import QDialog, QLabel, QComboBox, QPushButton, QApplication
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QStandardItemModel, QStandardItem
+
+import logging
+sys.path.append(os.path.join(os.getcwd(), '../'))
+from logs import client_log_config
+
+client_log = logging.getLogger('client_log')
+
+
+class AddContactDialog(QDialog):
+    def __init__(self, transport, database):
+        super().__init__()
+        self.transport = transport
+        self.database = database
+
+        self.setFixedSize(350, 120)
+        self.setWindowTitle('Pick contact for adding:')
+
+        self.setAttribute(Qt.WA_DeleteOnClose)
+        self.setModal(True)
+
+        self.selector_label = QLabel('Pick contact for adding:', self)
+        self.selector_label.setFixedSize(200, 20)
+        self.selector_label.move(10, 0)
+
+        self.selector = QComboBox(self)
+        self.selector.setFixedSize(200, 20)
+        self.selector.move(10, 30)
+
+        self.btn_refresh = QPushButton('Update list', self)
+        self.btn_refresh.setFixedSize(100, 30)
+        self.btn_refresh.move(60, 60)
+
+        self.btn_ok = QPushButton('Add', self)
+        self.btn_ok.setFixedSize(100, 30)
+        self.btn_ok.move(230, 20)
+
+        self.btn_cancel = QPushButton('Cancel', self)
+        self.btn_cancel.setFixedSize(100, 30)
+        self.btn_cancel.move(230, 60)
+        self.btn_cancel.clicked.connect(self.close)
+
+        self.possible_contacts_update()
+        self.btn_refresh.clicked.connect(self.update_possible_contacts)
+
+    def possible_contacts_update(self):
+        self.selector.clear()
+        contacts_list = set(self.database.get_contacts())
+        users_list = set(self.database.get_users())
+        users_list.remove(self.transport.username)
+        self.selector.addItems(users_list - contacts_list)
+
+    def update_possible_contacts(self):
+        try:
+            self.transport.user_list_update()
+        except OSError:
+            pass
+        else:
+            client_log.debug('User list updated from server')
+            self.possible_contacts_update()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    from client_database import ClientDatabase
+    database = ClientDatabase('test1')
+    from transport import ClientTransport
+    transport = ClientTransport('127.0.0.1', 7777, database, 'test1')
+    window = AddContactDialog(transport, database)
+    window.show()
+    app.exec_()
