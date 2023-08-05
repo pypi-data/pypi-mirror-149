be351128426a6c70db6f89d969ec7e17a69416c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:26:01 +0000

    Add package pyGenericSpreadSheet/0.0.11/pyGenericSpreadSheet-0.0.11-py3-none-any.whl

---
+++ packages/pyGenericSpreadSheet/pyGenericSpreadSheet-0.0.11-py3-none-any.whl/spreadsheet/version.py
@@ -0,0 +1,17 @@
+'''
+Created on 2022-04-21
+
+@author: wf
+'''
+
+class Version(object):
+    '''
+    Version handling for pyGenericSpreadSheet
+    '''
+    version='0.0.11'
+    date = '2022-04-21'
+    updated = '2022-05-03'
+    name='pyGenericSpreadSheet'
+    description='python API providing generic Access to Spreadsheet backends'
+
+        
