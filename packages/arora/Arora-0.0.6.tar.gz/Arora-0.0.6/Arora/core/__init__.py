d2cc99bf24ca6bbf4b046c43e582dd95a51205ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:16 +0000

    Add package arora/0.0.6/Arora-0.0.6.tar.gz

---
+++ packages/arora/Arora-0.0.6.tar.gz/Arora-0.0.6/Arora/core/__init__.py
@@ -0,0 +1,6 @@
+from .sleepdata import *
+from .bandpass import *
+from .EEG_freqbands import *
+from .find_features import *
+from .segmentation import *
+from .upper_lower_signal_envelopes import *
---
+++ packages/arora/Arora-0.0.6.tar.gz/Arora-0.0.6/Arora/read_mat.py
@@ -0,0 +1,10 @@
+from typing import List
+import pandas as pd
+from scipy.io import loadmat
+
+
+def load_mat(file: str) -> (List[int or float], pd.DataFrame):
+	# If mat files all have the same format then hardcode how to insert the code into the dataframe and signal list
+	data = loadmat(file)
+
+	return
