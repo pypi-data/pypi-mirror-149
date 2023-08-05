248ba09e8c3aa51556f085f2f38c7ede9c767dd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:24:41 +0000

    Add package messenger-clientlaitdark123456789/1.0.0/messenger_clientlaitdark123456789-1.0.0.tar.gz

---
+++ packages/messenger-clientlaitdark123456789/messenger_clientlaitdark123456789-1.0.0.tar.gz/messenger_clientlaitdark123456789-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="messenger_clientlaitdark123456789",
+    description="messenger_clientlaitdark123456789",
+    version="1.0.0",
+    packages=find_packages(),
+    author="Stoianov M.V.",
+    author_email="masklaite@gmail.com",
+    url="https://github.com/Laitdark/Python-Hometask"
+)
