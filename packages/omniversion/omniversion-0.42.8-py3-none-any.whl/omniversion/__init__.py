9157958dd5de5a28d4078461cf3d2dd826a75df6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:12:37 +0000

    Add package omniversion/0.42.8/omniversion-0.42.8-py3-none-any.whl

---
+++ packages/omniversion/omniversion-0.42.8-py3-none-any.whl/omniversion/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+"""Omniversion Python integration"""
+from .dependency import AvailableUpdates, VersionsMatch
+from .omniversion_data import OmniversionData
+from .pretty import pretty
