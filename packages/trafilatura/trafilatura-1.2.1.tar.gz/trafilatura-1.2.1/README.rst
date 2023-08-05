1854945afd0a0cb921f871985d01a8a20de4f586
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:00 +0000

    Add package trafilatura/1.2.1/trafilatura-1.2.1.tar.gz

---
+++ packages/trafilatura/trafilatura-1.2.1.tar.gz/trafilatura-1.2.1/README.rst
@@ -0,0 +1,189 @@
+trafilatura: Web scraping tool for text discovery and retrieval
+===============================================================
+
+
+.. image:: docs/trafilatura-logo.png
+    :alt: Logo as PNG image
+    :align: center
+    :width: 60%
+
+|
+
+.. image:: https://img.shields.io/pypi/v/trafilatura.svg
+    :target: https://pypi.python.org/pypi/trafilatura
+    :alt: Python package
+
+.. image:: https://img.shields.io/pypi/pyversions/trafilatura.svg
+    :target: https://pypi.python.org/pypi/trafilatura
+    :alt: Python versions
+
+.. image:: https://readthedocs.org/projects/trafilatura/badge/?version=latest
+    :target: http://trafilatura.readthedocs.org/en/latest/?badge=latest
+    :alt: Documentation Status
+
+.. image:: https://img.shields.io/codecov/c/github/adbar/trafilatura.svg
+    :target: https://codecov.io/gh/adbar/trafilatura
+    :alt: Code Coverage
+
+.. image:: https://static.pepy.tech/badge/trafilatura/month
+    :target: https://pepy.tech/project/trafilatura
+    :alt: Downloads
+
+|
+
+.. image:: docs/trafilatura-demo.gif
+    :alt: Demo as GIF image
+    :align: center
+    :width: 85%
+    :target: https://trafilatura.readthedocs.org/
+
+
+Description
+-----------
+
+*Trafilatura* is a Python software package and command-line tool which seamlessly downloads, parses, and scrapes web page data: it can extract text and metadata while preserving parts of text formatting and page structure. This light-weight package acts as a modular toolkit: no database is required, the output can be converted to different commonly used formats.
+
+
+
+
+Features
+~~~~~~~~
+
+- Seamless and parallel online/offline processing:
+   - Download and conversion utilities included
+   - URLs, HTML files or parsed HTML trees as input
+- Robust and efficient extraction:
+   - Main text and/or comments
+   - Structural elements preserved: paragraphs, titles, lists, quotes, code, line breaks, in-line text formatting
+   - Extraction of metadata (title, author, date, site name, categories and tags)
+- Several output formats supported:
+   - Text (minimal formatting or Markdown)
+   - CSV (with metadata, `tab-separated values <https://en.wikipedia.org/wiki/Tab-separated_values>`_)
+   - JSON (with metadata)
+   - XML (for metadata and structure) and `TEI-XML <https://tei-c.org/>`_
+- Link discovery and URL lists:
+   - Focused crawling and politeness rules
+   - Support for sitemaps (TXT, XML) and feeds (ATOM, JSON, RSS)
+   - Efficient and polite processing of URL queues
+   - Blacklisting
+- Optional add-ons:
+   - Language detection on extracted content
+   - Graphical user interface (GUI)
+
+
+Evaluation and alternatives
+~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+For more detailed results see the `benchmark <https://trafilatura.readthedocs.io/en/latest/evaluation.html>`_ and `evaluation script <https://github.com/adbar/trafilatura/blob/master/tests/comparison.py>`_. To reproduce the tests just clone the repository, install all necessary packages and run the evaluation script with the data provided in the *tests* directory.
+
+=============================== =========  ========== ========= ========= ======
+500 documents, 1487 text and 1496 boilerplate segments (2021-06-07)
+--------------------------------------------------------------------------------
+Python Package                  Precision  Recall     Accuracy  F-Score   Diff.
+=============================== =========  ========== ========= ========= ======
+justext 2.2.0 (custom)          0.870      0.584      0.749     0.699     6.1x
+newspaper3k 0.2.8               0.921      0.574      0.763     0.708     12.9x
+boilerpy3 1.0.2 (article mode)  0.851      0.696      0.788     0.766     4.8x
+goose3 3.1.9                    **0.950**  0.644      0.806     0.767     18.8x
+*baseline (text markup)*        0.746      0.804      0.766     0.774     **1x**
+dragnet 2.0.4                   0.906      0.689      0.810     0.783     3.1x
+readability-lxml 0.8.1          0.917      0.716      0.826     0.804     5.9x
+news-please 1.5.21              0.924      0.718      0.830     0.808     60x
+trafilatura 0.8.2 (fast)        0.925      0.868      0.899     0.896     3.9x
+trafilatura 0.8.2               0.934      **0.890**  **0.914** **0.912** 8.4x
+=============================== =========  ========== ========= ========= ======
+
+Other evaluations:
+^^^^^^^^^^^^^^^^^^
+
+- Most efficient open-source library in *ScrapingHub*'s `article extraction benchmark <https://github.com/scrapinghub/article-extraction-benchmark>`_
+- Best overall tool according to Gaël Lejeune & Adrien Barbaresi, `Bien choisir son outil d'extraction de contenu à partir du Web <https://hal.archives-ouvertes.fr/hal-02768510v3/document>`_ (2020, PDF, French)
+
+
+Usage and documentation
+-----------------------
+
+For more information please refer to `the documentation <https://trafilatura.readthedocs.io/>`_:
+
+- `Installation <https://trafilatura.readthedocs.io/en/latest/installation.html>`_
+- Usage: `On the command-line <https://trafilatura.readthedocs.io/en/latest/usage-cli.html>`_, `With Python <https://trafilatura.readthedocs.io/en/latest/usage-python.html>`_, `With R <https://trafilatura.readthedocs.io/en/latest/usage-r.html>`_
+- `Core Python functions <https://trafilatura.readthedocs.io/en/latest/corefunctions.html>`_
+- Python Notebook `Trafilatura Overview <Trafilatura_Overview.ipynb>`_
+- `Tutorials <https://trafilatura.readthedocs.io/en/latest/tutorials.html>`_
+
+For video tutorials see this Youtube playlist:
+
+- `Web scraping how-tos and tutorials <https://www.youtube.com/watch?v=8GkiOM17t0Q&list=PL-pKWbySIRGMgxXQOtGIz1-nbfYLvqrci>`_
+
+
+License
+-------
+
+*Trafilatura* is distributed under the `GNU General Public License v3.0 <https://github.com/adbar/trafilatura/blob/master/LICENSE>`_. If you wish to redistribute this library but feel bounded by the license conditions please try interacting `at arms length <https://www.gnu.org/licenses/gpl-faq.html#GPLInProprietarySystem>`_, `multi-licensing <https://en.wikipedia.org/wiki/Multi-licensing>`_ with `compatible licenses <https://en.wikipedia.org/wiki/GNU_General_Public_License#Compatibility_and_multi-licensing>`_, or `contacting me <https://github.com/adbar/trafilatura#author>`_.
+
+See also `GPL and free software licensing: What's in it for business? <https://www.techrepublic.com/blog/cio-insights/gpl-and-free-software-licensing-whats-in-it-for-business/>`_
+
+
+
+Context
+-------
+
+
+Contributing
+~~~~~~~~~~~~
+
+Contributions are welcome! See `CONTRIBUTING.md <https://github.com/adbar/trafilatura/blob/master/CONTRIBUTING.md>`_ for more information. Bug reports can be filed on the `dedicated page <https://github.com/adbar/trafilatura/issues>`_.
+
+Many thanks to the `contributors <https://github.com/adbar/trafilatura/graphs/contributors>`_ who submitted features and bugfixes!
+
+
+Roadmap
+~~~~~~~
+
+For planned enhancements and relevant milestones see `issues page <https://github.com/adbar/trafilatura/milestones>`_.
+
+
+Author
+~~~~~~
+
+This effort is part of methods to derive information from web documents in order to build `text databases for research <https://www.dwds.de/d/k-web>`_ (chiefly linguistic analysis and natural language processing). Extracting and pre-processing web texts to the exacting standards of scientific research presents a substantial challenge for those who conduct such research. Web corpus construction involves numerous design decisions, and this software package can help facilitate text data collection and enhance corpus quality.
+
+.. image:: https://zenodo.org/badge/DOI/10.5281/zenodo.3460969.svg
+   :target: https://doi.org/10.5281/zenodo.3460969
+
+- Barbaresi, A. `Trafilatura: A Web Scraping Library and Command-Line Tool for Text Discovery and Extraction <https://aclanthology.org/2021.acl-demo.15/>`_, Proceedings of ACL/IJCNLP 2021: System Demonstrations, 2021, p. 122-131.
+-  Barbaresi, A. "`Generic Web Content Extraction with Open-Source Software <https://hal.archives-ouvertes.fr/hal-02447264/document>`_", Proceedings of KONVENS 2019, Kaleidoscope Abstracts, 2019.
+-  Barbaresi, A. "`Efficient construction of metadata-enhanced web corpora <https://hal.archives-ouvertes.fr/hal-01371704v2/document>`_", Proceedings of the `10th Web as Corpus Workshop (WAC-X) <https://www.sigwac.org.uk/wiki/WAC-X>`_, 2016.
+
+
+.. code-block:: shell
+
+    @inproceedings{barbaresi-2021-trafilatura,
+      title = {{Trafilatura: A Web Scraping Library and Command-Line Tool for Text Discovery and Extraction}},
+      author = "Barbaresi, Adrien",
+      booktitle = "Proceedings of the Joint Conference of the 59th Annual Meeting of the Association for Computational Linguistics and the 11th International Joint Conference on Natural Language Processing: System Demonstrations",
+      pages = "122--131",
+      publisher = "Association for Computational Linguistics",
+      url = "https://aclanthology.org/2021.acl-demo.15",
+      year = 2021,
+    }
+
+
+You can contact me via my `contact page <https://adrien.barbaresi.eu/>`_ or `GitHub <https://github.com/adbar>`_.
+
+
+Software
+~~~~~~~~
+
+
+.. image:: docs/software-ecosystem.png
+    :alt: Software ecosystem 
+    :align: center
+    :width: 65%
+
+
+*Trafilatura*: `Italian word <https://en.wiktionary.org/wiki/trafilatura>`_ for `wire drawing <https://en.wikipedia.org/wiki/Wire_drawing>`_.
+
+`Known uses of the software <https://trafilatura.readthedocs.io/en/latest/used-by.html>`_.
+
+Corresponding posts on `Bits of Language <https://adrien.barbaresi.eu/blog/tag/trafilatura.html>`_ (blog).
---
+++ packages/trafilatura/trafilatura-1.2.1.tar.gz/trafilatura-1.2.1/trafilatura/xpaths.py
@@ -0,0 +1,201 @@
+"""
+X-Path expressions needed to extract and filter the main text content
+"""
+## This file is available from https://github.com/adbar/trafilatura
+## under GNU GPL v3 license
+
+
+BODY_XPATH = [
+    '''.//*[(self::article or self::div or self::main or self::section)][contains(@id, "content-main") or
+    contains(@class, "content-main") or contains(@class, "content_main") or
+    contains(@id, "content-body") or contains(@class, "content-body") or
+    contains(@class, "story-body") or @id="article" or @class="post" or @class="entry"]''',
+    # contains(@class, "main-content") or
+    '''.//*[(self::article or self::div or self::main or self::section)][
+    contains(@class, "post-text") or contains(@class, "post_text") or
+    contains(@class, "post-body") or contains(@class, "post-entry") or contains(@class, "postentry") or
+    contains(@class, "post-content") or contains(@class, "post_content") or
+    contains(@class, "postcontent") or contains(@class, "postContent") or
+    contains(@class, "article-text") or contains(@class, "articletext") or contains(@class, "articleText") or contains(@class, "field-body")]''',
+    '''.//*[(self::article or self::div or self::main or self::section)][contains(@id, "entry-content") or
+    contains(@class, "entry-content") or contains(@id, "article-content") or
+    contains(@class, "article-content") or contains(@id, "article__content") or
+    contains(@class, "article__content") or contains(@id, "article-body") or
+    contains(@class, "article-body") or contains(@id, "article__body") or
+    contains(@class, "article__body") or @itemprop="articleBody" or
+    contains(translate(@id, "B", "b"), "articlebody") or contains(translate(@class, "B", "b"), "articleBody")
+    or @id="articleContent" or
+    contains(@class, "ArticleContent") or contains(@class, "page-content") or
+    contains(@class, "text-content") or contains(@class, "content__body") or
+    contains(@id, "body-text") or contains(@class, "body-text") or
+    contains(@class, "article__container") or contains(@id, "art-content") or contains(@class, "art-content")
+    or contains(@id, "contentBody")]''',
+    './/article',
+    """.//*[(self::article or self::div or self::main or self::section)][contains(@class, 'post-bodycopy') or
+    contains(@class, 'storycontent') or contains(@class, 'story-content') or
+    @class='postarea' or @class='art-postcontent' or
+    contains(@class, 'theme-content') or contains(@class, 'blog-content') or
+    contains(@class, 'section-content') or contains(@class, 'single-content') or
+    contains(@class, 'single-post') or
+    contains(@class, 'main-column') or contains(@class, 'wpb_text_column') or
+    starts-with(@id, 'primary') or starts-with(@class, 'article ') or @class="text" or
+    @class="cell" or @id="story" or @class="story" or
+    contains(translate(@class, "FULTEX","fultex"), "fulltext")]""",
+    '''.//*[(self::article or self::div or self::main or self::section)][
+    contains(translate(@id, "CM","cm"), "main-content") or contains(translate(@class, "CM","cm"), "main-content")
+    or contains(translate(@class, "CP","cp"), "page-content")]''',
+    './/*[(self::article or self::div or self::section)][starts-with(@class, "main") or starts-with(@id, "main") or starts-with(@role, "main")]|//main',
+]
+# starts-with(@id, "article") or
+# or starts-with(@id, "story") or contains(@class, "story")
+# starts-with(@class, "content ") or contains(@class, " content")
+# '//div[contains(@class, "text") or contains(@class, "article-wrapper") or contains(@class, "content-wrapper")]',
+# '//div[contains(@class, "article-wrapper") or contains(@class, "content-wrapper")]',
+# |//*[(self::article or self::div or self::main or self::section)][contains(@class, "article") or contains(@class, "Article")]
+# @id="content"or @class="content" or @class="Content"
+# or starts-with(@class, 'post ')
+# './/span[@class=""]', # instagram?
+
+
+COMMENTS_XPATH = [
+    """.//*[(self::div or self::list or self::section)][contains(@id, 'commentlist')
+    or contains(@class, 'commentlist') or contains(@class, 'comment-page') or
+    contains(@id, 'comment-list') or contains(@class, 'comments-list') or
+    contains(@class, 'comments-content') or contains(@class, 'post-comments')]""",
+    """.//*[(self::div or self::section or self::list)][starts-with(@id, 'comments')
+    or starts-with(@class, 'comments') or starts-with(@class, 'Comments') or
+    starts-with(@id, 'comment-') or starts-with(@class, 'comment-') or
+    contains(@class, 'article-comments')]""",
+    """.//*[(self::div or self::section or self::list)][starts-with(@id, 'comol') or
+    starts-with(@id, 'disqus_thread') or starts-with(@id, 'dsq-comments')]""",
+    ".//*[(self::div or self::section)][starts-with(@id, 'social') or contains(@class, 'comment')]",
+]
+# or contains(@class, 'Comments')
+
+
+REMOVE_COMMENTS_XPATH = [
+    """.//*[(self::div or self::list or self::section)][
+    starts-with(translate(@id, "C","c"), 'comment') or
+    starts-with(translate(@class, "C","c"), 'comment') or
+    contains(@class, 'article-comments') or contains(@class, 'post-comments')
+    or starts-with(@id, 'comol') or starts-with(@id, 'disqus_thread')
+    or starts-with(@id, 'dsq-comments')
+    ]""",
+]
+# or self::span
+# or contains(@class, 'comment') or contains(@id, 'comment')
+
+
+PAYWALL_DISCARD_XPATH = [
+    '''.//*[(self::div or self::p)][
+    contains(@id, "paywall") or contains(@id, "premium") or
+    contains(@class, "paid-content") or contains(@class, "paidcontent") or
+    contains(@class, "obfuscated") or contains(@class, "blurred")
+    ]''',
+]
+
+
+OVERALL_DISCARD_XPATH = [
+    '''.//*[contains(@id, "footer") or contains(@class, "footer") or
+    contains(@id, "bottom") or contains(@class, "bottom")]''',
+
+    # related posts, sharing jp-post-flair jp-relatedposts, news outlets + navigation
+    '''.//*[(self::div or self::item or self::list
+             or self::p or self::section or self::span)][
+    contains(@id, "related") or contains(translate(@class, "R", "r"), "related") or
+    contains(@id, "viral") or contains(@class, "viral") or
+    starts-with(@id, "shar") or starts-with(@class, "shar") or
+    contains(@class, "share-") or
+    contains(translate(@id, "S", "s"), "share") or
+    contains(@id, "social") or contains(@class, "social") or contains(@class, "sociable") or
+    contains(@id, "syndication") or contains(@class, "syndication") or
+    starts-with(@id, "jp-") or starts-with(@id, "dpsp-content") or
+    contains(@class, "embedded") or contains(@class, "embed")
+    or contains(@id, "newsletter") or contains(@class, "newsletter")
+    or contains(@class, "subnav") or
+    contains(@id, "cookie") or contains(@class, "cookie") or contains(@id, "tags")
+    or contains(@class, "tags")  or contains(@id, "sidebar") or
+    contains(@class, "sidebar") or contains(@id, "banner") or contains(@class, "banner")
+    or contains(@class, "meta") or
+    contains(@id, "menu") or contains(@class, "menu") or
+    contains(translate(@id, "N", "n"), "nav") or contains(translate(@role, "N", "n"), "nav")
+    or starts-with(@class, "nav") or contains(translate(@class, "N", "n"), "navigation") or
+    contains(@class, "navbar") or contains(@class, "navbox") or starts-with(@class, "post-nav")
+    or contains(@id, "breadcrumb") or contains(@class, "breadcrumb") or
+    contains(@id, "bread-crumb") or contains(@class, "bread-crumb") or
+    contains(@id, "author") or contains(@class, "author") or
+    contains(@id, "button") or contains(@class, "button")
+    or contains(translate(@class, "B", "b"), "byline")
+    or contains(@class, "rating") or starts-with(@class, "widget") or
+    contains(@class, "attachment") or contains(@class, "timestamp") or
+    contains(@class, "user-info") or contains(@class, "user-profile") or
+    contains(@class, "-ad-") or contains(@class, "-icon")
+    or contains(@class, "article-infos") or
+    contains(translate(@class, "I", "i"), "infoline")
+    or contains(@data-component, "MostPopularStories")
+    or contains(@class, "options")
+    or contains(@class, "consent") or contains(@class, "modal-content")
+    or contains(@class, "paid-content") or contains(@class, "paidcontent")
+    or contains(@id, "premium-") or contains(@id, "paywall")
+    or contains(@class, "obfuscated") or contains(@class, "blurred")
+    or contains(@class, " ad ")
+    or contains(@class, "next-post")
+    or contains(@class, "message-container") or contains(@id, "message_container")
+    or contains(@class, "yin") or contains(@class, "zlylin") or
+    contains(@class, "xg1") or contains(@id, "bmdh")
+    or @data-lp-replacement-content]''',
+
+    # comment debris
+    '''.//*[@class="comments-title" or contains(@class, "comments-title") or contains(@class, "nocomments") or starts-with(@id, "reply-") or starts-with(@class, "reply-") or
+    contains(@class, "-reply-") or contains(@class, "message") or contains(@id, "akismet") or contains(@class, "akismet")]''',
+
+    # hidden
+    '''.//*[starts-with(@class, "hide-") or contains(@class, "hide-print") or contains(@id, "hidden")
+    or contains(@style, "hidden") or contains(@hidden, "hidden") or contains(@class, "noprint") or contains(@style, "display:none") or contains(@class, " hidden") or @aria-hidden="true" or contains(@class, "notloaded")]''',
+]
+# conflicts:
+# contains(@id, "header") or contains(@class, "header") or
+# class contains "cats" (categories, also tags?)
+# or contains(@class, "hidden ")  or contains(@class, "-hide")
+# or contains(@class, "paywall")
+# contains(@class, "content-info") or contains(@class, "content-title")
+# contains(translate(@class, "N", "n"), "nav") or
+
+
+# the following conditions focus on extraction precision
+ADDITIONAL_DISCARD_XPATH = [
+    '''.//*[(self::div or self::item or self::list
+             or self::p or self::section or self::span)][
+        contains(translate(@id, "T", "t"), "teaser") or contains(translate(@class, "T", "t"), "teaser")
+    ]''',
+]
+
+
+PRECISION_DISCARD_XPATH = [
+    './/header',
+    '''.//*[(self::div or self::item or self::list
+             or self::p or self::section or self::span)][
+        contains(@id, "link") or contains(@class, "link")
+    ]''',
+]
+
+
+DISCARD_IMAGE_ELEMENTS = [
+    '''.//*[(self::div or self::item or self::list
+             or self::p or self::section or self::span)][
+             contains(@id, "caption") or contains(@class, "caption")
+            ]
+    '''
+]
+
+
+COMMENTS_DISCARD_XPATH = [
+    './/*[(self::div or self::section)][starts-with(@id, "respond")]',
+    './/cite|.//quote',
+    '''.//*[@class="comments-title" or contains(@class, "comments-title") or
+    contains(@class, "nocomments") or starts-with(@id, "reply-") or
+    starts-with(@class, "reply-") or contains(@class, "-reply-") or contains(@class, "message")
+    or contains(@class, "signin") or
+    contains(@id, "akismet") or contains(@class, "akismet") or contains(@style, "display:none")]''',
+]
+
