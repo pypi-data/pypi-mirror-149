44dfd33ddf44de33f1e5a6d6c80ed66a8f14b41b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:42:27 +0000

    Add package bionetgen/0.7.3/bionetgen-0.7.3-py3-none-any.whl

---
+++ packages/bionetgen/bionetgen-0.7.3-py3-none-any.whl/bionetgen/core/main.py
@@ -0,0 +1,124 @@
+import os
+from bionetgen.tools import BNGInfo
+from bionetgen.tools import BNGVisualize
+from bionetgen.tools import BNGCLI
+from bionetgen.tools import BNGGdiff
+
+import os, sys
+
+
+# TODO Consolidate how config is being accessed. It's
+# almost like each function accesses the configs from
+# a different path
+
+
+def runCLI(config, args):
+    """
+    Convenience function to run BNG2.pl from the CLI app
+
+    Usage: runCLI(config, args)
+
+    Arguments
+    ---------
+    config : dict
+        configuration dictionary from BioNetGen cement app
+    args :  argparse.Namespace
+        arguments parsed from the command line with argparser.
+    """
+    # this pulls out the arguments
+    sys.tracebacklimit = args.traceback_depth
+    inp_file = args.input
+    output = args.output
+    log_file = args.log_file
+    # if you set args.bngpath it should take precedence
+    config_bngpath = config.get("bionetgen", "bngpath")
+    # and instantiates the CLI object
+    cli = BNGCLI(inp_file, output, config_bngpath, log_file=log_file)
+    cli.stdout = config.get("bionetgen", "stdout")
+    cli.stderr = config.get("bionetgen", "stderr")
+    cli.run()
+
+
+def plotDAT(inp, out=".", kw=dict()):
+    """
+    Convenience function to plot dat/scan files from the CLI
+
+    Usage: plotDAT(inp, out, kw)
+
+    Arguments
+    ---------
+    inp : str
+        input gdat/cdat/scan file to plot
+    out : str
+        (optional) output file path, can be used to define the
+        output format as well. Default is the current folder,
+        filename is the same as the input file and default format
+        is PNG.
+    kw : dict
+        (optional) this is a set of keyword arguments you want to
+        pass for certain matplotlib options. Check -h for details
+    """
+    # if we want to plot directly into the folder
+    # we are in we need to get the path correctly
+    if out == ".":
+        path, fname = os.path.split(inp)
+        fnoext, ext = os.path.splitext(fname)
+        out = os.path.join(path, "{}.png".format(fnoext))
+    # use the plotter object to get the plot
+    from bionetgen.tools import BNGPlotter
+
+    plotter = BNGPlotter(inp, out, **kw)
+    plotter.plot()
+
+
+def runAtomizeTool(config, args):
+    from bionetgen.atomizer import AtomizeTool
+
+    a = AtomizeTool(parser_namespace=args)
+    # do config specific stuff here if need be, or remove the config requirement
+    a.run()
+
+
+def printInfo(config, args):
+    """
+    Uses BNGInfo class to print BioNetGen information using
+    arguments and config from Cement framework.
+    """
+    # this pulls out the arguments
+    # inp_file = args.input
+    # output = args.output
+    # if you set args.bngpath it should take precedence
+    # config_bngpath = config.get("bionetgen", "bngpath")
+    # and instantiates the CLI object
+    info = BNGInfo(config=config)
+    info.gatherInfo()
+    info.messageGeneration()
+    info.run()
+
+
+def visualizeModel(config, args):
+    """
+    Uses BNGVisualize class to visualize BNGL models using
+    arguments and configuration from Cement framework.
+    """
+    inp = args.input
+    out = args.output
+    vtype = args.type
+    # if you set args.bngpath it should take precedence
+    config_bngpath = config.get("bionetgen", "bngpath")
+    viz = BNGVisualize(inp, output=out, vtype=vtype, bngpath=config_bngpath)
+    viz.run()
+
+
+def graphDiff(config, args):
+    # if you set args.bngpath it should take precedence
+    config_bngpath = config.get("bionetgen", "bngpath")
+    gdiff = BNGGdiff(
+        args.input,
+        args.input2,
+        out=args.output,
+        out2=args.output2,
+        mode=args.mode,
+        colors=args.colors,
+    )
+    gdiff.run()
---
+++ packages/bionetgen/bionetgen-0.7.3-py3-none-any.whl/bionetgen/modelapi/pattern.py
@@ -0,0 +1,341 @@
+# All classes that deal with patterns
+class Pattern:
+    """
+    Pattern object. Fundamentally it's just a list of molecules
+    which are defined later.
+
+    Attributes
+    ----------
+    _bonds : Bonds
+        setting a pattern requires you to keep track of all bonds to
+        correctly label them, this object tracks everything
+    compartment : str
+        compartment of the overall pattern (not the same thing as
+        molecule compartment, those have their own)
+    _label : str
+        label of the overall pattern (not the same thing as molecule
+        label, those have their own)
+    molecules : list[Molecule]
+        list of molecule objects that are in the pattern
+    fixed : bool
+        used for constant species, sets "$" at the beginning of the
+        pattern string
+    MatchOnce : bool
+        used for matchOnce syntax, "{MatchOnce}PatternStr"
+    """
+
+    def __init__(self, molecules=[], bonds=None, compartment=None, label=None):
+        self.molecules = molecules
+        self._bonds = bonds
+        self.compartment = compartment
+        self.label = label
+        self.fixed = False
+        self.MatchOnce = False
+        self.relation = None
+        self.quantity = None
+
+    @property
+    def compartment(self):
+        return self._compartment
+
+    @compartment.setter
+    def compartment(self, value):
+        # TODO: Build in logic to set the
+        # outer compartment
+        # print("Warning: Logical checks are not complete")
+        self._compartment = value
+        # by default, once the outer compartment is set
+        # we will set the compartment of each molecule
+        # to that new compartment.
+        self.consolidate_molecule_compartments()
+
+    def consolidate_molecule_compartments(self):
+        # if the molecule compartment matches overall pattern
+        # compartment, don't print the molecule compartments
+        overall_comp = self.compartment
+        if overall_comp is not None:
+            for molec in self.molecules:
+                if molec.compartment != overall_comp:
+                    molec.compartment = overall_comp
+                else:
+                    molec.compartment = None
+
+    @property
+    def label(self):
+        return self._label
+
+    @label.setter
+    def label(self, value):
+        # TODO: Build in logic to set
+        # the outer label
+        # print("Warning: Logical checks are not complete")
+        self._label = value
+
+    def __str__(self):
+        # need to make sure we don't print useless compartments
+        self.consolidate_molecule_compartments()
+        sstr = ""
+        # we first deal with the pattern compartment
+        if self.compartment is not None:
+            sstr += "@{}:".format(self.compartment)
+        # now loop over all molecules
+        for imol, mol in enumerate(self.molecules):
+            if imol == 0:
+                if self.fixed:
+                    sstr += "$"
+                if self.MatchOnce:
+                    sstr += "{MatchOnce}"
+            # FIXME: This label also probably behaves like compartments
+            if imol == 0 and self.label is not None:
+                sstr += "%{}:".format(self.label)
+            if imol > 0:
+                sstr += "."
+            sstr += str(mol)
+        if self.relation is not None:
+            sstr += f"{self.relation}{self.quantity}"
+        return sstr
+
+    def __repr__(self):
+        return str(self)
+
+    def __getitem__(self, key):
+        return self.molecules[key]
+
+    def __iter__(self):
+        return self.molecules.__iter__()
+
+    # TODO: Implement __contains__
+
+
+class Molecule:
+    """
+    Molecule object. A pattern is a list of molecules.
+    This object also handles molecule types where components
+    have a list of possible states.
+
+    Attributes
+    ----------
+    _name : str
+        name of the molecule
+    _compartment : str
+        compartment of the molecule
+    _label : str
+        label of the molecule
+    _components : list[Component]
+        list of components for this molecule
+
+    Methods
+    -------
+    add_component(name, state=None, states=[])
+        add a component object to the list of components with name
+        "name", current state "state" or a list of states
+        (for molecule types) "states"
+    """
+
+    def __init__(self, name="0", components=[], compartment=None, label=None):
+        self._name = name
+        self._components = components
+        self._compartment = compartment
+        self._label = label
+
+    def __getitem__(self, key):
+        if isinstance(key, int):
+            return self.components[key]
+
+    def __iter__(self):
+        return self.components.__iter__()
+
+    # TODO: implement __setitem__,  __contains__
+
+    def __str__(self):
+        mol_str = self.name
+        if self.label is not None:
+            mol_str += "%{}".format(self.label)
+        # we have a null species
+        if not self.name == "0":
+            mol_str += "("
+        # we _could_ just not do () if components
+        # don't exist but that has other issues,
+        # especially for extension highlighting
+        if len(self.components) > 0:
+            for icomp, comp in enumerate(self.components):
+                if icomp > 0:
+                    mol_str += ","
+                mol_str += str(comp)
+        # we have a null species
+        if not self.name == "0":
+            mol_str += ")"
+        if self.compartment is not None:
+            mol_str += "@{}".format(self.compartment)
+        return mol_str
+
+    ### PROPERTIES ###
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        # print("Warning: Logical checks are not complete")
+        # TODO: Check for invalid characters
+        self._name = value
+
+    @property
+    def components(self):
+        return self._components
+
+    @components.setter
+    def components(self, value):
+        # print("Warning: Logical checks are not complete")
+        self._components = value
+
+    def __repr__(self):
+        return str(self)
+
+    @property
+    def compartment(self):
+        return self._compartment
+
+    @compartment.setter
+    def compartment(self, value):
+        # print("Warning: Logical checks are not complete")
+        self._compartment = value
+
+    @property
+    def label(self):
+        return self._label
+
+    @label.setter
+    def label(self, value):
+        # print("Warning: Logical checks are not complete")
+        self._label = value
+
+    def _add_component(self, name, state=None, states=[]):
+        comp_obj = Component()
+        comp_obj.name = name
+        comp_obj.state = state
+        comp_obj.states = states
+        self.components.append(comp_obj)
+
+    def add_component(self, name, state=None, states=[]):
+        # TODO: Add built-in logic here
+        # print("Warning: Logical checks are not complete")
+        self._add_component(name, state, states)
+
+
+class Component:
+    """
+    Component object that describes the state, label and bonding
+    for each component. Molecules can optionally contain components
+
+    Attributes
+    ----------
+    name : str
+        name of the component
+    _label : str
+        label of the component
+    _state : str
+        state of the component, not used for molecule types
+    _states : list[str]
+        list of states for molecule types
+    _bonds : list[Bond]
+        list of bond objects that describes bonding of the component
+
+    Methods
+    -------
+    add_state()
+        not implemented. will eventually be used to add additional states
+        to an existing component
+    add_bond()
+        not implemented. will eventually be used to add additional bonds
+        to an existing component
+    """
+
+    def __init__(self):
+        self._name = ""
+        self._label = None
+        self._state = None
+        self._states = []
+        self._bonds = []
+
+    def __repr__(self):
+        return str(self)
+
+    def __str__(self):
+        comp_str = self.name
+        # only for molecule types
+        if len(self.states) > 0:
+            for istate, state in enumerate(self.states):
+                comp_str += "~{}".format(state)
+        # for any other pattern
+        if self.state is not None:
+            comp_str += "~{}".format(self.state)
+        if self.label is not None:
+            comp_str += "%{}".format(self.label)
+        if len(self.bonds) > 0:
+            for bond in self.bonds:
+                comp_str += "!{}".format(bond)
+        return comp_str
+
+    ### PROPERTIES ###
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        # TODO: Add built-in logic here
+        # print("Warning: Logical checks are not complete")
+        self._name = value
+
+    @property
+    def label(self):
+        return self._label
+
+    @label.setter
+    def label(self, value):
+        # TODO: Add built-in logic here
+        # print("Warning: Logical checks are not complete")
+        self._label = value
+
+    @property
+    def state(self):
+        return self._state
+
+    @state.setter
+    def state(self, value):
+        # TODO: Add built-in logic here
+        # print("Warning: Logical checks are not complete")
+        self._state = value
+
+    @property
+    def states(self):
+        return self._states
+
+    @states.setter
+    def states(self, value):
+        # TODO: Add built-in logic here
+        # print("Warning: Logical checks are not complete")
+        self._states = value
+
+    @property
+    def bonds(self):
+        return self._bonds
+
+    @bonds.setter
+    def bonds(self, value):
+        # TODO: Add built-in logic here
+        # print("Warning: Logical checks are not complete")
+        self._bonds = value
+
+    def _add_state(self):
+        raise NotImplementedError
+
+    def add_state(self):
+        self._add_state()
+
+    def _add_bond(self):
+        raise NotImplementedError
+
+    def add_bond(self):
+        self._add_bond()
