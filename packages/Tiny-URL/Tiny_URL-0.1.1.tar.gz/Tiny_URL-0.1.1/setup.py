d64872bc5682793feaff5d190fcc0de5ccf8282b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:40 +0000

    Add package Tiny-URL/0.1.1/Tiny_URL-0.1.1.tar.gz

---
+++ packages/Tiny-URL/Tiny_URL-0.1.1.tar.gz/Tiny_URL-0.1.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Tiny_URL",
+    version="0.1.1",
+    author="orarange",
+    author_email="arigatoudane@outlook.jp",
+    description="this is an tiny url Python wrapper",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/minato37103710/Tiny-url-Wrapper",
+    project_urls={
+        "Bug Tracker": "https://github.com/minato37103710/Tiny-url-Wrapper/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
