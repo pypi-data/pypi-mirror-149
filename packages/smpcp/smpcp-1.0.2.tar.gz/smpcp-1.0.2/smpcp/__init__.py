f2bb2f52f79c4b4a7843e08da84941c4c742bfb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:13:05 +0000

    Add package smpcp/1.0.2/smpcp-1.0.2.tar.gz

---
+++ packages/smpcp/smpcp-1.0.2.tar.gz/smpcp-1.0.2/smpcp/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+@Version: 1.0.2
+@Project: Secure-Multi-Party-Computation-Protocol
+@Author: Zhan Shi
+@Time  : 2022/5/6 13:26
+@File: __init__.py
+@License: MIT
+"""
+
+__name__ = 'smpcp'
