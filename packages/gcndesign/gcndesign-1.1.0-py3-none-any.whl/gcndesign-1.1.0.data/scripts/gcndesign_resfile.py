4b47656e7729bc7e587d25da1f50722fdeb1e713
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:12:29 +0000

    Add package gcndesign/1.1.0/gcndesign-1.1.0-py3-none-any.whl

---
+++ packages/gcndesign/gcndesign-1.1.0-py3-none-any.whl/gcndesign-1.1.0.data/scripts/gcndesign_resfile.py
@@ -0,0 +1,34 @@
+#!python
+
+import sys
+from os import path
+import argparse
+import torch
+
+dir_script = path.dirname(path.realpath(__file__))
+sys.path.append(dir_script+'/../')
+from gcndesign.predictor import Predictor
+
+# default processing device
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+# argument parser
+parser = argparse.ArgumentParser()
+parser.add_argument('pdb', type=str, default=None, metavar='[File]',
+                    help='PDB file input.')
+parser.add_argument('--prob-cut', '-c', type=float, default=0.8, metavar='[Float]',
+                    help='Probability cutoff. (default:{})'.format(0.8))
+parser.add_argument('--device', type=str, default=device, choices=['cpu', 'cuda'],
+                    help='Processing device. (default:\'cuda\' if available)')
+parser.add_argument('--param-in', '-p', type=str, default=None, metavar='[File]',
+                    help='NN parameter file. (default:{})'.format(None))
+args = parser.parse_args()
+
+# check files
+assert path.isfile(args.pdb), "PDB file {:s} was not found.".format(args.pdb)
+    
+# predictor
+predictor = Predictor(device=args.device, param=args.param_in)
+
+# resfile
+print(predictor.make_resfile(pdb=args.pdb, prob_cut=args.prob_cut))
