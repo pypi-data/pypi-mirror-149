fd26586bd12cf39517bffa749681e34391484f6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:42:21 +0000

    Add package quickstats/0.6.2.8.2/quickstats-0.6.2.8.2-py3-none-any.whl

---
+++ packages/quickstats/quickstats-0.6.2.8.2-py3-none-any.whl/quickstats/decorators.py
@@ -0,0 +1,25 @@
+from functools import partial
+import time
+
+class semistaticmethod(object):
+    def __init__(self, callable):
+        self.f = callable
+    def __get__(self, obj, type=None):
+        if (obj is None) and (type is not None):
+            return partial(self.f, type)
+        if (obj is not None):
+            return partial(self.f, obj)
+        return self.f
+    @property
+    def __func__(self):
+        return self.f
+    
+def cls_method_timer(func):
+    def wrapper(self, *args, **kwargs):
+        t1 = time.time()
+        result = func(self, *args, **kwargs)
+        t2 = time.time()
+        method_name = f"{type(self).__name__}::{func.__name__}"
+        self.stdout.info(f'INFO: Task {method_name!r} executed in {(t2-t1):.4f}s')
+        return result
+    return wrapper 
