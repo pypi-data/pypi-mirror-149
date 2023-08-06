860061113fe02ba7371df70886208ba16b75b060
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:24 +0000

    Add package pyqt-viewer-widget/0.0.11/pyqt-viewer-widget-0.0.11.tar.gz

---
+++ packages/pyqt-viewer-widget/pyqt-viewer-widget-0.0.11.tar.gz/pyqt-viewer-widget-0.0.11/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyqt-viewer-widget',
+    version='0.0.11',
+    author='Jung Gyu Yoon',
+    author_email='yjg30737@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    description='PyQt viewer widget which helps you make viewer application easily',
+    url='https://github.com/yjg30737/pyqt-viewer-widget.git',
+    install_requires=[
+        'PyQt5>=5.8',
+        'pyqt-toast>=0.0.1',
+        'pyqt-fitting-graphics-view @ git+https://git@github.com/yjg30737/pyqt-fitting-graphics-view.git@main'
+    ]
+)
