af6cf8884953cc4f8be028be22a692b6e5cfe459
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:04 +0000

    Add package cdst/0.1/cdst-0.1.tar.gz

---
+++ packages/cdst/cdst-0.1.tar.gz/cdst-0.1/setup.py
@@ -0,0 +1,30 @@
+# from setuptools import setup, find_packages
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='cdst',
+    packages=setuptools.find_packages(),
+    version='0.1',
+    author='Dr. Calvin Chan',
+    author_email='calvin.chan@bayer.com',
+    keywords=['deep learning', 'hyperparameter'],
+    url='https://github.com/Bayer-Group/cdst',
+    project_urls={
+        "Bug Tracker": "https://github.com/Bayer-Group/cdst/issues",
+    },
+    description="Calvin's Data Science Toolbox",
+    long_description=long_description,
+    install_requires=['cython>="0.29.24"',
+                      'numpy>="1.20.0"',
+                      'scikit-learn',
+                      'pandas',
+                      'torch>="1.9.0"',
+                      'skorch',
+                      'ray>="1.12.0"',
+                      'tensorboardX',
+                      'gekko>="1.0.2"',
+                      'tensorflow']
+)
