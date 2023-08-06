1e3fb8e5de285a56c402f8b1ca37900a7edc0ba2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:06 +0000

    Add package techlearn/1.4/TechLearn-1.4.tar.gz

---
+++ packages/techlearn/TechLearn-1.4.tar.gz/TechLearn-1.4/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+import io
+import os
+import sys
+from shutil import rmtree
+from setuptools import find_packages, setup, Command
+
+setup(
+  name = 'TechLearn',         # How you named your package folder
+  packages = ['kits'],   # Chose the same as "name"
+  version = '1.4',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'TestProgram',   # Give a short description about your library
+  author = 'WhereIsTom',                   # Type in your name
+  author_email = 'wzzhangzheyuan@163.com',      # Type in your E-Mail
+  url = 'https://github.com/GodOn514/TechLearn',   # Provide either the link to your github or to your website
+)
