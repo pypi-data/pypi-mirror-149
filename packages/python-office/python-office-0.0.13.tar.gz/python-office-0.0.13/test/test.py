b65a5e9a002ed2da626453a904169b4ec96d3fae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:45 +0000

    Add package python-office/0.0.13/python-office-0.0.13.tar.gz

---
+++ packages/python-office/python-office-0.0.13.tar.gz/python-office-0.0.13/test/test.py
@@ -0,0 +1,10 @@
+import office  # 导入python-office
+
+
+path = 'D:\\workplace\\code\\BaiduNetdiskWorkspace\\personal\\linux\\workplace\\pro\\git\\gitee\\python-office\\test\\docx'  # path这里，填写你存放word文件的位置，例如：C:/app/workbook
+office.word.docx2pdf(path=path)  # 程序就可以自动将该目录下的所有word文档，自动转换成pdf文档了
+
+
+
+
+
