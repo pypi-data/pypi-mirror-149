48f776f6b0af96815d38143a356c68f09497e430
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:51 +0000

    Add package arhmm/0.0.1/arhmm-0.0.1.tar.gz

---
+++ packages/arhmm/arhmm-0.0.1.tar.gz/arhmm-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup_requires = []
+
+install_requires = [
+    "numpy",
+    "scipy"
+]
+
+setup(
+    name='arhmm',
+    version='0.0.1',
+    description='',
+    license='MIT',
+    install_requires=install_requires,
+    package_data={'arhmm': ['py.typed']}
+)
