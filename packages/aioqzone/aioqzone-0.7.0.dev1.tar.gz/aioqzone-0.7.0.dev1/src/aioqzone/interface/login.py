f061bfbe1c26452ec0a9adfdc0f86ca29c4bba80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:21 +0000

    Add package aioqzone/0.7.0.dev1/aioqzone-0.7.0.dev1.tar.gz

---
+++ packages/aioqzone/aioqzone-0.7.0.dev1.tar.gz/aioqzone-0.7.0.dev1/src/aioqzone/interface/login.py
@@ -0,0 +1,60 @@
+import asyncio
+from abc import ABC, abstractmethod
+from time import time
+from typing import Dict
+
+from qqqr.encrypt import gtk
+
+from ..interface.hook import Emittable, LoginEvent
+
+
+class Loginable(ABC, Emittable[LoginEvent]):
+    last_login: float = 0
+
+    def __init__(self, uin: int) -> None:
+        super().__init__()
+        self.uin = uin
+        self._cookie = {}
+        self.lock = asyncio.Lock()
+
+    @property
+    def cookie(self) -> Dict[str, str]:  # type: ignore
+        """Get cookie in any way. Allow cached result.
+
+        Returns:
+            int: cookie. Cached cookie is preferable.
+        """
+        return self._cookie
+
+    @abstractmethod
+    async def _new_cookie(self) -> Dict[str, str]:
+        return
+
+    async def new_cookie(self):
+        """Get a new cookie. Means, cached cookie is not allowed.
+
+        Returns:
+            int: cookie. Shouldn't be a cached one.
+        """
+        if self.lock.locked():
+            # if there is coro. updating cookie, then wait for its result.
+            async with self.lock:
+                return self.cookie
+        else:
+            # let the first coro. get result from Qzone.
+            async with self.lock:
+                self._cookie = await self._new_cookie()
+                self.last_login = time()
+                return self._cookie
+
+    @property
+    def gtk(self) -> int:
+        """cal gtk from pskey.
+
+        Returns:
+            int: gtk. NOTE: 0 denotes no-login.
+        """
+        pskey = self.cookie.get("p_skey")
+        if pskey is None:
+            return 0
+        return gtk(pskey)
---
+++ packages/aioqzone/aioqzone-0.7.0.dev1.tar.gz/aioqzone-0.7.0.dev1/src/qqqr/type.py
@@ -0,0 +1,39 @@
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class PT_QR_APP:
+    app: str = ""
+    link: str = ""
+    register: str = ""
+    help: str = ""
+
+
+@dataclass(frozen=True)
+class Proxy:
+    proxy_url: str
+    s_url: str
+
+
+@dataclass(frozen=True)
+class APPID:
+    appid: int
+    daid: int
+
+
+@dataclass
+class CheckResult:
+    code: int
+    verifycode: str
+    salt: str
+    verifysession: str
+    isRandSalt: int
+    ptdrvs: str
+    session: str
+
+    def __post_init__(self):
+        self.code = int(self.code)
+        self.isRandSalt = int(self.isRandSalt)
+        salt = self.salt.split(r"\x")[1:]
+        salt = [chr(int(i, 16)) for i in salt]
+        self.salt = "".join(salt)
