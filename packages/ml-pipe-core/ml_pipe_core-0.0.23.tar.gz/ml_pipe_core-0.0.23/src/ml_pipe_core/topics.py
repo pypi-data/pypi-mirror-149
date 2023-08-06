824cf4f36930aafa7fb3b8b05763511fc0b1c5b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:51 +0000

    Add package ml-pipe-core/0.0.23/ml_pipe_core-0.0.23.tar.gz

---
+++ packages/ml-pipe-core/ml_pipe_core-0.0.23.tar.gz/ml_pipe_core-0.0.23/src/ml_pipe_core/topics.py
@@ -0,0 +1,10 @@
+NUM_OF_PARTITION = 3
+
+SERVICE_INPUT_TOPIC = 'service_input'
+SERVICE_OUTPUT_TOPIC = 'service_output'
+SERVICE_OBSERVE_TOPIC = 'service-observed'
+RECONFIG_TOPIC = 'reconfig_event'
+CONTROL_TOPIC = "control"
+IS_ALIVE = "healthy"
+ACTIVE_SERVICES_UPDATED_TOPIC = "active_services_updated"
+AGENT_EVENTS = "agent_events"
