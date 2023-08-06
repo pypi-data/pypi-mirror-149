da1612e18344db10a7a50bacc1aa9c21bb5f80fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:01 +0000

    Add package streamlit-gradio/2.8.2/streamlit_gradio-2.8.2.tar.gz

---
+++ packages/streamlit-gradio/streamlit_gradio-2.8.2.tar.gz/streamlit_gradio-2.8.2/setup.py
@@ -0,0 +1,31 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='streamlit_gradio',
+    version='2.8.2',
+    include_package_data=True,
+    description='A streamlit wrapper for gradio',
+    author='Rishikesh Anand',
+    author_email='rishi1998@gmail.com',
+    url='https://github.com/truefoundry/streamlit-gradio',
+    packages=['streamlit_gradio'],
+    license='Apache License 2.0',
+    keywords=['machine learning', 'visualization', 'reproducibility'],
+    install_requires=[
+        'streamlit',
+        'numpy',
+        'pydub',
+        'matplotlib',
+        'pandas',
+        'pillow',
+        'ffmpy',
+        'markdown2',
+        'pycryptodome',
+        'requests',
+        'paramiko',
+        'analytics-python',
+    ],
+)
