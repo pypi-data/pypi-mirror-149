fd2cb2bedb7a98c8f7187281ef218fc85f460726
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:53 +0000

    Add package accele/0.0.2/accele-0.0.2-py3-none-any.whl

---
+++ packages/accele/accele-0.0.2-py3-none-any.whl/base/sample.py
@@ -0,0 +1,642 @@
+#!/usr/bin/python
+# encoding=utf-8
+
+"""
+@Author  :  gri
+@Date    :  2021/11/13 17:50
+@Desc    :  
+"""
+
+gitignore_content = """.idea/
+.pytest_cache/
+.accele_allure_tmp/
+__pycache__/
+*.pyc
+reports/
+debug/"""
+
+conf_yaml_content = """env: qa"""
+
+conftest_content = """#!/usr/bin/python
+# encoding=utf-8
+
+\"\"\" 只能管理员编辑，只对外提供fixture。
+\"\"\"
+
+import os
+
+import pytest
+
+# 项目目录路径
+_project_dir = os.path.dirname(os.path.abspath(__file__))
+
+
+# 设置缓存供accele使用
+@pytest.fixture(scope="session", autouse=True)
+def _project_cache(request):
+    request.config.cache.set("project_dir", _project_dir)
+
+
+# 自动导入fixtures
+_fixtures_dir = os.path.join(_project_dir, "fixtures")
+for root, _, files in os.walk(_fixtures_dir):
+    for file in files:
+        if file.startswith("fixture_") and file.endswith(".py"):
+            full_path = os.path.join(root, file)
+            import_path = full_path.replace(_fixtures_dir, "").replace("\\\\", ".").replace("/", ".").replace(".py", "")
+            try:
+                fixture_path = "fixtures" + import_path
+                exec(f"from {fixture_path} import *")
+            except:
+                fixture_path = ".fixtures" + import_path
+                exec(f"from {fixture_path} import *")
+"""
+
+pytest_ini_content = """[pytest]
+markers =
+    smoke: smoke test
+    regress: regress test
+"""
+
+fixture_admin_content = """#!/usr/bin/python
+# encoding=utf-8
+
+\"\"\" Can only be modified by the administrator. Only fixtures are provided.
+\"\"\"
+
+from accele.fixture import *
+
+
+@pytest.fixture
+def common_created_by_admin():
+    pass
+"""
+
+fixture_env_vars_content = """#!/usr/bin/python
+# encoding=utf-8
+
+from accele.dao import mysql_engine
+from accele.fixture import *
+
+
+@pytest.fixture(scope="session")
+def env_vars(config):
+    class Clazz(TepVars):
+        env = config["env"]
+
+        \"\"\"变量定义开始\"\"\"
+        # 环境变量
+        mapping = {
+            \"qa\": {
+                "domain": "http://127.0.0.1:5000",
+                "mysql_engine": mysql_engine("127.0.0.1",  # host
+                                             "2306",  # port
+                                             "root",  # username
+                                             "123456",  # password
+                                             "qa"),  # db_name
+            },
+            "release": {
+                "domain": "https://release.com",
+                "mysql_engine": mysql_engine("127.0.0.1",
+                                             "2306",
+                                             "root",
+                                             "123456",
+                                             "release"),
+            }
+            # 继续添加
+        }
+        # 定义类属性，敲代码时会有智能提示
+        domain = mapping[env]["domain"]
+        mysql_engine = mapping[env]["mysql_engine"]
+        \"\"\"变量定义结束\"\"\"
+
+    return Clazz()
+"""
+
+fixture_login_content = """from accele.client import request
+from accele.fixture import *
+
+
+def _jwt_headers(token):
+    return {"Content-Type": "application/json", "authorization": f"Bearer {token}"}
+
+
+@pytest.fixture(scope="session")
+def login(env_vars):
+    # Code your login
+    logger.info("Administrator login")
+    response = request(
+        "post",
+        url=env_vars.domain + "/login",
+        headers={"Content-Type": "application/json"},
+        json={
+            "username": "gri",
+            "password": "123456",
+        }
+    )
+    assert response.status_code < 400
+    response_token = jmespath.search("token", response.json())
+
+    class Clazz:
+        token = response_token
+        jwt_headers = _jwt_headers(response_token)
+
+    return Clazz
+"""
+
+fixture_your_name_content = """#!/usr/bin/python
+# encoding=utf-8
+
+\"\"\" Please define your own fixture.
+\"\"\"
+
+from accele.fixture import *
+
+
+@pytest.fixture(scope="session")
+def env_vars_your_name(config):
+    class Clazz:
+        env = config["env"]
+
+        # Environment and variables
+        mapping = {
+            "qa": {
+                "your_var": "123",
+            },
+            "release": {
+                "your_var": "456",
+            }
+            # Add your environment and variables
+        }
+        # Define properties for auto display
+        your_var = mapping[env]["your_var"]
+
+    return Clazz()
+
+
+@pytest.fixture
+def share_your_name():
+    pass
+"""
+
+fixture_second_content = """#!/usr/bin/python
+# encoding=utf-8
+
+
+from accele.fixture import *
+
+
+@pytest.fixture
+def fixture_second():
+    logger.info("fixture_second")
+"""
+
+fixture_three_content = """#!/usr/bin/python
+# encoding=utf-8
+
+
+from accele.fixture import *
+
+
+@pytest.fixture
+def fixture_three():
+    logger.info("fixture_three")
+"""
+
+test_login_content = """from loguru import logger
+
+
+def test_login(login):
+    logger.info(login.token)
+"""
+
+test_post_content = """import jmespath
+from loguru import logger
+
+from accele.client import request
+
+
+def test_post(faker_ch, url, login):
+    # description
+    logger.info("test post")
+    # data
+    fake = faker_ch
+    # request
+    response = request(
+        "post",
+        url=url("/api/users"),
+        headers=login.jwt_headers,
+        json={
+            "name": fake.name()
+        }
+    )
+    # assert
+    assert response.status_code < 400
+    # extract
+    user_id = jmespath.search("id", response.json())
+"""
+
+test_mysql_content = """from loguru import logger
+from accele.dao import print_db_table
+
+
+def test_mysql(pd, env_vars):
+    data = pd.read_sql("select 1 from dual", env_vars.mysql_engine)
+    logger.info(print_db_table(data))
+"""
+
+test_request_content = """from accele.client import request
+
+request("get", url="", headers={}, json={})
+request("post", url="", headers={}, params={})
+request("put", url="", headers={}, json={})
+request("delete", url="", headers={})
+
+# upload excel
+file_name = ""
+file_path = ""
+request("post",
+        url="",
+        headers={},
+        files={
+            "file": (
+                file_name,
+                open(file_path, "rb"),
+                "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+            )
+        },
+        verify=False
+        )
+"""
+
+fastapi_mock_content = """#!/usr/bin/python
+# encoding=utf-8
+
+import uvicorn
+from fastapi import FastAPI, Request
+
+app = FastAPI()
+
+
+@app.post("/login")
+async def login(req: Request):
+    body = await req.json()
+    if body["username"] == "gri" and body["password"] == "123456":
+        return {"token": "de2e3ffu29"}
+    return ""
+
+
+@app.get("/searchSku")
+def search_sku(req: Request):
+    if req.headers.get("token") == "de2e3ffu29" and req.query_params.get("skuName") == "电子书":
+        return {"skuId": "222", "price": "2.3"}
+    return ""
+
+
+@app.post("/addCart")
+async def add_cart(req: Request):
+    body = await req.json()
+    if req.headers.get("token") == "de2e3ffu29" and body["skuId"] == "222":
+        return {"skuId": "222", "price": "2.3", "skuNum": "3", "totalPrice": "6.9"}
+    return ""
+
+
+@app.post("/order")
+async def order(req: Request):
+    body = await req.json()
+    if req.headers.get("token") == "de2e3ffu29" and body["skuId"] == "222":
+        return {"orderId": "333"}
+    return ""
+
+
+@app.post("/pay")
+async def pay(req: Request):
+    body = await req.json()
+    if req.headers.get("token") == "de2e3ffu29" and body["orderId"] == "333":
+        return {"success": "true"}
+    return ""
+
+
+if __name__ == '__main__':
+    uvicorn.run("fastapi_mock:app", host="127.0.0.1", port=5000)
+"""
+
+http_client_content = """#!/usr/bin/python
+# encoding=utf-8
+
+import decimal
+import json
+import time
+
+import allure
+from loguru import logger
+from accele import client
+from accele.client import TepResponse
+
+
+def request_monkey_patch(req, *args, **kwargs):
+    start = time.process_time()
+    desc = ""
+    if "desc" in kwargs:
+        desc = kwargs.get("desc")
+        kwargs.pop("desc")
+    response = req(*args, **kwargs)
+    end = time.process_time()
+    elapsed = str(decimal.Decimal("%.3f" % float(end - start))) + "s"
+    log4a = "{}\\n{}status:{}\\nresponse:{}\\nelapsed:{}"
+    try:
+        kv = ""
+        for k, v in kwargs.items():
+            # if not json, str()
+            try:
+                v = json.dumps(v, ensure_ascii=False)
+            except TypeError:
+                v = str(v)
+            kv += f"{k}:{v}\\n"
+        if args:
+            method = f'\\nmethod:"{args[0]}" '
+        else:
+            method = ""
+        request_response = log4a.format(method, kv, response.status_code, response.text, elapsed)
+        logger.info(request_response)
+        allure.attach(request_response, f'{desc} request & response', allure.attachment_type.TEXT)
+    except AttributeError:
+        logger.error("request failed")
+    except TypeError:
+        logger.warning(log4a)
+    return TepResponse(response)
+
+
+def request(method, url, **kwargs):
+    client.accele_request_monkey_patch = request_monkey_patch
+    return client.request(method, url, **kwargs)
+"""
+
+test_login_pay_content = """import jmespath
+from accele.client import request
+
+\"\"\"
+测试登录到下单流程，需要先运行utils/flask_mock_api.py
+\"\"\"
+
+
+def test(env_vars, login):
+    # 搜索商品
+    response = request(
+        "get",
+        url=env_vars.domain + "/searchSku",
+        headers={"token": login.token},
+        params={"skuName": "电子书"}
+    )
+    sku_id = jmespath.search("skuId", response.json())
+    sku_price = jmespath.search("price", response.json())
+    assert response.status_code < 400
+
+    # 添加购物车
+    sku_num = 3
+    response = request(
+        "post",
+        url=env_vars.domain + "/addCart",
+        headers={"token": login.token},
+        json={"skuId": sku_id, "skuNum": str(sku_num)}
+    )
+    total_price = jmespath.search("totalPrice", response.json())
+    assert response.status_code < 400
+
+    # 下单
+    response = request(
+        "post",
+        url=env_vars.domain + "/order",
+        headers={"token": login.token},
+        json={"skuId": sku_id, "price": sku_price, "skuNum": str(sku_num), "totalPrice": total_price}
+    )
+    order_id = jmespath.search("orderId", response.json())
+    assert response.status_code < 400
+
+    # 支付
+    response = request(
+        "post",
+        url=env_vars.domain + "/pay",
+        headers={"token": login.token},
+        json={"orderId": order_id, "payAmount": "6.9"}
+    )
+    assert response.status_code < 400
+    assert response.json()["success"] == "true"
+"""
+
+test_login_pay_httprunner_content = """from httprunner import HttpRunner, Config, Saccele, RunRequest
+
+\"\"\"
+测试登录到下单流程，需要先运行utils/flask_mock_api.py
+\"\"\"
+
+
+class TestLoginPay(HttpRunner):
+    config = (
+        Config("登录到下单流程")
+            .variables(
+            **{
+                "skuNum": "3"
+            }
+        )
+            .base_url("http://127.0.0.1:5000")
+    )
+
+    testsacceles = [
+        Saccele(
+            RunRequest("登录")
+                .post("/login")
+                .with_headers(**{"Content-Type": "application/json"})
+                .with_json({"username": "gri", "password": "123456"})
+                .extract()
+                .with_jmespath("body.token", "token")
+                .validate()
+                .assert_equal("status_code", 200)
+        ),
+        Saccele(
+            RunRequest("搜索商品")
+                .get("searchSku?skuName=电子书")
+                .with_headers(**{"token": "$token"})
+                .extract()
+                .with_jmespath("body.skuId", "skuId")
+                .with_jmespath("body.price", "skuPrice")
+                .validate()
+                .assert_equal("status_code", 200)
+        ),
+        Saccele(
+            RunRequest("添加购物车")
+                .post("/addCart")
+                .with_headers(**{"Content-Type": "application/json",
+                                 "token": "$token"})
+                .with_json({"skuId": "$skuId", "skuNum": "$skuNum"})
+                .extract()
+                .with_jmespath("body.totalPrice", "totalPrice")
+                .validate()
+                .assert_equal("status_code", 200)
+        ),
+        Saccele(
+            RunRequest("下单")
+                .post("/order")
+                .with_headers(**{"Content-Type": "application/json",
+                                 "token": "$token"})
+                .with_json({"skuId": "$skuId", "price": "$skuPrice", "skuNum": "$skuNum", "totalPrice": "$totalPrice"})
+                .extract()
+                .with_jmespath("body.orderId", "orderId")
+                .validate()
+                .assert_equal("status_code", 200)
+        ),
+        Saccele(
+            RunRequest("支付")
+                .post("/pay")
+                .with_headers(**{"Content-Type": "application/json",
+                                 "token": "$token"})
+                .with_json({"orderId": "$orderId", "payAmount": "6.9"})
+                .validate()
+                .assert_equal("status_code", 200)
+                .assert_equal("body.success", "true")
+        ),
+    ]
+"""
+
+Login_content = """from accele.client import BaseRequest
+
+
+class Login(BaseRequest):
+
+    def post(self):
+        response = self.request(
+            "post",
+            url=self.case_vars.get("domain") + "/login",
+            headers={"Content-Type": "application/json"},
+            json={
+                "username": "gri",
+                "password": "123456",
+            }
+        )
+        assert response.status_code < 400
+        self.case_vars.put("token", response.jmespath("token"))
+"""
+
+SearchSku_content = """from accele.client import BaseRequest
+
+
+class SearchSku(BaseRequest):
+
+    def get(self):
+        response = self.request(
+            "get",
+            url=self.case_vars.get("domain") + "/searchSku",
+            headers={"token": self.case_vars.get("token")},
+            params={"skuName": "电子书"}
+        )
+        self.case_vars.put("skuId", response.jmespath("skuId"))
+        self.case_vars.put("skuPrice", response.jmespath("price"))
+        assert response.status_code < 400
+"""
+
+AddCart_content = """from accele.client import BaseRequest
+
+
+class AddCart(BaseRequest):
+
+    def post(self):
+        response = self.request(
+            "post",
+            url=self.case_vars.get("domain") + "/addCart",
+            headers={"token": self.case_vars.get("token")},
+            json={"skuId": self.case_vars.get("skuId"), "skuNum": self.case_vars.get("skuNum")}
+        )
+        self.case_vars.put("totalPrice", response.jmespath("totalPrice"))
+        assert response.status_code < 400
+"""
+
+Order_content = """from accele.client import BaseRequest
+
+
+class Order(BaseRequest):
+
+    def post(self):
+        response = self.request(
+            "post",
+            url=self.case_vars.get("domain") + "/order",
+            headers={"token": self.case_vars.get("token")},
+            json={"skuId": self.case_vars.get("skuId"), "price": self.case_vars.get("skuPrice"),
+                  "skuNum": self.case_vars.get("skuNum"), "totalPrice": self.case_vars.get("totalPrice")}
+        )
+        self.case_vars.put("orderId", response.jmespath("orderId"))
+        assert response.status_code < 400
+"""
+
+Pay_content = """from accele.client import BaseRequest
+
+
+class Pay(BaseRequest):
+
+    def post(self):
+        response = self.request(
+            "post",
+            url=self.case_vars.get("domain") + "/pay",
+            headers={"token": self.case_vars.get("token")},
+            json={"orderId": self.case_vars.get("orderId"), "payAmount": "6.9"}
+        )
+        assert response.status_code < 400
+        assert response.jmespath("success") == "true"
+"""
+
+test_login_pay_mvc_content = """from accele.fixture import TepVars
+
+from services.AddCart import AddCart
+from services.Login import Login
+from services.Order import Order
+from services.Pay import Pay
+from services.SearchSku import SearchSku
+
+\"\"\"
+测试登录到下单流程，需要先运行utils / flask_mock_api.py
+\"\"\"
+
+
+class Test:
+    case_vars = TepVars()
+    case_vars.vars_ = {
+        "domain": "http://127.0.0.1:5000",
+        "skuNum": "3"
+    }
+
+    def test(self):
+        # 登录
+        Login(Test).post()
+        # 搜索商品
+        SearchSku(Test).get()
+        # 添加购物车
+        AddCart(Test).post()
+        # 下单
+        Order(Test).post()
+        # 支付
+        Pay(Test).post()
+"""
+
+test_multi_fixture_content = """#!/usr/bin/python
+# encoding=utf-8
+
+
+def test(fixture_second, fixture_three):
+    pass
+"""
+
+test_request_monkey_patch_content = """from utils.http_client import request
+
+
+def test_login(env_vars):
+    response = request(
+        "post",
+        url=env_vars.domain + "/login",
+        desc="登录",
+        headers={"Content-Type": "application/json"},
+        json={
+            "username": "gri",
+            "password": "123456",
+        }
+    )
+    assert response.status_code < 400
+"""
---
+++ packages/accele/accele-0.0.2-py3-none-any.whl/base/scaffold.py
@@ -0,0 +1,129 @@
+#!/usr/bin/python
+# encoding=utf-8
+
+"""
+@Author  :  gri
+@Date    :  7/23/2020 8:12 PM
+@Desc    :
+"""
+
+import os
+import platform
+import sys
+
+from loguru import logger
+
+from accele.sample import *
+
+
+class ExtraArgument:
+    create_venv = False
+
+
+def init_parser_scaffold(subparsers):
+    sub_parser_scaffold = subparsers.add_parser(
+        "startproject", help="Create a new project with template structure."
+    )
+    sub_parser_scaffold.add_argument(
+        "project_name", type=str, nargs="?", help="Specify new project name."
+    )
+    sub_parser_scaffold.add_argument(
+        "-venv",
+        dest="create_venv",
+        action="store_true",
+        help="Create virtual environment in the project, and install accele.",
+    )
+    return sub_parser_scaffold
+
+
+def create_scaffold(project_name):
+    """ Create scaffold with specified project name.
+    """
+    if os.path.isdir(project_name):
+        logger.warning(
+            f"Project folder {project_name} exists, please specify a new project name."
+        )
+        return 1
+    elif os.path.isfile(project_name):
+        logger.warning(
+            f"Project name {project_name} conflicts with existed file, please specify a new one."
+        )
+        return 1
+
+    logger.info(f"Create new project: {project_name}")
+    print(f"Project root dir: {os.path.join(os.getcwd(), project_name)}\n")
+
+    def create_folder(path):
+        os.makedirs(path)
+        msg = f"Created folder: {path}"
+        print(msg)
+
+    def create_file(path, file_content=""):
+        with open(path, "w", encoding="utf-8") as f:
+            f.write(file_content)
+        msg = f"Created file: {path}"
+        print(msg)
+
+    create_folder(project_name)
+    create_folder(os.path.join(project_name, "fixtures"))
+    create_folder(os.path.join(project_name, "fixtures", "second"))
+    create_folder(os.path.join(project_name, "fixtures", "second", "three"))
+    create_folder(os.path.join(project_name, "tests"))
+    create_folder(os.path.join(project_name, "files"))
+    create_folder(os.path.join(project_name, "reports"))
+    create_folder(os.path.join(project_name, "utils"))
+    create_folder(os.path.join(project_name, "services"))
+
+    create_file(os.path.join(project_name, ".gitignore"), gitignore_content)
+    create_file(os.path.join(project_name, "conf.yaml"), conf_yaml_content)
+    create_file(os.path.join(project_name, "conftest.py"), conftest_content)
+    create_file(os.path.join(project_name, "pytest.ini"), pytest_ini_content)
+    create_file(os.path.join(project_name, "fixtures", "__init__.py"))
+    create_file(os.path.join(project_name, "fixtures", "fixture_admin.py"), fixture_admin_content)
+    create_file(os.path.join(project_name, "fixtures", "fixture_env_vars.py"), fixture_env_vars_content)
+    create_file(os.path.join(project_name, "fixtures", "fixture_login.py"), fixture_login_content)
+    create_file(os.path.join(project_name, "fixtures", "fixture_your_name.py"), fixture_your_name_content)
+    create_file(os.path.join(project_name, "fixtures", "second", "__init__.py"))
+    create_file(os.path.join(project_name, "fixtures", "second", "fixture_second.py"), fixture_second_content)
+    create_file(os.path.join(project_name, "fixtures", "second", "three", "__init__.py"))
+    create_file(os.path.join(project_name, "fixtures", "second", "three", "fixture_three.py"), fixture_three_content)
+    create_file(os.path.join(project_name, "tests", "__init__.py"))
+    create_file(os.path.join(project_name, "tests", "test_login.py"), test_login_content)
+    create_file(os.path.join(project_name, "tests", "test_post.py"), test_post_content)
+    create_file(os.path.join(project_name, "tests", "test_mysql.py"), test_mysql_content)
+    create_file(os.path.join(project_name, "tests", "test_request.py"), test_request_content)
+    create_file(os.path.join(project_name, "tests", "test_login_pay.py"), test_login_pay_content)
+    create_file(os.path.join(project_name, "tests", "test_login_pay_httprunner.py"), test_login_pay_httprunner_content)
+    create_file(os.path.join(project_name, "tests", "test_login_pay_mvc.py"), test_login_pay_mvc_content)
+    create_file(os.path.join(project_name, "tests", "test_multi_fixture.py"), test_multi_fixture_content)
+    create_file(os.path.join(project_name, "tests", "test_request_monkey_patch.py"), test_request_monkey_patch_content)
+    create_file(os.path.join(project_name, "utils", "__init__.py"))
+    create_file(os.path.join(project_name, "utils", "fastapi_mock.py"), fastapi_mock_content)
+    create_file(os.path.join(project_name, "utils", "http_client.py"), http_client_content)
+    create_file(os.path.join(project_name, "services", "__init__.py"))
+    create_file(os.path.join(project_name, "services", "Login.py"), Login_content)
+    create_file(os.path.join(project_name, "services", "SearchSku.py"), SearchSku_content)
+    create_file(os.path.join(project_name, "services", "AddCart.py"), AddCart_content)
+    create_file(os.path.join(project_name, "services", "Order.py"), Order_content)
+    create_file(os.path.join(project_name, "services", "Pay.py"), Pay_content)
+
+    if ExtraArgument.create_venv:
+        os.chdir(project_name)
+        print("\nCreating virtual environment")
+        os.system("python -m venv .venv")
+        print("Created virtual environment: .venv")
+
+        print("Installing accele")
+        if platform.system().lower() == 'windows':
+            os.chdir(".venv")
+            os.chdir("Scripts")
+            os.system("pip install accele")
+        elif platform.system().lower() == 'linux':
+            os.chdir(".venv")
+            os.chdir("bin")
+            os.system("pip install accele")
+
+
+def main_scaffold(args):
+    ExtraArgument.create_venv = args.create_venv
+    sys.exit(create_scaffold(args.project_name))
