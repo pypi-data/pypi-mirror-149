ba137711a8d52932a8fb357e733f3c52ee65afdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:00 +0000

    Add package cg/22.56.2/cg-22.56.2-py3-none-any.whl

---
+++ packages/cg/cg-22.56.2-py3-none-any.whl/cg/cli/backup.py
@@ -0,0 +1,58 @@
+import logging
+from typing import Optional
+
+import click
+from cg.apps.pdc import PdcApi
+from cg.meta.backup import BackupApi
+from cg.models.cg_config import CGConfig
+from cg.store import Store, models
+
+LOG = logging.getLogger(__name__)
+MAX_FLOWCELLS_ON_DISK = 125000
+
+
+@click.group()
+@click.pass_obj
+def backup(context: CGConfig):
+    """Backup utilities."""
+    pdc_api = PdcApi()
+    context.meta_apis["backup_api"] = BackupApi(
+        status=context.status_db,
+        pdc_api=pdc_api,
+        max_flowcells_on_disk=context.max_flowcells or MAX_FLOWCELLS_ON_DISK,
+        root_dir=context.backup.root.dict(),
+    )
+
+
+@backup.command("fetch-flowcell")
+@click.option("-f", "--flowcell", help="Retrieve a specific flowcell")
+@click.option("--dry-run", is_flag=True, help="Don't retrieve from PDC or set flowcell's status")
+@click.pass_obj
+def fetch_flowcell(context: CGConfig, dry_run: bool, flowcell: str):
+    """Fetch the first flowcell in the requested queue from backup."""
+    status_api: Store = context.status_db
+    backup_api: BackupApi = context.meta_apis["backup_api"]
+
+    flowcell_obj: Optional[models.Flowcell] = None
+    if flowcell:
+        flowcell_obj: Optional[models.Flowcell] = status_api.flowcell(flowcell)
+        if flowcell_obj is None:
+            LOG.error(f"{flowcell}: not found in database")
+            raise click.Abort
+
+    retrieval_time: Optional[float] = backup_api.fetch_flowcell(
+        flowcell_obj=flowcell_obj, dry_run=dry_run
+    )
+
+    if retrieval_time:
+        hours = retrieval_time / 60 / 60
+        LOG.info(f"Retrieval time: {hours:.1}h")
+        return
+
+    if not flowcell:
+        return
+
+    if not dry_run:
+        LOG.info(f"{flowcell}: updating flowcell status to requested")
+        flowcell_obj.status = "requested"
+        status_api.commit()
---
+++ packages/cg/cg-22.56.2-py3-none-any.whl/cg/cli/compress/base.py
@@ -0,0 +1,64 @@
+"""Commands for compressing files, and cleaning uncompressed files"""
+import logging
+
+import click
+from cg.meta.compress import CompressAPI
+from cg.models.cg_config import CGConfig
+
+from .fastq import (
+    clean_fastq,
+    decompress_case,
+    decompress_flowcell,
+    decompress_sample,
+    decompress_ticket,
+    fastq_cmd,
+    fix_spring,
+)
+
+LOG = logging.getLogger(__name__)
+
+
+@click.group()
+@click.pass_obj
+def compress(context: CGConfig):
+    """Compress files"""
+
+    hk_api = context.housekeeper_api
+    crunchy_api = context.crunchy_api
+
+    compress_api = CompressAPI(
+        hk_api=hk_api, crunchy_api=crunchy_api, demux_root=context.demultiplex.out_dir
+    )
+    context.meta_apis["compress_api"] = compress_api
+
+
+compress.add_command(fastq_cmd)
+
+
+@compress.group()
+def clean():
+    """Clean uncompressed files"""
+
+
+clean.add_command(clean_fastq)
+clean.add_command(fix_spring)
+
+
+@click.group()
+@click.pass_obj
+def decompress(context: CGConfig):
+    """Decompress files"""
+    hk_api = context.housekeeper_api
+    crunchy_api = context.crunchy_api
+
+    compress_api = CompressAPI(
+        hk_api=hk_api, crunchy_api=crunchy_api, demux_root=context.demultiplex.out_dir
+    )
+    context.meta_apis["compress_api"] = compress_api
+    LOG.info("Running decompress spring")
+
+
+decompress.add_command(decompress_sample)
+decompress.add_command(decompress_case)
+decompress.add_command(decompress_ticket)
+decompress.add_command(decompress_flowcell)
