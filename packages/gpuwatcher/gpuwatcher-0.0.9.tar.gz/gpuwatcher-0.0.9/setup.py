5e2b19f40333f915378344d62e3450754a8ebdc0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:54 +0000

    Add package gpuwatcher/0.0.9/gpuwatcher-0.0.9.tar.gz

---
+++ packages/gpuwatcher/gpuwatcher-0.0.9.tar.gz/gpuwatcher-0.0.9/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.9'
+
+setup(name='gpuwatcher',
+      version=VERSION,
+      description="Monitor the memory usage and notify",
+      long_description='Monitor the memory usage and notify',
+      classifiers=[],  # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords=('gpu', 'watcher','notify', 'webhook', 'feishu'),
+      author='Kun song',
+      author_email='songkun666@outlook.com',
+      license='MIT',
+      packages=find_packages("","psutil", "nvidia-ml-py3"),
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[],
+      entry_points={
+          'console_scripts': [
+              'gpuwatcher = gpuwatcher.gpuwatcher:main'
+          ]}
+      )
