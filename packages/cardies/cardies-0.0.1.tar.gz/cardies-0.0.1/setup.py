776dac9aaefbd21bac49b9483d4e6abd2ce36d7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:18:35 +0000

    Add package cardies/0.0.1/cardies-0.0.1.tar.gz

---
+++ packages/cardies/cardies-0.0.1.tar.gz/cardies-0.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+from setuptools.dist import Distribution
+
+class BinaryDistribution(Distribution):
+    def is_pure(self):
+        return False
+
+		
+setup(name='cardies',
+      version='0.0.1',
+      url='https://github.com/iosonobert/cardies/',
+      description='The Cardinals',
+      author='Roberto Bertolucci',
+      author_email='roberto.bertolucci@gmail.com',
+      packages=find_packages(),
+      install_requires=['numpy','matplotlib', 'xarray', 'netCDF4', 'Proj'],
+      license='unlicensed to all but author',
+      include_package_data=True,
+      distclass=BinaryDistribution,
+    )
