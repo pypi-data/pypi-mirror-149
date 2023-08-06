4fb7bbd48e51f678b518304cf186f71171073a39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:37 +0000

    Add package onelinerise/1.0.0/onelinerise-1.0.0-py3-none-any.whl

---
+++ packages/onelinerise/onelinerise-1.0.0-py3-none-any.whl/onelinerise/__init__.py
@@ -0,0 +1,13 @@
+"""
+Weird Oneliner mini-lang
+~~~~~~~~~~~~~~~~~~~~~~~~
+Don't even think of using this for actual implementations
+:license: Unlicense License, see LICENSE for more details
+"""
+
+__title__ = "OneLinerise"
+__author__ = "Jus-Codin"
+__license__ = "Unlicense"
+__version__ = "1.0.0"
+
+from .onelineriser import OneLinerise
