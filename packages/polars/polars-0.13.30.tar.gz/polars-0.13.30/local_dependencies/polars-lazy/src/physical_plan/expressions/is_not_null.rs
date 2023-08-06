40741a2dd3b34adc13087a6a6b72c00753529b58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:24 +0000

    Add package polars/0.13.30/polars-0.13.30.tar.gz

---
+++ packages/polars/polars-0.13.30.tar.gz/polars-0.13.30/local_dependencies/polars-lazy/src/physical_plan/expressions/is_not_null.rs
@@ -0,0 +1,47 @@
+use crate::physical_plan::state::ExecutionState;
+use crate::prelude::*;
+use polars_core::frame::groupby::GroupsProxy;
+use polars_core::prelude::*;
+use std::sync::Arc;
+
+pub struct IsNotNullExpr {
+    physical_expr: Arc<dyn PhysicalExpr>,
+    expr: Expr,
+}
+
+impl IsNotNullExpr {
+    pub fn new(physical_expr: Arc<dyn PhysicalExpr>, expr: Expr) -> Self {
+        Self {
+            physical_expr,
+            expr,
+        }
+    }
+}
+
+impl PhysicalExpr for IsNotNullExpr {
+    fn as_expression(&self) -> &Expr {
+        &self.expr
+    }
+
+    fn evaluate(&self, df: &DataFrame, state: &ExecutionState) -> Result<Series> {
+        let series = self.physical_expr.evaluate(df, state)?;
+        Ok(series.is_not_null().into_series())
+    }
+    #[allow(clippy::ptr_arg)]
+    fn evaluate_on_groups<'a>(
+        &self,
+        df: &DataFrame,
+        groups: &'a GroupsProxy,
+        state: &ExecutionState,
+    ) -> Result<AggregationContext<'a>> {
+        let mut ac = self.physical_expr.evaluate_on_groups(df, groups, state)?;
+        let s = ac.flat_naive();
+        let s = s.is_not_null().into_series();
+        ac.with_series(s, false);
+
+        Ok(ac)
+    }
+    fn to_field(&self, _input_schema: &Schema) -> Result<Field> {
+        Ok(Field::new("is_not_null", DataType::Boolean))
+    }
+}
