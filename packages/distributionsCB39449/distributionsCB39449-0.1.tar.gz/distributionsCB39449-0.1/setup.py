98834f7af179fe5fd12a12798db1af930c4b7ae1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:30:37 +0000

    Add package distributionsCB39449/0.1/distributionsCB39449-0.1.tar.gz

---
+++ packages/distributionsCB39449/distributionsCB39449-0.1.tar.gz/distributionsCB39449-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributionsCB39449',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['distributionsCB39449'],
+      author = 'Michael J Swickrath',
+      author_email = 'michael.swickrath@gmail.com',
+      zip_safe=False)
