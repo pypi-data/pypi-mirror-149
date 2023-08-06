dbb2f53e317a9624f8ef5a20d3a154920d22b48d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:43:59 +0000

    Add package sensitive-test/1.0.0/sensitive_test-1.0.0.tar.gz

---
+++ packages/sensitive-test/sensitive_test-1.0.0.tar.gz/sensitive_test-1.0.0/README.md
@@ -0,0 +1,25 @@
+# tests
+
+```
+pip install sensitive_test
+```
+
+A small, simple, and fast ordered fail-fast tests suite for Python.
+
+```py
+import sensitive
+
+# any Exception thrown will make the test running it fail,
+# and the program will immediately return exit code 1.
+
+with sensitive.test("test name 1"):
+  print("Oh yes")
+
+with sensitive.test("test name 2"):
+  test.failfast()
+  
+  raise Exception("Trolled")
+
+with sensitive.test("test name 3"):
+  print("oh no")
+```
---
+++ packages/sensitive-test/sensitive_test-1.0.0.tar.gz/sensitive_test-1.0.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+  name='sensitive_test',
+  packages=['sensitive'],
+  version='1.0.0',
+  license='MIT',
+  description='A small, simple, and fast ordered failfast test-suite for Python.',
+  author='vierofernando',
+  long_description=open('README.md', 'r', encoding='utf-8').read(),
+  long_description_content_type='text/markdown',
+  author_email='vierofernando9@gmail.com',
+  keywords=['test', 'tests', 'ci', 'debug'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9'
+  ],
+  python_requires='>=3.6',
+)
