a1cc4fa1ad580e22e9ad01b0762104117f517783
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:20 +0000

    Add package fhirgenerator/0.0.10/fhirgenerator-0.0.10-py3-none-any.whl

---
+++ packages/fhirgenerator/fhirgenerator-0.0.10-py3-none-any.whl/fhirgenerator/resources/r4/patient.py
@@ -0,0 +1,46 @@
+'''File for handling all operations relating to the Patient resource'''
+
+import uuid
+import random
+import datetime
+from dateutil import parser
+from faker import Faker
+from fhir.resources.patient import Patient
+
+from fhirgenerator.types.humanName import generateName
+from fhirgenerator.types.address import generateAddress
+from fhirgenerator.types.identifier import generateMRNIdentifier
+
+gender_map = {
+    'M': 'male',
+    'F': 'female',
+    'O': 'other',
+    'U': 'unknown'
+}
+
+
+def generateBirthDateFromAge(age: int, startDate: str) -> str:
+    '''Generate a random birthday that matches the given age'''
+    start = parser.parse(startDate)
+    random_number_of_days = random.randrange(365)
+    birthdate = datetime.date(start.year - age - 1, start.month, start.day) + datetime.timedelta(days=random_number_of_days)
+    return str(birthdate)
+
+
+def generatePatient(configuration: dict) -> dict:
+    '''Generate Patient Resource'''
+
+    fake = Faker()
+
+    patient_id = str(uuid.uuid4())
+    patient_data = {
+        'id': patient_id,
+        'identifier': [generateMRNIdentifier()],
+        'name': [generateName(gender=configuration['gender'])],
+        'address': [generateAddress()],
+        'telecom': [{'system': 'phone', 'use': 'home', 'value': fake.phone_number()}],
+        'gender': gender_map[configuration['gender'].upper()],
+        'birthDate': generateBirthDateFromAge(configuration['age'], configuration['startDate'])
+    }
+    patient_resource = Patient(**patient_data).dict()
+    return patient_resource
