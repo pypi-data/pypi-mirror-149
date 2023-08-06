c7c372c5a8bd0ea498fb4cfe0938c62282604a91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:28 +0000

    Add package xonsh/0.12.3/xonsh-0.12.3.tar.gz

---
+++ packages/xonsh/xonsh-0.12.3.tar.gz/xonsh-0.12.3/tests/test_base_shell.py
@@ -0,0 +1,38 @@
+"""(A down payment on) Testing for ``xonsh.base_shell.BaseShell`` and associated classes"""
+import os
+
+from xonsh.base_shell import BaseShell
+from xonsh.shell import transform_command
+
+
+def test_pwd_tracks_cwd(xession, xonsh_execer, tmpdir_factory, monkeypatch):
+    asubdir = str(tmpdir_factory.mktemp("asubdir"))
+    cur_wd = os.getcwd()
+    xession.env.update(
+        dict(PWD=cur_wd, XONSH_CACHE_SCRIPTS=False, XONSH_CACHE_EVERYTHING=False)
+    )
+
+    monkeypatch.setattr(xonsh_execer, "cacheall", False, raising=False)
+    bc = BaseShell(xonsh_execer, None)
+
+    assert os.getcwd() == cur_wd
+
+    bc.default('os.chdir(r"' + asubdir + '")')
+
+    assert os.path.abspath(os.getcwd()) == os.path.abspath(asubdir)
+    assert os.path.abspath(os.getcwd()) == os.path.abspath(xession.env["PWD"])
+    assert "OLDPWD" in xession.env
+    assert os.path.abspath(cur_wd) == os.path.abspath(xession.env["OLDPWD"])
+
+
+def test_transform(xession):
+    @xession.builtins.events.on_transform_command
+    def spam2egg(cmd, **_):
+        if cmd == "spam":
+            return "egg"
+        else:
+            return cmd
+
+    assert transform_command("spam") == "egg"
+    assert transform_command("egg") == "egg"
+    assert transform_command("foo") == "foo"
