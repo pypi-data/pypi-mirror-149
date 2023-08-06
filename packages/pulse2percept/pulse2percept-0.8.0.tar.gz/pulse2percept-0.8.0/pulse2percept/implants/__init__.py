74cf5b1b0f1d3ffc70e679343aed3e137631591b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:44:26 +0000

    Add package pulse2percept/0.8.0/pulse2percept-0.8.0.tar.gz

---
+++ packages/pulse2percept/pulse2percept-0.8.0.tar.gz/pulse2percept-0.8.0/pulse2percept/implants/__init__.py
@@ -0,0 +1,48 @@
+"""
+Different retinal prosthetic implants, such as Argus II, Alpha-IMS, BVT-24,
+and PRIMA.
+
+.. autosummary::
+    : toctree: _api
+
+    base
+    electrodes
+    electrode_arrays
+    argus
+    alpha
+    bvt
+    prima
+
+.. seealso::
+
+    *  :ref:`Basic Concepts > Visual Prostheses <topics-implants>`
+"""
+from .base import ProsthesisSystem
+from .electrodes import (Electrode, PointSource, DiskElectrode,
+                         SquareElectrode, HexElectrode)
+from .electrode_arrays import ElectrodeArray, ElectrodeGrid
+from .argus import ArgusI, ArgusII
+from .alpha import AlphaIMS, AlphaAMS
+from .bvt import BVT24, BVT44
+from .prima import PhotovoltaicPixel, PRIMA, PRIMA75, PRIMA55, PRIMA40
+
+__all__ = [
+    'AlphaAMS',
+    'AlphaIMS',
+    'ArgusI',
+    'ArgusII',
+    'BVT24',
+    'DiskElectrode',
+    'Electrode',
+    'ElectrodeArray',
+    'ElectrodeGrid',
+    'HexElectrode',
+    'PhotovoltaicPixel',
+    'PointSource',
+    'PRIMA',
+    'PRIMA75',
+    'PRIMA55',
+    'PRIMA40',
+    'ProsthesisSystem',
+    'SquareElectrode'
+]
