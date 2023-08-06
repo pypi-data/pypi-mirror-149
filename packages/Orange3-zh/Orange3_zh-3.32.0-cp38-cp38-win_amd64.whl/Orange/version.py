4a2cb49725c979ea2327fe2d98a3a889597ecc10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:12 +0000

    Add package Orange3-zh/3.32.0/Orange3_zh-3.32.0-cp38-cp38-win_amd64.whl

---
+++ packages/Orange3-zh/Orange3_zh-3.32.0-cp38-cp38-win_amd64.whl/Orange/version.py
@@ -0,0 +1,11 @@
+
+# THIS FILE IS GENERATED FROM ORANGE SETUP.PY
+short_version = '3.32.0'
+version = '3.32.0'
+full_version = '3.32.0'
+git_revision = 'aa6f8cf2c50567c14ff53c3c067aa23787c3e975'
+release = True
+
+if not release:
+    version = full_version
+    short_version += ".dev"
