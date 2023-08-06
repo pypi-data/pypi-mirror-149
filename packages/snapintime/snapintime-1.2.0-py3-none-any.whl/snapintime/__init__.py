3d6e89159c334a82d2f3514f86fffefbc1bc7fa6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:37 +0000

    Add package snapintime/1.2.0/snapintime-1.2.0-py3-none-any.whl

---
+++ packages/snapintime/snapintime-1.2.0-py3-none-any.whl/snapintime/__init__.py
@@ -0,0 +1,16 @@
+__author__ = "Eric Mesa"
+__version__ = "1.2.0"
+__license__ = "GNU GPL v3.0"
+__copyright__: str = "(c) 2014 - 2022 Eric Mesa"
+__email__: str = "ericsbinaryworld at gmail dot com"
+
+
+import logging
+from rich.logging import RichHandler
+
+FORMAT = "%(message)s"
+logging.basicConfig(
+    level="INFO", format=FORMAT, datefmt="[%X]", handlers=[RichHandler()]
+)
+
+log = logging.getLogger("rich")
