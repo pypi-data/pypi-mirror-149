b6df2c3d8a1d58f298e54a662178f7f41201616d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:19 +0000

    Add package mtg-parser/0.0.1a20/mtg_parser-0.0.1a20-py3-none-any.whl

---
+++ packages/mtg-parser/mtg_parser-0.0.1a20-py3-none-any.whl/mtg_parser/parser.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import mtg_parser.aetherhub
+import mtg_parser.archidekt
+import mtg_parser.deckstats
+import mtg_parser.moxfield
+import mtg_parser.mtggoldfish
+import mtg_parser.tappedout
+import mtg_parser.tcgplayer
+import mtg_parser.decklist
+
+
+__all__ = [
+    'parse_deck',
+]
+
+
+def parse_deck(src):
+    parsers = [
+        mtg_parser.aetherhub,
+        mtg_parser.archidekt,
+        mtg_parser.deckstats,
+        mtg_parser.moxfield,
+        mtg_parser.mtggoldfish,
+        mtg_parser.tappedout,
+        mtg_parser.tcgplayer,
+        mtg_parser.decklist,
+    ]
+    for parser in parsers:
+        if parser.can_handle(src):
+            deck = parser.parse_deck(src)
+            if deck:
+                return deck
+    return None
