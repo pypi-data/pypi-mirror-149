2b167c78463f6fcd46d3009de86483b51a5c1c48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:36 +0000

    Add package feng-libs/0.2/feng_libs-0.2.tar.gz

---
+++ packages/feng-libs/feng_libs-0.2.tar.gz/feng_libs-0.2/README.md
@@ -0,0 +1,7 @@
+# 简介
+自己造的轮子，一些工具类。
+
+# 常用功能
+
+* 合并pdf文件
+> python feng_libs/feng_libs.py pdf_libs merge C:\Users\User\Downloads\pdf merge.pdf
---
+++ packages/feng-libs/feng_libs-0.2.tar.gz/feng_libs-0.2/setup.py
@@ -0,0 +1,37 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="feng_libs",
+    requires=[
+    ],
+    version=0.2,
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=[
+        "fire",
+        "PyPDF2",
+        "opencv-python"
+    ],
+    scripts=[],
+    author="jim",
+    author_email="zhengwenfeng37@gmail.com",
+    url="https://github.com/tenqaz/feng_libs",
+    license="jims Licence",
+    description="常用工具库",
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Natural Language :: Chinese (Simplified)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Utilities'
+    ],
+    entry_points={
+        'console_scripts': [
+            'feng_libs=feng_libs.__main__:main'
+        ]
+    }
+)
