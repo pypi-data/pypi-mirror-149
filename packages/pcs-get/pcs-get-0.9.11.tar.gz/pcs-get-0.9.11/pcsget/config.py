406f473853815e1cc48a3a192b04b8d46eac4a8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:18 +0000

    Add package pcs-get/0.9.11/pcs-get-0.9.11.tar.gz

---
+++ packages/pcs-get/pcs-get-0.9.11.tar.gz/pcs-get-0.9.11/pcsget/config.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+import json
+import os
+
+import log
+import utils
+
+CONFIG_FILE = "pcsget.cfg"
+K_USER = "user"
+K_PW = "password"
+K_LANG = "lang"
+K_SESSION = "session"
+K_TOKEN = "token"
+K_DEBUG = "debug"
+SUPPORTED_KEY = [K_USER, K_PW, K_LANG, K_SESSION, K_TOKEN, K_DEBUG]
+
+
+def setup_config(key, value):
+    """
+    :type key: str
+    :type value: str
+    """
+    if len(value) == 0:
+        log.e("--set with empty-str param is illegal!")
+        return
+    if key not in SUPPORTED_KEY:
+        log.e("--set for %s is not supported!" % key)
+        return
+    if key == K_LANG and (value != "java" and value != "kotlin"):
+        log.e("%s is illegal lang now! [java, kotlin]" % value)
+        return
+    d = {key: value}
+    update_config(**d)
+
+
+def read_config():
+    # 读取配置（用户目录下脚本目录）
+    try:
+        with open(_get_config_file_path(), 'r') as fp:
+            return json.load(fp)
+    except IOError as e:
+        return {K_USER: '', K_PW: '', K_SESSION: '', K_LANG: 'java', K_DEBUG: False}
+
+
+def is_debug_mode():
+    cfg = read_config()
+    return K_DEBUG in cfg and cfg[K_DEBUG]
+
+
+def update_config(**kw):
+    """
+    保存配置项
+    """
+    actual_updated = False
+    curr_cfg_json = read_config()
+    if K_USER in kw:
+        curr_cfg_json[K_USER] = kw[K_USER]
+        actual_updated = True
+    if K_PW in kw:
+        pw = kw[K_PW]
+        encrypted_pw = utils.encrypt(pw)
+        curr_cfg_json[K_PW] = encrypted_pw
+        actual_updated = True
+    if K_LANG in kw:
+        curr_cfg_json[K_LANG] = kw[K_LANG]
+        actual_updated = True
+    if K_SESSION in kw:
+        curr_cfg_json[K_SESSION] = kw[K_SESSION]
+        actual_updated = True
+    if K_TOKEN in kw:
+        curr_cfg_json[K_TOKEN] = kw[K_TOKEN]
+        actual_updated = True
+    if K_DEBUG in kw:
+        curr_cfg_json[K_DEBUG] = kw[K_DEBUG]
+        actual_updated = True
+
+    if actual_updated:
+        n_cfg_json = json.dumps(curr_cfg_json)
+        with open(_get_config_file_path(), 'w') as fp:
+            fp.write(n_cfg_json)
+            log.i("config for %s was updated!" % str(kw.keys()))
+    else:
+        log.e("none config was updated!")
+
+
+def _get_config_file_path():
+    """
+    获取配置文件路径
+    :rtype str
+    """
+    user_home = os.path.expanduser("~")
+    return os.path.join(user_home, CONFIG_FILE)
---
+++ packages/pcs-get/pcs-get-0.9.11.tar.gz/pcs-get-0.9.11/pcsget/login.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+import re
+
+import requests
+from lxml import etree
+
+import config
+import log
+
+
+def login(session, user, pw):
+    """
+    :type session: requests.Session
+    :type user: str
+    :type pw: str
+    :rtype: str
+    """
+    log.d("login invoked. n:" + user + " p:" + pw)
+    redirected_login_page = session.get("http://release.bigo.local:8081/#/demand", allow_redirects=True)
+    log.d("login page url:" + redirected_login_page.url)
+    log.d("login page status_code:" + str(redirected_login_page.status_code))
+    if redirected_login_page.status_code != 200:
+        return False
+    hidden_field = etree.HTML(redirected_login_page.text).xpath("//*[@id='fm1']/input[1]/@value")[0]
+    log.d("login page hidden_field_val:" + str(hidden_field))
+    login_res = session.post(
+        url='https://auth.bigo.sg/cas/login?service=http://release.bigo.local:8081/accounts/login/?next=%2F',
+        data={
+            'username': user,
+            'password': pw,
+            'execution': hidden_field,
+            '_eventId': 'submit',
+            'geolocation': '',
+        },
+        headers={
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) '
+                          'Chrome/99.0.4844.83 Safari/537.36',
+        },
+        allow_redirects=True
+    )
+    log.d("login api url:" + login_res.url)
+    log.d("login api status_code:" + str(login_res.status_code))
+    if login_res.status_code != 200 or (
+            login_res.url != 'http://release.bigo.local:8081/' and login_res.url != 'http://protocol.client.bigo.inner/'):
+        return False
+    for key, value in login_res.request.headers.items():
+        log.d("login headers:" + key + "=" + value)
+        if key == 'Cookie':
+            log.d("login cookie" + "=" + value)
+            if not value.endswith(';'):
+                value += ';'
+            r_token = re.match(r'.*csrftoken=(.*?);', value)
+            r_session = re.match(r'.*sessionid=(.*?);', value)
+            if r_token is not None and r_session is not None:
+                t = r_token.group(1)
+                s = r_session.group(1)
+                log.d("login cookie headers parsed, token:" + t + " session:" + s)
+                if len(t) > 0 and len(s) > 0:
+                    nc = {config.K_TOKEN: t, config.K_SESSION: s}
+                    config.update_config(**nc)
+                    log.d("login success!")
+                    return True
+    return False
+
+
+def is_session_login(session):
+    """
+    通过请求一个较小的协议来检查本地cookie是否仍然有效
+    :type session: requests.Session
+    """
+    response = session.get(
+        url="http://protocol.client.bigo.inner/get_user_message/",
+        allow_redirects=False
+    )
+    if response.status_code != 200:
+        return False
+    body = response.json()
+    if 'status' not in body or body['status'] != 200:
+        return False
+    return True
