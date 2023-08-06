1ee96cfb336adeef756c9d179f20fcb1723974c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:50 +0000

    Add package jrm33/1.0.3/jrm33-1.0.3-py3-none-any.whl

---
+++ packages/jrm33/jrm33-1.0.3-py3-none-any.whl/jrm33/__init__.py
@@ -0,0 +1,12 @@
+__version__ = '1.0.3'
+
+from . import Globals
+from ._ReadCoeffs import _ReadCoeffs
+from ._CoeffGrids import _CoeffGrids
+from ._Schmidt import _Schmidt
+from ._SphHarm import _SphHarm
+from ._Legendre import _Legendre
+from ._ScaledCoeffs import _ScaledCoeffs
+from .Model import Model
+from .ModelCart import ModelCart
+from .Test import Test
