d6a94a76c2354513c5652058a8f206c056ace138
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:43 +0000

    Add package omniduct/1.1.19/omniduct-1.1.19-py2.py3-none-any.whl

---
+++ packages/omniduct/omniduct-1.1.19-py2.py3-none-any.whl/omniduct/nosql/base.py
@@ -0,0 +1,41 @@
+from future.moves.urllib.parse import urljoin
+
+from omniduct.duct import Duct
+from omniduct.utils.docs import quirk_docs
+
+
+class NoSqlClient(Duct):
+    """
+    A simple generic NoSQL database client, providing a minimal common API across
+    NoSQL databases. Subclasses can extend this functionality either by exposing
+    another python client directly, or by adding additional methods as needed.
+    """
+
+    DUCT_TYPE = Duct.Type.NOSQL
+
+    @quirk_docs('_init', mro=True)
+    def __init__(self, server_protocol='http', assume_json=False, endpoint_prefix='', **kwargs):
+        """
+        This is a shim __init__ function that passes all arguments onto
+        `self._init`, which is implemented by subclasses. This allows subclasses
+        to instantiate themselves with arbitrary parameters.
+        """
+        Duct.__init_with_kwargs__(self, kwargs, port=80)
+
+        self.server_protocol = server_protocol
+        self.assume_json = assume_json
+        self.endpoint_prefix = endpoint_prefix
+
+        self._init(**kwargs)
+
+    def _init(self):
+        pass
+
+    def get(self):
+        pass
+
+    def put(self):
+        pass
+
+    def find(self):
+        pass
