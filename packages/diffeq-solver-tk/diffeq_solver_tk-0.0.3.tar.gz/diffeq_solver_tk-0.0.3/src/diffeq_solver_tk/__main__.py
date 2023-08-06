59e46fc9515fab9ec648a2edbc9999dc94a8f8de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:03 +0000

    Add package diffeq-solver-tk/0.0.3/diffeq_solver_tk-0.0.3.tar.gz

---
+++ packages/diffeq-solver-tk/diffeq_solver_tk-0.0.3.tar.gz/diffeq_solver_tk-0.0.3/src/diffeq_solver_tk/__main__.py
@@ -0,0 +1,17 @@
+from tkinter import Tk
+
+import diffeq_solver_tk.messages.common_messages as common_messages
+from .main_view import MainView
+
+
+def main():
+    app = Tk(className=common_messages.app_name)
+    app.resizable(width=False, height=False)
+    # app.iconbitmap('assets/icon.ico')
+    app.geometry("1100x600")
+    MainView(app).pack(side="top", fill="both", expand=True)
+    app.mainloop()
+
+
+if __name__ == "__main__":
+    main()
