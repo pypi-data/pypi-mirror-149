14c4f8850b8a7c3525805431f71d21a45ad377ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:44:41 +0000

    Add package elmock/1.1.0/elmock-1.1.0.tar.gz

---
+++ packages/elmock/elmock-1.1.0.tar.gz/elmock-1.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    version="1.1.0",
+    install_requires=[
+        "pydantic",
+    ],
+    extras_require={
+        "dev": [
+            "build",
+            "black",
+            "bandit",
+            "coverage",
+            "faker",
+            "flake8",
+            "freezegun",
+            "isort",
+            "mypy",
+            "pytest",
+            "twine",
+            "yoyo-migrations",
+        ],
+    },
+)
