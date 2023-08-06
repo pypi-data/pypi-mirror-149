8a3d8be51122314d183c67632fde266c2c2132b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:42:16 +0000

    Add package pytype/2022.5.5/pytype-2022.5.5-cp310-cp310-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/pytype/pytype-2022.5.5-cp310-cp310-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/pytype/overlays/typing_extensions_overlay.py
@@ -0,0 +1,45 @@
+"""Implementation of special members of typing_extensions."""
+
+from pytype.overlays import overlay
+from pytype.overlays import typing_overlay
+
+
+class TypingExtensionsOverlay(overlay.Overlay):
+  """A custom overlay for the 'typing_extensions' module."""
+
+  def __init__(self, ctx):
+    ast = ctx.loader.import_name("typing_extensions")
+    member_map = {
+        "runtime": _build("typing.runtime_checkable"),
+    }
+    for pyval in ast.aliases + ast.classes + ast.constants + ast.functions:
+      # Any public typing_extensions members that are not explicitly implemented
+      # are unsupported.
+      _, name = pyval.name.rsplit(".", 1)
+      if name.startswith("_"):
+        continue
+      if name in typing_overlay.typing_overlay:
+        member_map[name] = typing_overlay.typing_overlay[name][0]
+      elif f"typing.{name}" in ctx.loader.typing:
+        member_map[name] = _build(f"typing.{name}")
+      elif name not in member_map:
+        member_map[name] = _build_not_supported_yet(name, ast)
+    super().__init__(ctx, "typing_extensions", member_map, ast)
+
+  def _convert_member(self, name, member, subst=None):
+    var = super()._convert_member(name, member, subst)
+    for val in var.data:
+      # typing_extensions backports typing features to older versions.
+      # Pretending that the backports are in typing is easier than remembering
+      # to check for both typing.X and typing_extensions.X every time we match
+      # on an abstract value.
+      val.module = "typing"
+    return var
+
+
+def _build(name):
+  return lambda ctx: ctx.convert.name_to_value(name)
+
+
+def _build_not_supported_yet(name, ast):
+  return lambda ctx: typing_overlay.not_supported_yet(name, ctx, ast=ast)
