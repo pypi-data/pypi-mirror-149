02f6a9201db85715eafd6925c36546d17124b977
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:45 +0000

    Add package cdk-eks-cluster-module/0.0.2/cdk-eks-cluster-module-0.0.2.tar.gz

---
+++ packages/cdk-eks-cluster-module/cdk-eks-cluster-module-0.0.2.tar.gz/cdk-eks-cluster-module-0.0.2/src/cdk_eks_cluster_module/_jsii/__init__.py
@@ -0,0 +1,28 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk._jsii
+import cdk8s._jsii
+import cdk8s_cluster_autoscaler_aws._jsii
+import cdk8s_external_dns_route53._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@smallcase/cdk-eks-cluster-module",
+    "0.0.2",
+    __name__[0:-6],
+    "cdk-eks-cluster-module@0.0.2.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
