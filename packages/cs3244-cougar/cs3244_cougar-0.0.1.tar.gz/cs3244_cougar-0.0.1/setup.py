55327e10b3ebf4e2c87ab0969109f7265c087699
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:28 +0000

    Add package cs3244-cougar/0.0.1/cs3244_cougar-0.0.1.tar.gz

---
+++ packages/cs3244-cougar/cs3244_cougar-0.0.1.tar.gz/cs3244_cougar-0.0.1/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name="cs3244_cougar", 
+    version="0.0.1",
+    author="Eugene Lim",
+    author_email="elimwj@u.nus.edu",
+    description="NUS CS3244 Assignment",
+    long_description="Utilities for the assignments of NUS CS3244 Machine Learning",
+    install_requires=["torch", "matplotlib"],
+    keywords=["cs3244"],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
