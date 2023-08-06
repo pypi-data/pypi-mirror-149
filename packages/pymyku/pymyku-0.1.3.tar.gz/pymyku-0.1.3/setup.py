08d7401bcfacf7e5c7d1f2ac280ac4aa4656dbb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:24 +0000

    Add package pymyku/0.1.3/pymyku-0.1.3.tar.gz

---
+++ packages/pymyku/pymyku-0.1.3.tar.gz/pymyku-0.1.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name='pymyku',
+    version='0.1.3',
+    description='Python MyKU API Wrapper',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='phusitsom',
+    author_email='phusitsom@gmail.com',
+    packages=['pymyku'],
+    url='https://github.com/phusitsom/pymyku',
+    install_requires=['requests'],
+    license='GNU General Public License v3.0',
+    python_requires='>=3.6',
+    classifiers=[
+        'Natural Language :: English',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ]
+)
