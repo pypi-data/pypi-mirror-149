17dd5528149fb3065530b805c70283f4ed85e5be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:45 +0000

    Add package cdk8s-external-dns-route53/0.1.4/cdk8s-external-dns-route53-0.1.4.tar.gz

---
+++ packages/cdk8s-external-dns-route53/cdk8s-external-dns-route53-0.1.4.tar.gz/cdk8s-external-dns-route53-0.1.4/setup.py
@@ -0,0 +1,65 @@
+import json
+import setuptools
+
+kwargs = json.loads(
+    """
+{
+    "name": "cdk8s-external-dns-route53",
+    "version": "0.1.4",
+    "description": "@opencdk8s/cdk8s-external-dns-route53",
+    "license": "Apache-2.0",
+    "url": "https://github.com/opencdk8s/cdk8s-external-dns-route53",
+    "long_description_content_type": "text/markdown",
+    "author": "Hunter Thompson<aatman@auroville.org.in>",
+    "bdist_wheel": {
+        "universal": true
+    },
+    "project_urls": {
+        "Source": "https://github.com/opencdk8s/cdk8s-external-dns-route53"
+    },
+    "package_dir": {
+        "": "src"
+    },
+    "packages": [
+        "cdk8s_external_dns_route53",
+        "cdk8s_external_dns_route53._jsii"
+    ],
+    "package_data": {
+        "cdk8s_external_dns_route53._jsii": [
+            "cdk8s-external-dns-route53@0.1.4.jsii.tgz"
+        ],
+        "cdk8s_external_dns_route53": [
+            "py.typed"
+        ]
+    },
+    "python_requires": ">=3.6",
+    "install_requires": [
+        "aws-cdk-lib==2.22.0",
+        "cdk8s>=2.2.74, <3.0.0",
+        "constructs>=10.0.5, <11.0.0",
+        "jsii>=1.47.0, <2.0.0",
+        "publication>=0.0.3"
+    ],
+    "classifiers": [
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: JavaScript",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Typing :: Typed",
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved"
+    ],
+    "scripts": []
+}
+"""
+)
+
+with open("README.md", encoding="utf8") as fp:
+    kwargs["long_description"] = fp.read()
+
+
+setuptools.setup(**kwargs)
