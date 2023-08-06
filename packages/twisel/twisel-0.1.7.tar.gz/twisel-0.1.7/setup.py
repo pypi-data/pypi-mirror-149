8a8a51e037dee7ead9b75a8106e4c4b3ea7bb696
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:18:10 +0000

    Add package twisel/0.1.7/twisel-0.1.7.tar.gz

---
+++ packages/twisel/twisel-0.1.7.tar.gz/twisel-0.1.7/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'twisel',
+    packages=find_packages(include=['twisel']),
+    version='0.1.7',
+    description='Library for scraping twitter with selenium',
+    author='rafiksept',
+    license='MIT',
+    install_requires = ['pytest','selenium','pandas'],
+    test_suite = 'test'
+
+)
