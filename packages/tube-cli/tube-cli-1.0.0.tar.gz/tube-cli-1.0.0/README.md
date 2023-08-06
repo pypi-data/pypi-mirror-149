96b4e8259d53424343938c64e47d560757a60c4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:30 +0000

    Add package tube-cli/1.0.0/tube-cli-1.0.0.tar.gz

---
+++ packages/tube-cli/tube-cli-1.0.0.tar.gz/tube-cli-1.0.0/README.md
@@ -0,0 +1,18 @@
+# Underground Status CLI
+
+A command line tool written to provide up-to-date info on the status of the London Underground. It fetches data directly from the TfL API, and provides the status plus info of every tube line, plus the Overground, TfL Rail, Docklands Light Railway and Trams in South East London.
+
+## Installation
+
+`pip install london-underground-cli`
+
+## Usage
+
+The whole program (at the moment) is run through the base command `underground`. More options will be brought soon.
+
+Use:
+`underground`
+and an output like this will be provided:
+![](https://i.imgur.com/AohHSVa.png)
+
+If you find any issues please report them on the [GitHub Source](https://github.com/cxllm/tube-cli)
---
+++ packages/tube-cli/tube-cli-1.0.0.tar.gz/tube-cli-1.0.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+with open("README.md", "r") as rd:
+    long_description = rd.read()
+setup(
+    name="tube-cli",
+    version="1.0.0",
+    description="A command line tool that provides up-to-date status on the London Underground and various other TfL services",
+    url="https://github.com/cxllm/tube-cli",
+    author="Callum",
+    author_email="hello@cxllm.com",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: Freely Distributable",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Utilities",
+    ],
+    keywords=["tfl", "london", "undergound", "tube"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=["requests==2.27.1", "termcolor==1.1.0", "xmltodict==0.12.0"],
+    scripts=["bin/underground"],
+)
