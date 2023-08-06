6cb242ef3dbc1f5b2f27b2893077921e5fdbe9fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:39 +0000

    Add package upgradechatpy/1.0.5/upgradechatpy-1.0.5-py3-none-any.whl

---
+++ packages/upgradechatpy/upgradechatpy-1.0.5-py3-none-any.whl/upgradechat/__init__.py
@@ -0,0 +1,7 @@
+from .models import (  # noqa
+    UpgradeChat, UpgradeChatOrder, UpgradeChatUser, UpgradeChatOrderItem, UpgradeChatInterval, UpgradeChatProduct,
+    UpgradeChatItemType, UpgradeChatPaymentProcessor, UpgradeChatProductType, UpgradeChatError,
+)
+
+
+__version__ = "1.0.5"
