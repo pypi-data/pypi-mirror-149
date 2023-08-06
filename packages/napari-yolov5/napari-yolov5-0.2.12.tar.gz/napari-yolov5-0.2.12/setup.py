eff92bc99c19e8cb9fb36eeeda345ea9ae18e250
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:01 +0000

    Add package napari-yolov5/0.2.12/napari-yolov5-0.2.12.tar.gz

---
+++ packages/napari-yolov5/napari-yolov5-0.2.12.tar.gz/napari-yolov5-0.2.12/setup.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(package_data={'napari_yolov5': ['./*','data/*','data/*/*','resources/*','resources/Readme/*','models/*','models/hub/*','runs/*/*/*/*','utils/*/*','utils/*/*/*' ]})
