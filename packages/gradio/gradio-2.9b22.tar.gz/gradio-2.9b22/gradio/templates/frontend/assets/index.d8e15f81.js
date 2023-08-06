4fcc8eeada4bf49e3904a828a023b906147ec566
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:06 +0000

    Add package gradio/2.9b22/gradio-2.9b22.tar.gz

---
+++ packages/gradio/gradio-2.9b22.tar.gz/gradio-2.9b22/gradio/templates/frontend/assets/index.d8e15f81.js
@@ -0,0 +1,1 @@
+import{S as t,i as a,s as e}from"./index.48448e6d.js";class o extends t{constructor(s){super(),a(this,s,null,null,e,{})}}var r=o;const i=["static"];export{r as Component,i as modes};
