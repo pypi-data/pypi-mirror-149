9e18232814b590c5746d81582e7c76c62b56245e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:28 +0000

    Add package dler/0.5.6/dler-0.5.6-py3-none-any.whl

---
+++ packages/dler/dler-0.5.6-py3-none-any.whl/dler/constants.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author: wxnacy(wxnacy@gmail.com)
+"""
+
+"""
+
+import os
+
+__all__ = ['Constants']
+
+class Constants():
+    SERVER_PORT = 60609
+    SERVER_HOST = 'localhost'
+    MAX_TASK_PROCESS = 2
+    MAX_SUB_TASK_PROCESS = 8
+
+
+ENV_DOWNLOAD_DIR: str = os.path.expanduser(os.getenv("DLER_DOWNLOAD_DIR", ''))
+
+DOWNLOAD_DIR: str = os.path.expanduser('~/Downloads')
+DB_PATH: str = os.path.expanduser('~/dler.db')
+SEGMENT_SIZE: int = 1024 * 1024 * 16
