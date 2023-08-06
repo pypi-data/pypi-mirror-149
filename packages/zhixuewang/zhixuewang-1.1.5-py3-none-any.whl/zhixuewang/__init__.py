9651ac7de38076b393f9295f67db78ce6c13a1c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:10 +0000

    Add package zhixuewang/1.1.5/zhixuewang-1.1.5-py3-none-any.whl

---
+++ packages/zhixuewang/zhixuewang-1.1.5-py3-none-any.whl/zhixuewang/__init__.py
@@ -0,0 +1,20 @@
+__author__ = "anwenhu"
+__date__ = "2022/5/4 13:00"
+__version__ = "1.1.5"
+
+from zhixuewang.account import (login, login_id, rewrite_str, login_student, login_teacher,
+                            login_student_id, login_teacher_id, load_account)
+from zhixuewang.session import get_session, get_session_id
+VERSION = tuple(map(int, __version__.split('.')))
+__all__ = [
+    "login",
+    "login_id",
+    "rewrite_str",
+    "login_student",
+    "login_teacher",
+    "login_student_id",
+    "login_teacher_id",
+    "get_session",
+    "get_session_id",
+    "load_account",
+]
