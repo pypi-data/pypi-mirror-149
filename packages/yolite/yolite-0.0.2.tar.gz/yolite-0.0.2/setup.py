fb7c888bcd9a165062e352378ec1727cb7d2197f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:22 +0000

    Add package yolite/0.0.2/yolite-0.0.2.tar.gz

---
+++ packages/yolite/yolite-0.0.2.tar.gz/yolite-0.0.2/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup
+
+
+def readme():
+    with open("README.md") as f:
+        README = f.read()
+    return README
+
+def get_requirements():
+    with open("requirements.txt", encoding="utf8") as f:
+        return f.read().splitlines()
+
+
+setup(
+    name="yolite",
+    version="0.0.2",
+    description="Yolov5-Lite: Minimal YoloV5 Implementation",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/kadirnar/",
+    author="Kadir Nar",
+    author_email="kadir.nar@hotmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    packages=["yolite"],
+    include_package_data=True,
+    install_requires=get_requirements(),
+)
