a2532b4b841065a2faa48ae87df12cb9b3e6a25f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:25 +0000

    Add package uc-sso/0.1.9/uc-sso-0.1.9.tar.gz

---
+++ packages/uc-sso/uc-sso-0.1.9.tar.gz/uc-sso-0.1.9/uc_sso/__init__.py
@@ -0,0 +1,4 @@
+from .parsers import SSOHandshakeParser, SSOUserInfoParser
+from .main import InitialHandshakeData, ServiceTicket
+from .main import LoginFailed
+from .main import get_initial_handshake, get_user_info, get_ticket
