f711799f933db52e3b9f54c725cfe84af5d719b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:43:58 +0000

    Add package re-data/0.8.0b0/re_data-0.8.0b0.tar.gz

---
+++ packages/re-data/re_data-0.8.0b0.tar.gz/re_data-0.8.0b0/setup.py
@@ -0,0 +1,41 @@
+import pathlib
+from setuptools import find_packages, setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text(encoding='utf-8')
+
+
+setup(
+    name="re_data",
+    version="0.8.0b",
+    author="redata-team",
+    author_email="mateusz@getre.io",
+    description="re_data - data quality framework",
+    license="MIT",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/re-data/re-data",
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "click>=8,<9",
+        "dbt-core>=1.0.0,<1.2.0",
+        "yachalk>=0.1.0,<0.2.0",
+        "tabulate==0.8.9",
+        "jsonschema==3.0.0",
+        "analytics-python",
+        
+    ],
+    extras_require={"dev": ["isort", "black", "pre-commit"]},
+    entry_points={
+        "console_scripts": ["re_data=re_data.command_line:main"],
+    },
+    packages=find_packages(exclude=("tests",))
+)
