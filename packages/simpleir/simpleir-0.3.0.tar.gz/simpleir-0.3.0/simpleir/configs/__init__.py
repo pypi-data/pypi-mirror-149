a076a7f2238411aa03dffbbc2f069163ce02119c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:07 +0000

    Add package simpleir/0.3.0/simpleir-0.3.0.tar.gz

---
+++ packages/simpleir/simpleir-0.3.0.tar.gz/simpleir-0.3.0/simpleir/configs/__init__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+"""
+@date: 2022/4/19 下午7:11
+@file: __init__.py.py
+@author: zj
+@description: 
+"""
+
+from yacs.config import CfgNode
+
+
+def add_custom_config(_C: CfgNode) -> None:
+    # Add your own customized configs.
+    # Perform accuracy calculation in the training phase
+    # Applicable to classification model
+    _C.TRAIN.CALCULATE_ACCURACY = True
+
+    _C.METRIC = CfgNode()
+    # enhance type
+    _C.METRIC.ENHANCE_TYPE = 'normal'
+    # similarity type
+    _C.METRIC.SIMILARITY_TYPE = 'euclidean'
+    # sort type
+    _C.METRIC.RANK_TYPE = 'normal'
+
+
+def get_cfg_defaults() -> CfgNode:
+    from zcls2 import config
+
+    cfg = config.get_cfg_defaults()
+    add_custom_config(cfg)
+
+    return cfg
---
+++ packages/simpleir/simpleir-0.3.0.tar.gz/simpleir-0.3.0/simpleir/metric/helper.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+
+"""
+@date: 2022/4/27 下午5:09
+@file: helper.py
+@author: zj
+@description: 
+"""
+from typing import List, Tuple
+
+import numpy as np
+
+from .enhance import enhance
+from .similarity import similarity
+from .rank import rank
+
+
+class MetricHelper:
+    """
+    Calculation accuracy. Based on similarity measurement and rank
+    """
+
+    def __init__(self, max_num: int = 20) -> None:
+        super().__init__()
+
+        # Feature set, each category saves N features, first in first out
+        self.gallery_dict = dict()
+        self.max_num = max_num
+
+    def __call__(self, *args, **kwargs):
+        return self.run(*args, **kwargs)
+
+    def run(self, feats: np.ndarray, targets: np.ndarray, top_k_list: Tuple = (1, 5),
+            enhance_type='normal', similarity_type: str = 'euclidean', rank_type='normal') -> List:
+        # Flatten the eigenvector into a one-dimensional vector
+        feats = feats.reshape(feats.shape[0], -1)
+        feats = enhance(feats, enhance_type=enhance_type)
+
+        top_k_similarity_list = [0 for _ in top_k_list]
+        for feat, target in zip(feats, targets):
+            truth_key = int(target)
+            similarity_list = similarity(feat, self.gallery_dict, similarity_type=similarity_type)
+            if len(similarity_list) == 0:
+                pass
+            else:
+                sorted_list = rank(similarity_list, top_k=top_k_list[-1], rank_type=rank_type)
+
+                for i, k in enumerate(top_k_list):
+                    if truth_key in sorted_list[:k]:
+                        top_k_similarity_list[i] += 1
+
+            # Add feat to the atlas every time. If the category is full, the data added at the beginning will pop up
+            if truth_key not in self.gallery_dict.keys():
+                self.gallery_dict[truth_key] = list()
+            if len(self.gallery_dict[truth_key]) > self.max_num:
+                self.gallery_dict[truth_key].pop(0)
+            self.gallery_dict[truth_key].append(feat)
+
+        total_num = len(feats)
+        res = []
+        for k in top_k_similarity_list:
+            res.append(100.0 * k / total_num)
+        return res
+
+    def clear(self) -> None:
+        self.gallery_dict = dict()
