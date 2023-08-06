d14ffc056da9a11016273d73847767000c621e6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:05 +0000

    Add package notdb-viewer/1.2.5/notdb_viewer-1.2.5.tar.gz

---
+++ packages/notdb-viewer/notdb_viewer-1.2.5.tar.gz/notdb_viewer-1.2.5/notdb_viewer/__init__.py
@@ -0,0 +1,15 @@
+'''
+NotDB Viewer
+-----
+
+NotDB Simple viewer for NotDB Databases
+
+   $ pip install notdb-viewer
+
+Full documentation is avaliable on `Github <https://github.com/nawafalqari/NotDB_Viewer#readme>`_.
+'''
+
+from .app import create_app, viewer_html
+
+__version__ = '1.2.5'
+__all__ = ['create_app', 'viewer_html']
