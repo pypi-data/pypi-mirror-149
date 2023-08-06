9b2bdf14ffaa5d9429b530ee787ae1c78e5ecc88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:51 +0000

    Add package beksilter-messenger-server/0.0.1.1/beksilter_messenger_server-0.0.1.1.tar.gz

---
+++ packages/beksilter-messenger-server/beksilter_messenger_server-0.0.1.1.tar.gz/beksilter_messenger_server-0.0.1.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name="beksilter_messenger_server",
+      version="0.0.1.1",
+      description="Beksilter_Pyqt_Messenger_Server-side",
+      author="Beksilter",
+      author_email="Beksilter@mail.ru",
+      packages=find_packages(),
+      install_requires=['PyQt5', 'sqlalchemy', 'pycryptodome', 'pycryptodomex'],
+      scripts=['server/server_run']
+      )
