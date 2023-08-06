df940652cd693bad3eb9bce01cb409a5460c7644
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:19 +0000

    Add package yandex-yadoctool/0.0/yandex_yadoctool-0.0-py3-none-any.whl

---
+++ packages/yandex-yadoctool/yandex_yadoctool-0.0-py3-none-any.whl/yandex_yadoctool/main.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import argparse
+
+from yandex_yadoctool.operations.corrector import Corrector
+from yandex_yadoctool.operations.locales import Locales
+from yandex_yadoctool.operations.keyrefs import Keyrefs
+
+def collect_args():
+    parser = argparse.ArgumentParser(prog="yadoctool",
+                                     description='Инструмент для оптимизации работы с DITA-документацией.',
+                                     )
+                                     
+    subparsers = parser.add_subparsers(dest='command', title='subcommands', help='additional help')
+
+    corrector = subparsers.add_parser('corrector', help="Скрипт для поиска опечаток и простейших ошибок в документации.")
+    corrector.add_argument("--focus", type=str)
+
+    locales = subparsers.add_parser('locales', help="Интерфейс для разметки ru-ссылок на другие dita-локали.")
+    
+    keyrefs = subparsers.add_parser('keyrefs', help="Скрипт для автозамены внешних ссылок на ключи.")
+    
+def main():
+    args = collect_args()
+
+    if args.command == "corrector":
+        Corrector(focus=args.focus)
+    elif args.command == "locales":
+        Locales()
+    elif args.command == "keyrefs":
+        Keyrefs()
+    else:
+        print('Check help o.o')
+
+
+if __name__ == '__main__':
+    main()
