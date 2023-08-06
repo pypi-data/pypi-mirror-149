14cb9949724ffcb8b292f9baa015893366bc1c7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:09:33 +0000

    Add package campa/1.0.0/campa-1.0.0-py3-none-any.whl

---
+++ packages/campa/campa-1.0.0-py3-none-any.whl/campa/tl/__init__.py
@@ -0,0 +1,20 @@
+from ._losses import LossEnum
+from ._models import VAEModel, ModelEnum, BaseAEModel
+from ._cluster import (
+    Cluster,
+    create_cluster_data,
+    get_clustered_cells,
+    load_full_data_dict,
+    prepare_full_dataset,
+    project_cluster_data,
+    add_clustering_to_adata,
+)
+from ._evaluate import Predictor, ModelComparator
+from ._features import (
+    extract_features,
+    FeatureExtractor,
+    thresholded_count,
+    thresholded_median,
+)
+from ._estimator import Estimator
+from ._experiment import Experiment, run_experiments
