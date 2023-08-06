dc0e8f380a5d243bc5c955cec63df4bb1c07b5a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:59 +0000

    Add package yolov5facedetector/1.0.1/yolov5facedetector-1.0.1.tar.gz

---
+++ packages/yolov5facedetector/yolov5facedetector-1.0.1.tar.gz/yolov5facedetector-1.0.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+with open('requirements.txt','r', encoding='utf-8') as req_file:
+    requirements = req_file.read()
+    requirements = requirements.split('\n')
+
+setuptools.setup(
+    name="yolov5facedetector",
+    version="1.0.1",
+    author="BenedictusAryo",
+    author_email="benedictusaryo@outlook.com",
+    description="Packaged version YOLOv5 Face Detector",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent"
+    ],
+    python_requires='>=3.6.0',
+    install_requires=requirements
+)
