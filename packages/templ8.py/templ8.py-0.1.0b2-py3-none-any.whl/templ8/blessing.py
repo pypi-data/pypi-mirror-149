b1996d8a95c17cf512b839934a93675962ca0905
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:41:19 +0000

    Add package templ8.py/0.1.0b2/templ8.py-0.1.0b2-py3-none-any.whl

---
+++ packages/templ8.py/templ8.py-0.1.0b2-py3-none-any.whl/templ8/blessing.py
@@ -0,0 +1,63 @@
+import os
+
+TEMPL8_ASCII = """
+    ###                                                                 
+    ###                                                               ###
+    ###                                                               ###       #####
+###########        ###                                                ###     ##    ###
+###########     #########      ### ####    #####      ### ######      ###    ##      ###
+    ###        ###     ###     ###################    ############    ###    ##     ####
+    ###       ###       ###    ###    ####    ####    ###      ####   ###     ##   ####
+    ###       #############    ###     ###     ###    ###       ###   ###      #######
+    ###       #############    ###     ###     ###    ###      ####   ###    ####    ##
+  #######     ###              ###     ###     ###    ############    ###   ####       ##
+    ###       ####      ###    ###     ###     ###    ##########      ###   ###        ##
+    ###         ##########     ###     ###     ###    ###             ###    ###     ###
+    ##            ######       ###     ###     ###    ###             ###      ####### 
+    ##                                                ###
+    #"""
+DEF_BASEHTML_CONTENT = """PAGETITLE=##TITLE##
+-BEGINFILE-
+h2. ##TITLE##
+
+^##AUTHORS## - ##TAGS## - ##DATE##^
+
+##CONTENT##
+
+-BEGININDEX-
+h2. "##TITLE##":##LINK##
+
+^##AUTHORS## - ##TAGS## - ##DATE##^
+
+##INTRO##
+-BEGININDEX-
+PAGETITLE=Blog"""
+
+DEF_INPUT = "input"
+DEF_OUTPUT = "output"
+DEITY_PATH = "d8y"
+DEF_BASEHTML_PATH = "basehtml"
+DEF_REPLACE_PATH = "repl8ce"
+
+def makedir(path, warning = ""):
+    if not os.path.exists(path):
+        if warning != "":
+            print("WARNING: " + warning)
+        os.mkdir(path)
+
+
+def mod_replaces(input_replaces, header):
+    for i in header.split("\n"):
+        keyval = i.split("=")
+        if len(keyval) == 2:
+            input_replaces[keyval[0]] = keyval[1]
+
+
+def parse_content(content, ext):
+    if ext == ".textile":
+        return textile.textile(file_content)
+    elif ext == ".md":
+        return pypandoc.convert_text(file_content, "html5", format="md")
+    else:
+        raise Exception("Can't recognize the extension in " + os.join(subdir, file))
+        return ""
