bd76dd76ebb17b933b83e4f12c897e82df3ae1f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:54 +0000

    Add package captur-ml-sdk/0.1.63/captur-ml-sdk-0.1.63.tar.gz

---
+++ packages/captur-ml-sdk/captur-ml-sdk-0.1.63.tar.gz/captur-ml-sdk-0.1.63/captur_ml_sdk/dtypes/interfaces/MLEval/EvalEventData.py
@@ -0,0 +1,13 @@
+from captur_ml_sdk.dtypes.generics import Image
+from pydantic import BaseModel
+from typing import List, Optional, Dict
+
+class EvalEventData(BaseModel):
+    request_id: str
+    webhooks: str
+    images: Optional[List[Image]]
+    evaluation_file: Optional[str]
+    model_name: str
+    model_id: str
+    model_type: str
+    mapping: Optional[Dict[str, Dict[str, str]]]
