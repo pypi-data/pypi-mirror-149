0aa26c458a7b2f2d1149ca43ca1d85687c275d3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:49 +0000

    Add package outside/69/outside-69.tar.gz

---
+++ packages/outside/outside-69.tar.gz/outside-69/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+__name__ = "outside"
+__version__ = "69"
+setup(
+    name='outside',
+    version='69',
+    license='MIT',
+    author="ui",
+    author_email='email@example.com',
+    packages=find_packages(),
+    url='https://github.com/UiIsBack/shower',
+    keywords='shower, grass',
+    install_requires=[
+          '',
+      ],
+
+)
