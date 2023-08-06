c61c8f514f236b6a206cd11f9329e71dede3b414
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:20 +0000

    Add package pyas/1.2.1/pyas-1.2.1.tar.gz

---
+++ packages/pyas/pyas-1.2.1.tar.gz/pyas-1.2.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+setup(
+    name='pyas',
+    version='1.2.1',
+    license='MIT',
+    author='Elisha Hollander',
+    author_email='just4now666666@gmail.com',
+    description="Run machine code, assembly and webassembly directly in Python",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url='https://github.com/donno2048/pyas',
+    project_urls={
+        'Documentation': 'https://github.com/donno2048/pyas#readme',
+        'Bug Reports': 'https://github.com/donno2048/pyas/issues',
+        'Source Code': 'https://github.com/donno2048/pyas',
+    },
+    packages=find_packages(),
+    platforms=['linux'],
+)
