f270327ea49b6096ca5bf4c7e60a0291a3874daf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:18 +0000

    Add package pls/4.0.3/pls-4.0.3-py3-none-any.whl

---
+++ packages/pls/pls-4.0.3-py3-none-any.whl/pls/parser/args/pos.py
@@ -0,0 +1,40 @@
+import argparse
+from pathlib import Path
+
+from pls.exceptions import ArgException
+
+
+def _node(path_str: str) -> Path:
+    """
+    Parse the given path into a ``Path`` instance. The path is considered valid
+    if it points to an existing file or directory.
+
+    :param path_str: the path supplied as a CLI argument
+    :return: the ``Path`` instance wrapping the supplied path
+    :raise: ``ArgException``, if the path is invalid
+    """
+
+    path = Path(path_str)
+    try:
+        path.lstat()  # raises error if file does not exist`
+    except FileNotFoundError:
+        raise ArgException(
+            f"Path [repr.path]{path_str}[/] does not exist.", arg_name="node"
+        )
+    return path
+
+
+def add_args(parser: argparse.ArgumentParser):
+    """
+    Add the positional arguments to the given parser.
+
+    :param parser: the parser to which to add the arguments
+    """
+
+    parser.add_argument(
+        "node",
+        type=_node,
+        nargs=argparse.OPTIONAL,
+        default=Path.cwd(),
+        help="the node to list self or contents",
+    )
