0f1aa214dc1a43e50016c28e81305261bf21a022
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:06 +0000

    Add package v-cloud-market-cli-user/0.1.5/v_cloud_market_cli_user-0.1.5-py3-none-any.whl

---
+++ packages/v-cloud-market-cli-user/v_cloud_market_cli_user-0.1.5-py3-none-any.whl/market_place_cli/v_cloud_market_cli_common/config/server_config_copy.py
@@ -0,0 +1,8 @@
+PLATFORM_HOST = 'http://127.0.0.1:3002'
+# PLATFORM_HOST = 'https://yg79clz90f.execute-api.us-east-1.amazonaws.com/dev'
+# PLATFORM_HOST = 'https://o5mdw8b6mh.execute-api.us-east-1.amazonaws.com/test'
+API_VERSION = '/api/v1'
+NET = "T"
+
+######## TESTING #################
+MOCK_PAYMENT_SERVER = "http://127.0.0.1:3000"
