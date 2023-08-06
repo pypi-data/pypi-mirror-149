be819b7c1fb0dfd64933bca7a43d02e6e9ce1cce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:58 +0000

    Add package streamsbpy/1.0.3/streamsbpy-1.0.3.tar.gz

---
+++ packages/streamsbpy/streamsbpy-1.0.3.tar.gz/streamsbpy-1.0.3/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+setuptools.setup(
+    name="streamsbpy",
+    version="1.0.3",
+    author="code_xed",
+    author_email="prernanayak001@gmail.com",
+    description="An unoffcial API wrapper for streamsb.com",
+    license="GNU Lesser General Public License v3 or later (LGPLv3+)",
+    url="https://github.com/code_xed/streamsbpy",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    install_requires=[
+        'requests'
+    ],
+)
