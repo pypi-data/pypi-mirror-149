718bd1b32cb0e990b619bc2d1a7ed3805280e0fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:28 +0000

    Add package gardener-cicd-libs/1.1679.0/gardener_cicd_libs-1.1679.0-py3-none-any.whl

---
+++ packages/gardener-cicd-libs/gardener_cicd_libs-1.1679.0-py3-none-any.whl/ccc/delivery.py
@@ -0,0 +1,27 @@
+import logging
+
+import ci.log
+import ci.util
+import delivery.client
+
+ci.log.configure_default_logging()
+logger = logging.getLogger(__name__)
+
+
+def default_client_if_available():
+    if not ci.util._running_on_ci():
+        return None
+
+    import ctx
+    cfg_factory = ctx.cfg_factory()
+    cfg_set_name = ci.util.current_config_set_name()
+    cfg_set = cfg_factory.cfg_set(cfg_set_name)
+
+    try:
+        delivery_endpoints = cfg_set.delivery_endpoints()
+        routes = delivery.client.DeliveryServiceRoutes(
+            base_url=delivery_endpoints.base_url(),
+        )
+        return delivery.client.DeliveryServiceClient(routes=routes)
+    except Exception:
+        logger.warning('unable to build delivery client')
