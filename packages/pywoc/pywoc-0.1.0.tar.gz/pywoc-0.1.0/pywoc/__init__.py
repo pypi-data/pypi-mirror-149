d71bb60bdf36570456f916883b825ac2a19837ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:26 +0000

    Add package pywoc/0.1.0/pywoc-0.1.0.tar.gz

---
+++ packages/pywoc/pywoc-0.1.0.tar.gz/pywoc-0.1.0/pywoc/__init__.py
@@ -0,0 +1,16 @@
+
+"""
+================================================================================
+woc: 2-D spatial comparison algorithm
+================================================================================
+This code was originally published with the following pulication:
+...
+"""
+
+# from __future__ import absolute_import
+
+__author__ = 'Cristiano Sabiu'
+__version__ = '0.0.1'
+
+from pywoc.woc import *
+from pywoc.radial_profile import *
