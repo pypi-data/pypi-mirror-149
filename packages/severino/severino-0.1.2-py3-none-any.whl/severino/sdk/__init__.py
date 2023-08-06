1e463d0b365e9285dd8795720edd57e40fedd282
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:20:56 +0000

    Add package severino/0.1.2/severino-0.1.2-py3-none-any.whl

---
+++ packages/severino/severino-0.1.2-py3-none-any.whl/severino/sdk/__init__.py
@@ -0,0 +1,3 @@
+from severino.sdk.airflow import Airflow
+from severino.sdk.internal_recruitment import Movement, Stage
+from severino.sdk.store_data import StoreData, StoreDataTmp
---
+++ packages/severino/severino-0.1.2-py3-none-any.whl/severino/sdk/helpers/http_requests.py
@@ -0,0 +1,44 @@
+import json
+
+import requests
+
+from severino.settings import SEVERINO_API_TOKEN
+
+
+class Http:
+    def __init__(self):
+        self.headers = {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "Authorization": f"Token {SEVERINO_API_TOKEN}",
+        }
+
+    def __http_request(self, method: str, url: str, headers: dict = None, data=None):
+        if headers is None:
+            headers = {}
+
+        if data is None:
+            data = {}
+
+        headers = {**self.headers, **headers}
+        return requests.request(
+            method=method,
+            url=url,
+            headers=headers,
+            data=json.dumps(data),
+        )
+
+    def post(self, url: str, headers: dict = None, data: dict = None):
+        return self.__http_request(method="POST", url=url, headers=headers, data=data)
+
+    def get(self, url: str, headers: dict = None, data: dict = None):
+        return self.__http_request(method="GET", url=url, headers=headers, data=data)
+
+    def put(self, url: str, headers: dict = None, data: dict = None):
+        return self.__http_request(method="PUT", url=url, headers=headers, data=data)
+
+    def patch(self, url: str, headers: dict = None, data: dict = None):
+        return self.__http_request(method="PATCH", url=url, headers=headers, data=data)
+
+    def delete(self, url: str, headers: dict = None, data: dict = None):
+        return self.__http_request(method="DELETE", url=url, headers=headers, data=data)
