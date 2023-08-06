3ab9cc7ee67c6c43000180adec96762d8176fb72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:13 +0000

    Add package m14/0.5.3/m14-0.5.3.tar.gz

---
+++ packages/m14/m14-0.5.3.tar.gz/m14-0.5.3/m14/environ/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+from m14.meta import __version__
+# noinspection PyUnresolvedReferences
+from m14.meta.environ import GlobalInterface
+
+if __name__ == '__main__':
+    print(__version__)
+
+__all__ = []
---
+++ packages/m14/m14-0.5.3.tar.gz/m14-0.5.3/m14/environ/utils.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+# noinspection PyUnresolvedReferences
+from joker.meta.utils import *
+
+# noinspection PyUnresolvedReferences
+from m14.experimental.utils import *
