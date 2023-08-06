10ad45327802041d0db5ada848af239c79409112
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:24 +0000

    Add package dler/0.5.5/dler-0.5.5.tar.gz

---
+++ packages/dler/dler-0.5.5.tar.gz/dler-0.5.5/dler/cli/main.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author: wxnacy(wxnacy@gmail.com)
+"""
+
+"""
+import typer
+import time
+from typer import Argument, Option
+
+from dler import Dler
+
+app = typer.Typer()
+
+@app.command()
+def start(
+    url: str = Argument(..., help="URL 路径"),
+    name: str = Option(None, '-n', '--name', help="下载文件保存名"),
+    trans: str = Option(None, '-t', '--trans', help="转码"),
+):
+    """开始下载任务"""
+
+    begin = time.time()
+    dl = Dler(url, filename = name)
+    dl.run()
+
+    cost = time.time() - begin
+    typer.echo(f'下载完成，耗时: {cost}')
+
+
+@app.command()
+def run():
+    print('test')
+
+if __name__ == "__main__":
+    app()
