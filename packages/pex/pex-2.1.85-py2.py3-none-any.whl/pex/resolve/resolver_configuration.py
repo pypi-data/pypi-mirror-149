34d583451ae04d392385777b1f7e15fe5d3d70c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:57 +0000

    Add package pex/2.1.85/pex-2.1.85-py2.py3-none-any.whl

---
+++ packages/pex/pex-2.1.85-py2.py3-none-any.whl/pex/resolve/resolver_configuration.py
@@ -0,0 +1,64 @@
+# Copyright 2021 Pants project contributors (see CONTRIBUTORS.md).
+# Licensed under the Apache License, Version 2.0 (see LICENSE).
+
+from __future__ import absolute_import
+
+from pex.enum import Enum
+from pex.jobs import DEFAULT_MAX_JOBS
+from pex.network_configuration import NetworkConfiguration
+from pex.typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing import Callable, Optional, Tuple, Union
+
+    import attr  # vendor:skip
+
+    from pex.resolve.lockfile import Lockfile
+    from pex.result import Error
+else:
+    from pex.third_party import attr
+
+
+PYPI = "https://pypi.org/simple"
+
+
+class ResolverVersion(Enum["ResolverVersion.Value"]):
+    class Value(Enum.Value):
+        pass
+
+    PIP_LEGACY = Value("pip-legacy-resolver")
+    PIP_2020 = Value("pip-2020-resolver")
+
+
+@attr.s(frozen=True)
+class ReposConfiguration(object):
+    indexes = attr.ib(default=(PYPI,))  # type: Tuple[str, ...]
+    find_links = attr.ib(default=())  # type: Tuple[str, ...]
+
+
+@attr.s(frozen=True)
+class PipConfiguration(object):
+    resolver_version = attr.ib(default=ResolverVersion.PIP_LEGACY)  # type: ResolverVersion.Value
+    repos_configuration = attr.ib(default=ReposConfiguration())  # type: ReposConfiguration
+    network_configuration = attr.ib(default=NetworkConfiguration())  # type: NetworkConfiguration
+    allow_prereleases = attr.ib(default=False)  # type: bool
+    allow_wheels = attr.ib(default=True)  # type: bool
+    allow_builds = attr.ib(default=True)  # type: bool
+    prefer_older_binary = attr.ib(default=False)  # type: bool
+    use_pep517 = attr.ib(default=None)  # type: Optional[bool]
+    build_isolation = attr.ib(default=True)  # type: bool
+    transitive = attr.ib(default=True)  # type: bool
+    max_jobs = attr.ib(default=DEFAULT_MAX_JOBS)  # type: int
+
+
+@attr.s(frozen=True)
+class PexRepositoryConfiguration(object):
+    pex_repository = attr.ib()  # type: str
+    network_configuration = attr.ib(default=NetworkConfiguration())  # type: NetworkConfiguration
+    transitive = attr.ib(default=True)  # type: bool
+
+
+@attr.s(frozen=True)
+class LockRepositoryConfiguration(object):
+    parse_lock = attr.ib()  # type: Callable[[], Union[Lockfile, Error]]
+    pip_configuration = attr.ib()  # type: PipConfiguration
