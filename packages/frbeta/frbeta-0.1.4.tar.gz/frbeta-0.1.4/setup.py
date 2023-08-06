81c33eb9c2de39ca7aca3efd9cb7c57d008b107d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:48:21 +0000

    Add package frbeta/0.1.4/frbeta-0.1.4.tar.gz

---
+++ packages/frbeta/frbeta-0.1.4.tar.gz/frbeta-0.1.4/setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+setup(
+    name='frbeta',
+    packages=find_packages(),
+    version='0.1.4',
+    description='return in a fraction format',
+    author='resdon',
+    license='MIT',
+    install_requires=[],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    test_suite='test',
+    url = 'https://pypi.org/project/frbeta/0.1.4/#files'
+)
