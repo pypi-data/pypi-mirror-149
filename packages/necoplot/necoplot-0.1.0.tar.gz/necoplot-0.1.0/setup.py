60a8eb5d1238f3ebce2ee4bacb1ffe6727adb2e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:58 +0000

    Add package necoplot/0.1.0/necoplot-0.1.0.tar.gz

---
+++ packages/necoplot/necoplot-0.1.0.tar.gz/necoplot-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['necoplot']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['japanize-matplotlib>=1.1.3,<2.0.0', 'matplotlib>=3.5.1,<4.0.0']
+
+setup_kwargs = {
+    'name': 'necoplot',
+    'version': '0.1.0',
+    'description': 'Make it easier to plot by matplotlib',
+    'long_description': '# necoplot',
+    'author': 'guneco',
+    'author_email': 'gu3fav@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/guneco',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<3.11',
+}
+
+
+setup(**setup_kwargs)
