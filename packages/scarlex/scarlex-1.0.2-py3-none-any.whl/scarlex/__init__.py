875eadbf37a706c3205653f9dbfe3d7bd1883259
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:46 +0000

    Add package scarlex/1.0.2/scarlex-1.0.2-py3-none-any.whl

---
+++ packages/scarlex/scarlex-1.0.2-py3-none-any.whl/scarlex/__init__.py
@@ -0,0 +1,13 @@
+"""
+Scarlex API Wrapper
+-------------------
+A simple api wrapper for Scarlex.
+"""
+
+__title__ = 'scarlex'
+__author__ = 'avixity'
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright 2022 ScarlexDev'
+__version__ = '1.0.2'
+
+from .__main__ import ApiClient
