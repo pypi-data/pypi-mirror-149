cca6c2a4cfe6249bb872410e649e0b51158ce33c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:54 +0000

    Add package captur-ml-sdk/0.1.63/captur_ml_sdk-0.1.63-py3-none-any.whl

---
+++ packages/captur-ml-sdk/captur_ml_sdk-0.1.63-py3-none-any.whl/captur_ml_sdk/dtypes/generics/Meta.py
@@ -0,0 +1,20 @@
+from pydantic import (
+    BaseModel, HttpUrl
+)
+from typing import Optional
+
+
+class Meta(BaseModel):
+    webhooks: Optional[HttpUrl]
+
+
+class TrainingMeta(BaseModel):
+    webhooks: Optional[HttpUrl]
+    budget_milli_node_hours: Optional[int] = 8000
+
+
+class EvaluationMeta(BaseModel):
+    webhooks: Optional[HttpUrl]
+    write_to_file: Optional[bool]
+    last_file: Optional[bool]
+    request_id: Optional[str]
