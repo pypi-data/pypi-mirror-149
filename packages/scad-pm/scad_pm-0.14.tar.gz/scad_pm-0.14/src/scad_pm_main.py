2fef865c80f0dc0727ec486ea248c90c3cb15044
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:11 +0000

    Add package scad-pm/0.14/scad_pm-0.14.tar.gz

---
+++ packages/scad-pm/scad_pm-0.14.tar.gz/scad_pm-0.14/src/scad_pm_main.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import logging
+import argparse
+
+from scopeton.scope import Scope
+
+import config
+import dependency_resolver
+import git_util
+from config import ArgsConfig
+
+parser = argparse.ArgumentParser(description='Openscad package manager')
+parser.add_argument('--v', action='store_true', help='verbose output')
+parser.add_argument('--upgrade', action='store_true', help='upgrade packages')
+parser.add_argument('--lib-path', dest='lib_path', default='./lib', help='library path')
+parser.add_argument('--pm-file', dest='pm_file', default='scad_pm.yaml', help='pm file name')
+
+args = parser.parse_args()
+args_config = ArgsConfig()
+args_config.__dict__ = vars(args)
+
+logging.basicConfig(level=logging.DEBUG if args.v else logging.INFO)
+
+scope = Scope()
+scope.registerInstance(ArgsConfig, args_config)
+scope.registerBean(
+    config.YamlConfigParser,
+    config.MainConfigParser,
+    config.PmConfigParser,
+    dependency_resolver.DependencyResolver,
+    git_util.GitFetcher
+)
+
+resolver = scope.getInstance(dependency_resolver.DependencyResolver)
+parser = scope.getInstance(config.MainConfigParser)
+
+main_file = parser.parse(args_config.pm_file)
+resolver.resolve(main_file)
