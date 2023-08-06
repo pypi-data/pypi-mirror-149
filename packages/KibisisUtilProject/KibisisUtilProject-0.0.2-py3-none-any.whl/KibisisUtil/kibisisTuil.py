134932c7958ce2dfcb16a4053898c3cf22b2cfd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:14:44 +0000

    Add package KibisisUtilProject/0.0.2/KibisisUtilProject-0.0.2-py3-none-any.whl

---
+++ packages/KibisisUtilProject/KibisisUtilProject-0.0.2-py3-none-any.whl/KibisisUtil/kibisisTuil.py
@@ -0,0 +1,5 @@
+
+if __name__ == '__main__':
+    a = 1
+    b = 2
+    print(a + b)
---
+++ packages/KibisisUtilProject/KibisisUtilProject-0.0.2-py3-none-any.whl/KibisisUtil/kibisisUtil.py
@@ -0,0 +1,10 @@
+
+class KibisisUtil:
+    def hello(self):
+        print("hello")
+
+
+if __name__ == '__main__':
+    a = 1
+    b = 2
+    print(a + b)
