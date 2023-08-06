02fc3d449bd15b6e7ad2af89ec5fc40f344ee4e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:01 +0000

    Add package ams-brief/0.1.43/ams-brief-0.1.43.tar.gz

---
+++ packages/ams-brief/ams-brief-0.1.43.tar.gz/ams-brief-0.1.43/src/ams_brief/mv.py
@@ -0,0 +1,20 @@
+import os
+from pathlib import Path
+from datetime import date
+
+
+def create_mv_folder(projectpath: str, verbose: bool):
+    p = Path(projectpath)
+    mv_probe = p.joinpath("MV")
+    print(f"Probing {mv_probe}...") if verbose else None
+    mv_subfolders = []
+    for x in [x for x in Path(mv_probe).iterdir() if x.is_dir()]:
+        if str(x.stem[0:2]).isdigit():
+            mv_subfolders.append(str(x.stem[0:2]))
+
+    newfolder = str(int(max(mv_subfolders))+1).zfill(2)
+    projectcode = str(projectpath)[8:15]
+    foldername = f"{newfolder}_{date.today().strftime('%Y%m%d')}_{projectcode}_{os.getlogin()}_Briefing"
+    resultpath = mv_probe.joinpath(foldername)
+    os.mkdir(resultpath)
+    return resultpath
