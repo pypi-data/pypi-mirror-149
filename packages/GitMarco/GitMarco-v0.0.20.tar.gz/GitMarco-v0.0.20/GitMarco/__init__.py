57e972713707ffc5ba2a6eb05e4a5bd8b67b8826
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:43 +0000

    Add package GitMarco/0.0.20/GitMarco-v0.0.20.tar.gz

---
+++ packages/GitMarco/GitMarco-v0.0.20.tar.gz/GitMarco-v0.0.20/GitMarco/__init__.py
@@ -0,0 +1,13 @@
+from GitMarco.energy import *
+from GitMarco.graphics import *
+from GitMarco.notebooks import *
+from GitMarco.statistics import *
+from GitMarco.tf import *
+from GitMarco.torch import *
+from GitMarco.utils import *
+from GitMarco.pipes import *
+
+import logging
+
+logging.basicConfig(format='GitMarco - %(levelname)s: %(message)s',
+                    level=logging.WARNING)
