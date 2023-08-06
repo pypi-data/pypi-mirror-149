c296e8b2e4d5b0c35c6f15b206b0f03e12de7bff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:44 +0000

    Add package youmirror/0.5.2/youmirror-0.5.2-py3-none-any.whl

---
+++ packages/youmirror/youmirror-0.5.2-py3-none-any.whl/youmirror/template.py
@@ -0,0 +1,25 @@
+__all__ = ["template"]
+
+'''
+This dictionary is used to make a fresh config file
+'''
+
+template = {
+    "youmirror": 
+    {
+    "name": "",
+    },
+
+    "channel":
+    {
+
+    },
+    "playlist":
+    {
+
+    },
+    "single":
+    {
+
+    }
+}
