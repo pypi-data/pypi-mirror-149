b3ac1840d274ee25a31ba43fd4f8116d1bd7b2ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:44 +0000

    Add package wiizin-plots/0.0.2.1/wiizin_plots-0.0.2.1.tar.gz

---
+++ packages/wiizin-plots/wiizin_plots-0.0.2.1.tar.gz/wiizin_plots-0.0.2.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.2.1'
+DESCRIPTION = 'Fast EDA plots'
+LONG_DESCRIPTION = 'A package that helps you to perform a fast and awesome EDA of your classification and regression problems.'
+
+# Setting up
+setup(
+    name="wiizin_plots",
+    version=VERSION,
+    author="Wiizin",
+    author_email="<carugaxdopw@hotmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['pandas', 'numpy', 'matplotlib','seaborn'],
+    keywords=['python', 'eda', 'plots'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
---
+++ packages/wiizin-plots/wiizin_plots-0.0.2.1.tar.gz/wiizin_plots-0.0.2.1/wiizin_plots/__init__.py
@@ -0,0 +1,5 @@
+from wiizin_plots.wiizin_plot import basic_info
+from wiizin_plots.wiizin_plot import get_cat_num_variables
+from wiizin_plots.wiizin_plot import eda_class_plot
+from wiizin_plots.wiizin_plot import eda_reg_plot
+from wiizin_plots.wiizin_plot import best_interval
