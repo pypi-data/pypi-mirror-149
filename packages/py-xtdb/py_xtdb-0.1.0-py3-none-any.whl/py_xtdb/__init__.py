00921b036f75f6149f754e06737c23e1d68e3380
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:08 +0000

    Add package py-xtdb/0.1.0/py_xtdb-0.1.0-py3-none-any.whl

---
+++ packages/py-xtdb/py_xtdb-0.1.0-py3-none-any.whl/py_xtdb/__init__.py
@@ -0,0 +1,12 @@
+__version__ = '0.1.0'
+
+import py_xtdb.xt as xt
+
+queryf    = xt.queryf
+query     = xt.query
+entity    = xt.entity
+submit_tx = xt.submit_tx
+
+
+if __name__ == "__main__":
+    pass
