0521e9dcce82b21afe5eef36f09f0b5e4855a6a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:06 +0000

    Add package model-one/0.1.6/model_one-0.1.6.tar.gz

---
+++ packages/model-one/model_one-0.1.6.tar.gz/model_one-0.1.6/model_one/__init__.py
@@ -0,0 +1,24 @@
+from model_one.cli import (
+    create_classifier,
+    create_generator,
+    get_model,
+    models,
+    train_generator,
+    train_classifier,
+    BeyondmlModel,
+    api_url,
+    API_KEY
+)
+
+__all__ = [
+    'model_one',
+    'API_KEY',
+    'api_url',
+    'create_classifier',
+    'create_generator',
+    'get_model',
+    'models',
+    'train_generator',
+    'train_classifier',
+    'BeyondmlModel'
+]
