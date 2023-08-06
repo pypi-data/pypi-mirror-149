3c15718403880458934ecebcb116cb61ec0abd96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:55 +0000

    Add package ezyquant/0.0.1/ezyquant-0.0.1.tar.gz

---
+++ packages/ezyquant/ezyquant-0.0.1.tar.gz/ezyquant-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+VERSION = "0.0.1"
+
+setup(
+    name="ezyquant",
+    version=VERSION,
+    packages=find_packages(),
+    python_requires=">=3.8.0",
+    install_requires=["pandas>=1.4", "sqlalchemy>=1.4"],
+)
