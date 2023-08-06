2bf77b96fb9bbaaea871fa2a530dc652e5ec55d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:42:03 +0000

    Add package httpie/3.2.0/httpie-3.2.0-py3-none-any.whl

---
+++ packages/httpie/httpie-3.2.0-py3-none-any.whl/httpie/__init__.py
@@ -0,0 +1,9 @@
+"""
+HTTPie: modern, user-friendly command-line HTTP client for the API era.
+
+"""
+
+__version__ = '3.2.0'
+__date__ = '2022-05-05'
+__author__ = 'Jakub Roztocil'
+__licence__ = 'BSD'
