6165bd2d290d2802fe64d53e732454b38b14fce7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:20:32 +0000

    Add package django-ory-auth/0.1.0/django_ory_auth-0.1.0.tar.gz

---
+++ packages/django-ory-auth/django_ory_auth-0.1.0.tar.gz/django_ory_auth-0.1.0/django_ory_auth/apps.py
@@ -0,0 +1,12 @@
+from django.apps import AppConfig
+from django.conf import settings
+
+class OryAuthConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'django_ory_auth'
+
+
+def config():
+    return {
+        'ORY_SDK_URL': getattr(settings, 'ORY_SDK_URL', 'http://127.0.0.1:4433'),
+    }
