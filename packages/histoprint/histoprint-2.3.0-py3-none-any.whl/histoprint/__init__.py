4e7153922d46d2b74151b0cd631e02cbf7c232d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:44:11 +0000

    Add package histoprint/2.3.0/histoprint-2.3.0-py3-none-any.whl

---
+++ packages/histoprint/histoprint-2.3.0-py3-none-any.whl/histoprint/__init__.py
@@ -0,0 +1,7 @@
+"""histoprint - Pretty-print histograms to the terminal"""
+
+from histoprint.formatter import HistFormatter, print_hist, text_hist
+
+from .version import version as __version__
+
+__all__ = ("print_hist", "text_hist", "formatter", "HistFormatter", "__version__")
