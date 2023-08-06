51b8ac3c296f2ef1dafcb558ba6303f4412a7105
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:41 +0000

    Add package winmail/0.2/winmail-0.2-py3.8.egg

---
+++ packages/winmail/winmail-0.2-py3.8.egg/winmail/Api/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding:utf-8 -*-
+
+from OpenApi import OpenApi
+
+__all__ = [OpenApi]
---
+++ packages/winmail/winmail-0.2-py3.8.egg/winmail/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding:utf-8 -*-
+
+from .OpenApi import OpenApi
+from .ComApi import ComApi
+
+__all__ = ["OpenApi", "ComApi"]
