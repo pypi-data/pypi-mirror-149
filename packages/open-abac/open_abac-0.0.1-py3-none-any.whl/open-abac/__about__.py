958bcfa971d1ebf5365367fe117a71d5e92e3e5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:37 +0000

    Add package open-abac/0.0.1/open_abac-0.0.1-py3-none-any.whl

---
+++ packages/open-abac/open_abac-0.0.1-py3-none-any.whl/open-abac/__about__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__all__ = [
+    "__title__",
+    "__summary__",
+    "__uri__",
+    "__version__",
+    "__author__",
+    "__maintainer__",
+    "__email__",
+    "__license__",
+    "__copyright__",
+    "__status__"
+]
+
+__title__ = "open-abac"
+__summary__ = "Open ABAC (Attribute Based Access Control)"
+__uri__ = "https://www.open-abac.org/"
+__version__ = "0.0.1"
+__author__ = "Yuriy Petrovskiy, MCode GmbH"
+__maintainer__ = __author__
+__email__ = "python@open-abac.org"
+__license__ = "LGPL"
+__copyright__ = "Copyright 2018-2022 %s" % __author__
+__status__ = "1 - Planning"
