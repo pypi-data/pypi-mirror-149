2c20ef19ae8451db4db3695a76df6e152d3b3bab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:24 +0000

    Add package classevivaAPI/0.1.1.1/classevivaAPI-0.1.1.1.tar.gz

---
+++ packages/classevivaAPI/classevivaAPI-0.1.1.1.tar.gz/classevivaAPI-0.1.1.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+setuptools.setup(
+    name='classevivaAPI',
+    packages=['classeviva'],
+    package_dir={'': 'src'},
+    version='0.1.1.1',
+    requires=["selenium"],
+    description='Classeviva Python API',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='FLAK-ZOSO',
+    author_email='mattia.marchese.2006@gmail.com',
+    url='https://github.com/Lioydiano/Classeviva',
+    download_url='https://github.com/Lioydiano/Classeviva/archive/refs/tags/v0.1.1-alfa.1.tar.gz',
+    keywords=['classeviva', 'school', 'api'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires=">=3.10"
+)
