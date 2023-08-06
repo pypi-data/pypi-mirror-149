d2756679a807732bce65bab86a4d722d6428d530
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:11 +0000

    Add package napari-skimage-regionprops/0.4.5/napari-skimage-regionprops-0.4.5.tar.gz

---
+++ packages/napari-skimage-regionprops/napari-skimage-regionprops-0.4.5.tar.gz/napari-skimage-regionprops-0.4.5/napari_skimage_regionprops/_parametric_images.py
@@ -0,0 +1,31 @@
+from napari_tools_menu import register_function
+import numpy
+
+@register_function(menu="Visualization > Measurements on labels (nsr)")
+def visualize_measurement_on_labels(labels_layer:"napari.layers.Labels", column:str = "label", viewer:"napari.Viewer" = None) -> "napari.types.ImageData":
+    import pandas as pd
+
+    labels = labels_layer.data
+    table = pd.DataFrame(labels_layer.properties)
+
+    if len(labels.shape) == 4:
+        current_timepoint = viewer.dims.current_step[0]
+        labels = labels[current_timepoint]
+
+        if "frame" in table.keys():
+            table = table[table['frame'] == current_timepoint]
+
+    measurements = table[column]
+
+    if isinstance(measurements, numpy.ndarray):
+        measurements = measurements.tolist()
+
+    try:
+        import pyclesperanto_prototype as cle
+        return cle.pull(cle.replace_intensities(labels, numpy.asarray([0] + measurements)))
+    except ImportError:
+        return relabel_numpy(labels, measurements)
+
+
+def relabel_numpy(image, measurements):
+    return numpy.take(numpy.array([0] + measurements), image)
