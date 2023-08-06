0929c44ac12f960346eb8e732962ca430aeb6c7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:42:06 +0000

    Add package thousandwords.core/0.6.0/thousandwords.core-0.6.0.tar.gz

---
+++ packages/thousandwords.core/thousandwords.core-0.6.0.tar.gz/thousandwords.core-0.6.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['thousandwords_core']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['cloudpickle>=1.3.0', 'pandas>=1.3.5']
+
+setup_kwargs = {
+    'name': 'thousandwords.core',
+    'version': '0.6.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Edouard Godfrey',
+    'author_email': 'edouard@1000words-hq.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7.1,<4.0',
+}
+
+
+setup(**setup_kwargs)
