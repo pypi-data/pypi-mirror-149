b5631163e9252cb54230ab767e3c96b0e890a2ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:08 +0000

    Add package xinstall/0.51.1/xinstall-0.51.1.tar.gz

---
+++ packages/xinstall/xinstall-0.51.1.tar.gz/xinstall-0.51.1/setup.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['xinstall',
+ 'xinstall.data.ipython',
+ 'xinstall.data.jupyter',
+ 'xinstall.data.linux.autokey.data.Sample Scripts']
+
+package_data = \
+{'': ['*'],
+ 'xinstall': ['data/SpaceVim/SpaceVim.d/*',
+              'data/SpaceVim/SpaceVim/*',
+              'data/chrome/vimium/*',
+              'data/cygwin/*',
+              'data/darglint/*',
+              'data/eclipse/*',
+              'data/firefox/*',
+              'data/firefox/noscripts/config/*',
+              'data/firefox/noscripts/whitelist/*',
+              'data/flake8/*',
+              'data/geany/*',
+              'data/geany/colorschemes/*',
+              'data/geany/filedefs/*',
+              'data/geany/templates/*',
+              'data/git/*',
+              'data/git/mac/*',
+              'data/hyper/*',
+              'data/intellij/*',
+              'data/jupyter-book/*',
+              'data/linux/*',
+              'data/linux/applications/*',
+              'data/linux/apt/*',
+              'data/linux/autokey/data/My Phrases/*',
+              'data/linux/autokey/data/My Phrases/Addresses/*',
+              'data/linux/autostart/*',
+              'data/linux/crontab/*',
+              'data/linux/dictionary/*',
+              'data/linux/docker/*',
+              'data/linux/eclipse/*',
+              'data/linux/gedit/*',
+              'data/linux/gmail/*',
+              'data/linux/jabref/*',
+              'data/linux/lightdm/*',
+              'data/linux/mail/*',
+              'data/linux/network/*',
+              'data/linux/postfix/*',
+              'data/linux/r/*',
+              'data/linux/remmina/*',
+              'data/linux/rsnapshot/*',
+              'data/linux/synaptics/*',
+              'data/linux/tcpd/*',
+              'data/linux/terminator/*',
+              'data/linux/texlive/*',
+              'data/linux/uget/*',
+              'data/linux/wget/*',
+              'data/linux/xdg/*',
+              'data/linux/xsession/*',
+              'data/ls/*',
+              'data/nfs/*',
+              'data/nomachine/desktop/*',
+              'data/proxychains/*',
+              'data/pylint/*',
+              'data/pytype/*',
+              'data/rstudio-desktop/*',
+              'data/spark/*',
+              'data/ssh/client/*',
+              'data/ssh/server/*',
+              'data/teamdrive/*',
+              'data/tmux/*',
+              'data/vscode/*',
+              'data/xonsh/*',
+              'data/yapf/*']}
+
+install_requires = \
+['distro>=1.5.0',
+ 'dulwich>=0.20.24',
+ 'findspark>=1.4.2',
+ 'packaging>=20.4',
+ 'requests>=2.25.0',
+ 'tomlkit>=0.7.0',
+ 'tqdm>=4.48.2']
+
+entry_points = \
+{'console_scripts': ['xinstall = xinstall:main.main']}
+
+setup_kwargs = {
+    'name': 'xinstall',
+    'version': '0.51.1',
+    'description': 'Easy Cross-platform Installation and Configuration of Apps.',
+    'long_description': None,
+    'author': 'Benjamin Du',
+    'author_email': 'longendu@yahoo.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7.1,<4',
+}
+
+
+setup(**setup_kwargs)
