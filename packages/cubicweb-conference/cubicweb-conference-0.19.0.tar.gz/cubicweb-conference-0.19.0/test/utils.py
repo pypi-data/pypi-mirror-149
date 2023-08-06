a7d35296839523cc88c17540689ebbf1cc75554a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:15:13 +0000

    Add package cubicweb-conference/0.19.0/cubicweb-conference-0.19.0.tar.gz

---
+++ packages/cubicweb-conference/cubicweb-conference-0.19.0.tar.gz/cubicweb-conference-0.19.0/test/utils.py
@@ -0,0 +1,15 @@
+# -*- encoding: utf-8 -*-
+
+from cubicweb.devtools.testlib import CubicWebTC
+
+
+class ConferenceTC(CubicWebTC):
+    def setup_database(self):
+        with self.admin_access.repo_cnx() as cnx:
+            self.location = cnx.create_entity(
+                "PostalAddress",
+                street="1, rue du chat qui pêche",
+                postalcode="75005",
+                city="Paris",
+            ).eid
+            cnx.commit()
