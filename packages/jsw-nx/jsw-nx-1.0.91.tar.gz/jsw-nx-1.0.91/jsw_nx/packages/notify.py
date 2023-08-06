32c2cc6a5e0a60c605e04764f79690dcd782566e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:00 +0000

    Add package jsw-nx/1.0.91/jsw-nx-1.0.91.tar.gz

---
+++ packages/jsw-nx/jsw-nx-1.0.91.tar.gz/jsw-nx-1.0.91/jsw_nx/packages/notify.py
@@ -0,0 +1,15 @@
+import requests
+import os
+
+BASE_URL = 'https://api.day.app'
+
+
+def notify(**kwargs):
+    title = kwargs.get('title')
+    icon = kwargs.get('icon')
+    body = kwargs.get('body')
+    sound = kwargs.get('sound')
+    bark_key = kwargs.get('bark_key') or os.environ["BARK_SDK_KEY"]
+    dict_args = {title, icon, body, sound}
+    api_url = f'{BASE_URL}/{bark_key}'
+    return requests.post(api_url, json=dict_args)
