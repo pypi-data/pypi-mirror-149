35949302f80586c1fafe0e65a9ad5f48959fdf52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:21:08 +0000

    Add package omniversion/0.42.28/omniversion-0.42.28.tar.gz

---
+++ packages/omniversion/omniversion-0.42.28.tar.gz/omniversion-0.42.28/test_omniversion/pretty/pretty_test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+"""Test the pretty module"""
+from colorama import Fore, Back, Style
+
+from omniversion.pretty import pretty
+
+
+def test_black_on_white():
+    """`black_on_white` shows """
+    formatted_text = pretty.black_on_white("test")
+    assert Fore.BLACK in formatted_text
+    assert Back.WHITE in formatted_text
+    assert "test" in formatted_text
+    assert Style.RESET_ALL in formatted_text
