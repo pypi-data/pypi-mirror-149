b74ace73dc2ac8547dfc3ec2c243643e78f80b5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:40 +0000

    Add package checkov/2.0.1112/checkov-2.0.1112-py3-none-any.whl

---
+++ packages/checkov/checkov-2.0.1112-py3-none-any.whl/checkov/common/checks_infra/solvers/attribute_solvers/regex_match_attribute_solver.py
@@ -0,0 +1,21 @@
+import logging
+import re
+from typing import List, Optional, Any, Dict
+
+from checkov.common.graph.checks_infra.enums import Operators
+from checkov.common.checks_infra.solvers.attribute_solvers.base_attribute_solver import BaseAttributeSolver
+
+
+class RegexMatchAttributeSolver(BaseAttributeSolver):
+    operator = Operators.REGEX_MATCH
+
+    def __init__(self, resource_types: List[str], attribute: Optional[str], value: Any) -> None:
+        super().__init__(resource_types=resource_types,
+                         attribute=attribute, value=value)
+
+    def _get_operation(self, vertex: Dict[str, Any], attribute: Optional[str]) -> bool:
+        try:
+            return re.match(str(self.value), str(vertex.get(attribute))) is not None
+        except re.error as e:
+            logging.warn(f'failed to run regex {self.value} for attribute: {vertex.get(attribute)}, ', e)
+            return False
