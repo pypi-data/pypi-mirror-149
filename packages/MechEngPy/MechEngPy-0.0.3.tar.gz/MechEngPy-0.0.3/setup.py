d3158906059bca9c4345efc662c6cf1c220a0128
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:55 +0000

    Add package MechEngPy/0.0.3/MechEngPy-0.0.3.tar.gz

---
+++ packages/MechEngPy/MechEngPy-0.0.3.tar.gz/MechEngPy-0.0.3/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='MechEngPy',
+    version='0.0.3',
+    license='GPT-3',
+    author="Valter Alvares Gonzaga Filho",
+    author_email='valtergonzagafilho@gmail.com',
+    packages=find_packages('MechEngPy'),
+    package_dir={'': 'MechEngPy'},
+    url='https://github.com/valtervg13/MechPy/tree/main/MechPy',
+    keywords='Mechanical Engineering, Engineering, Mechanical Design, Machine Design',
+    install_requires=[
+          'NumPy',
+          'MatPlotLib',
+          'Scipy',
+          'Glob',
+          'Pandas'
+      ],
+
+)
