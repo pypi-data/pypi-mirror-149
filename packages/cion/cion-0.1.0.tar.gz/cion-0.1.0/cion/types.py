61e801473a80946d914e4fa08065245c80aba561
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:54 +0000

    Add package cion/0.1.0/cion-0.1.0.tar.gz

---
+++ packages/cion/cion-0.1.0.tar.gz/cion-0.1.0/cion/types.py
@@ -0,0 +1,52 @@
+from cion.exceptions import ValidatorError
+
+__all__ = (
+    "string",
+    "integer",
+)
+
+
+def string(cast: bool = True):
+    """A string type
+
+    Used when defining fields in :class:`cion.Schema`
+
+    Args:
+        cast (bool): Whether or not to try to cast the value to a :obj:`str`
+
+    Returns:
+        InnerValidator: The inner validator
+    """
+
+    def inner(value: str):
+        if not isinstance(value, str):
+            if cast:
+                value = str(value)
+            else:
+                raise ValidatorError("Field must be a valid string")
+        return value
+
+    return inner
+
+
+def integer(cast: bool = False):
+    """An integer type
+
+    Used when defining fields in :class:`cion.Schema`
+
+    Args:
+        cast (bool): Whether or not to try to cast the value to an :obj:`int`
+
+    Returns:
+        InnerValidator: The inner validator
+    """
+
+    def inner(value: int):
+        if not isinstance(value, int):
+            if cast:
+                value = int(value)
+            else:
+                raise ValidatorError("Field must be a valid integer")
+        return value
+
+    return inner
