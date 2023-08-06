a3664700afc61c08321cb5f430da554147a4f219
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:39:56 +0000

    Add package oso/0.26.1/oso-0.26.1-cp38-cp38-manylinux_2_12_x86_64.manylinux2010_x86_64.whl

---
+++ packages/oso/oso-0.26.1-cp38-cp38-manylinux_2_12_x86_64.manylinux2010_x86_64.whl/oso/__init__.py
@@ -0,0 +1,9 @@
+from polar import (
+    Variable,
+    Predicate,
+    Relation,
+    DataFilter,
+)
+from .oso import Oso
+from .exceptions import AuthorizationError, ForbiddenError, NotFoundError
+from polar.exceptions import OsoError
