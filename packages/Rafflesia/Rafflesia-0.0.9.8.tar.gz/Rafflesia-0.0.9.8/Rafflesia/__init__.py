847c526bd81f5cc58f484c83356ca6edb8893a39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:22 +0000

    Add package Rafflesia/0.0.9.8/Rafflesia-0.0.9.8.tar.gz

---
+++ packages/Rafflesia/Rafflesia-0.0.9.8.tar.gz/Rafflesia-0.0.9.8/Rafflesia/__init__.py
@@ -0,0 +1,8 @@
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "0"
+
+__version__ = "0.0.9.8"
+
+from Rafflesia.AudioManager import *
+from Rafflesia.GraphicsManager import *
+from Rafflesia.UtilsManager import *
