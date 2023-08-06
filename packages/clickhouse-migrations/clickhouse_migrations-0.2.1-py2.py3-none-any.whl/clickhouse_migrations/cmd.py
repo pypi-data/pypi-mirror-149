fd6f72e942c73b4072bd93d92d8c5d8f42bbbd74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:00 +0000

    Add package clickhouse-migrations/0.2.1/clickhouse_migrations-0.2.1-py2.py3-none-any.whl

---
+++ packages/clickhouse-migrations/clickhouse_migrations-0.2.1-py2.py3-none-any.whl/clickhouse_migrations/cmd.py
@@ -0,0 +1,62 @@
+import os
+import sys
+from argparse import ArgumentParser
+from pathlib import Path
+
+from clickhouse_migrations.clickhouse_cluster import ClickhouseCluster
+from clickhouse_migrations.defaults import (
+    DB_HOST,
+    DB_NAME,
+    DB_PASSWORD,
+    DB_USER,
+    MIGRATIONS_DIR,
+)
+
+
+def get_context(args):
+    parser = ArgumentParser()
+    # detect configuration
+    parser.add_argument(
+        "--db-host",
+        default=os.environ.get("DB_HOST", DB_HOST),
+        help="Clickhouse database hostname",
+    )
+    parser.add_argument(
+        "--db-user",
+        default=os.environ.get("DB_USER", DB_USER),
+        help="Clickhouse user",
+    )
+    parser.add_argument(
+        "--db-password",
+        default=os.environ.get("DB_PASSWORD", DB_PASSWORD),
+        help="Clickhouse password",
+    )
+    parser.add_argument(
+        "--db-name",
+        default=os.environ.get("DB_NAME", DB_NAME),
+        help="Clickhouse database name",
+    )
+    parser.add_argument(
+        "--migrations-dir",
+        default=os.environ.get("MIGRATIONS_DIR", MIGRATIONS_DIR),
+        help="Path to list of migration files",
+    )
+    parser.add_argument(
+        "--multi-statement",
+        default=os.environ.get("MULTI_STATEMENT", "1"),
+        help="Path to list of migration files",
+    )
+
+    return parser.parse_args(args)
+
+
+def migrate(context) -> int:
+    cluster = ClickhouseCluster(context.db_host, context.db_user, context.db_password)
+    cluster.migrate(
+        context.db_name, Path(context.migrations_dir), int(context.multi_statement) == 1
+    )
+    return 0
+
+
+def main() -> int:
+    return migrate(get_context(sys.argv[1:]))
