5ad4384e3ed5b7bc45b4c7d3a840660305c22696
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:17:39 +0000

    Add package dbyml/1.2.0/dbyml-1.2.0-py3-none-any.whl

---
+++ packages/dbyml/dbyml-1.2.0-py3-none-any.whl/dbyml/registry.py
@@ -0,0 +1,70 @@
+import re
+from typing import Optional
+
+import requests
+from requests.auth import HTTPBasicAuth
+
+
+class Registry:
+    def __init__(
+        self,
+        protocol: str = "http",
+        host: str = "",
+        port: str = "",
+        name: str = "",
+        tag: str = "",
+        namespace: str = "",
+        username: str = "",
+        password: str = "",
+    ) -> None:
+        self.protocol = protocol
+        self.host = host
+        self.port = port
+        if self.port != "" and self.host != "":
+            self.registry = f"{self.host}:{self.port}"
+        else:
+            self.registry = ""
+
+        self.name = name
+        self.tag = tag
+        self.image_name = f"{self.name}:{self.tag}"
+
+        self.namespace = namespace
+        if self.namespace != "":
+            self.url = f"{self.protocol}://{self.registry}/v2/{self.namespace}/{self.name}/manifests/{self.tag}"
+            self.repository = f"{self.registry}/{self.namespace}/{self.image_name}"
+        else:
+            self.url = (
+                f"{self.protocol}://{self.registry}/v2/{self.name}/manifests/{self.tag}"
+            )
+            self.repository = f"{self.registry}/{self.name}"
+
+        self.username = username
+        self.password = password
+        self.auth = HTTPBasicAuth(self.username, self.password)
+
+        self.headers = {
+            "Accept": "application/vnd.docker.distribution.manifest.v2+json"
+        }
+
+    def get_digest(self) -> Optional[str]:
+        ret = requests.get(self.url, headers=self.headers, auth=self.auth)
+        if ret.status_code == 200:
+            return ret.headers.get("Docker-Content-Digest")
+        else:
+            return None
+
+    def remove_repo_image(self) -> None:
+        self.digest = self.get_digest()
+        if self.digest is not None:
+            # Put the digest into url.
+            url = re.sub(r"/[^/]+$", f"/{self.digest}", self.url)
+            ret = requests.delete(url, headers=self.headers, auth=self.auth)
+            if ret.status_code == 202:
+                print(
+                    f"{self.repository} has been successfully removed from repository."
+                )
+            else:
+                print(f"{self.repository} cannot be removed. ...skip.")
+        else:
+            print(f"{self.image_name} not found in {self.registry}.")
