4800082a795d85f607430820a4a9378989d1a436
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:22:40 +0000

    Add package ert/2.35.0/ert-2.35.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/ert/ert-2.35.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/res/enkf/local_obsdata.py
@@ -0,0 +1,3 @@
+from res._lib.local.local_obsdata import LocalObsdata
+
+__all__ = ["LocalObsdata"]
---
+++ packages/ert/ert-2.35.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/res/enkf/local_obsdata_node.py
@@ -0,0 +1,6 @@
+# flake8: noqa
+# fmt: off
+from res._lib.local.local_obsdata_node import LocalObsdataNode  # pylint: disable=unused-import
+# fmt: on
+
+__all__ = ["LocalObsDataNode"]
