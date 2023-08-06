c86e772304f733a4ea48d66868c21d75249aa685
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:39:15 +0000

    Add package luogu/0.1.0/luogu-0.1.0-py3-none-any.whl

---
+++ packages/luogu/luogu-0.1.0-py3-none-any.whl/luogu/__init__.py
@@ -0,0 +1,25 @@
+"""
+PyLuogu
+=======
+
+A model-based Python implement for Luogu API client
+
+洛谷 API 客户端基于模型的 Python 实现
+"""
+
+from .exceptions import AccessDeniedHttpException, HttpException, NotFoundHttpException
+from .models.main import Problem, User
+from .models.paste import Paste
+from .session import Session
+
+__version__ = "0.1.0"
+
+__all__ = (
+    "AccessDeniedHttpException",
+    "HttpException",
+    "NotFoundHttpException",
+    "Paste",
+    "Problem",
+    "Session",
+    "User",
+)
