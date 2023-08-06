c1d67b251fc8769056edc7bea86161fb79be7692
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:42 +0000

    Add package polars-u64-idx/0.13.31/polars_u64_idx-0.13.31.tar.gz

---
+++ packages/polars-u64-idx/polars_u64_idx-0.13.31.tar.gz/polars_u64_idx-0.13.31/local_dependencies/polars-lazy/src/physical_plan/expressions/not.rs
@@ -0,0 +1,50 @@
+use crate::physical_plan::state::ExecutionState;
+use crate::prelude::*;
+use polars_core::frame::groupby::GroupsProxy;
+use polars_core::prelude::*;
+use std::sync::Arc;
+
+pub struct NotExpr(Arc<dyn PhysicalExpr>, Expr);
+
+impl NotExpr {
+    pub fn new(physical_expr: Arc<dyn PhysicalExpr>, expr: Expr) -> Self {
+        Self(physical_expr, expr)
+    }
+
+    fn finish(&self, series: Series) -> Result<Series> {
+        if let Ok(ca) = series.bool() {
+            Ok((!ca).into_series())
+        } else {
+            Err(PolarsError::InvalidOperation(
+                format!("NotExpr expected a boolean type, got: {:?}", series).into(),
+            ))
+        }
+    }
+}
+impl PhysicalExpr for NotExpr {
+    fn as_expression(&self) -> &Expr {
+        &self.1
+    }
+
+    fn evaluate(&self, df: &DataFrame, state: &ExecutionState) -> Result<Series> {
+        let series = self.0.evaluate(df, state)?;
+        self.finish(series)
+    }
+    #[allow(clippy::ptr_arg)]
+    fn evaluate_on_groups<'a>(
+        &self,
+        df: &DataFrame,
+        groups: &'a GroupsProxy,
+        state: &ExecutionState,
+    ) -> Result<AggregationContext<'a>> {
+        let mut ac = self.0.evaluate_on_groups(df, groups, state)?;
+        let s = ac.flat_naive().into_owned();
+        ac.with_series(self.finish(s)?, false);
+
+        Ok(ac)
+    }
+
+    fn to_field(&self, _input_schema: &Schema) -> Result<Field> {
+        Ok(Field::new("not", DataType::Boolean))
+    }
+}
