5d5cb8b4d42f6a56c547952b6449518d6dd40380
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:43 +0000

    Add package jin-test-packet/0.0.1/jin_test_packet-0.0.1-py3-none-any.whl

---
+++ packages/jin-test-packet/jin_test_packet-0.0.1-py3-none-any.whl/deri_log/say_hello.py
@@ -0,0 +1,29 @@
+# _*_ coding:utf-8 _*_
+# ------------------------
+# @Author   :    Jack    -
+# @Time     : 2022-04-14 -
+#   Time never speaks,   -
+# but proves many things -
+# ------------------------
+
+
+def say_hello():
+    """
+    say hello
+    :return:
+    """
+    print('Hello World')
+
+
+def say_name(name: str):
+    """可以打印任何人的名字并说你好。
+
+    这就是我玩着玩的
+
+    Args:
+        name: 任意字符串
+
+    Returns:
+        一个字符串
+    """
+    print(f"hello {name},恭喜你成为尊贵的体验者，这是梦开始的地方，但绝不止于此")
