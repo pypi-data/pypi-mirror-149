641aa40c16887909b85f4ef1efd9a03c54792c62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:14:29 +0000

    Add package neoteroi-mkdocs/0.0.4/neoteroi-mkdocs-0.0.4.tar.gz

---
+++ packages/neoteroi-mkdocs/neoteroi-mkdocs-0.0.4.tar.gz/neoteroi-mkdocs-0.0.4/setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup
+
+
+def readme():
+    with open("README.md", mode="rt", encoding="utf8") as readme:
+        return readme.read()
+
+
+setup(
+    name="neoteroi-mkdocs",
+    version="0.0.4",
+    description="Plugins for MkDocs and Python Markdown",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+    ],
+    url="https://github.com/Neoteroi/mkdocs-plugins",
+    author="RobertoPrevato",
+    author_email="roberto.prevato@gmail.com",
+    keywords="MkDocs OpenAPI Swagger Markdown plugins extensions documentation",
+    license="MIT",
+    packages=[
+        "neoteroi.markdown",
+        "neoteroi.markdown.tables",
+        "neoteroi.mkdocsoad",
+        "neoteroi.spantable",
+    ],
+    install_requires=["essentials-openapi[full]", "mkdocs"],
+    entry_points={
+        "mkdocs.plugins": [
+            "neoteroi.mkdocsoad = neoteroi.mkdocsoad:MkDocsOpenAPIDocumentationPlugin",
+        ],
+        "markdown.extensions": [
+            "neoteroi.spantable = neoteroi.spantable:SpanTableExtension"
+        ],
+    },
+    include_package_data=True,
+    zip_safe=False,
+)
