33b02507583529a811b5d4d908bd8c1b9f5d7ae4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:46:00 +0000

    Add package house-pricing-package/1.0.0.5/house-pricing-package-1.0.0.5.tar.gz

---
+++ packages/house-pricing-package/house-pricing-package-1.0.0.5.tar.gz/house-pricing-package-1.0.0.5/setup.py
@@ -0,0 +1,20 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='house-pricing-package',
+    version='1.0.0.5',
+    license='MIT',
+    author="Patrick Saade",
+    author_email='patrick_saade@hotmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/Patrick844/dsp-patrick-saade/',
+    keywords='house pricing project',
+    install_requires=[
+          'scikit-learn',
+          'pandas',
+          'numpy',
+      ],
+
+)
