d46503a0656186b9ea98a014baab4f3274aeb9e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:26:22 +0000

    Add package freecad-stubs/1.0.11/freecad_stubs-1.0.11-py3-none-any.whl

---
+++ packages/freecad-stubs/freecad_stubs-1.0.11-py3-none-any.whl/TechDrawGui-stubs/__init__.pyi
@@ -0,0 +1,34 @@
+# AppTechDrawGuiPy.cpp
+def export(arg1, arg2: str, /) -> None:
+    """
+    TechDraw hook for FC Gui exporter.
+    Possible exceptions: (Exception, TypeError).
+    """
+
+
+def exportPageAsPdf(DrawPageObject, FilePath: str, /) -> None:
+    """
+    exportPageAsPdf(DrawPageObject,FilePath) -- print page as Pdf to file.
+    Possible exceptions: (TypeError, Exception).
+    """
+
+
+def exportPageAsSvg(DrawPageObject, FilePath: str, /) -> None:
+    """
+    exportPageAsSvg(DrawPageObject,FilePath) -- print page as Svg to file.
+    Possible exceptions: (TypeError, Exception).
+    """
+
+
+def copyActiveViewToSvgFile(arg1, arg2: str, arg3: float = None, arg4: float = None, arg5=None, arg6=None, arg7: float = None, arg8: float = None, arg9: int = None, /) -> float:
+    """
+    copyActiveViewToSvgFile(DrawPageObject,FilePath) -- copy ActiveView to Svg file.
+    Possible exceptions: (TypeError, Exception).
+    """
+
+
+def addQGIToView(View, QGraphicsItem, /) -> None:
+    """
+    addQGIToView(View, QGraphicsItem) -- insert graphics item into view's graphic.
+    Possible exceptions: (TypeError, Exception, RuntimeError).
+    """
