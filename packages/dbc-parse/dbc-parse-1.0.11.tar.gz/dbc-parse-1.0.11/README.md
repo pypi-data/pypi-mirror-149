346edd89fffb06e91347b2f976210e2a6e449660
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:40 +0000

    Add package dbc-parse/1.0.11/dbc-parse-1.0.11.tar.gz

---
+++ packages/dbc-parse/dbc-parse-1.0.11.tar.gz/dbc-parse-1.0.11/README.md
@@ -0,0 +1,12 @@
+# parse-dbc  Parse DBC Files
+
+## 说明
+
+这个库是用来解析CAN Bus DBC文件的
+
+## 使用
+
+```python
+pip install parse-dbc
+```
+
---
+++ packages/dbc-parse/dbc-parse-1.0.11.tar.gz/dbc-parse-1.0.11/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+# Create Time: 2022/2/13 11:11
+# Author: nzj
+# Function：
+from setuptools import find_packages, setup
+
+
+def read_long_description_from_readme():
+    with open("README.md", "r", encoding="utf-8") as fh:
+        return fh.read()
+
+setup(
+    name="dbc-parse",
+    version="1.0.11",
+    packages=find_packages(include=["dbc_parse"]),
+    description="Database Parse",
+    long_description=read_long_description_from_readme(),
+    long_description_content_type="text/markdown",
+    author="nzj",
+    author_email="nzjwoaini9@gmail.com",
+    keywords=["database"],
+    package_data={
+        # Include anything in the data directory
+        "openstef_dbc": ["data/*", "*.license"]
+    },
+    python_requires=">=3.9.0",
+    classifiers=[
+        r"Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        r"License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
