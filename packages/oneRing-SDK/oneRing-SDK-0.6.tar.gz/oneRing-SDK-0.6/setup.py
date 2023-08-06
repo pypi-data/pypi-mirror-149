99ab22e43fa1351bc2c49b33359acddc38b46e56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:10 +0000

    Add package oneRing-SDK/0.6/oneRing-SDK-0.6.tar.gz

---
+++ packages/oneRing-SDK/oneRing-SDK-0.6.tar.gz/oneRing-SDK-0.6/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+setup(
+  name = 'oneRing-SDK',
+  packages = ['oneRing'],
+  version = '0.6',
+  license='MIT',
+  description = 'SDK for a public Lord of the Rings API',
+  author = 'Aaron Hesse',
+  author_email = 'aaronhesse@protonmail.com',
+  url = 'https://github.com/aaronhesse/oneRing-SDK',
+  download_url = 'https://github.com/aaronhesse/oneRing-SDK/archive/refs/tags/v0.6.tar.gz',
+  keywords = ['API', 'SDK', 'Lord of The Rings', 'oneRing'],
+  install_requires = [
+          'requests',
+          'urllib3'
+      ],
+  classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
