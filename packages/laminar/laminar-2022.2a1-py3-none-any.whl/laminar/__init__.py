ef3060b1d84cdd7a25f589086f0311431a7d5d3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:36 +0000

    Add package laminar/2022.2a1/laminar-2022.2a1-py3-none-any.whl

---
+++ packages/laminar/laminar-2022.2a1-py3-none-any.whl/laminar/__init__.py
@@ -0,0 +1,6 @@
+from laminar.components import Flow as Flow
+from laminar.components import Layer as Layer
+from laminar.version import VERSION as VERSION
+
+__all__ = ["Flow", "Layer"]
+__version__ = VERSION
