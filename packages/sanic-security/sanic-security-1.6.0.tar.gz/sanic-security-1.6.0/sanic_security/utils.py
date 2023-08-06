826babe4fefe82f63dfe3a3312c3677034c72ad3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:39 +0000

    Add package sanic-security/1.6.0/sanic-security-1.6.0.tar.gz

---
+++ packages/sanic-security/sanic-security-1.6.0.tar.gz/sanic-security-1.6.0/sanic_security/utils.py
@@ -0,0 +1,70 @@
+import os
+
+from sanic.request import Request
+from sanic.response import json as sanic_json, HTTPResponse
+
+"""
+An effective, simple, and async security library for the Sanic framework.
+Copyright (C) 2021 Aidan Stewart
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+
+def get_ip(request: Request) -> str:
+    """
+    Retrieves ip address from client request.
+
+    Args:
+        request (Request): Sanic request parameter.
+
+    Returns:
+        ip
+    """
+    return request.remote_addr if request.remote_addr else request.ip
+
+
+def dir_exists(path: str) -> bool:
+    """
+    Checks if path exists and isn't empty. Creates new path if neither of these conditions are met.
+
+    Args:
+        path (str): Path being checked.
+
+    Returns:
+        exists
+    """
+    try:
+        os.makedirs(path)
+        exists = False
+    except FileExistsError:
+        exists = os.listdir(path)
+    return exists
+
+
+def json(message: str, data, status_code: int = 200) -> HTTPResponse:
+    """
+    A preformatted Sanic json response.
+
+    Args:
+        message (int): Message describing data or relaying human readable information.
+        data (Any): Raw information to be used by client.
+        status_code (int): HTTP response code.
+
+    Returns:
+        json
+    """
+    return sanic_json(
+        {"message": message, "code": status_code, "data": data}, status=status_code
+    )
