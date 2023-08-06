9c8a25ffad0596f13c2bc675e507541079c3680f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:48 +0000

    Add package pyqt-media-slider/0.0.1/pyqt-media-slider-0.0.1.tar.gz

---
+++ packages/pyqt-media-slider/pyqt-media-slider-0.0.1.tar.gz/pyqt-media-slider-0.0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyqt-media-slider',
+    version='0.0.1',
+    author='Jung Gyu Yoon',
+    author_email='yjg30737@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    package_data={'pyqt_media_slider.style': ['slider.css']},
+    description='PyQt media slider',
+    url='https://github.com/yjg30737/pyqt-media-slider.git',
+    install_requires=[
+        'PyQt5>=5.8',
+        'pyqt-resource-helper>=0.0.1'
+    ]
+)
