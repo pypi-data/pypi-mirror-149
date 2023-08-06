7e3b8ce6d1e5d29981732e68e5910e415f04dbb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:52 +0000

    Add package cnext/0.3.4/cnext-0.3.4.tar.gz

---
+++ packages/cnext/cnext-0.3.4.tar.gz/cnext-0.3.4/cyc_next_app/lib/components/Logo.tsx
@@ -0,0 +1,18 @@
+import { Box, CssBaseline } from "@mui/material";
+import React from "react";
+import { Logo, LogoIcon } from "./StyledComponents";
+
+const LogoComponent = () => {
+  return (
+    <Box>
+        <Logo>
+            <LogoIcon
+                alt=""
+                src="/favicon.ico"
+            />
+        </Logo>
+    </Box>
+  );
+};
+
+export default LogoComponent;
