6d773544eb48fc92f99965528160a5509a10b4f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:53 +0000

    Add package bibcodex/1.1.1/bibcodex-1.1.1.tar.gz

---
+++ packages/bibcodex/bibcodex-1.1.1.tar.gz/bibcodex-1.1.1/bibcodex/_version.py
@@ -0,0 +1,9 @@
+__version__ = "1.1.1"
+
+"""
+Version 1.1.1: Fix bug from requirements not in setup.py
+Version 1.1.0: Semantic embeddings (SPECTER) available.
+Version 1.0.2: Fix bug in validate. Progres bar not shown when empty.
+Version 1.0.1: Add tqdm status bar to downloads.
+Version 1.0.0: First release. Basic downloading enabled.
+"""
