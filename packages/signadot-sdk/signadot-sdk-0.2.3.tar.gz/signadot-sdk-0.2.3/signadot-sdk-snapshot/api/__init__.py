35d8671752e95206bfc8c7e5c08b0fa107a8158a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:46:10 +0000

    Add package signadot-sdk/0.2.3/signadot-sdk-0.2.3.tar.gz

---
+++ packages/signadot-sdk/signadot-sdk-0.2.3.tar.gz/signadot-sdk-0.2.3/signadot-sdk-snapshot/api/__init__.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from signadot-sdk-snapshot.api.cluster_api import ClusterApi
+from signadot-sdk-snapshot.api.sandboxes_api import SandboxesApi
