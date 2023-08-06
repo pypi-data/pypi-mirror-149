4cc74dce0628bac45ba97209c80422a97efe739d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:17 +0000

    Add package BioSAK/1.43.15/BioSAK-1.43.15.tar.gz

---
+++ packages/BioSAK/BioSAK-1.43.15.tar.gz/BioSAK-1.43.15/tmp_scripts/tmp_9.py
@@ -0,0 +1,55 @@
+
+import math
+import random
+import argparse
+import seaborn as sns
+
+
+def get_color_list(color_num):
+
+    if color_num <= 8:
+        color_list_combined = ['#3787c0', '#39399f', '#ffb939', '#399f39', '#9f399f', '#fb694a', '#9f9f39', '#959595']
+
+    elif 8 < color_num <= 16:
+        color_list_combined = ['#2b7bba', '#89bedc', '#2e2e99', '#8a8acc', '#ffa500', '#ffc55c', '#2e992e', '#8acc8a', '#992e99', '#cc8acc', '#d52221', '#fc8161', '#99992e', '#cccc8a', '#5c5c5c', '#adadad']
+
+    else:
+        color_num_each = math.ceil(color_num/8) + 2
+
+        color_list_1 = sns.color_palette('Blues',  n_colors=color_num_each).as_hex()
+        color_list_2 = sns.light_palette('navy',   n_colors=color_num_each).as_hex()
+        color_list_3 = sns.light_palette('orange', n_colors=color_num_each).as_hex()
+        color_list_4 = sns.light_palette('green',  n_colors=color_num_each).as_hex()
+        color_list_5 = sns.light_palette('purple', n_colors=color_num_each).as_hex()
+        color_list_6 = sns.color_palette('Reds',   n_colors=color_num_each).as_hex()
+        color_list_7 = sns.light_palette('olive',  n_colors=color_num_each).as_hex()
+        color_list_8 = sns.color_palette('Greys', n_colors=color_num_each).as_hex()
+
+        color_list_combined = []
+        for color_list in [color_list_1, color_list_2, color_list_3, color_list_4, color_list_5, color_list_6, color_list_7, color_list_8]:
+            for color in color_list[2:][::-1]:
+                color_list_combined.append(color)
+
+    color_list_to_return = random.sample(color_list_combined, color_num)
+
+    color_list_to_return_sorted = []
+    for color_to_return in color_list_combined:
+        if color_to_return in color_list_to_return:
+            color_list_to_return_sorted.append(color_to_return)
+
+    return color_list_to_return_sorted
+
+
+# for each in open('/Users/songweizhi/Desktop/bin_list_with_path.txt'):
+#
+#     pwd_bin = each.strip()
+#     bin_id = pwd_bin.split('/')[-1].split('.')[0]
+#
+#     blastn_cmd = 'blastn -query %s -db combined_hc_refs.fa -out %s_vs_ref.tab -evalue 1e-5 -outfmt "6 qseqid sseqid pident length mismatch gapopen qstart qend sstart send evalue bitscore qlen slen" -task blastn -num_threads 12' % (pwd_bin, bin_id)
+#     print(blastn_cmd)
+
+
+color_list = get_color_list(5)
+print(color_list)
+print(random.shuffle(color_list))
+
