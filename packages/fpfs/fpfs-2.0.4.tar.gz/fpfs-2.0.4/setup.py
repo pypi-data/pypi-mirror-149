4c028dc8a902591c5d8b979dd8d5ae0e29f39e5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:44 +0000

    Add package fpfs/2.0.4/fpfs-2.0.4.tar.gz

---
+++ packages/fpfs/fpfs-2.0.4.tar.gz/fpfs-2.0.4/setup.py
@@ -0,0 +1,22 @@
+import numpy
+from setuptools import setup, find_packages
+
+setup(
+    name='fpfs',
+    version='2.0.4',
+    description='FPFS shear estimator',
+    author='Xiangchong Li et al.',
+    author_email='mr.superonion@hotmail.com',
+    python_requires='>=3.6',
+    install_requires=[
+        'numba',
+        'numpy',
+        'scipy',
+        'galsim',
+    ],
+    include_dirs=numpy.get_include(),
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    url = "https://github.com/mr-superonion/FPFS/",
+)
