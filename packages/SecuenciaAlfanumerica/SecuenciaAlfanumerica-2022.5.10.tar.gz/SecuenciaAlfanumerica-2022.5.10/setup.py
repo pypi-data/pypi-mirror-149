74bec4162dfd85082cad94a3b05824505583aba2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:33 +0000

    Add package SecuenciaAlfanumerica/2022.5.10/SecuenciaAlfanumerica-2022.5.10.tar.gz

---
+++ packages/SecuenciaAlfanumerica/SecuenciaAlfanumerica-2022.5.10.tar.gz/SecuenciaAlfanumerica-2022.5.10/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+readme = open("./README.md", "r") # Usar contenido de archivo (No detecta acentos)
+
+setup(
+    name="SecuenciaAlfanumerica",
+    packages=['SecuenciaAlfanumerica'], # Este debe ser el mismo que el nombre de la carpeta
+    version="2022.05.10",
+    description="Convierte de un número o números a un grupo de caracteres y viceversa",
+    long_description=readme.read(),
+    long_description_content_type='text/markdown',
+    author="Xaival",
+    author_email="xaival.dark@gmail.com",
+    url='https://github.com/Xaival/Libreria-Python-Secuencia-alfanumerica', # Usar la URL del repositorio de github
+    keywords=['conversions', 'sequence', 'alphanumeric', 'binary'],
+    classifiers=[ ],
+    license='MIT',
+    include_package_data=True
+)
