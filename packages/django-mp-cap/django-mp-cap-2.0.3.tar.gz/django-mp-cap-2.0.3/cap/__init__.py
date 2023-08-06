61b6309aae3927cfa856e04b42046e47bd9d8107
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:55 +0000

    Add package django-mp-cap/2.0.3/django-mp-cap-2.0.3.tar.gz

---
+++ packages/django-mp-cap/django-mp-cap-2.0.3.tar.gz/django-mp-cap-2.0.3/cap/__init__.py
@@ -0,0 +1,23 @@
+
+from django.conf.global_settings import DATE_INPUT_FORMATS
+
+
+def setup_settings(settings, **kwargs):
+
+    settings['INSTALLED_APPS'] += [
+        app for app in [
+            'cap.configs.SuitConfig',
+            'django.contrib.admin.apps.SimpleAdminConfig',
+            'adminplus',
+            'adminsortable2',
+            'easy_select2'
+        ] if app not in settings['INSTALLED_APPS']
+    ]
+
+    if 'DATE_INPUT_FORMATS' not in settings:
+        settings['DATE_INPUT_FORMATS'] = DATE_INPUT_FORMATS
+
+    date_format = '%d/%m/%Y'
+
+    if date_format not in settings['DATE_INPUT_FORMATS']:
+        settings['DATE_INPUT_FORMATS'].append(date_format)
