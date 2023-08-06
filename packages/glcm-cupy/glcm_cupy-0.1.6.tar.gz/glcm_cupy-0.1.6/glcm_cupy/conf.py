2e9219f33cce0523d539e409852f6040363435dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:10 +0000

    Add package glcm-cupy/0.1.6/glcm_cupy-0.1.6.tar.gz

---
+++ packages/glcm-cupy/glcm_cupy-0.1.6.tar.gz/glcm_cupy-0.1.6/glcm_cupy/conf.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).parent.parent.absolute()
+
+MAX_VALUE_SUPPORTED = 256
+NO_OF_VALUES_SUPPORTED = 256 ** 2
+
+MAX_THREADS = 512  # Lowest Maximum supported threads.
+
+NO_OF_FEATURES = 6
+
+# For a 10000 x 256 x 256 GLCM, you need ~ 600MB of memory.
+MAX_PARTITION_SIZE = 10000
+
+HOMOGENEITY = 0
+CONTRAST = 1
+ASM = 2
+MEAN = 3
+VAR = 4
+CORRELATION = 5
