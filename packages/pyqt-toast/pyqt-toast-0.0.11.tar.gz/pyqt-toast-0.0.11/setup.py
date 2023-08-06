8b8d7ef14cf9bb86576fd292d648b933cba11458
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:14 +0000

    Add package pyqt-toast/0.0.11/pyqt-toast-0.0.11.tar.gz

---
+++ packages/pyqt-toast/pyqt-toast-0.0.11.tar.gz/pyqt-toast-0.0.11/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pyqt-toast',
+    version='0.0.11',
+    author='Jung Gyu Yoon',
+    author_email='yjg30737@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    package_data={'pyqt_toast.style': ['background.css', 'foreground.css']},
+    description='PyQt Toast (Small message displayed on the screen, visible for a short time)',
+    url='https://github.com/yjg30737/pyqt-toast.git',
+    install_requires=[
+        'PyQt5>=5.8',
+        'pyqt-resource-helper>=0.0.1'
+    ]
+)
