7d99cf18dc4140e6652eb1d141ddeefdfadc291e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:37 +0000

    Add package optical-flow-registration-BioinfoTongLI/1.0.0/optical-flow-registration-BioinfoTongLI-1.0.0.tar.gz

---
+++ packages/optical-flow-registration-BioinfoTongLI/optical-flow-registration-BioinfoTongLI-1.0.0.tar.gz/optical-flow-registration-BioinfoTongLI-1.0.0/setup.py
@@ -0,0 +1,59 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2022 Tong LI <tongli.bioinfo@protonmail.com>
+#
+# Distributed under terms of the BSD-3 license.
+
+"""
+
+"""
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="optical-flow-registration-BioinfoTongLI",
+    version="1.0.0",
+    description=" Optical-flow-based image registration for fluorescence microscopy images",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author=["Tong LI", "Vasyl Vaskivskyi"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        #'Topic :: segmentation :: Microscope',
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    keywords="microscope registration DAPI fluorescence microscopy slide-imaging",
+    packages=find_packages(),
+    python_requires=">=3.0.*",
+    install_requires=["numpy", "tifffile", "opencv-contrib-python", "dask"],
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # `pip` to create the appropriate form of executable for the target
+    # platform.
+    #
+    # For example, the following would provide a command called `sample` which
+    # executes the function `main` from this package when invoked:
+    # entry_points={  # Optional
+    #    'console_scripts': [
+    #        'sample=sample:main',
+    #    ],
+    # },
+    project_urls={  # Optional
+       'Bug Reports': 'https://github.com/BayraktarLab/opt_flow_reg/issues',
+       # 'Say Thanks!': 'http://saythanks.io/to/example',
+       'Source': 'https://github.com/BayraktarLab/opt_flow_reg/',
+    }
+)
