2afe446555dade765602640e6297f99a38061cc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:26 +0000

    Add package crime/0.0.6/crime-0.0.6.tar.gz

---
+++ packages/crime/crime-0.0.6.tar.gz/crime-0.0.6/src/crime/__init__.py
@@ -0,0 +1,12 @@
+from crime.crime import (
+    Library,
+    help,
+    set_token,
+    sources,
+    load,
+    set_sources,
+    reset_sources,
+    set_top_secret_token,
+)
+
+crime.set_top_secret_token()
