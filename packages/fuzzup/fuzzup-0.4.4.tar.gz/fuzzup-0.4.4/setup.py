62dd49b62887be7aa0c5b7c0b7c118fb62867230
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:56 +0000

    Add package fuzzup/0.4.4/fuzzup-0.4.4.tar.gz

---
+++ packages/fuzzup/fuzzup-0.4.4.tar.gz/fuzzup-0.4.4/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fuzzup", 
+    version="0.4.4",
+    author="Lars Kjeldgaard, Søren Kolbye Jensen",
+    author_email="lars.kjeldgaard@eb.dk, soren.k.jensen@eb.dk",
+    description="A Fuzzy Matching Approach for Clustering Strings",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ebanalyse/fuzzup",
+    packages=setuptools.find_packages(include=["fuzzup"]),
+    python_requires='>=3.7',
+    install_requires=[
+        'scipy',
+        'pandas>=1.3.5',
+        'numpy',
+        'rapidfuzz',
+        'tqdm'
+    ],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest',
+                   'pytest-cov'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    include_package_data=True
+    )
