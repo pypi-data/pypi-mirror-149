6911b1ea6f15b6ee5b6d91717b6e2d657352ae42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:55 +0000

    Add package pi2143ooppg/0.2/pi2143ooppg-0.2.tar.gz

---
+++ packages/pi2143ooppg/pi2143ooppg-0.2.tar.gz/pi2143ooppg-0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='pi2143ooppg',
+      version='0.2',
+      description='OOP tasks with pygame',
+      packages=['pi2143ooppg'],
+      author_email='good-i-evil@yandex.ru',
+      zip_safe=False)
