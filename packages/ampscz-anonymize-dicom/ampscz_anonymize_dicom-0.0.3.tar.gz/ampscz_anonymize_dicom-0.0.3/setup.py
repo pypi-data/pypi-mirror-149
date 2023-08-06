85367b103da362de08d1c6111eca2b19dad9aa92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:45 +0000

    Add package ampscz-anonymize-dicom/0.0.3/ampscz_anonymize_dicom-0.0.3.tar.gz

---
+++ packages/ampscz-anonymize-dicom/ampscz_anonymize_dicom-0.0.3.tar.gz/ampscz_anonymize_dicom-0.0.3/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+from os.path import join
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ampscz_anonymize_dicom",
+    version="0.0.3",
+    author="Kevin Cho",
+    author_email="kevincho@bwh.harvard.edu",
+    description="AMP-SCZ Dicom anonymizer",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AMP-SCZ/ampscz_anonymize_dicom",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires='>=3.7',
+    install_requires=['tk>=0.1.0',
+                      'pydicom>=2.1.2',
+                      'tkinterdnd2>=0.3.0'],
+    scripts=['scripts/ampscz_anonymize_dicom']
+)
+
+
