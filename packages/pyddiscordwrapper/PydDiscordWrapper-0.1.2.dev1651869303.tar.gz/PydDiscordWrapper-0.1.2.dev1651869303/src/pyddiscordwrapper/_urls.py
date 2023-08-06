919545d4b08e3110f2b0c507077c2a59b036bc4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:00 +0000

    Add package pyddiscordwrapper/0.1.2.dev1651869303/PydDiscordWrapper-0.1.2.dev1651869303.tar.gz

---
+++ packages/pyddiscordwrapper/PydDiscordWrapper-0.1.2.dev1651869303.tar.gz/PydDiscordWrapper-0.1.2.dev1651869303/src/pyddiscordwrapper/_urls.py
@@ -0,0 +1,4 @@
+API_BASE_URL = 'https://discordapp.com/api'
+
+AUTHORIZATION_URL = API_BASE_URL + '/oauth2/authorize?client_id={client_id}&scope={scope}&redirect_uri={redirect_uri}'
+TOKEN_URL = API_BASE_URL + '/oauth2/token'
