79d0e330a00a90c02c3dc1df26cdbd68b7d4470d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:09:46 +0000

    Add package DeepXDE/1.2.0/DeepXDE-1.2.0-py3-none-any.whl

---
+++ packages/DeepXDE/DeepXDE-1.2.0-py3-none-any.whl/deepxde/data/data.py
@@ -0,0 +1,39 @@
+import abc
+
+
+class Data(abc.ABC):
+    """Data base class."""
+
+    @abc.abstractmethod
+    def losses(self, targets, outputs, loss, model):
+        """Return a list of losses, i.e., constraints."""
+
+    @abc.abstractmethod
+    def train_next_batch(self, batch_size=None):
+        """Return a training dataset of the size `batch_size`."""
+
+    @abc.abstractmethod
+    def test(self):
+        """Return a test dataset."""
+
+
+class Tuple(Data):
+    """Dataset with each data point as a tuple.
+
+    Each data tuple is split into two parts: input tuple (x) and output tuple (y).
+    """
+
+    def __init__(self, train_x, train_y, test_x, test_y):
+        self.train_x = train_x
+        self.train_y = train_y
+        self.test_x = test_x
+        self.test_y = test_y
+
+    def losses(self, targets, outputs, loss, model):
+        return [loss(targets, outputs)]
+
+    def train_next_batch(self, batch_size=None):
+        return self.train_x, self.train_y
+
+    def test(self):
+        return self.test_x, self.test_y
