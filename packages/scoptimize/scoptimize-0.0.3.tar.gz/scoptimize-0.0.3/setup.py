c2046c05b522bf6acb7c20785ade1797fd0a7db0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:56 +0000

    Add package scoptimize/0.0.3/scoptimize-0.0.3.tar.gz

---
+++ packages/scoptimize/scoptimize-0.0.3.tar.gz/scoptimize-0.0.3/setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+setup(
+  name = 'scoptimize',
+  packages = ['scoptimize'],
+  version = '0.0.3',
+  license='MIT',
+  description = 'Supply Chain Optimization Package',
+  author = 'Connor Makowski',
+  author_email = 'conmak@mit.edu',
+  url = 'https://github.com/connor-makowski/scoptimize',
+  download_url = 'https://github.com/connor-makowski/scoptimize/dist/scoptimize-0.0.3.tar.gz',
+  keywords = [],
+  install_requires=["PuLP==2.6.0", "type_enforced==0.0.4"],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+  ],
+)
