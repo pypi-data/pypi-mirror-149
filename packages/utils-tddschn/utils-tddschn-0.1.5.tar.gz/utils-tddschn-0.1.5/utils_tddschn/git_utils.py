2f5ee9593fc73fc7309de26b85208caa6766bba2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:10:49 +0000

    Add package utils-tddschn/0.1.5/utils-tddschn-0.1.5.tar.gz

---
+++ packages/utils-tddschn/utils-tddschn-0.1.5.tar.gz/utils-tddschn-0.1.5/utils_tddschn/git_utils.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import subprocess
+
+
+def git_root_dir() -> str:
+    """Return the root directory of the current git repository."""
+    # https://stackoverflow.com/questions/22081209/find-the-root-of-the-git-repository-where-the-file-lives
+    return subprocess.check_output(['git', 'rev-parse', '--show-toplevel'
+                                    ]).decode('utf-8').strip()
