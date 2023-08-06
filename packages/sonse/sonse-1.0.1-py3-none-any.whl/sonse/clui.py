9394fa68970b7aae1a87584f0a58f9f083bd4e7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:54 +0000

    Add package sonse/1.0.1/sonse-1.0.1-py3-none-any.whl

---
+++ packages/sonse/sonse-1.0.1-py3-none-any.whl/sonse/clui.py
@@ -0,0 +1,39 @@
+"""
+Click base group and type definitions.
+"""
+
+from typing import Optional
+
+import click
+from click import Context, Parameter
+
+from sonse import jobs
+from sonse import VERSION_STRING
+
+
+@click.group(name="sonse", no_args_is_help=True)
+@click.help_option("-h", "--help")
+@click.version_option("", "-v", "--version", message=VERSION_STRING)
+@click.pass_context
+def group(ctx: Context):
+    """
+    Stephen's Obsessive Note-Storage Engine.
+    See github.com/rattlerake/sonse for help and bugs.
+    """
+
+    ctx.obj = ctx.obj or jobs.init("SONSE")
+
+
+class Name(click.ParamType):
+    """
+    A Custom Click type for name strings.
+    """
+
+    name = "name"
+
+    def convert(self, value: str, param: Optional[Parameter], ctx: Optional[Context]):
+        """
+        Convert a user value to a name string.
+        """
+
+        return str(value).lower().strip()
