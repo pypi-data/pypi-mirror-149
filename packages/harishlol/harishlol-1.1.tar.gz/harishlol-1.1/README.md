6a3aa91815a966326f37481265563c1787a1a5ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:42 +0000

    Add package harishlol/1.1/harishlol-1.1.tar.gz

---
+++ packages/harishlol/harishlol-1.1.tar.gz/harishlol-1.1/README.md
@@ -0,0 +1,1 @@
+This is our homepage of our project.
---
+++ packages/harishlol/harishlol-1.1.tar.gz/harishlol-1.1/harishlol/pdf2text.py
@@ -0,0 +1,15 @@
+""" this module provides function to convert a PDF to text. """
+
+def convert():
+    """ 
+    Converts the given pdf to text. 
+    
+    pdf......
+
+    parameters:
+    path (str): The path to pdf file.s
+
+    returns:
+    str: the content of pdf file as text.
+    """
+    print("convert")
