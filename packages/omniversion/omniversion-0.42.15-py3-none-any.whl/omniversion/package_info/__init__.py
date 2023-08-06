5ed2455342fd9a72d208f30628fa26e11f632f56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:31 +0000

    Add package omniversion/0.42.15/omniversion-0.42.15-py3-none-any.whl

---
+++ packages/omniversion/omniversion-0.42.15-py3-none-any.whl/omniversion/package_info/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+"""Model classes for various types of dependency"""
+from .list.available_updates import AvailableUpdates
+from .list.package_infos_list import PackageInfosList
+from .list.versions_match import VersionsMatch
+from .list.vulnerabilities import Vulnerabilities
+from .advisory import Advisory
+from .package_info import PackageInfo
