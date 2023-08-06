608c1f785ab937f2367c0bd282cda16dedbd5624
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:06 +0000

    Add package v-cloud-market-cli-user/0.1.5/v_cloud_market_cli_user-0.1.5.tar.gz

---
+++ packages/v-cloud-market-cli-user/v_cloud_market_cli_user-0.1.5.tar.gz/v_cloud_market_cli_user-0.1.5/market_place_cli/v_cloud_market_cli_common/service/service_common.py
@@ -0,0 +1,18 @@
+import sys
+
+from market_place_cli.v_cloud_market_cli_common.utils.service_error import (
+    HttpNotFoundException,
+    HttpBadRequestException
+)
+
+class ServiceCommon:
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def validate_response(resp):
+        if not isinstance(resp, dict) and not isinstance(resp, list):
+            raise HttpNotFoundException
+        elif 'message' in resp:
+            raise HttpBadRequestException
