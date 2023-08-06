5e19e040c14ca5e8a45a939ad69979df650d6839
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:42:30 +0000

    Add package readycheck/1.0.0/readycheck-1.0.0.tar.gz

---
+++ packages/readycheck/readycheck-1.0.0.tar.gz/readycheck-1.0.0/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+import readycheck
+
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    readme = fh.read()
+
+setuptools.setup(
+    name="readycheck",
+    version=readycheck.__version__,
+    author=readycheck.__author__,
+    author_email="loic.simon@espci.org",
+    description="Run custom checks on classes attributes when accessing them",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/loic-simon/readycheck",
+    py_modules=["readycheck"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[],
+    python_requires='>=3.9',
+)
+
+# python3 setup.py sdist bdist_wheel
+# twine upload dist/*
