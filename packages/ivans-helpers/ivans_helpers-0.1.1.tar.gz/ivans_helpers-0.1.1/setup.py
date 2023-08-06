03d61e91acd9f1e9a47cf25d59311623823ba430
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:17 +0000

    Add package ivans-helpers/0.1.1/ivans_helpers-0.1.1.tar.gz

---
+++ packages/ivans-helpers/ivans_helpers-0.1.1.tar.gz/ivans_helpers-0.1.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['ivans_helpers', 'ivans_helpers.ivans_helpers']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'ivans-helpers',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Ivan Thung',
+    'author_email': 'ivanthung@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/ivanthung/ivan_helpers',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
