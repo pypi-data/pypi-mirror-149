8b313b71ede38e9b731e7edb097330b1494b3304
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:07:46 +0000

    Add package hgd/0.2.1/hgd-0.2.1.tar.gz

---
+++ packages/hgd/hgd-0.2.1.tar.gz/hgd-0.2.1/hgd/config.py
@@ -0,0 +1,18 @@
+"""
+Config.
+"""
+from dataclasses import dataclass
+from typing import Tuple
+
+from hgd import __weights_version__
+
+
+@dataclass
+class Config:
+    gesture_labels: Tuple[str, ...] = ("nodding", "turning")
+    undefined_gesture_label: str = "undefined"
+    stationary_label: str = "stationary"
+    npz_filename: str = "landmarks.npz"
+    seq_length: int = 60
+    num_features: int = 14
+    weights_filename: str = f"hgd_weights_v{__weights_version__}.h5"
