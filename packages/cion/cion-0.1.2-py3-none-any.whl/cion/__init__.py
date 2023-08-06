c6477e580510901935eee895be2a67421a493c46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:33 +0000

    Add package cion/0.1.2/cion-0.1.2-py3-none-any.whl

---
+++ packages/cion/cion-0.1.2-py3-none-any.whl/cion/__init__.py
@@ -0,0 +1,7 @@
+from .exceptions import ValidationError, ValidatorError
+from .options import Options
+from .schema import Field, Schema
+
+__all__ = ("Schema", "Field", "Options", "ValidatorError", "ValidationError")
+
+__version__ = "0.1.2"
