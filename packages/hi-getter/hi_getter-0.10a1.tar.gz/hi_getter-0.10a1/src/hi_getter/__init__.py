b3a908acdf8dc39f30c4ccb465ee01b729d32078
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:25 +0000

    Add package hi-getter/0.10a1/hi_getter-0.10a1.tar.gz

---
+++ packages/hi-getter/hi_getter-0.10a1.tar.gz/hi_getter-0.10a1/src/hi_getter/__init__.py
@@ -0,0 +1,15 @@
+###################################################################################################
+#                              MIT Licence (C) 2022 Cubicpath@Github                              #
+###################################################################################################
+"""Package initialization module. Import the Client and version info from here."""
+
+__all__ = (
+    'Client',
+)
+
+from ._version import __version__
+from ._version import __version_info__
+from .network import Client
+
+__author__ = 'Cubicpath@Github <cubicpath@pm.me>'
+"""Author's information."""
