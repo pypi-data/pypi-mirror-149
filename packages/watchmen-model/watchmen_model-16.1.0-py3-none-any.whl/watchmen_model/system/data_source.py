acdf5d349497e16182ec3f6a5a41ab87c62d6a99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:31 +0000

    Add package watchmen-model/16.1.0/watchmen_model-16.1.0-py3-none-any.whl

---
+++ packages/watchmen-model/watchmen_model-16.1.0-py3-none-any.whl/watchmen_model/system/data_source.py
@@ -0,0 +1,32 @@
+from enum import Enum
+from typing import List
+
+from pydantic import BaseModel
+
+from watchmen_model.common import DataModel, DataSourceId, OptimisticLock, TenantBasedTuple
+
+
+class DataSourceParam(DataModel, BaseModel):
+	name: str = None
+	value: str = None
+
+
+class DataSourceType(str, Enum):
+	MYSQL = 'mysql',
+	ORACLE = 'oracle',
+	MONGODB = 'mongodb',
+	MSSQL = 'mssql'
+	POSTGRESQL = 'postgresql'
+
+
+class DataSource(TenantBasedTuple, OptimisticLock, BaseModel):
+	dataSourceId: DataSourceId = None
+	dataSourceCode: str = None
+	dataSourceType: DataSourceType = None
+	host: str = None
+	port: str = None
+	username: str = None
+	password: str = None
+	name: str = None
+	url: str = None
+	params: List[DataSourceParam] = []
