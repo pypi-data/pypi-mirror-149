d086c2a79120bd1fac064f8e8cc0b65da3b2eded
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:47 +0000

    Add package gitignore-cli-tddschn/0.1.1/gitignore-cli-tddschn-0.1.1.tar.gz

---
+++ packages/gitignore-cli-tddschn/gitignore-cli-tddschn-0.1.1.tar.gz/gitignore-cli-tddschn-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['gitignore_cli_tddschn']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['gitignore = gitignore_cli_tddschn.cli:main']}
+
+setup_kwargs = {
+    'name': 'gitignore-cli-tddschn',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': '',
+    'author': 'Xinyuan Chen',
+    'author_email': '45612704+tddschn@users.noreply.github.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/tddschn/gitignore-cli-tddschn',
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
