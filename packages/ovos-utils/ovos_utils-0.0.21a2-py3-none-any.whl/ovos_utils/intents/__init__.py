e6d0e90474d639035c7d43fac4fa66828295659f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:42 +0000

    Add package ovos-utils/0.0.21a2/ovos_utils-0.0.21a2-py3-none-any.whl

---
+++ packages/ovos-utils/ovos_utils-0.0.21a2-py3-none-any.whl/ovos_utils/intents/__init__.py
@@ -0,0 +1,4 @@
+from ovos_utils.intents.intent_service_interface import IntentQueryApi, \
+    IntentServiceInterface
+from ovos_utils.intents.converse import ConverseTracker
+from ovos_utils.intents.layers import IntentLayers
