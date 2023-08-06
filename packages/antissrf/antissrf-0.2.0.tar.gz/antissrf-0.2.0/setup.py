96ad66ece9fc080669404e623a0820f8d892479f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:00 +0000

    Add package antissrf/0.2.0/antissrf-0.2.0.tar.gz

---
+++ packages/antissrf/antissrf-0.2.0.tar.gz/antissrf-0.2.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['antissrf']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'antissrf',
+    'version': '0.2.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Jesus Aviles',
+    'author_email': 'the.jesus.aviles@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
