866729937e3ec98e823ab4b7be4eef908d191acf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:44:24 +0000

    Add package pulse2percept/0.8.0/pulse2percept-0.8.0-cp39-cp39-win32.whl

---
+++ packages/pulse2percept/pulse2percept-0.8.0-cp39-cp39-win32.whl/pulse2percept/models/__init__.py
@@ -0,0 +1,49 @@
+"""Computational models of the retina, such as phosphene and neural response models.
+
+.. autosummary::
+    :toctree: _api
+
+    base
+    temporal
+    thompson2003
+    horsager2009
+    nanduri2012
+    beyeler2019
+    granley2021
+
+.. seealso::
+
+    *  :ref:`Basic Concepts > Computational Models <topics-models>`
+
+"""
+from .base import (BaseModel, Model, NotBuiltError, SpatialModel,
+                   TemporalModel)
+from .temporal import FadingTemporal
+from .beyeler2019 import (ScoreboardModel, ScoreboardSpatial, AxonMapSpatial,
+                          AxonMapModel)
+from .horsager2009 import Horsager2009Model, Horsager2009Temporal
+from .nanduri2012 import (Nanduri2012Model, Nanduri2012Spatial,
+                          Nanduri2012Temporal)
+from .granley2021 import BiphasicAxonMapModel, BiphasicAxonMapSpatial
+from .thompson2003 import Thompson2003Model, Thompson2003Spatial
+
+__all__ = [
+    'AxonMapModel',
+    'AxonMapSpatial',
+    'BaseModel',
+    'FadingTemporal',
+    'Horsager2009Model',
+    'Horsager2009Temporal',
+    'Model',
+    'Nanduri2012Model',
+    'Nanduri2012Spatial',
+    'Nanduri2012Temporal',
+    'BiphasicAxonMapModel',
+    'NotBuiltError',
+    'ScoreboardModel',
+    'ScoreboardSpatial',
+    'SpatialModel',
+    'TemporalModel',
+    'Thompson2003Model',
+    'Thompson2003Spatial'
+]
