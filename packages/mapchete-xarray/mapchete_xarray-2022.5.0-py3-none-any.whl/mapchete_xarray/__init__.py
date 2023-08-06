85817d95a8cc9e07afa69644835cca532830b4b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:40 +0000

    Add package mapchete-xarray/2022.5.0/mapchete_xarray-2022.5.0-py3-none-any.whl

---
+++ packages/mapchete-xarray/mapchete_xarray-2022.5.0-py3-none-any.whl/mapchete_xarray/__init__.py
@@ -0,0 +1,9 @@
+from mapchete_xarray._xarray import (
+    InputTile,
+    METADATA,
+    OutputDataWriter,
+    OutputDataReader,
+)
+
+__all__ = ["InputTile", "METADATA", "OutputDataWriter", "OutputDataReader"]
+__version__ = "2022.5.0"
