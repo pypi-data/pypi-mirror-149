8bd0c1e6aa09850049a59448d6b808b2c2fba772
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:31 +0000

    Add package edc-csf/0.1.4/edc_csf-0.1.4-py3-none-any.whl

---
+++ packages/edc-csf/edc_csf-0.1.4-py3-none-any.whl/edc_csf/constants.py
@@ -0,0 +1,4 @@
+THERAPEUTIC_PL = "therapeutic_lp"
+CN_PALSY_LEFT_OTHER = "cn_palsy_left_other"
+CN_PALSY_RIGHT_OTHER = "cn_palsy_right_other"
+FOCAL_NEUROLOGIC_DEFICIT_OTHER = "focal_neurologic_deficit_other"
