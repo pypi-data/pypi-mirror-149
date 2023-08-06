b0a7de5f8721817981f3e138f147a0477b9156ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:02 +0000

    Add package datahub-extras/0.0.1/datahub_extras-0.0.1.tar.gz

---
+++ packages/datahub-extras/datahub_extras-0.0.1.tar.gz/datahub_extras-0.0.1/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="datahub_extras",
+    version="0.0.1",
+    author="Hugh Nguyen",
+    description="This package extends datahub and makes it easier to interact with datahub APIs",
+    packages=["datahub_extras"]
+)
