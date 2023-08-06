f4195e3d280924f6276c8057f158d00103fc317e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:45:52 +0000

    Add package nextstrain-augur/15.0.2/nextstrain-augur-15.0.2.tar.gz

---
+++ packages/nextstrain-augur/nextstrain-augur-15.0.2.tar.gz/nextstrain-augur-15.0.2/augur/io.py
@@ -0,0 +1,196 @@
+#!/usr/bin/env python3
+"""Interfaces for reading and writing data also known as input/output (I/O)
+"""
+import Bio.SeqIO
+import Bio.SeqRecord
+from contextlib import contextmanager
+import pandas as pd
+from pathlib import Path
+from xopen import xopen
+
+
+@contextmanager
+def open_file(path_or_buffer, mode="r", **kwargs):
+    """Opens a given file path and returns the handle.
+
+    Transparently handles compressed inputs and outputs.
+
+    Parameters
+    ----------
+    path_or_buffer : str or Path-like or IO buffer
+        Name of the file to open or an existing IO buffer
+
+    mode : str
+        Mode to open file (read or write)
+
+    Returns
+    -------
+    IO
+        File handle object
+
+    """
+    try:
+        with xopen(path_or_buffer, mode, **kwargs) as handle:
+            yield handle
+    except TypeError:
+        yield path_or_buffer
+
+
+def read_metadata(metadata_file, id_columns=("strain", "name"), chunk_size=None):
+    """Read metadata from a given filename and into a pandas `DataFrame` or
+    `TextFileReader` object.
+
+    Parameters
+    ----------
+    metadata_file : str
+        Path to a metadata file to load.
+    id_columns : list[str]
+        List of possible id column names to check for, ordered by priority.
+    chunk_size : int
+        Size of chunks to stream from disk with an iterator instead of loading the entire input file into memory.
+
+    Returns
+    -------
+    pandas.DataFrame or pandas.TextFileReader
+
+    Raises
+    ------
+    KeyError :
+        When the metadata file does not have any valid index columns.
+
+
+    For standard use, request a metadata file and get a pandas DataFrame.
+
+    >>> read_metadata("tests/functional/filter/metadata.tsv").index.values[0]
+    'COL/FLR_00024/2015'
+
+    Requesting an index column that doesn't exist should produce an error.
+
+    >>> read_metadata("tests/functional/filter/metadata.tsv", id_columns=("Virus name",))
+    Traceback (most recent call last):
+      ...
+    Exception: None of the possible id columns (('Virus name',)) were found in the metadata's columns ('strain', 'virus', 'accession', 'date', 'region', 'country', 'division', 'city', 'db', 'segment', 'authors', 'url', 'title', 'journal', 'paper_url')
+
+    We also allow iterating through metadata in fixed chunk sizes.
+
+    >>> for chunk in read_metadata("tests/functional/filter/metadata.tsv", chunk_size=5):
+    ...     print(chunk.shape)
+    ...
+    (5, 14)
+    (5, 14)
+    (2, 14)
+
+    """
+    kwargs = {
+        "sep": None,
+        "engine": "python",
+        "skipinitialspace": True,
+        "na_filter": False,
+    }
+
+    if chunk_size:
+        kwargs["chunksize"] = chunk_size
+
+    # Inspect the first chunk of the metadata, to find any valid index columns.
+    metadata = pd.read_csv(
+        metadata_file,
+        iterator=True,
+        **kwargs,
+    )
+    chunk = metadata.read(nrows=1)
+    metadata.close()
+
+    id_columns_present = [
+        id_column
+        for id_column in id_columns
+        if id_column in chunk.columns
+    ]
+
+    # If we couldn't find a valid index column in the metadata, alert the user.
+    if not id_columns_present:
+        raise Exception(f"None of the possible id columns ({id_columns!r}) were found in the metadata's columns {tuple(chunk.columns)!r}")
+    else:
+        index_col = id_columns_present[0]
+
+    # If we found a valid column to index the DataFrame, specify that column and
+    # also tell pandas that the column should be treated like a string instead
+    # of having its type inferred. This latter argument allows users to provide
+    # numerical ids that don't get converted to numbers by pandas.
+    kwargs["index_col"] = index_col
+    kwargs["dtype"] = {index_col: "string"}
+
+    return pd.read_csv(
+        metadata_file,
+        **kwargs
+    )
+
+
+def read_sequences(*paths, format="fasta"):
+    """Read sequences from one or more paths.
+
+    Automatically infer compression mode (e.g., gzip, etc.) and return a stream
+    of sequence records in the requested format (e.g., "fasta", "genbank", etc.).
+
+    Parameters
+    ----------
+    paths : list of str or Path-like objects
+        One or more paths to sequence files of any type supported by BioPython.
+
+    format : str
+        Format of input sequences matching any of those supported by BioPython
+        (e.g., "fasta", "genbank", etc.).
+
+    Yields
+    ------
+    Bio.SeqRecord.SeqRecord
+        Sequence record from the given path(s).
+
+    """
+    for path in paths:
+        # Open the given path as a handle, inferring the file's compression.
+        # This way we can pass a handle to BioPython's SeqIO interface
+        # regardless of the compression mode.
+        with open_file(path) as handle:
+            sequences = Bio.SeqIO.parse(handle, format)
+
+            for sequence in sequences:
+                yield sequence
+
+
+def write_sequences(sequences, path_or_buffer, format="fasta"):
+    """Write sequences to a given path in the given format.
+
+    Automatically infer compression mode (e.g., gzip, etc.) based on the path's
+    filename extension.
+
+    Parameters
+    ----------
+    sequences : iterable of Bio.SeqRecord.SeqRecord objects
+        A list-like collection of sequences to write
+
+    path_or_buffer : str or Path-like object or IO buffer
+        A path to a file to write the given sequences in the given format.
+
+    format : str
+        Format of input sequences matching any of those supported by BioPython
+        (e.g., "fasta", "genbank", etc.)
+
+    Returns
+    -------
+    int :
+        Number of sequences written out to the given path.
+
+    """
+    with open_file(path_or_buffer, "wt") as handle:
+        # Bio.SeqIO supports writing to the same handle multiple times for specific
+        # file formats. For the formats we use, this function call should work for
+        # both a newly opened file handle or one that is provided by the caller.
+        # For more details see:
+        # https://github.com/biopython/biopython/blob/25f5152f4aeefe184a323db25694fbfe0593f0e2/Bio/SeqIO/__init__.py#L233-L251
+        sequences_written = Bio.SeqIO.write(
+            sequences,
+            handle,
+            format
+        )
+
+    return sequences_written
