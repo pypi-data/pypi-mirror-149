f07d1bb3de042677daa7e57e892aa1c79f089b6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:12 +0000

    Add package black-knight/0.0.5/black_knight-0.0.5-py3-none-any.whl

---
+++ packages/black-knight/black_knight-0.0.5-py3-none-any.whl/black_knight/static/dist/main.5143d6dd156ef0a80f02.js
@@ -0,0 +1,516 @@
+(self["webpackChunkblack_knight"] = self["webpackChunkblack_knight"] || []).push([[179],{
+
+/***/ 326:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+// THIS FILE IS AUTO GENERATED
+var GenIcon = (__webpack_require__(720)/* .GenIcon */ .w_)
+module.exports.FaSortDown = function FaSortDown (props) {
+  return GenIcon({"tag":"svg","attr":{"viewBox":"0 0 320 512"},"child":[{"tag":"path","attr":{"d":"M41 288h238c21.4 0 32.1 25.9 17 41L177 448c-9.4 9.4-24.6 9.4-33.9 0L24 329c-15.1-15.1-4.4-41 17-41z"}}]})(props);
+};
+
+
+/***/ }),
+
+/***/ 430:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+// THIS FILE IS AUTO GENERATED
+var GenIcon = (__webpack_require__(720)/* .GenIcon */ .w_)
+module.exports.GiBackwardTime = function GiBackwardTime (props) {
+  return GenIcon({"tag":"svg","attr":{"viewBox":"0 0 512 512"},"child":[{"tag":"path","attr":{"d":"M208.242 24.629l-52.058 95.205 95.207 52.059 17.271-31.586-42.424-23.198A143.26 143.26 0 0 1 256 114c78.638 0 142 63.362 142 142s-63.362 142-142 142-142-63.362-142-142c0-16.46 2.785-32.247 7.896-46.928l-32.32-16.16C82.106 212.535 78 233.798 78 256c0 98.093 79.907 178 178 178s178-79.907 178-178S354.093 78 256 78c-13.103 0-25.875 1.44-38.18 4.148l22.008-40.25-31.586-17.27zm104.27 130.379L247 253.275V368h18V258.725l62.488-93.733-14.976-9.984z"}}]})(props);
+};
+
+
+/***/ }),
+
+/***/ 720:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, {
+  "w_": () => (/* reexport */ GenIcon)
+});
+
+// UNUSED EXPORTS: DefaultContext, IconBase, IconContext, IconsManifest
+
+;// CONCATENATED MODULE: ./node_modules/@react-icons/all-files/lib/esm/iconsManifest.js
+var IconsManifest = [
+  {
+    "id": "fa",
+    "name": "Font Awesome",
+    "projectUrl": "https://fontawesome.com/",
+    "license": "CC BY 4.0 License",
+    "licenseUrl": "https://creativecommons.org/licenses/by/4.0/"
+  },
+  {
+    "id": "io",
+    "name": "Ionicons 4",
+    "projectUrl": "https://ionicons.com/",
+    "license": "MIT",
+    "licenseUrl": "https://github.com/ionic-team/ionicons/blob/master/LICENSE"
+  },
+  {
+    "id": "io5",
+    "name": "Ionicons 5",
+    "projectUrl": "https://ionicons.com/",
+    "license": "MIT",
+    "licenseUrl": "https://github.com/ionic-team/ionicons/blob/master/LICENSE"
+  },
+  {
+    "id": "md",
+    "name": "Material Design icons",
+    "projectUrl": "http://google.github.io/material-design-icons/",
+    "license": "Apache License Version 2.0",
+    "licenseUrl": "https://github.com/google/material-design-icons/blob/master/LICENSE"
+  },
+  {
+    "id": "ti",
+    "name": "Typicons",
+    "projectUrl": "http://s-ings.com/typicons/",
+    "license": "CC BY-SA 3.0",
+    "licenseUrl": "https://creativecommons.org/licenses/by-sa/3.0/"
+  },
+  {
+    "id": "go",
+    "name": "Github Octicons icons",
+    "projectUrl": "https://octicons.github.com/",
+    "license": "MIT",
+    "licenseUrl": "https://github.com/primer/octicons/blob/master/LICENSE"
+  },
+  {
+    "id": "fi",
+    "name": "Feather",
+    "projectUrl": "https://feathericons.com/",
+    "license": "MIT",
+    "licenseUrl": "https://github.com/feathericons/feather/blob/master/LICENSE"
+  },
+  {
+    "id": "gi",
+    "name": "Game Icons",
+    "projectUrl": "https://game-icons.net/",
+    "license": "CC BY 3.0",
+    "licenseUrl": "https://creativecommons.org/licenses/by/3.0/"
+  },
+  {
+    "id": "wi",
+    "name": "Weather Icons",
+    "projectUrl": "https://erikflowers.github.io/weather-icons/",
+    "license": "SIL OFL 1.1",
+    "licenseUrl": "http://scripts.sil.org/OFL"
+  },
+  {
+    "id": "di",
+    "name": "Devicons",
+    "projectUrl": "https://vorillaz.github.io/devicons/",
+    "license": "MIT",
+    "licenseUrl": "https://opensource.org/licenses/MIT"
+  },
+  {
+    "id": "ai",
+    "name": "Ant Design Icons",
+    "projectUrl": "https://github.com/ant-design/ant-design-icons",
+    "license": "MIT",
+    "licenseUrl": "https://opensource.org/licenses/MIT"
+  },
+  {
+    "id": "bs",
+    "name": "Bootstrap Icons",
+    "projectUrl": "https://github.com/twbs/icons",
+    "license": "MIT",
+    "licenseUrl": "https://opensource.org/licenses/MIT"
+  },
+  {
+    "id": "ri",
+    "name": "Remix Icon",
+    "projectUrl": "https://github.com/Remix-Design/RemixIcon",
+    "license": "Apache License Version 2.0",
+    "licenseUrl": "http://www.apache.org/licenses/"
+  },
+  {
+    "id": "fc",
+    "name": "Flat Color Icons",
+    "projectUrl": "https://github.com/icons8/flat-color-icons",
+    "license": "MIT",
+    "licenseUrl": "https://opensource.org/licenses/MIT"
+  },
+  {
+    "id": "gr",
+    "name": "Grommet-Icons",
+    "projectUrl": "https://github.com/grommet/grommet-icons",
+    "license": "Apache License Version 2.0",
+    "licenseUrl": "http://www.apache.org/licenses/"
+  },
+  {
+    "id": "hi",
+    "name": "Heroicons",
+    "projectUrl": "https://github.com/refactoringui/heroicons",
+    "license": "MIT",
+    "licenseUrl": "https://opensource.org/licenses/MIT"
+  },
+  {
+    "id": "si",
+    "name": "Simple Icons",
+    "projectUrl": "https://simpleicons.org/",
+    "license": "CC0 1.0 Universal",
+    "licenseUrl": "https://creativecommons.org/publicdomain/zero/1.0/"
+  },
+  {
+    "id": "im",
+    "name": "IcoMoon Free",
+    "projectUrl": "https://github.com/Keyamoon/IcoMoon-Free",
+    "license": "CC BY 4.0 License"
+  },
+  {
+    "id": "bi",
+    "name": "BoxIcons",
+    "projectUrl": "https://github.com/atisawd/boxicons",
+    "license": "CC BY 4.0 License"
+  },
+  {
+    "id": "cg",
+    "name": "css.gg",
+    "projectUrl": "https://github.com/astrit/css.gg",
+    "license": "MIT",
+    "licenseUrl": "https://opensource.org/licenses/MIT"
+  },
+  {
+    "id": "vsc",
+    "name": "VS Code Icons",
+    "projectUrl": "https://github.com/microsoft/vscode-codicons",
+    "license": "CC BY 4.0",
+    "licenseUrl": "https://creativecommons.org/licenses/by/4.0/"
+  }
+]
+// EXTERNAL MODULE: ./node_modules/react/index.js
+var react = __webpack_require__(294);
+;// CONCATENATED MODULE: ./node_modules/@react-icons/all-files/lib/esm/iconContext.js
+
+var DefaultContext = {
+  color: undefined,
+  size: undefined,
+  className: undefined,
+  style: undefined,
+  attr: undefined
+};
+var IconContext = react.createContext && react.createContext(DefaultContext);
+;// CONCATENATED MODULE: ./node_modules/@react-icons/all-files/lib/esm/iconBase.js
+var __assign = undefined && undefined.__assign || function () {
+  __assign = Object.assign || function (t) {
+    for (var s, i = 1, n = arguments.length; i < n; i++) {
+      s = arguments[i];
+
+      for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p)) t[p] = s[p];
+    }
+
+    return t;
+  };
+
+  return __assign.apply(this, arguments);
+};
+
+var __rest = undefined && undefined.__rest || function (s, e) {
+  var t = {};
+
+  for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0) t[p] = s[p];
+
+  if (s != null && typeof Object.getOwnPropertySymbols === "function") for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
+    if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i])) t[p[i]] = s[p[i]];
+  }
+  return t;
+};
+
+
+
+
+function Tree2Element(tree) {
+  return tree && tree.map(function (node, i) {
+    return react.createElement(node.tag, __assign({
+      key: i
+    }, node.attr), Tree2Element(node.child));
+  });
+}
+
+function GenIcon(data) {
+  return function (props) {
+    return react.createElement(IconBase, __assign({
+      attr: __assign({}, data.attr)
+    }, props), Tree2Element(data.child));
+  };
+}
+function IconBase(props) {
+  var elem = function (conf) {
+    var attr = props.attr,
+        size = props.size,
+        title = props.title,
+        svgProps = __rest(props, ["attr", "size", "title"]);
+
+    var computedSize = size || conf.size || "1em";
+    var className;
+    if (conf.className) className = conf.className;
+    if (props.className) className = (className ? className + ' ' : '') + props.className;
+    return react.createElement("svg", __assign({
+      stroke: "currentColor",
+      fill: "currentColor",
+      strokeWidth: "0"
+    }, conf.attr, attr, svgProps, {
+      className: className,
+      style: __assign(__assign({
+        color: props.color || conf.color
+      }, conf.style), props.style),
+      height: computedSize,
+      width: computedSize,
+      xmlns: "http://www.w3.org/2000/svg"
+    }), title && react.createElement("title", null, title), props.children);
+  };
+
+  return IconContext !== undefined ? react.createElement(IconContext.Consumer, null, function (conf) {
+    return elem(conf);
+  }) : elem(DefaultContext);
+}
+;// CONCATENATED MODULE: ./node_modules/@react-icons/all-files/lib/esm/index.js
+
+
+
+
+/***/ }),
+
+/***/ 491:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }),
+
+/***/ 918:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }),
+
+/***/ 662:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }),
+
+/***/ 889:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }),
+
+/***/ 353:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }),
+
+/***/ 745:
+/***/ ((__unused_webpack_module, exports, __webpack_require__) => {
+
+"use strict";
+
+
+var m = __webpack_require__(935);
+if (true) {
+  exports.createRoot = m.createRoot;
+  exports.hydrateRoot = m.hydrateRoot;
+} else { var i; }
+
+
+/***/ }),
+
+/***/ 2:
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+"use strict";
+
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+var react_1 = __importDefault(__webpack_require__(294));
+var react_router_dom_1 = __webpack_require__(102);
+__webpack_require__(353);
+var Dashboard_1 = __importDefault(__webpack_require__(506));
+var App = function () {
+    return (react_1.default.createElement(react_router_dom_1.Routes, null,
+        react_1.default.createElement(react_router_dom_1.Route, { path: '/', element: react_1.default.createElement(Dashboard_1.default, null) })));
+};
+exports["default"] = App;
+
+
+/***/ }),
+
+/***/ 51:
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+"use strict";
+
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+var react_1 = __importDefault(__webpack_require__(294));
+__webpack_require__(662);
+var DashboardSidebar = function () {
+    return (react_1.default.createElement("div", { className: 'sidebar-container' },
+        react_1.default.createElement("div", { className: 'sidebar-wrapper' },
+            react_1.default.createElement("div", { className: 'sidebar-category-wrappper' },
+                react_1.default.createElement("div", { className: 'category title_small' },
+                    react_1.default.createElement("span", null, "App 1")),
+                react_1.default.createElement("div", { className: 'column description' },
+                    react_1.default.createElement("div", { className: 'icon' }),
+                    react_1.default.createElement("div", { className: 'holder' }, "Model 1")),
+                react_1.default.createElement("div", { className: 'column description' },
+                    react_1.default.createElement("div", { className: 'icon' }),
+                    react_1.default.createElement("div", { className: 'holder' }, "Model 2"))),
+            react_1.default.createElement("div", { className: 'sidebar-category-wrappper' },
+                react_1.default.createElement("div", { className: 'category title_small' },
+                    react_1.default.createElement("span", null, "App 2")),
+                react_1.default.createElement("div", { className: 'column description' },
+                    react_1.default.createElement("div", { className: 'icon' }),
+                    react_1.default.createElement("div", { className: 'holder' }, "Users"))))));
+};
+exports["default"] = DashboardSidebar;
+
+
+/***/ }),
+
+/***/ 164:
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+"use strict";
+
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+var react_1 = __importDefault(__webpack_require__(294));
+__webpack_require__(491);
+var DashboardData = function () {
+    return react_1.default.createElement("div", { className: 'dashboard-data' });
+};
+exports["default"] = DashboardData;
+
+
+/***/ }),
+
+/***/ 242:
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+"use strict";
+
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+var react_1 = __importDefault(__webpack_require__(294));
+__webpack_require__(918);
+var FaSortDown_1 = __webpack_require__(326);
+var GiBackwardTime_1 = __webpack_require__(430);
+var default_img = __webpack_require__(248);
+var DashboardHeader = function () {
+    return (react_1.default.createElement("div", { className: 'dashboard-header' },
+        react_1.default.createElement("div", { className: 'active-section title_small' }, "--ACTIVE SECTION--"),
+        react_1.default.createElement("div", { className: 'user-section' },
+            react_1.default.createElement("div", { className: 'user-section-wrapper' },
+                react_1.default.createElement("div", { className: 'recent-actions' },
+                    react_1.default.createElement(GiBackwardTime_1.GiBackwardTime, { size: 24 })),
+                react_1.default.createElement("div", { className: 'profile-img' },
+                    react_1.default.createElement("img", { src: default_img, alt: '' })),
+                react_1.default.createElement("div", { className: 'dropdown-icon' },
+                    react_1.default.createElement(FaSortDown_1.FaSortDown, { size: 24 }))),
+            react_1.default.createElement("div", { className: 'dropdown' }))));
+};
+exports["default"] = DashboardHeader;
+
+
+/***/ }),
+
+/***/ 712:
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+"use strict";
+
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+var react_1 = __importDefault(__webpack_require__(294));
+var client_1 = __webpack_require__(745);
+var App_1 = __importDefault(__webpack_require__(2));
+var react_router_dom_1 = __webpack_require__(102);
+var Root = function () {
+    return (react_1.default.createElement(react_router_dom_1.BrowserRouter, { basename: '/admin' },
+        react_1.default.createElement(App_1.default, null)));
+};
+(0, client_1.createRoot)(document.getElementById('root')).render(react_1.default.createElement(Root, null));
+
+
+/***/ }),
+
+/***/ 506:
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+"use strict";
+
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+var react_1 = __importDefault(__webpack_require__(294));
+__webpack_require__(889);
+var DashbaordSidebar_1 = __importDefault(__webpack_require__(51));
+var DashboardData_1 = __importDefault(__webpack_require__(164));
+var DashboardHeader_1 = __importDefault(__webpack_require__(242));
+var Dashboard = function () {
+    return (react_1.default.createElement("div", { className: 'dashboard-container' },
+        react_1.default.createElement(DashboardHeader_1.default, null),
+        react_1.default.createElement("div", { className: 'dashboard-wrapper' },
+            react_1.default.createElement(DashbaordSidebar_1.default, null),
+            react_1.default.createElement(DashboardData_1.default, null))));
+};
+exports["default"] = Dashboard;
+
+
+/***/ }),
+
+/***/ 248:
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+"use strict";
+module.exports = __webpack_require__.p + "assets/800968034643d1861007.png";
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ __webpack_require__.O(0, [913], () => (__webpack_exec__(712)));
+/******/ var __webpack_exports__ = __webpack_require__.O();
+/******/ }
+]);
+//# sourceMappingURL=source_maps/main.5143d6dd156ef0a80f02.js.map
