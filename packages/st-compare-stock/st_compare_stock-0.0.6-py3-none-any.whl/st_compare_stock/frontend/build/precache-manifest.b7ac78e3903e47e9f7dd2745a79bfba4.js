4004dc84fb6754051cb93f63b399ed6f7f72c491
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:56:51 +0000

    Add package st-compare-stock/0.0.6/st_compare_stock-0.0.6-py3-none-any.whl

---
+++ packages/st-compare-stock/st_compare_stock-0.0.6-py3-none-any.whl/st_compare_stock/frontend/build/precache-manifest.b7ac78e3903e47e9f7dd2745a79bfba4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07fe2890e4ce52d406eb23a716146ee0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f47c735a011a05067020",
+    "url": "./static/js/2.a6b6baee.chunk.js"
+  },
+  {
+    "revision": "3fc7fb5bfeeec1534560a2c962e360a7",
+    "url": "./static/js/2.a6b6baee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74a0e6bd0d481f571f39",
+    "url": "./static/js/main.b4eb2086.chunk.js"
+  },
+  {
+    "revision": "408ebecd98387c19777b",
+    "url": "./static/js/runtime-main.e691ffc4.js"
+  }
+]);
