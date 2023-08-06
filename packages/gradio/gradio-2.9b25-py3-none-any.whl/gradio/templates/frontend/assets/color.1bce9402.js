59bde4b2ef2415561d20a79aa7c124ee97d4eeca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:08:22 +0000

    Add package gradio/2.9b25/gradio-2.9b25-py3-none-any.whl

---
+++ packages/gradio/gradio-2.9b25-py3-none-any.whl/gradio/templates/frontend/assets/color.1bce9402.js
@@ -0,0 +1,1 @@
+import"./index.61dd5212.js";import{o}from"./colors.621f0c00.js";const n=r=>o[r%o.length];export{n as g};
---
+++ packages/gradio/gradio-2.9b25-py3-none-any.whl/gradio/templates/frontend/assets/index.49effca0.js
@@ -0,0 +1,1 @@
+import{a4 as s}from"./index.61dd5212.js";const o=["static"];export{s as Component,o as modes};
