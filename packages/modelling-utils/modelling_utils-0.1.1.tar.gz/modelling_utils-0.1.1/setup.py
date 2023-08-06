486c5649f1c1ff4d5c167fa3a11ea90c0ce045c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:53 +0000

    Add package modelling-utils/0.1.1/modelling_utils-0.1.1.tar.gz

---
+++ packages/modelling-utils/modelling_utils-0.1.1.tar.gz/modelling_utils-0.1.1/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['modelling_utils']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['PyYAML>=6.0,<7.0',
+ 'cycler>=0.11.0,<0.12.0',
+ 'loguru>=0.6.0,<0.7.0',
+ 'matplotlib>=3.5.2,<4.0.0',
+ 'numpy>=1.22.3,<2.0.0',
+ 'pandas>=1.4.2,<2.0.0',
+ 'scipy>=1.8.0,<2.0.0',
+ 'seaborn>=0.11.2,<0.12.0']
+
+setup_kwargs = {
+    'name': 'modelling-utils',
+    'version': '0.1.1',
+    'description': 'Python scripts for modelling a 8x Gain, temperature-insensitive Residue Amplifier',
+    'long_description': None,
+    'author': 'Diogo André',
+    'author_email': 'das.dias6@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': '',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<3.11',
+}
+
+
+setup(**setup_kwargs)
