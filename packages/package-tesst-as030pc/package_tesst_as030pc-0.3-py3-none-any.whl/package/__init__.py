1d781d9561e6a2e2e06a4b187edf6d9f87434fac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:44:12 +0000

    Add package package-tesst-as030pc/0.3/package_tesst_as030pc-0.3-py3-none-any.whl

---
+++ packages/package-tesst-as030pc/package_tesst_as030pc-0.3-py3-none-any.whl/package/__init__.py
@@ -0,0 +1,14 @@
+def add_number(a,b):
+    return a + b
+
+def rest_number(a,b):
+    return a - b
+
+def mult_number(a,b):
+    return a*b
+
+def div_number(a,b):
+    return a/b
+
+def nueva(c):
+    return c
