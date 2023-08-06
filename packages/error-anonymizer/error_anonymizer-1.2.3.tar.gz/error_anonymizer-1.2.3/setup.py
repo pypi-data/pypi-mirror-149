b605573adc9b24ab04734ec684f6fe1f67478fc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:35:01 +0000

    Add package error-anonymizer/1.2.3/error_anonymizer-1.2.3.tar.gz

---
+++ packages/error-anonymizer/error_anonymizer-1.2.3.tar.gz/error_anonymizer-1.2.3/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+from error_anonymizer import __version__
+
+setup(
+	name='error_anonymizer',
+	version=__version__,
+
+	url='https://github.com/maskedband1t/error-anonymizer',
+	author='Anurag Akkiraju',
+	author_email='anurag.akkiraju@gmail.com',
+
+	py_modules=['error_anonymizer'],
+	packages=find_packages(),
+
+	install_requires=[
+		'setuptools',
+		'click',
+		'pathlib',
+	],
+
+	entry_points={
+		'console_scripts': [
+			'main=error_anonymizer:main',
+		],
+	},
+
+	classifiers=[
+		'Intended Audience :: Developers',
+
+		'Programming Language :: Python',
+		'Programming Language :: Python :: 3',
+    	],
+
+	extra_test = [
+    		'pytest>=4',
+    		'pytest-cov>=2',
+	],
+)
