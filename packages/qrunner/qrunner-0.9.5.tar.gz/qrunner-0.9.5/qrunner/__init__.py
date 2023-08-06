8700a8ffd4e9cebfe6dd66f171571af796a935af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:10 +0000

    Add package qrunner/0.9.5/qrunner-0.9.5.tar.gz

---
+++ packages/qrunner/qrunner-0.9.5.tar.gz/qrunner-0.9.5/qrunner/__init__.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+from qrunner.running.runner import main
+from qrunner.case import TestCase, Page, H5Page
+from qrunner.core.h5.driver import H5Driver
+from qrunner.utils.log import logger
+from qrunner.utils.decorate import *
+from qrunner.utils.mock import mock, mock_en
+
+__version__ = "0.9.5"
+__description__ = "全栈自动化测试框架"
+
