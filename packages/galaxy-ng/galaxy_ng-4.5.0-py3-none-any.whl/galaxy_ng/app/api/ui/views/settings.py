d9f3353e85c357f5be526792a89fc589e9b027bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:00 +0000

    Add package galaxy-ng/4.5.0/galaxy_ng-4.5.0-py3-none-any.whl

---
+++ packages/galaxy-ng/galaxy_ng-4.5.0-py3-none-any.whl/galaxy_ng/app/api/ui/views/settings.py
@@ -0,0 +1,23 @@
+from django.conf import settings
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
+
+from galaxy_ng.app.api import base as api_base
+
+
+class SettingsView(api_base.APIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get(self, request, *args, **kwargs):
+        data = {}
+        keyset = [
+            "GALAXY_ENABLE_UNAUTHENTICATED_COLLECTION_ACCESS",
+            "GALAXY_ENABLE_UNAUTHENTICATED_COLLECTION_DOWNLOAD",
+            "GALAXY_FEATURE_FLAGS",
+            "GALAXY_TOKEN_EXPIRATION",
+            "GALAXY_REQUIRE_CONTENT_APPROVAL",
+            "GALAXY_COLLECTION_SIGNING_SERVICE",
+            "GALAXY_AUTO_SIGN_COLLECTIONS",
+        ]
+        data = {key: settings.as_dict().get(key, None) for key in keyset}
+        return Response(data)
