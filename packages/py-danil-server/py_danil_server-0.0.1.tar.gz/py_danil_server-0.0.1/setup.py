f365ea94897311372afcdfb878eff597f343f058
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:23 +0000

    Add package py-danil-server/0.0.1/py_danil_server-0.0.1.tar.gz

---
+++ packages/py-danil-server/py_danil_server-0.0.1.tar.gz/py_danil_server-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name="py_danil_server",
+      version="0.0.1",
+      description="Server app",
+      author="Danil Mosin",
+      author_email="vinniopo@mail.ru",
+      packages=find_packages(),
+      install_requires=['PyQt5', 'sqlalchemy', 'pycryptodome', 'pycryptodomex'],
+      scripts=['server/server_run']
+      )
