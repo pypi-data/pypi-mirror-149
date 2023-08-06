f69b4548defebd197d21d199fed2cbacc6bb9672
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:15 +0000

    Add package shifre/0.1.2/shifre-0.1.2.tar.gz

---
+++ packages/shifre/shifre-0.1.2.tar.gz/shifre-0.1.2/shifre/shifre.py
@@ -0,0 +1,55 @@
+import random
+
+__upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+__lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz"
+__symbol_list = """~`!@#$%^&*()_-+={[}]|\:;"'<,>.?/"""
+__digit_list = "0123456789"
+
+def __catchErrors(c:int, d:int,s:int):
+    assert (c >= 0), "Character count cannot be lower than zero"
+    
+    assert (d >= 0), "Digit count cannot be lower than zero"
+    
+    assert (s >= 0), "Symbol count cannot be lower than zero"
+    
+    assert (c >= d+s), "You cannot use digits and symbols more than character count."
+
+# This generates a new string from a given wordlist
+def __generateStr(wordlist:str, targetCount:int) -> str:
+    temp_word = ""
+    for _ in range(targetCount):
+        temp_word += wordlist[random.randint(0,len(wordlist)-1)]
+    return temp_word
+
+# Used this to randomize every aspect of selection in our sum word list (all digits, symbols, letters)
+def __randomizeStr(wordlist:list) -> str:
+    temp_word = ""
+    while(len(wordlist) > 0):
+        temp_rnd = random.randint(0,len(wordlist)-1)
+        temp_word += wordlist[temp_rnd]
+        del wordlist[temp_rnd]
+    return temp_word
+
+def __generateLetterStr(letterCount:int, allowUpper:bool) -> str:
+    temp_word = ""
+    if allowUpper and letterCount >= 2:
+        temp_word += __upperCaseLetters[random.randint(0,len(__upperCaseLetters)-1)]
+        temp_word += __lowerCaseLetters[random.randint(0,len(__lowerCaseLetters)-1)]
+        letterCount -= 2
+    
+    temp_word += __generateStr(__lowerCaseLetters+__upperCaseLetters,letterCount) if allowUpper else __generateStr(__lowerCaseLetters,letterCount) 
+    return temp_word
+
+
+
+
+
+# Main generator function
+def generatePassword(charCount:int,digitCount:int,symbolCount:int,allowUpper:bool) -> str:
+        __catchErrors(charCount,digitCount,symbolCount)
+        letterCount = charCount - (digitCount+symbolCount)
+        letters = __generateLetterStr(letterCount,allowUpper)
+        symbols = __generateStr(__symbol_list,symbolCount)
+        digits = __generateStr(__digit_list,digitCount)
+        password =  __randomizeStr(list(letters+symbols+digits))
+        return password
