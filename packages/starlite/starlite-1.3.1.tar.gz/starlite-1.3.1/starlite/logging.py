db93a86076d3eafca9e0ab272edad9e21b282301
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:56 +0000

    Add package starlite/1.3.1/starlite-1.3.1.tar.gz

---
+++ packages/starlite/starlite-1.3.1.tar.gz/starlite-1.3.1/starlite/logging.py
@@ -0,0 +1,31 @@
+from logging import config
+from typing import Any, Dict, List, Optional, Union
+
+from pydantic import BaseModel
+from typing_extensions import Literal
+
+
+class LoggingConfig(BaseModel):
+    version: Literal[1] = 1
+    incremental: bool = False
+    disable_existing_loggers: bool = False
+    filters: Optional[Dict[str, Dict[str, Any]]] = None
+    formatters: Dict[str, Dict[str, Any]] = {
+        "standard": {"format": "%(levelname)s - %(asctime)s - %(name)s - %(module)s - %(message)s"}
+    }
+    handlers: Dict[str, Dict[str, Any]] = {
+        "console": {"class": "logging.StreamHandler", "level": "DEBUG", "formatter": "standard"},
+        "queue_listener": {"class": "starlite.QueueListenerHandler", "handlers": ["cfg://handlers.console"]},
+    }
+    loggers: Dict[str, Dict[str, Any]] = {
+        "starlite": {
+            "level": "INFO",
+            "handlers": ["queue_listener"],
+        },
+    }
+    root: Dict[str, Union[Dict[str, Any], List[Any], str]] = {"handlers": ["console"], "level": "WARNING"}
+
+    def configure(self) -> None:
+        """Configure logging by converting 'self' to dict and passing it to logging.config.dictConfig"""
+
+        config.dictConfig(self.dict(exclude_none=True))
