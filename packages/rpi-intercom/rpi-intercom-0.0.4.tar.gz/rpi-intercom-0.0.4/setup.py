f04f44cb1677f16d2ff9d9c438c3eb697de5a350
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:53 +0000

    Add package rpi-intercom/0.0.4/rpi-intercom-0.0.4.tar.gz

---
+++ packages/rpi-intercom/rpi-intercom-0.0.4.tar.gz/rpi-intercom-0.0.4/setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='rpi-intercom',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+    ],
+    packages=find_packages(include=['rpi_intercom']),
+    version='0.0.4',
+    description='Intercom library for a raspberrypi',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=["pymumble", "pyalsaaudio", "gpiozero", "schema", "pyyaml", "pyOpenSSL", "numpy"],
+    author="Stephen Beechen",
+    author_email="stephen@beechens.com",
+    python_requires=">=3.9",
+    license='MIT',
+)
