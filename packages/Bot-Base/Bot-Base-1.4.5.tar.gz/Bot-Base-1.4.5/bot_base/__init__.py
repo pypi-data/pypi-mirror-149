52d8e2c187096d8b58f7042b45643224e01c4944
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:39 +0000

    Add package Bot-Base/1.4.5/Bot-Base-1.4.5.tar.gz

---
+++ packages/Bot-Base/Bot-Base-1.4.5.tar.gz/Bot-Base-1.4.5/bot_base/__init__.py
@@ -0,0 +1,19 @@
+import logging
+from collections import namedtuple
+
+from .bot import BotBase
+from .context import BotContext
+from .exceptions import *
+
+__version__ = "1.4.5"
+
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+VersionInfo = namedtuple("VersionInfo", "major minor micro releaselevel serial")
+version_info = VersionInfo(
+    major=1,
+    minor=4,
+    micro=5,
+    releaselevel="production",
+    serial=0,
+)
