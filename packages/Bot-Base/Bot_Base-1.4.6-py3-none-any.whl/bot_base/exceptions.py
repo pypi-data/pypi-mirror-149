815e6f26772677a6a48354a2f2e628f1270bed91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:43 +0000

    Add package Bot-Base/1.4.6/Bot_Base-1.4.6-py3-none-any.whl

---
+++ packages/Bot-Base/Bot_Base-1.4.6-py3-none-any.whl/bot_base/exceptions.py
@@ -0,0 +1,21 @@
+try:
+    from nextcord import DiscordException
+except ModuleNotFoundError:
+    from disnake import DiscordException
+
+
+class PrefixNotFound(DiscordException):
+    """A prefix for this guild was not found."""
+
+
+class ExistingEntry(DiscordException):
+    """An entry was already found in the cache with this key."""
+
+
+class NonExistentEntry(DiscordException):
+    """No entry found in the cache with this key."""
+
+
+class BlacklistedEntry(DiscordException):
+    def __init__(self, message: str):
+        self.message: str = message
