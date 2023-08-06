35d780e99a952f736e129f5931a98cb6dc059942
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:17:26 +0000

    Add package WES-Calculation/1.5.10/WES_Calculation-1.5.10-py3-none-any.whl

---
+++ packages/WES-Calculation/WES_Calculation-1.5.10-py3-none-any.whl/WES_Calculation/__init__.py
@@ -0,0 +1,15 @@
+'''
+Author: BDFD
+Date: 2021-10-27 18:42:50
+LastEditTime: 2022-04-20 13:16:30
+LastEditors: BDFD
+Description: In User Settings Edit
+FilePath: \5.2-PyPi-WES_Calculation\WES_Calculation\__init__.py
+'''
+
+from WES_Calculation.templateproj import add_one
+from WES_Calculation.templateproj import add_two
+from WES_Calculation.gumbel import gumbel
+from WES_Calculation.greenampt import greenampt
+from WES_Calculation.wave import wave
+
