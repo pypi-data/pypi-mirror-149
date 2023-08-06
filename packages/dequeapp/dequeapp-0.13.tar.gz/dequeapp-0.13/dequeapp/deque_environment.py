27346556b1f8bedec3cf76026e47b1948d8e30a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:42:07 +0000

    Add package dequeapp/0.13/dequeapp-0.13.tar.gz

---
+++ packages/dequeapp/dequeapp-0.13.tar.gz/dequeapp-0.13/dequeapp/deque_environment.py
@@ -0,0 +1,15 @@
+import os
+
+AGENT_API_SERVICE_URL = 'https://apis-staging.deque.app'
+api_environment = os.getenv("DEQUE_API_ENVIRONMENT")
+if api_environment.lower() == "production":
+
+    AGENT_API_SERVICE_URL = 'https://apis.deque.app'
+
+
+else:
+
+    AGENT_API_SERVICE_URL = 'http://127.0.0.1:5000'#'https://apis-staging.deque.app'
+    REDIS_URL = "redis://localhost:6379"
+    #REDIS_URL = "redis://:czAr8PW5UuqvycZd0V9W0hPwum5m7yQV@redis-15261.c60.us-west-1-2.ec2.cloud.redislabs.com:15261/0"
+
