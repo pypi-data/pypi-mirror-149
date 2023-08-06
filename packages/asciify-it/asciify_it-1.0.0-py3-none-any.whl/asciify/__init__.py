c93dae74ca95e653166560b3f0ad461d208736fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:10:50 +0000

    Add package asciify-it/1.0.0/asciify_it-1.0.0-py3-none-any.whl

---
+++ packages/asciify-it/asciify_it-1.0.0-py3-none-any.whl/asciify/__init__.py
@@ -0,0 +1,10 @@
+from .algo import AsciiManager, draw_char, get_tiles, score_similarity
+from .cli import asciify
+
+__all__ = [
+    'asciify',
+    'AsciiManager',
+    'draw_char',
+    'get_tiles',
+    'score_similarity',
+]
