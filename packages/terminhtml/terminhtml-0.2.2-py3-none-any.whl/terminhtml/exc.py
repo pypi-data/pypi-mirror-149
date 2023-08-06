4acbe5989c872c8812683db844bbb2316c9d8b69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:46:22 +0000

    Add package terminhtml/0.2.2/terminhtml-0.2.2-py3-none-any.whl

---
+++ packages/terminhtml/terminhtml-0.2.2-py3-none-any.whl/terminhtml/exc.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+
+from terminhtml.base_exc import TerminHTMLException
+from terminhtml.output import Output
+
+
+class TerminHTMLUserException(TerminHTMLException):
+    """
+    Base class for all expected exceptions in TerminHTML.
+    """
+
+    pass
+
+
+class UserCommandException(TerminHTMLUserException):
+    """
+    Exception for user commands.
+    """
+
+    def __init__(self, message: str, output: Output, cwd: Path) -> None:
+        self.output = output
+        self.cwd = cwd
+        super().__init__(message)
+
+    def __str__(self) -> str:
+        return f"{super().__str__()}\n{self.cwd=} with output:\n{self.output}"
