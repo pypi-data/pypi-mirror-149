51f160116c5516a5799ab58447bf9467e01d4fdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:11 +0000

    Add package zhixuewang/1.1.7/zhixuewang-1.1.7-py3-none-any.whl

---
+++ packages/zhixuewang/zhixuewang-1.1.7-py3-none-any.whl/zhixuewang/exceptions.py
@@ -0,0 +1,49 @@
+class Error(Exception):
+    value: str
+
+    def __str__(self):
+        return str(self.value)
+
+
+class LoginError(Error):
+    def __init__(self, value):
+        self.value = value
+
+
+class UserOrPassError(LoginError):
+    def __init__(self, value=None):
+        super().__init__(value or "用户名或密码错误!")
+
+
+class UserNotFoundError(LoginError):
+    def __init__(self, value=None):
+        super().__init__(value or "用户不存在!")
+
+
+class UserDefunctError(LoginError):
+    def __init__(self, value=None):
+        super().__init__(value or "用户已失效!")
+
+
+class RoleError(Error):
+    def __init__(self, value=None):
+        self.value = value or "账号是未知用户"
+
+
+class ArgError(Error):
+    def __init__(self, value=None):
+        self.value = value or "请输入正确的参数!"
+
+
+class PageConnectionError(Error):
+    def __init__(self, value):
+        self.value = value
+
+
+class PageInformationError(Error):
+    def __init__(self, value):
+        self.value = value
+
+class GetOriginalError(Error):
+    def __init__(self, error_code: int, error_info: str):
+        self.value = f"获取原卷错误:{error_info}({error_code})"
