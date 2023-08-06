29d0f32741be3e03519bb997401c2a1cad554115
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:44 +0000

    Add package osbuilder/3.0.4.2/osbuilder-3.0.4.2.tar.gz

---
+++ packages/osbuilder/osbuilder-3.0.4.2.tar.gz/osbuilder-3.0.4.2/osbuilder/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+print('##############################################')
+print('# Welcome to the OSBuilder')
+print('# Write by Zarhin')
+print('# Email: lizaixianvip@163.com')
+print('##############################################')
