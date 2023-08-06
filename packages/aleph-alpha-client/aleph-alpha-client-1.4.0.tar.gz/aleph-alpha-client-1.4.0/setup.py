a51b4dbd28d7943553c948f39be020f660713eec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:25 +0000

    Add package aleph-alpha-client/1.4.0/aleph-alpha-client-1.4.0.tar.gz

---
+++ packages/aleph-alpha-client/aleph-alpha-client-1.4.0.tar.gz/aleph-alpha-client-1.4.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(
+    name="aleph-alpha-client",
+    url="https://github.com/Aleph-Alpha/aleph-alpha-client",
+    author="Aleph Alpha",
+    author_email="support@aleph-alpha.com",
+    packages=["aleph_alpha_client"],
+    setup_requires=['setuptools_scm'],
+    install_requires=["requests"],
+    tests_require=["pytest", "pytest-cov", "python-dotenv"],
+    extras_require={
+        "test": ["pytest", "pytest-cov", "python-dotenv"],
+    },
+    use_scm_version=True,
+    license="MIT",
+    description="python client to interact with Aleph Alpha api endpoints",
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+)
