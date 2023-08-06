1648bf68633acd04be7c17ad9ba7454aab641d04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:53 +0000

    Add package je-tk-plot/0.0.1/je_tk_plot-0.0.1-py3-none-any.whl

---
+++ packages/je-tk-plot/je_tk_plot-0.0.1-py3-none-any.whl/je_matplotlib_wrapper/barh/barh_tkinter.py
@@ -0,0 +1,38 @@
+import tkinter
+import matplotlib.pyplot as plt
+import numpy as np
+from tkinter import (TOP, BOTH)
+from matplotlib.backends.backend_tkagg import (
+    FigureCanvasTkAgg)
+from textwrap import wrap
+
+
+def make_barh_graph(y_content_list: list, x_content_list: list, x_label: str = "", title: str = "", **kwargs):
+    figure, axes = plt.subplots(**kwargs)
+    y_pos = np.arange(len(y_content_list))
+    axes.barh(y_pos, x_content_list, align='center')
+    y_content_list = ['\n'.join(wrap(content, 20)) for content in y_content_list]
+    axes.set_yticks(y_pos, labels=y_content_list)
+    axes.invert_yaxis()  # labels read top-to-bottom
+    axes.set_xlabel(x_label)
+    axes.set_title(title)
+    return figure
+
+
+def set_tkinter_embed_matplotlib_barh(y_content_list: list, x_content_list: list,
+                                      show_figure_window: tkinter.Tk = None,
+                                      x_label: str = "", title: str = "", **kwargs):
+    if show_figure_window is None:
+        show_figure_window = tkinter.Tk()
+        show_figure_window.title = ""
+        window_width, window_height = show_figure_window.winfo_screenwidth(), show_figure_window.winfo_screenheight()
+        show_figure_window.geometry("%dx%d+0+0" % (window_width, window_height))
+    else:
+        show_figure_window.title = ""
+        window_width, window_height = show_figure_window.winfo_screenwidth(), show_figure_window.winfo_screenheight()
+        show_figure_window.geometry("%dx%d+0+0" % (window_width, window_height))
+    figure = make_barh_graph(y_content_list, x_content_list, x_label, title)
+    canvas = FigureCanvasTkAgg(figure, master=show_figure_window)  # A tk.DrawingArea.
+    canvas.draw()
+    canvas.get_tk_widget().pack(side=TOP, fill=BOTH, expand=1)
+    show_figure_window.mainloop()
