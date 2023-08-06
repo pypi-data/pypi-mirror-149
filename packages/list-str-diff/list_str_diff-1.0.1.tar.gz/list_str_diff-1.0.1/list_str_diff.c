6e8567e9868a5f9ab526469322236f8ed341d1a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:36 +0000

    Add package list-str-diff/1.0.1/list_str_diff-1.0.1.tar.gz

---
+++ packages/list-str-diff/list_str_diff-1.0.1.tar.gz/list_str_diff-1.0.1/list_str_diff.c
@@ -0,0 +1,68 @@
+#include <Python.h>
+
+static PyObject *method_list_str_diff(PyObject *self, PyObject *args) {
+
+    PyObject *letters;
+    char *word;
+
+
+    if(!PyArg_ParseTuple(args, "Os", &letters, &word)) {
+        return NULL;
+    }
+
+    int len_letters = PyList_Size(letters);
+    size_t len_word = strlen(word);
+
+    char *word_copy = (char*)malloc(len_word * sizeof(char));
+    strncpy(word_copy, word, len_word);
+
+    PyObject *ret_list = PyList_New(0);
+
+    PyObject *item;
+    char *c_item;
+    int in_flag;
+
+    for (int i = 0; i < len_letters; i++){
+
+        item = PyList_GetItem(letters, i);
+        c_item = PyUnicode_AsUTF8(item);
+
+        in_flag = 0;
+
+        for (int j = 0; j < len_word; j++){
+            if (*c_item == word[j]){
+                word[j] = '\1';
+                in_flag = 1;
+                break;
+            }
+        }
+        if (!in_flag){
+            PyList_Append(ret_list, item);
+        }
+    }
+
+    strncpy(word, word_copy, len_word);
+    free(word_copy);
+
+    return ret_list;
+}
+
+
+static PyMethodDef list_str_diffMethods[] = {
+    {"list_str_diff", method_list_str_diff, METH_VARARGS, "Takes the difference of a list and a string."},
+    {NULL, NULL, 0, NULL}
+};
+
+
+static struct PyModuleDef list_str_diff = {
+    PyModuleDef_HEAD_INIT,
+    "list_str_diff",
+    "Takes the difference of a list and a string.",
+    -1,
+    list_str_diffMethods
+};
+
+
+PyMODINIT_FUNC PyInit_list_str_diff(void) {
+    return PyModule_Create(&list_str_diff);
+}
---
+++ packages/list-str-diff/list_str_diff-1.0.1.tar.gz/list_str_diff-1.0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup, Extension
+
+def main():
+    setup(
+          name="list_str_diff",
+          version="1.0.1",
+          description="Takes the difference of a list and a string.",
+          author="Nagy Zoltán",
+          author_email="zolika3400@gmail.com",
+          ext_modules=[Extension('list_str_diff', ['list_str_diff.c'])]
+          )
+
+if __name__ == "__main__":
+    main()
