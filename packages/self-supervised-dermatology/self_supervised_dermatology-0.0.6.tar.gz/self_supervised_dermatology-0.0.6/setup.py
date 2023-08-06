e9c94bd6f4f68b36298529257c548b4037be272f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:00 +0000

    Add package self-supervised-dermatology/0.0.6/self_supervised_dermatology-0.0.6.tar.gz

---
+++ packages/self-supervised-dermatology/self_supervised_dermatology-0.0.6.tar.gz/self_supervised_dermatology-0.0.6/setup.py
@@ -0,0 +1,62 @@
+import re
+import os.path
+import codecs
+
+from setuptools import setup, find_packages
+from os.path import abspath, dirname, join
+
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+
+def parse_requirements(filename):
+    """
+    load requirements from a pip requirements file
+    """
+    line_iter = (line.strip() for line in open(filename))
+    return [line for line in line_iter if line and not line.startswith("#")]
+
+
+README_MD = open(join(dirname(abspath(__file__)), "README.md")).read()
+PACKAGE_NAME = 'self_supervised_dermatology'
+SOURCE_DIRECTORY = 'src'
+SOURCE_PACKAGE_REGEX = re.compile(rf'^{SOURCE_DIRECTORY}')
+
+source_packages = find_packages(
+    include=[SOURCE_DIRECTORY, f'{SOURCE_DIRECTORY}.*'])
+proj_packages = [
+    SOURCE_PACKAGE_REGEX.sub(PACKAGE_NAME, name) for name in source_packages
+]
+
+setup(name=PACKAGE_NAME,
+      packages=proj_packages,
+      package_dir={PACKAGE_NAME: SOURCE_DIRECTORY},
+      version='0.0.6',
+      author='Fabian Groeger',
+      author_email='fabian.groeger@hslu.ch',
+      description=
+      'Unsupervised Pre-Training for Texture Segmentation in Dermatology',
+      long_description=README_MD,
+      long_description_content_type="text/markdown",
+      url='https://github.com/FabianGroeger96/',
+      install_reqs=parse_requirements('requirements.txt'),
+      classifiers=[
+          'Development Status :: 1 - Planning',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: BSD License',
+          'Operating System :: POSIX :: Linux',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.6',
+      ])
---
+++ packages/self-supervised-dermatology/self_supervised_dermatology-0.0.6.tar.gz/self_supervised_dermatology-0.0.6/src/datasets/downstream/pad_ufes_20_dataset.py
@@ -0,0 +1,67 @@
+import os
+import torch
+import pandas as pd
+from PIL import Image
+from pathlib import Path
+from typing import Union
+
+from src.datasets.downstream.base_dataset import BaseDataset
+
+
+class PADUFES20Dataset(BaseDataset):
+    """PAD-UFES-20 dataset."""
+    IMG_COL = 'img_id'
+    LBL_COL = 'diagnostic'
+
+    def __init__(self,
+                 csv_file: Union[str, Path] = 'data/PAD-UFES-20/metadata.csv',
+                 root_dir: Union[str, Path] = 'data/PAD-UFES-20/images/',
+                 transform=None):
+        """
+        Initializes the dataset.
+
+        Sets the correct path for the needed arguments.
+
+        Parameters
+        ----------
+        csv_file : str
+            Path to the csv file with metadata, including annotations.
+        root_dir : str
+            Directory with all the images.
+        transform : Union[callable, optional]
+            Optional transform to be applied to the images.
+        """
+        super().__init__(transform=transform)
+        # check if the dataset path exists
+        self.csv_file = Path(csv_file)
+        if not self.csv_file.exists():
+            raise ValueError(
+                f'CSV metadata path must exist, path: {self.csv_file}')
+        self.root_dir = Path(root_dir)
+        if not self.root_dir.exists():
+            raise ValueError(
+                f'Image path must exist, path: {self.root_dir}')
+
+        # load the metadata and encode the label
+        self.meta_data = pd.DataFrame(pd.read_csv(self.csv_file))
+        self.meta_data[self.LBL_COL] = self.labelencoder.fit_transform(
+            self.meta_data[self.LBL_COL])
+        self.classes = self.labelencoder.classes_
+        self.n_classes = len(self.classes)
+
+    def __len__(self):
+        return len(self.meta_data)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        img_name = os.path.join(self.root_dir,
+                                self.meta_data.iloc[idx][self.IMG_COL])
+        image = Image.open(img_name)
+        image = image.convert('RGB')
+        if self.transform:
+            image = self.transform(image)
+
+        diagnosis = self.meta_data.iloc[idx][self.LBL_COL]
+        return image, int(diagnosis)
