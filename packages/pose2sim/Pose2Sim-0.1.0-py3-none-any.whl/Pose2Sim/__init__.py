01a3ab74bacf8c1baeaefb4c6709bea6be1a73db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:07 +0000

    Add package pose2sim/0.1.0/Pose2Sim-0.1.0-py3-none-any.whl

---
+++ packages/pose2sim/Pose2Sim-0.1.0-py3-none-any.whl/Pose2Sim/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+
+name = 'Pose2Sim'
+
+__version__ = '0.1.0'
+__author__ = "David Pagnon"
+__author_email__ = "contact@david-pagnon.com"
+__description__ = """Perform a markerless kinematic analysis from multiple calibrated views as a unified workflow from an OpenPose input to an OpenSim result."""
+__url__ = "https://gitlab.inria.fr/perfanalytics/pose2sim"
+
+VERSION = __version__
