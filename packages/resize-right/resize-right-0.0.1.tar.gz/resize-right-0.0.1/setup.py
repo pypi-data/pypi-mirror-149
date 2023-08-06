487a05c638c6c89caf485baed55dd43265433cf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:51 +0000

    Add package resize-right/0.0.1/resize-right-0.0.1.tar.gz

---
+++ packages/resize-right/resize-right-0.0.1.tar.gz/resize-right-0.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'resize-right',
+  packages = find_packages(exclude=[]),
+  version = '0.0.1',
+  license = 'MIT',
+  description = 'Resize Right',
+  author = 'Assaf Shocher',
+  author_email = 'assafshocher@gmail.com',
+  url = 'https://github.com/assafshocher/ResizeRight',
+  keywords = [
+    'deep learning',
+    'image resize'
+  ],
+  install_requires=[
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
