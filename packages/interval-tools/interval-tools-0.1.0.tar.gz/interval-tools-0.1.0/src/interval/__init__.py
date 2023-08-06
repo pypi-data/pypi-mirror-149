eeba8b71bd7e151dbcfbe3e91855b913bd961ed7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:58:29 +0000

    Add package interval-tools/0.1.0/interval-tools-0.1.0.tar.gz

---
+++ packages/interval-tools/interval-tools-0.1.0.tar.gz/interval-tools-0.1.0/src/interval/__init__.py
@@ -0,0 +1,11 @@
+"""
+interval
+~~~~~~~~
+
+Utility functions and classes used in applications of Interval Design.
+
+:copyright: (c) 2022 by Interval Design.
+:license: MIT, see LICENSE for more details.
+"""
+
+from . import utils
