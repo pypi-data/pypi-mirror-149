ae976e8f377854047fd92e8b1909487937e9c1f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:44 +0000

    Add package reactionmenu/3.1.0b3/reactionmenu-3.1.0b3.tar.gz

---
+++ packages/reactionmenu/reactionmenu-3.1.0b3.tar.gz/reactionmenu-3.1.0b3/reactionmenu/__init__.py
@@ -0,0 +1,39 @@
+"""
+reactionmenu • discord pagination
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+A library to create a discord paginator. Supports pagination with Discords Buttons feature and reactions.
+
+:copyright: (c) 2021-present Defxult#8269
+:license: MIT
+
+"""
+
+from .buttons import ReactionButton, ViewButton
+from .core import ReactionMenu
+from .views_menu import ViewMenu, ViewSelect
+from .abc import Page
+
+
+def version_info():
+    """Shows the current version, release type, and patch of the library
+
+    - `version` Current version of the library
+    - `releasetype` Either "final" (the PyPI version) or "pre-release" (the GitHub version)
+    - `patch` The last significant bug fix
+    
+    >>> print(reactionmenu.version_info())
+    """
+    from collections import namedtuple
+    VersionInfo = namedtuple('VersionInfo', ['version', 'releasetype', 'patch'])
+    return VersionInfo(version='3.1.0b3', releasetype='pre-release', patch='1b')
+
+__source__ = 'https://github.com/Defxult/reactionmenu'
+__all__ = (
+    'ReactionMenu',
+    'ReactionButton',
+    'ViewMenu',
+    'ViewButton',
+    'ViewSelect',
+    'Page'
+)
---
+++ packages/reactionmenu/reactionmenu-3.1.0b3.tar.gz/reactionmenu-3.1.0b3/setup.py
@@ -0,0 +1,72 @@
+from setuptools import setup, find_packages
+
+def _get_readme():
+    with open('README.md', encoding='utf-8') as fp:
+        return fp.read()
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'Topic :: Software Development :: Libraries',
+    'Topic :: Software Development :: Libraries :: Python Modules'
+]
+
+tags = [
+    'buttons',
+    'buttons paginator',
+    'buttons menu',
+    'discord',
+    'discord.py',
+    'discord.py 2.0',
+    'd.py',
+    'components',
+    'components paginator',
+    'components menu',
+    'discord components',
+    'discord components menu',
+    'discord buttons',
+    'discord buttons paginator',
+    'discord buttons menu',
+    'discord paginator',
+    'discord pagination',
+    'discord reaction menu',
+    'discord reactions',
+    'discord embed',
+    'discord menu',
+    'discord interactions',
+    'embed menu',
+    'embed reaction menu',
+    'embed paginator',
+    'interactions',
+    'interactions menu',
+    'interactions paginator',
+    'menus',
+    'paginator',
+    'pagination',
+    'pagination menu',
+    'reaction menu'
+]
+
+details = {
+    #! 'Changelog' : 'https://github.com/Defxult/reactionmenu/blob/main/CHANGELOG.md' - Uncomment this when it's time for release
+    'Changelog' : 'https://github.com/Defxult/reactionmenu/blob/discord.py-2.0-support/CHANGELOG.md'
+}
+
+setup(
+    author='Defxult#8269',
+    name='reactionmenu',
+    version='3.1.0b3',
+    description='A library to create a discord.py 2.0+ paginator. Supports pagination with buttons, reactions, and category selection using selects.',
+    url='https://github.com/Defxult/reactionmenu',
+    project_urls=details,
+    classifiers=classifiers,
+    long_description=_get_readme(),
+    long_description_content_type='text/markdown',
+    license='MIT',
+    keywords=tags,
+    packages=find_packages()
+    #! install_requires=['discord.py>=2.0.0']
+)
