3dcf5233d349767ac28d0ce9c27b5150b5e06004
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:39 +0000

    Add package data-preprocessors/0.11.0/data-preprocessors-0.11.0.tar.gz

---
+++ packages/data-preprocessors/data-preprocessors-0.11.0.tar.gz/data-preprocessors-0.11.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['data_preprocessors']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'data-preprocessors',
+    'version': '0.11.0',
+    'description': 'An easy to use tool for Data Preprocessing specially for Text Preprocessing',
+    'long_description': '<h1 align="center">\n    <img src="branding/logo.png">\n</h1>\n\n<p align="center">\n    An easy to use tool for Data Preprocessing specially for Text Preprocessing\n</p>\n\n## **Installation**\nInstall the latest stable release<br>\n**For windows**<br>\n`$ pip install -U data-preprocessors`\n\n**For Linux/WSL2**<br>\n`$ pip3 install -U data-preprocessors`\n\n## **Quick Start**\n```python\nfrom data_preprocessors import text_preprocessor as tp\nsentence = "bla! bla- ?bla ?bla."\nsentence = tp.remove_punc(sentence)\nprint(sentence)\n\n>> bla bla bla bla\n```\n\n',
+    'author': 'Md. Musfiqur Rahaman',
+    'author_email': 'musfiqur.rahaman@northsouth.edu',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/MusfiqDehan/data-preprocessors',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
