4f7aef7dc7359928c133f3a58ff4a8626875fdd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:01 +0000

    Add package CosmoTech-Acceleration-Library/0.0.9/CosmoTech_Acceleration_Library-0.0.9.tar.gz

---
+++ packages/CosmoTech-Acceleration-Library/CosmoTech_Acceleration_Library-0.0.9.tar.gz/CosmoTech_Acceleration_Library-0.0.9/setup.py
@@ -0,0 +1,20 @@
+# Copyright (c) Cosmo Tech corporation.
+# Licensed under the MIT license.
+import setuptools
+
+VERSION = "0.0.9"
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setuptools.setup(
+    name='CosmoTech_Acceleration_Library',
+    version=VERSION,
+    packages=setuptools.find_packages(),
+    url='https://github.com/Cosmo-Tech/CosmoTech-Acceleration-Library',
+    license='MIT',
+    author='afossart',
+    author_email='alexis.fossart@cosmotech.com',
+    description='Acceleration library for CosmoTech cloud based solution development',
+    install_requires=required
+)
