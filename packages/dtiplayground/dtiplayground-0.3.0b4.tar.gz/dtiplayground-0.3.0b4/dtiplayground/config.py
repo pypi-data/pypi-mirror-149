886a6c1a98d87108a65a7e1a69f880f041934487
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:25 +0000

    Add package dtiplayground/0.3.0b4/dtiplayground-0.3.0b4.tar.gz

---
+++ packages/dtiplayground/dtiplayground-0.3.0b4.tar.gz/dtiplayground-0.3.0b4/dtiplayground/config.py
@@ -0,0 +1,12 @@
+
+INFO = {
+  "dtiplayground": { 
+    "version" : "0.3.0-beta4"
+  },
+  "dmriprep": {
+    "version" : "0.3.0-beta4"
+  },
+  "dtiatlasbuilder": {
+    "version" : "0.1.0"
+  }
+}
