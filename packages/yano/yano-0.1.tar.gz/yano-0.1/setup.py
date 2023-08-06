ac7246d279d5f615ee07322ccf9edd7c77a761d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:37 +0000

    Add package yano/0.1/yano-0.1.tar.gz

---
+++ packages/yano/yano-0.1.tar.gz/yano-0.1/setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+
+
+setup(
+  name = 'yano',         
+  packages = ['yano'],   
+  version = '0.1',      
+  license='apache-2.0',        
+  description = 'Python module providing anomaly detection datasets and tools for evaluating algorithms',   
+  author = 'Simon Klüttermann',                   
+  author_email = 'Simon.Kluettermann@cs.tu-dortmund.de',      
+  url = 'https://github.com/psorus/yano',   
+  download_url = 'https://github.com/psorus/yano/archive/v_01.tar.gz',    
+  keywords = ['DATA', 'ANOMALY DETECTION', 'OUTLIER DETECTION', 'ML'],   
+  install_requires=[            
+          'numpy',
+          'tqdm',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Science/Research',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: Apache Software License',
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+  ],
+)
