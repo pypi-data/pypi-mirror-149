499641677b372ac3101e666238ed1e4cafb5ccd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:00:00 +0000

    Add package cassarrow/0.1.0/cassarrow-0.1.0.tar.gz

---
+++ packages/cassarrow/cassarrow-0.1.0.tar.gz/cassarrow-0.1.0/cassarrow/__init__.py
@@ -0,0 +1,5 @@
+from cassarrow.impl import install_cassarrow, result_set_to_table, metadata_to_schema
+
+__version__ = "0.1.0"
+
+__all__ = ["install_cassarrow", "result_set_to_table", "metadata_to_schema"]
