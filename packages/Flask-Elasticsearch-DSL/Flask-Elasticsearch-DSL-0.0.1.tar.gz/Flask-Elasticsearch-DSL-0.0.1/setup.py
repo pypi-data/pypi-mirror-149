78a430695c9d46e6084d684a870ae03dc67fea03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:57 +0000

    Add package Flask-Elasticsearch-DSL/0.0.1/Flask-Elasticsearch-DSL-0.0.1.tar.gz

---
+++ packages/Flask-Elasticsearch-DSL/Flask-Elasticsearch-DSL-0.0.1.tar.gz/Flask-Elasticsearch-DSL-0.0.1/setup.py
@@ -0,0 +1,35 @@
+from codecs import open
+
+from setuptools import setup
+
+
+setup(
+    name='Flask-Elasticsearch-DSL',
+
+    version='0.0.1',
+
+    description='Placeholder',
+    long_description="Placeholder",
+
+    url='https://github.com/eelkevdbos/Flask-Elasticsearch-DSL',
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+
+        'Operating System :: POSIX',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
+    ],
+
+    packages=['flask_elasticsearch_dsl'],
+)
