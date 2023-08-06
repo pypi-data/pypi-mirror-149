a4770d87a396ff308e9804c68592aea472d7013e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:59:37 +0000

    Add package ColorateStart/1.0/ColorateStart-1.0.tar.gz

---
+++ packages/ColorateStart/ColorateStart-1.0.tar.gz/ColorateStart-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ColorateStart',
+    version='1.0',
+    packages=['ColorateStart'],
+    url='https://github.com/InvalidAccount69/ColorateStart',
+    license='Apache License 2.0',
+    author='Rainbow_Rain',
+    author_email='',
+    description='Another Library'
+)
