2c967ac86434a54de2c465739ad7dffc25b31945
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:06 +0000

    Add package ampalibe/1.0.6/ampalibe-1.0.6.tar.gz

---
+++ packages/ampalibe/ampalibe-1.0.6.tar.gz/ampalibe-1.0.6/ampalibe/__init__.py
@@ -0,0 +1,7 @@
+from .requete import Model
+from .messenger import Messenger
+from .core import webserver, Extra as init
+from .utils import action, command, download_file, Payload
+
+__version__ = '1.0.6'
+__author__ = 'iTeam-$'
