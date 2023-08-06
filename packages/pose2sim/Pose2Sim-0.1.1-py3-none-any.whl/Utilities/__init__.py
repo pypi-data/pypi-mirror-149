47d071595b8739539ba2b34bac8b9071ccbbe160
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:43 +0000

    Add package pose2sim/0.1.1/Pose2Sim-0.1.1-py3-none-any.whl

---
+++ packages/pose2sim/Pose2Sim-0.1.1-py3-none-any.whl/Utilities/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+
+name = 'Pose2Sim Utilities'
+
+__version__ = '0.1.0'
+__author__ = "David Pagnon"
+__author_email__ = "contact@david-pagnon.com"
+__description__ = """Standalone Utilities for Pose2Sim."""
+__url__ = "https://gitlab.inria.fr/perfanalytics/pose2sim"
+
+VERSION = __version__
