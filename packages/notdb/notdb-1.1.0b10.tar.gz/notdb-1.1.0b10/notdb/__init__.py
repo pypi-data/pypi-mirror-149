afd7849110408892e7af186fc6201ce6a59fe9c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:08 +0000

    Add package notdb/1.1.0b10/notdb-1.1.0b10.tar.gz

---
+++ packages/notdb/notdb-1.1.0b10.tar.gz/notdb-1.1.0b10/notdb/__init__.py
@@ -0,0 +1,29 @@
+r'''
+
+NotDB is an open source document-oriented database that uses PYON-like documents
+
+Developed by Nawaf Alqari in 2022
+
+[Documentation](https://github.com/nawafalqari/NotDB)
+
+Example:
+First you need to make your database
+you can make it manually by just creating a `.ndb` file
+
+or by using the command line tool:
+$ notdb [filename]
+if you want to secure your db with a password you can use the `--password` or `-p` flag
+$ notdb [filename] --password
+
+>>> import notdb
+>>> db = notdb.NotDBClient('dbName.ndb')
+>>> db.get({}) # will return every document in the db
+
+'''
+
+from .ndb import NotDBClient, UTypes, create_db, get_password
+from .errors import *
+
+__version__ = '1.1.0b10'
+__all__ = ['InvalidDictError', 'InvalidHostError', 'NotDBClient', 'UTypes', 'WrongPasswordError',
+            'checkTypes', '__version__']
