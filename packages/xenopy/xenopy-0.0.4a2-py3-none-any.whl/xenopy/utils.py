ad0db95f6ed748c7025f0b93a6f04c09e014314d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:32 +0000

    Add package xenopy/0.0.4a2/xenopy-0.0.4a2-py3-none-any.whl

---
+++ packages/xenopy/xenopy-0.0.4a2-py3-none-any.whl/xenopy/utils.py
@@ -0,0 +1,18 @@
+def chunks(lst, n):
+    """Assign tasks evenly to multiple processes."""
+    to_return = []
+    portion = len(lst) // n 
+    for i in range(0, n):
+        if i == (n - 1):
+            last_port = lst[portion*i:]
+            diff = len(last_port) - portion
+            
+            for j in range(diff):
+                to_return[j].append(last_port[j])
+                
+            last_port = last_port[diff:]
+            to_return.append(last_port)
+            
+        else:
+            to_return.append(lst[portion*i: portion*(i+1)])
+    return to_return
