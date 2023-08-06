dde63df5524319bcc84c03dac61aa9f66c74b34b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:06 +0000

    Add package aacrgenie/13.3.0/aacrgenie-13.3.0.tar.gz

---
+++ packages/aacrgenie/aacrgenie-13.3.0.tar.gz/aacrgenie-13.3.0/genie_registry/patientRetraction.py
@@ -0,0 +1,13 @@
+import logging
+import os
+
+import pandas as pd
+
+from .sampleRetraction import sampleRetraction
+
+logger = logging.getLogger(__name__)
+
+
+class patientRetraction(sampleRetraction):
+
+    _fileType = "patientRetraction"
