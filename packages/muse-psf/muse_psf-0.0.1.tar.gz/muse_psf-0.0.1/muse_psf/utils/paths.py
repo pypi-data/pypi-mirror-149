94263011a07d6c27136205af0a077a75d6e2e297
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:11:23 +0000

    Add package muse-psf/0.0.1/muse_psf-0.0.1.tar.gz

---
+++ packages/muse-psf/muse_psf-0.0.1.tar.gz/muse_psf-0.0.1/muse_psf/utils/paths.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 28 16:48:59 2022
+
+@author: macarena
+"""
+#### HARDCODED PATHS ####
+
+filterdir = '/diska/home/opsproj2/aomuse/macarena/tutorial/filter_images/' # where single_iband images will be stored
+catdir = '/diska/home/opsproj2/aomuse/macarena/tutorial/catalogues/' # where the catalogues will be stored
+analysisdir = '/diska/home/opsproj2/aomuse/macarena/tutorial/psf/'# where the prm and psf files will be stored
+filterlist = '/diska/home/opsproj2/aomuse/macarena/tutorial/filters/filter_list.fits'
+zero_point_list = '/diska/home/opsproj2/aomuse/macarena/tutorial/filters/zeropoints.dat'
+pampelmuse_file = '/diska/home/opsproj2/aomuse/macarena/tutorial/config/pampelmuse.json'
+
+#### PARAMETERS ####
+
+passband = 'Cousins_I'
+muse_gain = 1.1
+
