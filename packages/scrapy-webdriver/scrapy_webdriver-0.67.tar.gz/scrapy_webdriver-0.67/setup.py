d014a6f86e5693c7ef771b03cb8096a1bfe2aeac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:44:01 +0000

    Add package scrapy-webdriver/0.67/scrapy_webdriver-0.67.tar.gz

---
+++ packages/scrapy-webdriver/scrapy_webdriver-0.67.tar.gz/scrapy_webdriver-0.67/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+setuptools.setup(
+	name="scrapy_webdriver",
+	version="0.67",
+	author="Aleksey Kvyatkovsky",
+	author_email="kvyatkovsky@mail.ru",
+	description="Class based on selenium webdriver.Firefox with methods  for scraping.",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/KvyatkovskyAleksey/ScrapeWebdriver",
+	packages=setuptools.find_packages(),
+	install_requires=[
+		'beautifulsoup4~=4.9.3',
+		'selenium~=3.141.0',
+		'webdriver-manager~=3.4.2',
+		'Scrapy~=2.5.1',
+		'PyVirtualDisplay~=2.2',
+		# 'selenium_wire==4.3.1'
+    ],
+	package_data={'': ['license.txt']},
+	include_package_data=True,
+	classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],)
