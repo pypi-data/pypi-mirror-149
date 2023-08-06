676ec06cac5ea62afa2aec28911afdfee017dafc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:10 +0000

    Add package wellmap/3.4.0/wellmap-3.4.0-py2.py3-none-any.whl

---
+++ packages/wellmap/wellmap-3.4.0-py2.py3-none-any.whl/wellmap/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""\
+File format for 96-well plate layouts.
+"""
+
+__version__ = '3.4.0'
+
+from .util import *
+from .file import *
+from .plot import show, UsageError
+from . import plot
