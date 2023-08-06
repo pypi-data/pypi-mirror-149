fcfa0fdd3bafd4762c10351307f7821f749f799f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:46 +0000

    Add package pollination-streamlit-io/0.21.0/pollination_streamlit_io-0.21.0-py2.py3-none-any.whl

---
+++ packages/pollination-streamlit-io/pollination_streamlit_io-0.21.0-py2.py3-none-any.whl/pollination_streamlit_io/button/get/precache-manifest.6eb930de4110e6f90dc0fa079f493cd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1a6d171f9097c25fa3a70d4d2b4be1e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5ca99a83e9a94a537a53",
+    "url": "./static/css/main.3ccb58d0.chunk.css"
+  },
+  {
+    "revision": "c0ee6d7a5133b2df426b",
+    "url": "./static/js/2.dc22e782.chunk.js"
+  },
+  {
+    "revision": "3fc7fb5bfeeec1534560a2c962e360a7",
+    "url": "./static/js/2.dc22e782.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ca99a83e9a94a537a53",
+    "url": "./static/js/main.2ab0e0ae.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
