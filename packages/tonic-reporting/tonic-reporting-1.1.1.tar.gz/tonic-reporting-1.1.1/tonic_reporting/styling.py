73afa4253fb0fdadb237491fbc342284d71bc6cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:22 +0000

    Add package tonic-reporting/1.1.1/tonic-reporting-1.1.1.tar.gz

---
+++ packages/tonic-reporting/tonic-reporting-1.1.1.tar.gz/tonic-reporting-1.1.1/tonic_reporting/styling.py
@@ -0,0 +1,19 @@
+class Colors:
+    REAL = "mediumpurple"
+    SYNTH = "mediumturquoise"
+    ALPHA = 0.5
+
+
+class PlotsSizes:
+    LARGE_FIGURE = (16, 10)
+    XTRA_LARGE_FIGURE = (20, 10)
+
+
+class Labels:
+    REAL = "Real"
+    SYNTH = "Synth"
+
+
+class FontSizes:
+    DEFAULT = 14
+    LARGE = 16
