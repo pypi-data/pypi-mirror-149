cfa57fd823c67564213736d32e29479625e17fa2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:17 +0000

    Add package castor-extractor/0.0.12/castor-extractor-0.0.12.tar.gz

---
+++ packages/castor-extractor/castor-extractor-0.0.12.tar.gz/castor-extractor-0.0.12/castor_extractor/warehouse/redshift/client.py
@@ -0,0 +1,22 @@
+from ...utils import uri_encode
+from ..abstract import SqlalchemyClient
+
+REDSHIFT_URI = "redshift+psycopg2://{user}:{password}@{host}:{port}/{database}"
+
+DEFAULT_PORT = 5439
+
+
+class RedshiftClient(SqlalchemyClient):
+    """redshift client"""
+
+    def _engine_options(self, credentials: dict) -> dict:
+        return {"connect_args": {"sslmode": "verify-ca"}}
+
+    def _build_uri(self, credentials: dict) -> str:
+        return REDSHIFT_URI.format(
+            user=credentials["user"],
+            password=uri_encode(credentials["password"]),
+            host=credentials["host"],
+            port=credentials.get("port") or DEFAULT_PORT,
+            database=credentials["database"],
+        )
