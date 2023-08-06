6d5e40279ca7e6a7ef81741338663899f8993a59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:41:45 +0000

    Add package tessfov/0.1.0/tessfov-0.1.0.tar.gz

---
+++ packages/tessfov/tessfov-0.1.0.tar.gz/tessfov-0.1.0/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['tessfov']
+
+package_data = \
+{'': ['*'], 'tessfov': ['data/*']}
+
+install_requires = \
+['matplotlib>=3.5.2,<4.0.0',
+ 'numpy>=1.22.3,<2.0.0',
+ 'pandas>=1.4.2,<2.0.0',
+ 'tess-point>=0.7.1,<0.8.0']
+
+setup_kwargs = {
+    'name': 'tessfov',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'Christina Hedges',
+    'author_email': 'christina.l.hedges@nasa.gov',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
