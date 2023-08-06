c25002e7efdd0fc4823e5d5912c1508e39556a94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:30 +0000

    Add package AthenaColor/4.0.1/AthenaColor-4.0.1-py3-none-any.whl

---
+++ packages/AthenaColor/AthenaColor-4.0.1-py3-none-any.whl/AthenaColor/__init__.py
@@ -0,0 +1,36 @@
+# ----------------------------------------------------------------------------------------------------------------------
+# - All -
+# ----------------------------------------------------------------------------------------------------------------------
+# This "__all__" is in a permanent structure.
+#   Things can be added (and probably will) in future releases
+#   Nothing should ever be removed from this list, once something is added in a full version
+
+__all__ = [
+    "init",
+    "ColorTupleConversion","ColorObjectConversion",
+    "RGB","RGBA","HEX","HEXA","HSV","HSL","CMYK",
+    "Fore","Back","Underline","Style","Basic",
+    "ForeNest","BackNest","UnderlineNest","StyleNest","BasicNest"
+]
+# ----------------------------------------------------------------------------------------------------------------------
+# - Package Imports -
+# ----------------------------------------------------------------------------------------------------------------------
+from AthenaColor.InitClass import init
+from AthenaColor.Objects.Color.ColorSystem import (
+    RGB,RGBA,HEX,HEXA,HSV,HSL,CMYK
+)
+from AthenaColor.Objects.Console.Styling.Inline.Bodies import (
+    Fore,Back,Underline
+)
+from AthenaColor.Objects.Console.Styling.Inline.MakeUp import (
+    Style,Basic
+)
+from AthenaColor.Objects.Console.Styling.Nested.Nested_Bodies import (
+    ForeNest,BackNest,UnderlineNest
+)
+from AthenaColor.Objects.Console.Styling.Nested.Nested_MakeUp import (
+    StyleNest,BasicNest
+)
+from AthenaColor.Objects.Color import (
+    ColorTupleConversion, ColorObjectConversion
+)
