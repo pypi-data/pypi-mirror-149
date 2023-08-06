736dd9978999e39cedf4503c38d545af1ae351d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:11 +0000

    Add package Jong/1.0.5/Jong-1.0.5.tar.gz

---
+++ packages/Jong/Jong-1.0.5.tar.gz/Jong-1.0.5/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="Jong",
+    version="1.0.5",
+    license='MIT',
+    author="Syniez",
+    author_email="js990204@naver.com",
+    description="https://github.com/Syniez/Syniez_module.git",
+    long_description=open('README.md').read(),
+    url="https://github.com/Syniez/Syniez_module.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires='>=3.6',
+)
