af86d50fce3c12a0324fa9fc9fa1b41a2ab5969f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:21:05 +0000

    Add package persuader-technology-automata-data-stream/0.0.5/persuader-technology-automata-data-stream-0.0.5.tar.gz

---
+++ packages/persuader-technology-automata-data-stream/persuader-technology-automata-data-stream-0.0.5.tar.gz/persuader-technology-automata-data-stream-0.0.5/data/message/DataMessageProcessor.py
@@ -0,0 +1,10 @@
+class DataMessageProcessor:
+
+    def __init__(self, listen_to_stream):
+        self.listen_to_stream = listen_to_stream
+
+    def get_listen_to_stream(self):
+        pass
+
+    def process_message(self, message):
+        pass
