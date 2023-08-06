4c5d9104eadb6441254a1a207d2a3deb99fec9e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:34 +0000

    Add package omniversion/0.42.12/omniversion-0.42.12.tar.gz

---
+++ packages/omniversion/omniversion-0.42.12.tar.gz/omniversion-0.42.12/README.md
@@ -0,0 +1,5 @@
+[![Software License](https://img.shields.io/badge/license-AGPL--3.0-green.svg?style=for-the-badge)](/LICENSE)
+
+# omniversion/python
+
+
---
+++ packages/omniversion/omniversion-0.42.12.tar.gz/omniversion-0.42.12/omniversion/pretty/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+"""Pretty-print some data in a human-readable string"""
+from colorama import init
+
+from . import pretty
+
+init(strip=None, convert=None, wrap=False)
