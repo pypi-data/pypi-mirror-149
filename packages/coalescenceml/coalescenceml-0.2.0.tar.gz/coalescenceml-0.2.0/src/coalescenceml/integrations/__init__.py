c9c60c6e7de0f362f6373fec7c543946fe7b8ff7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:25:27 +0000

    Add package coalescenceml/0.2.0/coalescenceml-0.2.0.tar.gz

---
+++ packages/coalescenceml/coalescenceml-0.2.0.tar.gz/coalescenceml-0.2.0/src/coalescenceml/integrations/__init__.py
@@ -0,0 +1,9 @@
+"""
+The CoalescenceML integrations module contains sub-modules for each integration that we
+support. This includes orchestrators like Apache Airflow as well as deep
+learning libraries like PyTorch.
+"""
+# Imports here when we have them lol
+from coalescenceml.integrations.sklearn import SKLearnIntegration
+from coalescenceml.integrations.statsmodels import StatsmodelsIntegration
+from coalescenceml.integrations.tensorflow import TFIntegration
