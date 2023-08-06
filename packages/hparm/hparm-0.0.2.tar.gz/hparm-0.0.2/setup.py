0649a500272e38776eced9861d5573bca151b697
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:25 +0000

    Add package hparm/0.0.2/hparm-0.0.2.tar.gz

---
+++ packages/hparm/hparm-0.0.2.tar.gz/hparm-0.0.2/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.02'
+DESCRIPTION = 'Loads yaml file to a python structure'
+LONG_DESCRIPTION = '_'
+
+# Setting up
+setup(
+    name="hparm",
+    version=VERSION,
+    author="knoriy",
+    author_email="<knoriy@bournemouth.ac.uk>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['PyYAML'],
+    keywords=['python', 'yaml'],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
