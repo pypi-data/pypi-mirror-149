93022b30823060f076da8b1323e39456c4320baa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:27 +0000

    Add package pydantic-model-parser/1.0.1/pydantic_model_parser-1.0.1-py3-none-any.whl

---
+++ packages/pydantic-model-parser/pydantic_model_parser-1.0.1-py3-none-any.whl/model_parser/__init__.py
@@ -0,0 +1,7 @@
+from pydantic import ValidationError
+
+from model_parser.pydantic_parser import Parser
+from model_parser.mapper import BaseMapper
+from model_parser.custom_types import MappingError, Mapping, JsonObject
+
+__version__ = "1.0.1"
---
+++ packages/pydantic-model-parser/pydantic_model_parser-1.0.1-py3-none-any.whl/model_parser/__main__.py
@@ -0,0 +1,19 @@
+"""A simple package to transform/map dictionaries, before parsing it into Pydantic.
+
+The package does not have a cli script. Please refer to documentation below for the intended usage.
+
+More information is available at:
+- https://github.com/ctgjdw/pydantic-model-parser
+
+Version:
+--------
+- pydantic-model-parser 1.0.1
+"""
+
+
+def main():
+    print(__doc__)
+
+
+if __name__ == "__main__":
+    main()
